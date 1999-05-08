ce5c3d28dd1805f23505dab8aa93bf9b69598d2a
Eddie Runia <eddie@runia.com>
1999-05-08 19:54:59 +0000

    Unrelease memory block in aEval.

--- harbour/source/rtl/arrays.c
+++ harbour/source/rtl/arrays.c
@@ -479,6 +479,7 @@ HARBOUR AEVAL( void )
    {
 	  ArrayEval( pArray, bBlock, _parnl( 3 ), _parnl( 4 ) );
 	  ItemCopy( &stack.Return, pArray ); /* AEval() returns the array itself */
+      ItemRelease( pArray );             /* Release array holders once       */
    }
    else
 	  _ret();
