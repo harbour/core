2533412bac054247d10eb4c290a046d8d7ebb465
Leslee Griffith <les.griffith@vantagesystems.ca>
1999-05-27 03:52:47 +0000

    commit of gtxxx

---
+++ harbour/source/rtl/gtxxx.c
@@ -0,0 +1,54 @@
+/*
+ *  GTXXX.C: Video subsystem template.
+ *
+ *  This module is based on VIDMGR by Andrew Clarke and modified for
+ *  the Harbour project
+ */
+
+/* TODO: include any standard headers here */
+
+#include <gtapi.h>
+
+void gtInit(void)
+{
+}
+
+void gtDone(void)
+{
+}
+
+char gtGetScreenWidth(void)
+{
+    return (char)0;
+}
+
+char gtGetScreenHeight(void)
+{
+    return (char)0;
+}
+
+void gtGotoXY(char x, char y)
+{
+}
+
+void gtSetCursorStyle(int style)
+{
+}
+
+int gtGetCursorStyle(void)
+{
+    return(0);
+}
+
+void gtPuts(char x, char y, char attr, char *str, int len)
+{
+}
+
+void gtGetText(char x1, char y1, char x2, char y2, char *dest)
+{
+}
+
+void gtPutText(char x1, char y1, char x2, char y2, char *srce)
+{
+}
+
