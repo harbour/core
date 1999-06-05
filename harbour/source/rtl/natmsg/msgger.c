69c54a2e7817590170bfe3ecbd10cf354c1f26d6
Eddie Runia <eddie@runia.com>
1999-06-05 13:01:01 +0000

    see changelog

---
+++ harbour/source/rtl/natmsg/msgger.c
@@ -0,0 +1,13 @@
+#include <extend.h>
+#include <ctype.h>
+
+char *hb_monthsname[ 12 ] = {
+   "Januar", "Februar", "März",
+   "April", "Mai", "Juni", "Juli",
+   "August", "September", "Oktober",
+   "November", "Dezember" };
+
+char *hb_daysname[ 7 ] = {
+   "Sonntag", "Montag", "Dienstag",
+   "Mittwoch", "Donnerstag", "Freitag",
+   "Samstag" };
