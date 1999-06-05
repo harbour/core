607fcda3e93e89848b5402371911c6848e31e69a
Eddie Runia <eddie@runia.com>
1999-06-05 13:29:42 +0000

    Hungarian also supported

---
+++ harbour/source/rtl/natmsg/msghu.c
@@ -0,0 +1,33 @@
+
+/* Hungarian language module (2 char. ISO language code: HU) */
+/* Codepage: 852 */
+
+/* TODO: Decide which codepage to use, and how to implement the whole
+/*       codepage issue */
+
+char *hb_monthsname[ 12 ] =
+{
+   "janu†r",
+   "febru†r",
+   "m†rcius",
+   "†prilis",
+   "m†jus",
+   "j£nius",
+   "j£lius",
+   "augusztus",
+   "szeptember",
+   "okt¢ber",
+   "november",
+   "december"
+};
+
+char *hb_daysname[ 7 ] =
+{
+   "vas†rnap",
+   "hÇtfã",
+   "kedd",
+   "szerda",
+   "csÅtîrtîk",
+   "pÇntek",
+   "szombat"
+};
