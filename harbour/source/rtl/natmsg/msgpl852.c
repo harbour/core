0219ca654467df65d12bb6b23f330f6c6e5b1a58
Alexander S.Kresin <alex@belacy.belgorod.su>
1999-06-10 07:32:12 +0000

    Polish language files

---
+++ harbour/source/rtl/natmsg/msgpl852.c
@@ -0,0 +1,18 @@
+/*
+ * $Id$
+ */
+/* Polish language module   */
+/* Codepage: Latin II - 852 */
+
+char *hb_monthsname[ 12 ] = {
+   "Styczeä", "Luty", "Marzec",
+   "Kwiecieä", "Maj", "Czerwiec", "Lipiec",
+   "Sierpieä", "Wrzesieä", "Pa«d«iernik",
+   "Listopad", "Grudzieä" };
+
+char *hb_daysname[ 7 ] = {
+   "Niedziela", "Poniedziaˆek", "Wtorek",
+   "—roda", "Czwartek", "Pi¥tek",
+   "Sobota" };
+
+
---
+++ harbour/source/rtl/natmsg/msgplmaz.c
@@ -0,0 +1,18 @@
+/*
+ * $Id$
+ */
+/* Polish language module     */
+/* Codepage: Polish - Mazovia */
+
+char *hb_monthsname[ 12 ] = {
+   "Stycze¤", "Luty", "Marzec",
+   "Kwiecie¤", "Maj", "Czerwiec", "Lipiec",
+   "Sierpie¤", "Wrzesie¤", "Pa¦d¦iernik",
+   "Listopad", "Grudzie¤" };
+
+char *hb_daysname[ 7 ] = {
+   "Niedziela", "Poniedzia’ek", "Wtorek",
+   "˜roda", "Czwartek", "Pi†tek",
+   "Sobota" };
+
+
