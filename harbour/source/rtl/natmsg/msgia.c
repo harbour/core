820368e62443a22013ec909a1a3dcbaec523d683
Andi Jahja <xharbour@cbn.net.id>
1999-06-16 01:23:01 +0000

    Adding Languange Module for Indonesian

---
+++ harbour/source/rtl/natmsg/msgia.c
@@ -0,0 +1,17 @@
+/*
+ * $Id$
+ */
+
+/* Indonesian language module */
+/* Codepage: N/A           */
+
+char *hb_monthsname[ 12 ] = {
+   "Januari", "Februari", "Maret",
+   "April", "Mei", "Juni", "Juli",
+   "Agustus", "September", "Oktober",
+   "Nopember", "Desember" };
+
+char *hb_daysname[ 7 ] = {
+   "Minggu", "Senin", "Selasa",
+   "Rabu", "Kamis", "Jumat",
+   "Sabtu" };
