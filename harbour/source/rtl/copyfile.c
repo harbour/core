aea1bb8847f7b27242899829b8d9506751ce11d8
Andi Jahja <xharbour@cbn.net.id>
1999-07-17 00:46:57 +0000

    Added Copyright

--- harbour/source/rtl/copyfile.c
+++ harbour/source/rtl/copyfile.c
@@ -1,6 +1,26 @@
 /*
  * $Id$
- */
+
+   Copyright(C) 1999 by Andi Jahja
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR
+   PURPOSE.  See the GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this program; if not, write to:
+
+   The Free Software Foundation, Inc.,
+   675 Mass Ave, Cambridge, MA 02139, USA.
+
+   You can contact me at: andij@aonlippo.co.id
+*/
 
 #include <itemapi.h>
 #include <extend.h>
--- harbour/source/tools/hb_f.c
+++ harbour/source/tools/hb_f.c
@@ -1,20 +1,28 @@
 /*
  * $Id$
- */
 
-/*
- * nanforum ft_f*() clones:
- *
- *  hb_fuse
- *  hb_frecno
- *  hb_flastrec
- *  hb_fgotop
- *  hb_fgobottom
- *  hb_fgoto
- *  hb_feof
- *  hb_freadln
- *  hb_fskip
- */
+   Copyright(C) 1999 by Andi Jahja
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR
+   PURPOSE.  See the GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this program; if not, write to:
+
+   The Free Software Foundation, Inc.,
+   675 Mass Ave, Cambridge, MA 02139, USA.
+
+   You can contact me at: andij@aonlippo.co.id
+*/
+
+/* please run $(HARBOUR)\tests\working\testhbf.prg for testing */
 
 #include <filesys.api>
 
