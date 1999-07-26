45295f4dc252ea2b283daa5c7fea465346bdf550
Eddie Runia <eddie@runia.com>
1999-07-26 18:29:22 +0000

    see changelog

--- harbour/tests/working/hscript/bldguest.bat
+++ harbour/tests/working/hscript/bldguest.bat
@@ -1,13 +1,14 @@
 @echo off
 
-..\..\bin\harbour guestbk /n
-..\..\bin\harbour inifiles
-..\..\bin\harbour testcgi
+..\..\..\bin\harbour guestbk /n
+..\..\..\bin\harbour ..\inifiles
+..\..\..\bin\harbour ..\testcgi
 
-echo -O2 -eguestbk.exe -I..\..\include ..\..\source\vm\hvm.c guestbk.c > b32.bc
+echo -O2 -eguestbk.exe -I..\..\..\include ..\..\..\source\vm\hvm.c guestbk.c > b32.bc
 echo inifiles.c testcgi.c                                        >> b32.bc
-echo ..\..\libs\b32\harbour.lib  ..\..\libs\b32\terminal.lib >> b32.bc
-echo ..\..\libs\b32\hbgt.lib ..\..\libs\b32\hbpp.lib >> b32.bc
+echo ..\..\..\libs\b32\harbour.lib  ..\..\..\libs\b32\terminal.lib >> b32.bc
+echo ..\..\..\libs\b32\hbgt.lib ..\..\..\libs\b32\hbpp.lib >> b32.bc
+echo ..\..\..\libs\b32\rdd.lib >> b32.bc
 bcc32 @b32.bc
 del b32.bc
 
