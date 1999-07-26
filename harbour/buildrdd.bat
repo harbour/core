551b9a5d73d521fc67e191636d8af6bc3e7b5337
Paul Tucker <ptucker@sympatico.ca>
1999-07-26 04:41:28 +0000

    *** empty log message ***

--- harbour/buildrdd.bat
+++ harbour/buildrdd.bat
@@ -1,3 +1,4 @@
 @echo off
 bin\harbour source\rdd\rddsys /n /osource\rdd /iinclude
+bin\harbour source\rdd\dbfntx0 /n /osource\rdd /iinclude
 make -frdd.b32
--- harbour/tests/working/bld32w.bat
+++ harbour/tests/working/bld32w.bat
@@ -5,6 +5,8 @@ IF A%2 == A GOTO :NOOUTPUT
 
 echo -O2 -e%2.exe -DWINDOWS -tW -I..\..\include > b32.bc
 echo ..\..\source\vm\hvm.c %1.c ..\..\libs\b32\harbour.lib  ..\..\libs\win32\terminal.lib >> b32.bc
+echo ..\..\libs\b32\hbgt.lib ..\..\libs\b32\hbpp.lib >> b32.bc
+echo ..\..\libs\b32\rdd.lib >> b32.bc
 bcc32 @b32.bc
 del b32.bc
 GOTO :END
@@ -12,6 +14,8 @@ GOTO :END
 :NOOUTPUT
 echo -O2 -e%1.exe -DWINDOWS -tW -I..\..\include > b32.bc
 echo ..\..\source\vm\hvm.c %1.c ..\..\libs\b32\harbour.lib  ..\..\libs\win32\terminal.lib >> b32.bc
+echo ..\..\libs\b32\hbgt.lib ..\..\libs\b32\hbpp.lib >> b32.bc
+echo ..\..\libs\b32\rdd.lib >> b32.bc
 bcc32 @b32.bc
 del b32.bc
 GOTO :END
