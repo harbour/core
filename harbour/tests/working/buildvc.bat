aff8a6aee1703f3d56d72492c6a455ab148ced8c
Paul Tucker <ptucker@sympatico.ca>
1999-07-31 21:23:50 +0000

    *** empty log message ***

--- harbour/tests/working/buildvc.bat
+++ harbour/tests/working/buildvc.bat
@@ -1,2 +1,2 @@
 ..\..\bin\harbour %1 /n
-cl -Fd..\..\bin\harbour -w -Zi -TP -GZ -GA -DDEBUG -DHARBOUR_USE_GTAPI -DHARBOUR_USE_WIN_GTAPI -I..\..\include %1.c /link /subsystem:CONSOLE ..\..\obj\symbols.obj ..\..\libs\vc\harbour.lib ..\..\libs\vc\terminal.lib ..\..\libs\vc\rdd.lib ..\..\libs\vc\hbtools.lib ..\..\libs\vc\dbfntx.lib
+cl -Fd..\..\bin\harbour -w -Zi -TP -GZ -GA -DDEBUG -DHARBOUR_USE_GTAPI -DHARBOUR_USE_WIN_GTAPI -I..\..\include %1.c /link /subsystem:CONSOLE ..\..\obj\symbols.obj ..\..\libs\vc\harbour.lib ..\..\libs\vc\terminal.lib ..\..\libs\vc\hbtools.lib ..\..\libs\vc\dbfntx.lib
