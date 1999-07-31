8b99ddd9be2bd9c6e26ef098296b1f366a19f80d
Patrick Mast <harbour@winfakt.com>
1999-07-31 09:53:46 +0000

    build40.bat and makefile.b40

--- harbour/build40.bat
+++ harbour/build40.bat
@@ -1,11 +1,12 @@
 @echo off
-del build40.err
+rem del build40.err
 make -fhbpplib.b32  > build40.err
 make -fmakefile.b40 >> build40.err
 make -fterminal.b32 >> build40.err
 call bldgt32.bat >> build40.err
 call bldhbpp.bat >> build40.err
+call buildrdd.bat >> build40.err
 cd tests\working
 call bld32exe.bat ..\..\source\runner\runner runner >> build40.err
 cd ..\..
-edit build40.err
+Notepad build40.err
