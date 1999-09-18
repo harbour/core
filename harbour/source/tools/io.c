83308a11f829d20f3b899d20d83f6315fe6b27c6
David G. Holm <dholm@jsd-llc.com>
1999-09-18 06:11:47 +0000

    See ChangeLog entry 19990918-01:55 EDT David G. Holm <dholm@jsd-llc.com>

--- harbour/source/tools/io.c
+++ harbour/source/tools/io.c
@@ -10,6 +10,125 @@
 #include <bios.h>
 #endif
 
+/*  $DOC$
+ *  $FUNCNAME$
+ *     CD()
+ *  $CATEGORY$
+ *     DOS
+ *  $ONELINER$
+ *  $SYNTAX$
+ *     CD(<NDIR>)
+ *  $ARGUMENTS$
+ *     <NDIR> DIR TO BE CHANGED
+ *  $RETURNS$
+ *     .T. IF SUCEFUL
+ *     .F. IF NOT
+ *
+ *  $DESCRIPTION$
+ *     CHANGE THE CURRENT DIRECTORY
+ *  $EXAMPLES$
+ *     IF CD("OLA")
+ *        RETURN(.T.)
+ *     ELSE
+ *        RETURN(.F.)
+ *     ENDIF
+ *
+ *  $SEEALSO$
+ *
+ *  $INCLUDE$
+ *     extend.h dos.h dir.h bios.h
+ *  $END$
+ */
+
+HARBOUR HB_CD( void )
+{
+#ifdef __DOS__
+   hb_retni( ISCHAR( 1 ) ? chdir( hb_parc( 1 ) ) : 0 );
+#else
+   hb_retni( 0 );
+#endif
+}
+
+/*  $DOC$
+ *  $FUNCNAME$
+ *     MD()
+ *  $CATEGORY$
+ *     DOS
+ *  $ONELINER$
+ *
+ *  $SYNTAX$
+ *     MD(<NDIR>)
+ *  $ARGUMENTS$
+ *    <NDIR> DIRECTORY TO BE CREATED
+ *  $RETURNS$
+ *    .T. IF SUCEFUL
+ *    .F. IF NOT
+ *
+ *  $DESCRIPTION$
+ *    CREATE A  DIRECTORY
+ *  $EXAMPLES$
+ *  IF MD("OLA")
+ *      RETURN(.T.)
+ *  ELSE
+ *      RETURN(.F.)
+ *  ENDIF
+ *
+ *  $SEEALSO$
+ *
+ *  $INCLUDE$
+ *     extend.h dos.h dir.h bios.h
+ *  $END$
+ */
+
+HARBOUR HB_MD(void)
+{
+#ifdef __DOS__
+   hb_retni( ISCHAR( 1 ) ? mkdir( hb_parc( 1 ) ) : 0 );
+#else
+   hb_retni( 0 );
+#endif
+}
+
+/*  $DOC$
+ *  $FUNCNAME$
+ *     RD()
+ *  $CATEGORY$
+ *     DOS
+ *  $ONELINER$
+ *
+ *  $SYNTAX$
+ *     RD(<NDIR>)
+ *  $ARGUMENTS$
+ *    <NDIR> DIR TO BE DELETED
+ *  $RETURNS$
+ *    .T. IF SUCEFUL
+ *    .F. IF NOT
+ *
+ *  $DESCRIPTION$
+ *    REMOVE A  DIRECTORY
+ *  $EXAMPLES$
+ *  IF RD("OLA")
+ *      RETURN(.T.)
+ *  ELSE
+ *      RETURN(.F.)
+ *  ENDIF
+ *
+ *  $SEEALSO$
+ *
+ *  $INCLUDE$
+ *     extend.h dos.h dir.h bios.h
+ *  $END$
+ */
+
+HARBOUR HB_RD( void )
+{
+#ifdef __DOS__
+   hb_retni( ISCHAR( 1 ) ? rmdir( hb_parc( 1 ) ) : 0 );
+#else
+   hb_retni( 0 );
+#endif
+}
+
 HARBOUR HB_DISKUSED( void )
 {
 #ifdef DOS
@@ -26,6 +145,21 @@ HARBOUR HB_DISKUSED( void )
               ( long ) disk.bytes_per_sector;
 
    hb_retnl( bytsfull - bytsfree );
+#else
+   hb_retnl( 0 );
+#endif
+}
+
+HARBOUR HB_DISKFREE( void )
+{
+#ifdef DOS
+   struct diskfree_t disk;
+
+   _dos_getdiskfree( 0, &disk );
+
+   hb_retnl( ( long ) disk.avail_clusters *
+             ( long ) disk.sectors_per_cluster *
+             ( long ) disk.bytes_per_sector );
 #else
    hb_retnl( 0 );
 #endif
