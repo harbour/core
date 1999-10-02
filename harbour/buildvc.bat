8ee4d83ad9537653384fb288ca73add22b798849
Paul Tucker <ptucker@sympatico.ca>
1999-10-02 07:39:25 +0000

    *** empty log message ***

--- harbour/buildvc.bat
+++ harbour/buildvc.bat
@@ -4,6 +4,5 @@ rem $Id$
 rem 
 
 if exist buildvc.err del buildVC.err
-masm source\vm\symbols.asm obj\symbols.obj obj\symbols.lst obj\symbols.crf > BuildVC.err
-nmake /f makefile.vc %1 >> BuildVC.err
+nmake /f makefile.vc %1 > BuildVC.err
 notepad buildVC.err
