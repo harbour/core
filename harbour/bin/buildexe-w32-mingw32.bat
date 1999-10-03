664ded62b4f31231d7bec3fbc32c6cf1de967de0
Viktor Szakats <harbour@syenar.net>
1999-10-03 23:26:09 +0000

    19991004-01:14 GMT+1

---
+++ harbour/bin/buildexe-w32-mingw32.bat
@@ -0,0 +1,27 @@
+@echo off
+rem 
+rem $Id$
+rem 
+
+rem Harbour executable builder batch file
+rem
+rem Compiler: Cygwin GCC compiler / MingW32
+rem Platform: Windows 32 bit (console mode)
+rem
+rem Maintainer: Victor Szel <info@szelvesz.hu>
+
+if not "%1" == "" goto compile
+
+echo.
+echo Usage: makeexe.bat name
+echo.
+echo - 'name' is the .prg filename *without* extension.
+echo - Don't forget to make a MAIN function for you application.
+echo - This batch file assumes you are in some directory off the main harbour dir
+exit
+
+:compile
+
+harbour %1.prg /n /i..\include
+gcc %1.c -mno-cygwin -I..\include -L..\lib -lrtl -lrdd -lvm -lrdd -lrtl -ldbfntx
+del %1.c
--- harbour/genbuild.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem 
-rem $Id$
-rem 
-
-cd..
-pkzip -r -p harb21-2 -x*.exe -x*.bak -x*.sim -x*.out -xsave.bat -xlexyy.c -xy_tab.c -xy_tab.h -x*.obj -x*.lib -x*.zip
