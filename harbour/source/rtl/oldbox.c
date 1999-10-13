b8ec64d2448de41c8a09d598784fa7d665e949d5
Paul Tucker <ptucker@sympatico.ca>
1999-10-13 21:00:16 +0000

    Correct cast on expression

--- harbour/source/rtl/oldbox.c
+++ harbour/source/rtl/oldbox.c
@@ -42,7 +42,7 @@ HARBOUR HB___BOX( void )
    if( ISNUM( 1 ) && ISNUM( 2 ) && ISNUM( 3 ) && ISNUM( 4 ) && ISCHAR( 5 ) )
       hb_gtBox( hb_parni( 1 ), hb_parni( 2 ),
                 hb_parni( 3 ), hb_parni( 4 ),
-                ( BYTE * ) frame ? frame : " " );
+                ( BYTE * ) ( frame ? frame : " " ) );
 }
 
 HARBOUR HB___BOXD( void )
