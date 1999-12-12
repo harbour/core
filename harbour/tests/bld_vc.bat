35f821f83ccfb158f6cedf1c48be40fff55cea6f
Paul Tucker <ptucker@sympatico.ca>
1999-12-12 19:53:08 +0000

    *** empty log message ***

--- harbour/tests/bld_vc.bat
+++ harbour/tests/bld_vc.bat
@@ -4,5 +4,5 @@ rem $Id$
 rem
 
 ..\bin\harbour %1 /n /i..\include
-cl -Fd..\bin\harbour -w -Zi -TP -GZ -GA -DDEBUG -DHARBOUR_USE_WIN_GTAPI -I..\include %1.c /link /subsystem:CONSOLE ..\lib\vc\harbour.lib ..\lib\vc\terminal.lib ..\lib\vc\hbtools.lib ..\lib\vc\dbfntx.lib ..\lib\vc\dbfcdx.lib ..\lib\vc\debug.lib
+cl -Fd..\bin\harbour -w -Zi -TP -GZ -GA -DDEBUG -DHARBOUR_USE_WIN_GTAPI -I..\include %1.c /link /subsystem:CONSOLE ..\lib\vc\harbour.lib ..\lib\vc\terminal.lib ..\lib\vc\hbtools.lib ..\lib\vc\dbfntx.lib ..\lib\vc\dbfcdx.lib ..\lib\vc\debug.lib ..\lib\vc\macro.lib
 del %1.c
