23f7719b73ec0826007d0d355c50de59c1361139
Antonio Linares <alinares@fivetechsoft.com>
1999-12-15 12:22:38 +0000

    *** empty log message ***

--- harbour/tests/bld_b32.bat
+++ harbour/tests/bld_b32.bat
@@ -4,5 +4,5 @@ rem $Id$
 rem
 
 ..\bin\harbour %1 %2 /n /i..\include
-bcc32 -e%1.exe -O2 -I..\include -L..\lib\b32 -v harbour.lib terminal.lib hbpp.lib hbgt.lib rdd.lib %1.c
+bcc32 -e%1.exe -O2 -I..\include -L..\lib\b32 -v harbour.lib terminal.lib hbpp.lib hbgt.lib common.lib rdd.lib %1.c
 rem del %1.c
