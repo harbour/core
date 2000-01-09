dec17754d6b66ac20c5e3f2f8b4a7dbd57994310
Luiz Rafael Culik <culik@sl.conex.net>
2000-01-09 17:21:56 +0000

    Initial revision

---
+++ harbour/ngdoc/bldfthe.bat
@@ -0,0 +1,8 @@
+@echo off
+rem
+rem $Id$
+rem
+
+..\bin\harbour %1 %2 /n /iinclude /DNANFOR
+bcc32 -e..\bin\%1.exe -O2 -I..\include -L..\lib\b32 -v harbour.lib terminal.lib hbpp.lib hbgt.lib common.lib rdd.lib %1.c
+rem del %1.c
