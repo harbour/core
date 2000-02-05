3d373af043b589d270a88ee129a4fd432bc84b6c
Viktor Szakats <harbour@syenar.net>
2000-02-05 15:56:57 +0000

    20000205-17:00 GMT+1 Victor Szakats <info@szelvesz.hu>

--- harbour/source/compiler/hbusage.c
+++ harbour/source/compiler/hbusage.c
@@ -6,7 +6,7 @@
  * Harbour Project source code:
  * Compile help & info related functions
  *
- * Copyright 1999 {list of individual authors and e-mail addresses}
+ * Copyright 1999 Victor Szakats <info@szelvesz.hu>
  * www - http://www.harbour-project.org
  *
  * This program is free software; you can redistribute it and/or modify
@@ -40,47 +40,53 @@
  */
 void hb_compPrintUsage( char * szSelf )
 {
-   printf( "\nSyntax:  %s <file[.prg]> [options]"
-           "\n"
-           "\nOptions:  /a               automatic memvar declaration"
-           "\n          /b               debug info"
-           "\n          /d<id>[=<val>]   #define <id>"
-           "\n          /es[<level>]     set exit severity"
-           "\n          /g<type>         output type generated is <type> (see below)"
-           "\n          /gc[<type>]      output type: C source (.c) (default)"
-           "\n                           <type>: 0=without comments, 1=normal (default)"
+   static const char * szOptions [] =
+   {
+           "\nOptions:  %ca               automatic memvar declaration",
+           "\n          %cb               debug info",
+           "\n          %cd<id>[=<val>]   #define <id>",
+           "\n          %ces[<level>]     set exit severity",
+           "\n          %cg<type>         output type generated is <type> (see below)",
+           "\n          %cgc[<type>]      output type: C source (.c) (default)",
+           "\n                           <type>: 0=without comments, 1=normal (default)",
 #ifdef HARBOUR_OBJ_GENERATION
-           "\n          /gf              output type: Windows/DOS OBJ32 (.obj)"
+           "\n          %cgf              output type: Windows/DOS OBJ32 (.obj)",
 #endif
-           "\n          /gh              output type: Harbour Portable Object (.hrb)"
-           "\n          /gj              output type: Java source (.java)"
-           "\n          /gp              output type: Pascal source (.pas)"
-           "\n          /gr              output type: Windows resource (.rc)"
-           "\n          /i<path>         add #include file search path"
-           "\n          /l               suppress line number information"
-/* TODO:   "\n          /m               compile module only" */
-           "\n          /n               no implicit starting procedure"
-           "\n          /o<path>         object file drive and/or path"
-           "\n          /p               generate pre-processed output (.ppo) file"
-           "\n          /q               quiet"
-           "\n          /q0              quiet and don't display program header"
-/* TODO:   "\n          /r[<lib>]        request linker to search <lib> (or none)" */
-           "\n          /s               syntax check only"
-/* TODO:   "\n          /t<path>         path for temp file creation" */
-/* TODO:   "\n          /u[<file>]       use command def set in <file> (or none)" */
-           "\n          /v               variables are assumed M->"
-           "\n          /w[<level>]      set warning level number (0..3, default 1)"
-           "\n          /x[<prefix>]     set symbol init function name prefix (for .c only)"
+           "\n          %cgh              output type: Harbour Portable Object (.hrb)",
+           "\n          %cgj              output type: Java source (.java)",
+           "\n          %cgp              output type: Pascal source (.pas)",
+           "\n          %cgr              output type: Windows resource (.rc)",
+           "\n          %ci<path>         add #include file search path",
+           "\n          %cl               suppress line number information",
+/* TODO:   "\n          %cm               compile module only", */
+           "\n          %cn               no implicit starting procedure",
+           "\n          %co<path>         object file drive and/or path",
+           "\n          %cp               generate pre-processed output (.ppo) file",
+           "\n          %cq               quiet",
+           "\n          %cq0              quiet and don't display program header",
+/* TODO:   "\n          %cr[<lib>]        request linker to search <lib> (or none)", */
+           "\n          %cs               syntax check only",
+/* TODO:   "\n          %ct<path>         path for temp file creation", */
+/* TODO:   "\n          %cu[<file>]       use command def set in <file> (or none)", */
+           "\n          %cv               variables are assumed M->",
+           "\n          %cw[<level>]      set warning level number (0..3, default 1)",
+           "\n          %cx[<prefix>]     set symbol init function name prefix (for .c only)",
 #ifdef YYDEBUG
-           "\n          /y               trace lex & yacc activity"
+           "\n          %cy               trace lex & yacc activity",
 #endif
-           "\n          /z               suppress shortcutting (.and. & .or.)"
-           "\n          /10              restrict symbol length to 10 significant characters"
-/* TODO:   "\n           @<file>         compile list of modules in <file>" */
+           "\n          %cz               suppress shortcutting (.and. & .or.)",
+           "\n          %c10              restrict symbol length to 10 significant characters",
+/* TODO:   "\n           @<file>         compile list of modules in <file>", */
            "\n"
-           "\nNotes:  Use the specific option character for your platform"
-           "\n        instead of \'/\'."
-           , szSelf );
+   };
+
+   int iLine;
+
+   printf( "\nSyntax:  %s <file[.prg]> [options]"
+           "\n", szSelf );
+
+   for( iLine = 0; iLine < ( sizeof( szOptions ) / sizeof( char * ) ); iLine++ )
+      printf( szOptions[ iLine ], OS_OPT_DELIMITER_LIST[ 0 ] );
 }
 
 /*
@@ -98,6 +104,6 @@ void hb_compPrintLogo( void )
 {
    printf( "Harbour Compiler, Build %i%s (%04d.%02d.%02d)\n",
       hb_build, hb_revision, hb_year, hb_month, hb_day );
-   printf( "Copyright 1999, http://www.harbour-project.org\n" );
+   printf( "Copyright 1999-2000, http://www.harbour-project.org\n" );
 }
 
