d7176c9810cfa5bd483aaddaad3f53654825d1fa
Luiz Rafael Culik <culik@sl.conex.net>
2000-02-19 11:23:03 +0000

    See changelog 20000219 09:10

--- harbour/bin/bld_b32w.bat
+++ harbour/bin/bld_b32w.bat
@@ -4,5 +4,5 @@ rem $Id$
 rem
 
 ..\bin\harbour %1 %2 %3 /n /i..\include
-bcc32 %1.c -e%1.exe -O2 -I..\include -L..\lib\b32 -v -tW common.lib dbfcdx.lib dbfntx.lib debug.lib harbour.lib pp.lib macro.lib runner.lib terminal.lib tools.lib
+bcc32 %1.c -e%1.exe -O2 -I..\include -L..\lib\b32 -5 -tW common.lib dbfcdx.lib dbfntx.lib debug.lib harbour.lib pp.lib macro.lib runner.lib terminal.lib tools.lib
 rem del %1.c
