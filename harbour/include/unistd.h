04e124bd3c24744077276ea0c7fc6bb2f1d147f4
Viktor Szakats <harbour@syenar.net>
2000-03-06 16:37:20 +0000

    20000306-17:34 GMT+1 Victor Szakats <info@szelvesz.hu>

--- harbour/include/unistd.h
+++ harbour/include/unistd.h
@@ -8,9 +8,9 @@
          - This file is intentionally empty. 
          - This file was intentionally not added to Makefile, since the it's 
            not needed to build a final Harbour application.
-         - The file should not conflict with other platforms since the unistd.h 
-           file is first checked in the compilers include path, and this one 
-           will only be used when it's not found there.
+         - This file should not conflict with other platforms since the 
+           unistd.h file is first checked in the compilers include path, and 
+           this one will only be used when it's not found there.
 
          [vszakats] */
 
