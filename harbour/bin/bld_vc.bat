9bbb8791660fe43630b628520e8fa707f7a28e8c
Paul Tucker <ptucker@sympatico.ca>
2000-03-07 08:42:10 +0000

    Change library names

--- harbour/bin/bld_vc.bat
+++ harbour/bin/bld_vc.bat
@@ -4,5 +4,5 @@ rem $Id$
 rem
 
 ..\bin\harbour %1 %2 %3 /n /i..\include
-cl -Fd..\bin\harbour -w -Zi -TP -GZ -GA -DDEBUG -DHARBOUR_USE_WIN_GTAPI -I..\include %1.c /link /subsystem:CONSOLE ..\lib\vc\tools.lib ..\lib\vc\harbour.lib ..\lib\vc\pp.lib ..\lib\vc\terminal.lib ..\lib\vc\dbfntx.lib ..\lib\vc\dbfcdx.lib ..\lib\vc\debug.lib ..\lib\vc\macro.lib ..\lib\vc\common.lib
+cl -Fd..\bin\harbour -w -Zi -TP -GZ -GA -DDEBUG -DHARBOUR_USE_WIN_GTAPI -I..\include %1.c /link /subsystem:CONSOLE  ..\lib\vc\rtl.lib ..\lib\vc\common.lib ..\lib\vc\tools.lib ..\lib\vc\rdd.lib ..\lib\vc\nulsys.lib ..\lib\vc\dbfntx.lib ..\lib\vc\dbfcdx.lib ..\lib\vc\debug.lib ..\lib\vc\macro.lib ..\lib\vc\vm.lib
 del %1.c
