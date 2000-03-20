10eb30aebc4bcb8e23835c7fb212fdf6a7d796c8
Viktor Szakats <harbour@syenar.net>
2000-03-20 21:28:38 +0000

    20000320-22:30 GMT+1 Victor Szakats <info@szelvesz.hu>

--- harbour/samples/guestbk/bld_b32.bat
+++ harbour/samples/guestbk/bld_b32.bat
@@ -3,9 +3,9 @@ rem
 rem $Id$
 rem
 
-..\..\bin\harbour /n guestbk
-..\..\bin\harbour /n ..\..\tests\inifiles
-..\..\bin\harbour /n ..\..\tests\testcgi
+..\..\bin\harbour /n guestbk  /i..\..\include\
+..\..\bin\harbour /n inifiles /i..\..\include\
+..\..\bin\harbour /n testcgi  /i..\..\include\
 
 echo -O2 -I..\..\include -L..\..\lib > build.tmp
 echo -eguestbk.exe >> build.tmp
@@ -14,6 +14,7 @@ echo tools.lib >> build.tmp
 echo debug.lib >> build.tmp
 echo vm.lib >> build.tmp
 echo rtl.lib >> build.tmp
+echo gtwin.lib >> build.tmp
 echo rdd.lib >> build.tmp
 echo macro.lib >> build.tmp
 echo pp.lib >> build.tmp
