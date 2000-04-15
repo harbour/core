6070497732feb2c0ab45458fc6290193ca8e0957
Viktor Szakats <harbour@syenar.net>
2000-04-15 19:51:26 +0000

    20000415-21:39 GMT+1 Victor Szakats <info@szelvesz.hu>

--- harbour/source/lang/msges.c
+++ harbour/source/lang/msges.c
@@ -2,91 +2,189 @@
  * $Id$
  */
 
-/* Language Support Module */
+/*
+ * Harbour Project source code:
+ * Language Support Module (ES)
+ *
+ * Copyright 2000 Jose Lalin <dezac@corevia.com>
+ * www - http://www.harbour-project.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version, with one exception:
+ *
+ * The exception is that if you link the Harbour Runtime Library (HRL)
+ * and/or the Harbour Virtual Machine (HVM) with other files to produce
+ * an executable, this does not by itself cause the resulting executable
+ * to be covered by the GNU General Public License. Your use of that
+ * executable is in no way restricted on account of linking the HRL
+ * and/or HVM code into it.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA (or visit
+ * their web site at http://www.gnu.org/).
+ *
+ */
 
 /* Language name: Spanish */
 /* ISO language code (2 chars): ES */
 /* Codepage: ???? */
 
-#include "hbdefs.h"
+#include "hbapilng.h"
 
-char *hb_dateMonthsName[ 12 ] =
+static HB_LANG s_lang =
 {
-   "Enero",
-   "Febrero",
-   "Marzo",
-   "Abril",
-   "Mayo",
-   "Junio",
-   "Julio",
-   "Agosto",
-   "Septiembre",
-   "Octubre",
-   "Noviembre",
-   "Diciembre"
+   {
+      /* Identification */
+   
+      "ES",                        /* ID */
+      "Spanish",                   /* Name (in English) */
+      "Espa¤ol",                   /* Name (in native language) */
+      "ES",                        /* RFC ID */
+      "850",                       /* Codepage */
+      "$Revision$ $Date$",         /* Version */
+   
+      /* Month names */
+   
+      "Enero",
+      "Febrero",
+      "Marzo",
+      "Abril",
+      "Mayo",
+      "Junio",
+      "Julio",
+      "Agosto",
+      "Septiembre",
+      "Octubre",
+      "Noviembre",
+      "Diciembre",
+   
+      /* Day names */
+
+      "Domingo",
+      "Lunes",
+      "Martes",
+      "Mi‚rcoles",
+      "Jueves",
+      "Viernes",
+      "S bado",
+   
+      /* CA-Cl*pper compatible natmsg items */
+   
+      "Database Files    # Records    Last Update     Size",
+      "Do you want more samples?",
+      "Page No.",
+      "** Subtotal **",
+      "* Subsubtotal *",
+      "*** Total ***",
+      "Ins",
+      "   ",
+      "Invalid date",
+      "Range: ",
+      " - ",
+      "Y/N",
+      "INVALID EXPRESSION",
+   
+      /* Error description names */
+   
+      "Error desconocido",
+      "Error de argumento",
+      "Error de rango",
+      "Desbordamiento en cadena de caracteres",
+      "Desbordamiento num‚rico",
+      "Divisi¢n por cero",
+      "Error num‚rico",
+      "Error de sintaxis",
+      "Operaci¢n demasiado compleja",
+      "",
+      "",
+      "Poca memoria",
+      "Funci¢n no definida",
+      "No existe el m‚todo",
+      "No existe la variable",
+      "No existe el alias",
+      "No existe la variable de instancia",
+      "Alias con caracteres no v lidos",
+      "Alias actualmente en uso",
+      "",
+      "Error de creaci¢n",
+      "Error de apertura",
+      "Error de cierre",
+      "Error de lectura",
+      "Error de escritura",
+      "Error de impresi¢n",
+      "",
+      "",
+      "",
+      "",
+      "Operaci¢n no soportada",
+      "L¡mite excedido",
+      "Se detect¢ corrupci¢n",
+      "Error de tipo de datos",
+      "Error de anchura de datos",
+      "Area de trabajo no usada",
+      "Area de trabajo no indexada",
+      "Se requiere uso exclusivo",
+      "Se requiere bloqueo",
+      "Escritura no autorizada",
+      "Fallo en el bloqueo de adici¢n",
+      "Fallo en bloqueo",
+      "",
+      "",
+      "",
+      "",
+      "array access",
+      "array assign",
+      "array dimension", 
+      "not an array",
+      "conditional",
+   
+      /* Internal error names */
+   
+      "Unrecoverable error %lu: ",
+      "Error recovery failure",
+      "No ERRORBLOCK() for error", 
+      "Too many recursive error handler calls", 
+      "RDD invalid or failed to load",
+      "Invalid method type from %s", 
+      "hb_xgrab can't allocate memory", 
+      "hb_xrealloc called with a NULL pointer", 
+      "hb_xrealloc called with an invalid pointer", 
+      "hb_xrealloc can't reallocate memory", 
+      "hb_xfree called with an invalid pointer", 
+      "hb_xfree called with a NULL pointer", 
+      "Can\'t locate the starting procedure: \'%s\'",
+      "No starting procedure", 
+      "Unsupported VM opcode", 
+      "Symbol item expected from %s",
+      "Invalid symbol type for self from %s", 
+      "Codeblock expected from %s", 
+      "Incorrect item type on the stack trying to pop from %s",
+      "Stack underflow", 
+      "An item was going to be copied to itself from %s", 
+      "Invalid symbol item passed as memvar %s",
+   
+      /* Texts */
+   
+      "DD/MM/YYYY",
+      "S",
+      "N"
+   }
 };
 
-char *hb_dateDaysName[ 7 ] =
-{
-   "Domingo",
-   "Lunes",
-   "Martes",
-   "Mi‚rcoles",
-   "Jueves",
-   "Viernes",
-   "S bado"
-};
+HB_LANG_ANNOUNCE( ES );
 
-char *hb_errorsGeneric[] =
-{
-   "Error desconocido",
-   "Error de argumento",
-   "Error de rango",
-   "Desbordamiento en cadena de caracteres",
-   "Desbordamiento num‚rico",
-   "Divisi¢n por cero",
-   "Error num‚rico",
-   "Error de sintaxis",
-   "Operaci¢n demasiado compleja",
-   "",
-   "",
-   "Poca memoria",
-   "Funci¢n no definida",
-   "No existe el m‚todo",
-   "No existe la variable",
-   "No existe el alias",
-   "No existe la variable de instancia",
-   "Alias con caracteres no v lidos",
-   "Alias actualmente en uso",
-   "",
-   "Error de creaci¢n",
-   "Error de apertura",
-   "Error de cierre",
-   "Error de lectura",
-   "Error de escritura",
-   "Error de impresi¢n",
-   "",
-   "",
-   "",
-   "",
-   "Operaci¢n no soportada",
-   "L¡mite excedido",
-   "Se detect¢ corrupci¢n",
-   "Error de tipo de datos",
-   "Error de anchura de datos",
-   "Area de trabajo no usada",
-   "Area de trabajo no indexada",
-   "Se requiere uso exclusivo",
-   "Se requiere bloqueo",
-   "Escritura no autorizada",
-   "Fallo en el bloqueo de adici¢n",
-   "Fallo en bloqueo",
-   "",
-   "",
-   "",
-   "N£mero incorrecto de argumentos",
-   "array access",
-   "array assign",
-   "not an array",
-   "conditional"
-};
+HB_CALL_ON_STARTUP_BEGIN( hb_lang_Init_ES )
+   hb_langRegister( &s_lang );
+HB_CALL_ON_STARTUP_END( hb_lang_Init_ES )
+#if ! defined(__GNUC__) && ! defined(_MSC_VER)
+   #pragma startup hb_lang_Init_ES
+#endif
+
--- harbour/source/lang/msgeu.c
+++ harbour/source/lang/msgeu.c
@@ -2,91 +2,189 @@
  * $Id$
  */
 
-/* Language Support Module */
+/*
+ * Harbour Project source code:
+ * Language Support Module (EU)
+ *
+ * Copyright 2000 Nicolas del Pozo <niko@geroa.com>
+ * www - http://www.harbour-project.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version, with one exception:
+ *
+ * The exception is that if you link the Harbour Runtime Library (HRL)
+ * and/or the Harbour Virtual Machine (HVM) with other files to produce
+ * an executable, this does not by itself cause the resulting executable
+ * to be covered by the GNU General Public License. Your use of that
+ * executable is in no way restricted on account of linking the HRL
+ * and/or HVM code into it.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA (or visit
+ * their web site at http://www.gnu.org/).
+ *
+ */
 
 /* Language name: Basque */
 /* ISO language code (2 chars): EU */
 /* Codepage: ???? */
 
-#include "hbdefs.h"
+#include "hbapilng.h"
 
-char *hb_dateMonthsName[ 12 ] =
+static HB_LANG s_lang =
 {
-   "Urtarrila",
-   "Otsaila",
-   "Martxoa",
-   "Apirila",
-   "Maitza",
-   "Ekaina",
-   "Uztaila",
-   "Abuztua",
-   "Iraila",
-   "Urria",
-   "Azaroa",
-   "Abendua"
+   {
+      /* Identification */
+
+      "EU",                        /* ID */
+      "Basque",                    /* Name (in English) */
+      "English",                   /* Name (in native language) */
+      "EU",                        /* RFC ID */
+      "850",                       /* Codepage */
+      "$Revision$ $Date$",         /* Version */
+
+      /* Month names */
+
+      "Urtarrila",
+      "Otsaila",
+      "Martxoa",
+      "Apirila",
+      "Maitza",
+      "Ekaina",
+      "Uztaila",
+      "Abuztua",
+      "Iraila",
+      "Urria",
+      "Azaroa",
+      "Abendua",
+
+      /* Day names */
+
+      "Igandea",
+      "Astelehena",
+      "Asteartea",
+      "Asteazkena",
+      "Osteguna",
+      "Ostirala",
+      "Larunbata",
+
+      /* CA-Cl*pper compatible natmsg items */
+   
+      "Database Files    # Records    Last Update     Size",
+      "Do you want more samples?",
+      "Page No.",
+      "** Subtotal **",
+      "* Subsubtotal *",
+      "*** Total ***",
+      "Ins",
+      "   ",
+      "Invalid date",
+      "Range: ",
+      " - ",
+      "Y/N",
+      "INVALID EXPRESSION",
+
+      /* Error description names */
+   
+      "Errore ezezaguna",
+      "Argumentu-errore",
+      "Maila-errore",
+      "Kate-gainezkatze",
+      "Zenbaki-gainezkatze",
+      "Zero-zatiketa",
+      "Zenbaki-errore",
+      "Sintaxi-errore",
+      "Eragiketa konplexuegia",
+      "",
+      "",
+      "Memoria gutxi",
+      "Funtzio-definitugabea",
+      "Modu-esportagabea",
+      "Aldagai-ezezaguna",
+      "Aldagai-",
+      "Alias-okerra",
+      "Illegal characters in alias",
+      "Alias-bikoiztua",
+      "",
+      "Sortze-errore",
+      "Irekitze-errore",
+      "Itxiera-errore",
+      "Irrakurketa-errore",
+      "Idazketa-errore",
+      "Imprimaketa-errore",
+      "",
+      "",
+      "",
+      "",
+      "Onartugabeko eragiketa",
+      "Muga gainditua",
+      "Indize-hondaketa igarria",
+      "Datu-mota okerra",
+      "Datu-zabalera luzeegia",
+      "Laneko area erabili gabea",
+      "Laneko area ez indexatua",
+      "Modu-esklusiboa behar da",
+      "Blokeoa behar da",
+      "Idazkera-debekatua",
+      "Erregistro eransketan blokeo errorea",
+      "Blokeo-errore",
+      "",
+      "",
+      "",
+      "",
+      "taula-atzipena",
+      "yaula-esleipen",
+      "array dimension",
+      "ez da taula",
+      "baldintza-sententzia",
+
+      /* Internal error names */
+
+      "Unrecoverable error %lu: ",
+      "Error recovery failure",
+      "No ERRORBLOCK() for error", 
+      "Too many recursive error handler calls", 
+      "RDD invalid or failed to load",
+      "Invalid method type from %s", 
+      "hb_xgrab can't allocate memory", 
+      "hb_xrealloc called with a NULL pointer", 
+      "hb_xrealloc called with an invalid pointer", 
+      "hb_xrealloc can't reallocate memory", 
+      "hb_xfree called with an invalid pointer", 
+      "hb_xfree called with a NULL pointer", 
+      "Can\'t locate the starting procedure: \'%s\'",
+      "No starting procedure", 
+      "Unsupported VM opcode", 
+      "Symbol item expected from %s",
+      "Invalid symbol type for self from %s", 
+      "Codeblock expected from %s", 
+      "Incorrect item type on the stack trying to pop from %s",
+      "Stack underflow", 
+      "An item was going to be copied to itself from %s", 
+      "Invalid symbol item passed as memvar %s",
+
+      /* Texts */
+
+      "DD/MM/YYYY",
+      "E",
+      "N"
+   }
 };
 
-char *hb_dateDaysName[ 7 ] =
-{
-   "Igandea",
-   "Astelehena",
-   "Asteartea",
-   "Asteazkena",
-   "Osteguna",
-   "Ostirala",
-   "Larunbata"
-};
+HB_LANG_ANNOUNCE( EU );
 
-char *hb_errorsGeneric[] =
-{
-   "Errore ezezaguna",
-   "Argumentu-errore",
-   "Maila-errore",
-   "Kate-gainezkatze",
-   "Zenbaki-gainezkatze",
-   "Zero-zatiketa",
-   "Zenbaki-errore",
-   "Sintaxi-errore",
-   "Eragiketa konplexuegia",
-   "",
-   "",
-   "Memoria gutxi",
-   "Funtzio-definitugabea",
-   "Modu-esportagabea",
-   "Aldagai-ezezaguna",
-   "Aldagai-",
-   "Alias-okerra",
-   "Illegal characters in alias",
-   "Alias-bikoiztua",
-   "",
-   "Sortze-errore",
-   "Irekitze-errore",
-   "Itxiera-errore",
-   "Irrakurketa-errore",
-   "Idazketa-errore",
-   "Imprimaketa-errore",
-   "",
-   "",
-   "",
-   "",
-   "Onartugabeko eragiketa",
-   "Muga gainditua",
-   "Indize-hondaketa igarria",
-   "Datu-mota okerra",
-   "Datu-zabalera luzeegia",
-   "Laneko area erabili gabea",
-   "Laneko area ez indexatua",
-   "Modu-esklusiboa behar da",
-   "Blokeoa behar da",
-   "Idazkera-debekatua",
-   "Erregistro eransketan blokeo errorea",
-   "Blokeo-errore",
-   "",
-   "",
-   "",
-   "Argumentu kopuru okerra",
-   "Taula-atzipena",
-   "Taula-esleipen",
-   "Ez da taula",
-   "Baldintza-sententzia"
-};
+HB_CALL_ON_STARTUP_BEGIN( hb_lang_Init_EU )
+   hb_langRegister( &s_lang );
+HB_CALL_ON_STARTUP_END( hb_lang_Init_EU )
+#if ! defined(__GNUC__) && ! defined(_MSC_VER)
+   #pragma startup hb_lang_Init_EU
+#endif
+
