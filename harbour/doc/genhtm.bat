ee5bb53c63176db6b262f0dc6e6d9f2e847e719d
Luiz Rafael Culik <culik@sl.conex.net>
2000-04-30 07:10:48 +0000

    See changelog 20000430 04:10 gmt-3

--- harbour/doc/genhtm.bat
+++ harbour/doc/genhtm.bat
@@ -7,5 +7,6 @@ hbdoc -htm genwww.lnk genwww.rsp
 cd htm
 echo renaming Harbour.htm to index.htm
 ren harbour.htm index.htm
-
+del genwww.lnk
+ren genwww.old genwww.lnk
 
--- harbour/doc/genhtm.cmd
+++ harbour/doc/genhtm.cmd
@@ -7,5 +7,5 @@ hbdoc -htm genwww.lnk genwww.rsp
 cd htm
 echo renaming Harbour.htm to index.htm
 ren harbour.htm index.htm
-
-
+del genwww.lnk
+ren genwww.old genwww.lnk
