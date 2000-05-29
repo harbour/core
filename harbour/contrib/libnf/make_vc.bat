97784b3855012a4ce16b46f793d8a144d46314c9
Paul Tucker <ptucker@sympatico.ca>
2000-05-29 07:27:48 +0000

    *** empty log message ***

--- harbour/contrib/libnf/make_vc.bat
+++ harbour/contrib/libnf/make_vc.bat
@@ -3,9 +3,6 @@ rem
 rem $Id$
 rem 
 
-if "%1" == "clean" goto CLEAN
-if "%1" == "CLEAN" goto CLEAN
-
 :BUILD
 
    nmake /f makefile.vc %1 %2 %3 > make_vc.log
@@ -19,12 +16,6 @@ if "%1" == "CLEAN" goto CLEAN
 :BUILD_ERR
 
    notepad make_vc.log
-   goto EXIT
-
-:CLEAN
-
-   call %0 /A %2 %3 %4
-   goto EXIT
 
 :EXIT
 
--- harbour/contrib/rdd_ads/make_vc.bat
+++ harbour/contrib/rdd_ads/make_vc.bat
@@ -3,9 +3,6 @@ rem
 rem $Id$
 rem 
 
-if "%1" == "clean" goto CLEAN
-if "%1" == "CLEAN" goto CLEAN
-
 :BUILD
 
    nmake /f makefile.vc %1 %2 %3 > make_vc.log
@@ -19,11 +16,6 @@ if "%1" == "CLEAN" goto CLEAN
 :BUILD_ERR
 
    notepad make_vc.log
-   goto EXIT
-
-:CLEAN
-
-   goto EXIT
 
 :EXIT
 
