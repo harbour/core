3cdcdb805f0c460dad6d1664eadf6c4e0d0c47e3
Viktor Szakats <harbour@syenar.net>
2000-06-02 00:51:40 +0000

    2000-06-02 02:54 UTC+0100 Victor Szakats <info@szelvesz.hu>

--- harbour/include/hbver.h
+++ harbour/include/hbver.h
@@ -37,11 +37,11 @@
 #define HB_VER_H_
 
 #define HB_VER_MAJOR    0       /* Major version number */
-#define HB_VER_MINOR    33      /* Minor version number */
-#define HB_VER_REVISION "b"     /* Revision letter */
-#define HB_VER_BUILD    33      /* Build number */
+#define HB_VER_MINOR    34      /* Minor version number */
+#define HB_VER_REVISION ""      /* Revision letter */
+#define HB_VER_BUILD    34      /* Build number */
 #define HB_VER_YEAR     2000    /* Build year */
-#define HB_VER_MONTH    05      /* Build month */
-#define HB_VER_DAY      22      /* Build day */
+#define HB_VER_MONTH    06      /* Build month */
+#define HB_VER_DAY      02      /* Build day */
 
 #endif /* HB_VER_H_ */
