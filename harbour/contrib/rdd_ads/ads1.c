8fb66e4215cf6aec2253a92d059646ed11317213
Ron Pinkas <ron@profit-master.com>
2001-06-24 04:24:45 +0000

    Recorrected ')' placement in 2 type casts

--- harbour/contrib/rdd_ads/ads1.c
+++ harbour/contrib/rdd_ads/ads1.c
@@ -1035,7 +1035,7 @@ static ERRCODE adsCreate( ADSAREAP pArea, LPDBOPENINFO pCreateInfo)
 
    HB_TRACE(HB_TR_DEBUG, ("adsCreate(%p, %p)", pArea, pCreateInfo));
 
-   pArea->szDataFileName = (char *) hb_xgrab( strlen( (char *) ( pCreateInfo->abName+1 ) ) );
+   pArea->szDataFileName = (char *) hb_xgrab( strlen( (char *) ( pCreateInfo->abName ) ) + 1 );
    strcpy( pArea->szDataFileName, ( char * ) pCreateInfo->abName );
    uiLen = (pArea->uiFieldCount * 22) + 1;
    ucfieldDefs = (UNSIGNED8 *) hb_xgrab( uiLen );
@@ -1203,7 +1203,7 @@ static ERRCODE adsOpen( ADSAREAP pArea, LPDBOPENINFO pOpenInfo )
 
    if( pOpenInfo->atomAlias )
    {
-      pArea->szDataFileName = (char *) hb_xgrab( strlen( (char *) ( pOpenInfo->abName+1 ) ) );
+      pArea->szDataFileName = (char *) hb_xgrab( strlen( (char *) ( pOpenInfo->abName ) ) + 1 );
       strcpy( pArea->szDataFileName, ( char * ) pOpenInfo->abName );
       pArea->atomAlias = hb_dynsymGet( ( char * ) pOpenInfo->atomAlias );
       if( ( ( PHB_DYNS ) pArea->atomAlias )->hArea )
