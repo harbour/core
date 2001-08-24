2e3d4909a59613b4dbf267ca9be1fae57b095611
Maurilio Longo <maurilio.longo@libero.it>
2001-08-24 14:09:47 +0000

    2001-08-24 16:03 GMT+2 Maurilio Longo <maurilio.longo@libero.it>    + contrib/os2pm    + contrib/os2pm/os2pm.c    + contrib/os2pm/tform.prg    + contrib/os2pm/os2pm.ch    + contrib/os2pm/Makefile      + added files of an early implementation of a generic GUI        framework for harbour. At present time they work only under OS/2 PM

---
+++ harbour/contrib/os2pm/os2pm.c
@@ -0,0 +1,64 @@
+
+#define INCL_BASE
+#define INCL_PM
+
+
+#include <os2.h>
+#include "hbapi.h"
+
+
+HAB hb_pm_GetHab( void );
+
+
+HB_FUNC( WINREGISTERCLASS )
+{
+
+   hb_retl( WinRegisterClass( hb_pm_GetHab(),   /* anchor block handle */
+                              hb_parc( 1 ),     /* Class Name */
+                              ( PFNWP ) WinDefWindowProc,  /* default Class procedure */
+                              hb_parnl( 2 ),     /* style */
+                              hb_parnl( 3 ) ) ); /* extra bytes */
+}
+
+HB_FUNC( WINCREATESTDWINDOW )
+{
+   ULONG lFrame = hb_parnl( 3 );
+   HWND hWndClient, hWndFrame;
+
+   hb_retnl( ( LONG ) hWndFrame =
+             WinCreateStdWindow( ( HWND ) hb_parnl( 1 ), /* hWndParent */
+                                 hb_parnl( 2 ), /* style */
+                                 &lFrame,       /* lFrame */
+                                 hb_parc( 4 ),  /* cClassName */
+                                 hb_parc( 5 ),  /* cCaption */
+                                 hb_parnl( 6 ), /* lStyleClient */
+                                 hb_parnl( 7 ), /* hModule */
+                                 hb_parnl( 8 ), /* nId */
+                       ( PHWND ) &hWndClient ) ); /* Window client handle */
+
+   hb_stornl( ( LONG ) hWndClient, 9 );
+}
+
+HB_FUNC( HB_PM_SHOWMODAL )
+{
+   QMSG qmsg;
+   HAB  hab = hb_pm_GetHab();
+
+   while( WinGetMsg( hab, &qmsg, 0, 0, 0 ) )
+   {
+      WinDispatchMsg( hab, &qmsg );
+   }
+}
+
+/* nOr() is a very used function */
+
+HB_FUNC( NOR )
+{
+   LONG lRet = 0;
+   USHORT i = 0;
+
+   while( i < hb_pcount() )
+      lRet = lRet | hb_parnl( ++i );
+
+   hb_retnl( lRet );
+}
---
+++ harbour/contrib/os2pm/tform.prg
@@ -0,0 +1,54 @@
+#include "common.ch"
+#include "hbclass.ch"
+#include "os2pm.ch"   // Needed to store some OS/2 PM constant values
+
+CLASS TForm
+
+   DATA      hWnd
+
+   CLASSDATA lRegistered
+
+   METHOD    New()
+
+   METHOD    ShowModal()
+
+ENDCLASS
+
+METHOD New() CLASS TForm
+
+   local hWndClient
+
+   DEFAULT ::lRegistered TO .f.
+
+   if ! ::lRegistered
+
+      // Notice that this code may be moved to a method Register()
+      // so we hide again the OS API details
+
+      WinRegisterClass( "HB_TFORM",;
+                        nOr( CS_SIZEREDRAW, 0x2000001 ), 0 )
+      ::lRegistered = .t.
+   endif
+
+   // Again this code may be moved to a method Create() to hide the
+   // OS API details
+
+   ::hWnd = WinCreateStdWindow( HWND_DESKTOP,;
+                                WS_VISIBLE,;
+                                nOr( FCF_TITLEBAR, FCF_SYSMENU,;
+                                FCF_SIZEBORDER, FCF_TASKLIST,;
+                                FCF_MINMAX, FCF_SHELLPOSITION ),;
+                                "HB_TFORM", "Harbour TForm",;
+                                nOr( WS_SYNCPAINT, WS_VISIBLE ),,,;
+                                @hWndClient ) // Not used yet
+
+return Self
+
+METHOD ShowModal() CLASS TForm
+
+   HB_PM_ShowModal()
+
+return nil
+
+
+
