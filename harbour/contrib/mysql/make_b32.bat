ced7c91925b67c66a56a0229718742c9c1a05a58
Patrick Mast <harbour@winfakt.com>
2002-01-28 19:43:34 +0000

    2002-01-28 20:43 UTC+0100 Patrick Mast <email@patrickmast.com>    * contrib/mysql      + Added CLEAN to make_b32.bat

--- harbour/contrib/mysql/make_b32.bat
+++ harbour/contrib/mysql/make_b32.bat
@@ -20,6 +20,13 @@ if "%1" == "CLEAN" goto CLEAN
 
 :CLEAN
 
+   if exist ..\..\lib\b32\mysql.lib   del ..\..\lib\b32\mysql.lib
+   if exist ..\..\lib\b32\mysql.bak   del ..\..\lib\b32\mysql.bak
+   if exist ..\..\obj\b32\mysql.obj   del ..\..\obj\b32\mysql.obj
+   if exist ..\..\obj\b32\tsqlbrw.c   del ..\..\obj\b32\tsqlbrw.c
+   if exist ..\..\obj\b32\tmysql.c    del ..\..\obj\b32\tmysql.c
+   if exist ..\..\obj\b32\tsqlbrw.obj del ..\..\obj\b32\tsqlbrw.obj
+   if exist ..\..\obj\b32\tmysql.obj  del ..\..\obj\b32\tmysql.obj
    goto EXIT
 
 :EXIT
