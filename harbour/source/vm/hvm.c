f7cae939f6919fd126ea757998a46d6ffda0e106
Antonio Linares <alinares@fivetechsoft.com>
2002-02-01 05:42:06 +0000

    Some minor fixes at hb_vmPushAliasedVar()

--- harbour/source/vm/hvm.c
+++ harbour/source/vm/hvm.c
@@ -4010,9 +4010,9 @@ static void hb_vmPushAliasedVar( PHB_SYMB pSym )
 
    if( HB_IS_STRING( pAlias ) )
    {
-      char * szAlias = hb_strUpper( pAlias->item.asString.value, pAlias->item.asString.length );
+      char * szAlias = pAlias->item.asString.value;
 
-      if( szAlias[ 0 ] == 'M' && szAlias[ 1 ] == '\0' )
+      if( ( szAlias[ 0 ] == 'M' || szAlias[ 0 ] == 'm' ) && szAlias[ 1 ] == '\0' )
       {  /* M->variable */
          hb_memvarGetValue( pAlias, pSym );
       }
