8d52b93f12c06fdd99b9546fbe2b3fda822329ba
David G. Holm <dholm@jsd-llc.com>
2002-03-20 17:40:35 +0000

    See ChangeLog entry 2002-03-19 12:45 UTC-0500 David G. Holm <dholm@jsd-llc.com>

--- harbour/source/rtl/set.c
+++ harbour/source/rtl/set.c
@@ -795,8 +795,13 @@ void hb_setInitialize( void )
    hb_set.HB_SET_PATH = ( char * ) hb_xgrab( 1 );
    hb_set.HB_SET_PATH[ 0 ] = '\0';
    hb_set.HB_SET_PRINTER = FALSE;
+#ifdef HB_OS_UNIX
+   hb_set.HB_SET_PRINTFILE = ( char * ) hb_xgrab( 5 );
+   memcpy( hb_set.HB_SET_PRINTFILE, "|lpr", 5 ); /* Default printer device */
+#else
    hb_set.HB_SET_PRINTFILE = ( char * ) hb_xgrab( 4 );
    memcpy( hb_set.HB_SET_PRINTFILE, "PRN", 4 ); /* Default printer device */
+#endif
    hb_set.hb_set_printhan = FS_ERROR;
    hb_set.HB_SET_SCOREBOARD = TRUE;
    hb_set.HB_SET_SCROLLBREAK = TRUE;
