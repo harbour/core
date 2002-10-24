7844307b090d773a54e4e15345ea83fbd83c6435
Alexander S.Kresin <alex@belacy.belgorod.su>
2002-10-24 16:56:24 +0000

    2002-10-24 20:58 UTC+0300 Alexander Kresin <alex@belacy.belgorod.su>

--- harbour/include/hbapicdp.h
+++ harbour/include/hbapicdp.h
@@ -85,7 +85,7 @@ extern PHB_CODEPAGE hb_cdpSelect( PHB_CODEPAGE );
 extern PHB_CODEPAGE hb_cdpFind( char * );
 extern void hb_cdpTranslate( char*, PHB_CODEPAGE, PHB_CODEPAGE );
 extern void hb_cdpnTranslate( char*, PHB_CODEPAGE, PHB_CODEPAGE, unsigned int );
-extern int hb_cdpcmp( char*, char*, int );
+extern int hb_cdpcmp( char*, char*, int, PHB_CODEPAGE );
 
 #endif /* HB_APICDP_H_ */
 
