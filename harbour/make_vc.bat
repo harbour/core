eff82a243d01d4af019ff2552bd7c6eecb1f72ef
Paul Tucker <ptucker@sympatico.ca>
2002-11-06 05:29:47 +0000

    See Changelog 2002-11-07 00:00 UTC-0500

--- harbour/make_vc.bat
+++ harbour/make_vc.bat
@@ -11,6 +11,19 @@ rem Instead, make a local copy and modify that one, or make a call to
 rem this batch file from your customized one. [vszakats]
 rem ---------------------------------------------------------------
 
+set rem=rem
+if "%1"=="/?" set rem=echo.
+%rem% ---------------------------------------------------------------
+%rem% Usage: make_vc [/y] [/a or CLEAN or other specific target]
+%rem% Call with nothing, /Y, /A, or CLEAN
+%rem% nothing - compiles what needs it.
+%rem% CLEAN, delete targets.
+%rem% /A clean, then compile all
+%rem% /Y non batch mode (forces makefile.vc)
+%rem% ---------------------------------------------------------------
+set rem=
+if "%1"=="/?" goto exit
+
 if not exist obj md obj
 if not exist obj\vc md obj\vc
 if not exist lib md lib
@@ -18,16 +31,16 @@ if not exist lib\vc md lib\vc
 if not exist bin md bin
 if not exist bin\vc md bin\vc
 
-if "%1" == "clean" goto CLEAN
-if "%1" == "CLEAN" goto CLEAN
 set MK_FILE=makefile.vc
 if "%OS%" == "Windows_NT" set MK_FILE=makefile.nt
 if "%1" == "/Y" set MK_FILE=makefile.vc
 if "%1" == "/y" set MK_FILE=makefile.vc
+if "%1" == "clean" goto CLEAN
+if "%1" == "CLEAN" goto CLEAN
 
 :BUILD
 
-   nmake /f%MK_FILE% %1 %2 %3 > make_vc.log
+   nmake /f %MK_FILE% %1 %2 %3 > make_vc.log
    if errorlevel 1 goto BUILD_ERR
 
 :BUILD_OK
@@ -43,16 +56,10 @@ if "%1" == "/y" set MK_FILE=makefile.vc
 
 :CLEAN
 
-   if exist bin\vc\*.exe del bin\vc\*.exe
-   if exist bin\vc\*.pdb del bin\vc\*.pdb
-   if exist bin\vc\*.ilk del bin\vc\*.ilk
-   if exist bin\vc\*.map del bin\vc\*.map
-   if exist lib\vc\*.lib del lib\vc\*.lib
-   if exist obj\vc\*.obj del obj\vc\*.obj
-   if exist obj\vc\*.c   del obj\vc\*.c
-   if exist obj\vc\*.h   del obj\vc\*.h
-   if exist obj\vc\*.pch del obj\vc\*.pch
-   if exist make_vc.log  del make_vc.log
+   nmake /f %MK_FILE% %1
+   rem in this case, the makefile handles most cleanup. Add what you need here
+   if exist make_vc.log del make_vc.log
+   rem etc.
 
 :EXIT
 SET MK_FILE=
