09aea7b646201da2c5ea6744798453b0ad770e2d
Alexander S.Kresin <alex@belacy.belgorod.su>
2003-05-06 09:53:37 +0000

    2003-05-06 12:55 UTC+0200 Przemyslaw Czerpak <druzus@polbox.com>

--- harbour/source/rtl/set.c
+++ harbour/source/rtl/set.c
@@ -463,6 +463,8 @@ HB_FUNC( SET )
             close_text( hb_set.hb_set_althan );
             if( hb_set.HB_SET_ALTFILE && strlen( hb_set.HB_SET_ALTFILE ) > 0 )
                hb_set.hb_set_althan = open_handle( hb_set.HB_SET_ALTFILE, bFlag, ".txt", HB_SET_ALTFILE );
+            else
+               hb_set.hb_set_althan = FS_ERROR;
          }
          break;
       case HB_SET_AUTOPEN    :
@@ -650,6 +652,8 @@ HB_FUNC( SET )
             close_text( hb_set.hb_set_extrahan );
             if( hb_set.HB_SET_EXTRAFILE && strlen( hb_set.HB_SET_EXTRAFILE ) > 0 )
                hb_set.hb_set_extrahan = open_handle( hb_set.HB_SET_EXTRAFILE, bFlag, ".prn", HB_SET_EXTRAFILE );
+            else
+               hb_set.hb_set_extrahan = FS_ERROR;
          }
          break;
       case HB_SET_FIXED      :
