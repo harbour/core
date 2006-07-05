4391696c36d9f872485c5e9ab45cbae0a5618d95
Marek Paliwoda <paliwoda@inetia.pl>
2006-07-05 16:42:44 +0000

    2006-07-05 18:30 UTC+0100 Marek Paliwoda (mpaliwoda at interia pl)    - harbour/make_b16.bat    - harbour/make_vc64.bat    - harbour/mkbcdll.bat    - harbour/hrbdll.bc    - harbour/makefile.nt    - harbour/makefile64.nt    - harbour/hrbdll.vc      - removed no more needed files
       * harbour/makefile.bc
       * harbour/makefile.vc
       * harbour/make_b32.bat
       * harbour/make_vc.bat
         * totaly rewritten to support sharing common file lists,
           automatic DLL creation and using implicit rules as a
           default approach
       + harbour/Common.mak
         + common file lists and definitions shared between Bcc
           and Msc makefiles
    
       TODO : add automatic building of contrib libs/dll(s)

--- harbour/make_b32.bat
+++ harbour/make_b32.bat
@@ -4,44 +4,89 @@ rem $Id$
 rem
 
 rem ---------------------------------------------------------------
-rem This is a generic template file, if it doesn't fit your own needs 
+rem This is a generic template file, if it doesn't fit your own needs
 rem please DON'T MODIFY IT.
 rem
-rem Instead, make a local copy and modify that one, or make a call to 
+rem Instead, make a local copy and modify that one, or make a call to
 rem this batch file from your customized one. [vszakats]
+rem ---------------------------------------------------------------
+
+if "%MAKE_PROGRAM%" == "" set MAKE_PROGRAM=make.exe
+
 rem ---------------------------------------------------------------
 
 if "%1" == "clean" goto CLEAN
 if "%1" == "CLEAN" goto CLEAN
 
-:BUILD
+rem ---------------------------------------------------------------
 
-   make -fmakefile.bc %1 %2 %3 > make_b32.log
+rem Unblock any of the below settings
+rem if you want to costumize your build
+rem process
+
+rem set HB_BUILD_MODE=C
+rem set HB_BUILD_DLL=yes
+rem set HB_DEBUG_BUILD=yes
+rem set SHOW_COMMANDS=yes
+
+rem ---------------------------------------------------------------
+
+:BUILD
+   if not exist obj\nul          md obj
+   if not exist obj\b32\nul      md obj\b32
+   if not exist obj\dll\nul      md obj\dll
+   if not exist obj\dll\b32\nul  md obj\dll\b32
+   if not exist lib\nul          md lib
+   if not exist lib\b32\nul      md lib\b32
+   if not exist bin\nul          md bin
+   if not exist bin\b32\nul      md bin\b32
+
+   %MAKE_PROGRAM% -r -fmakefile.bc %1 %2 %3 > make_b32.log
    if errorlevel 1 goto BUILD_ERR
 
-:BUILD_OK
+rem ---------------------------------------------------------------
 
+:BUILD_OK
    copy bin\b32\*.exe bin\*.* > nul
    copy lib\b32\*.lib lib\*.* > nul
    goto EXIT
 
-:BUILD_ERR
+rem ---------------------------------------------------------------
 
+:BUILD_ERR
    notepad make_b32.log
    goto EXIT
 
-:CLEAN
+rem ---------------------------------------------------------------
 
+:CLEAN
    if exist bin\b32\*.exe del bin\b32\*.exe
+   if exist bin\b32\*.dll del bin\b32\*.dll
+   if exist bin\b32\*.lib del bin\b32\*.lib
    if exist bin\b32\*.tds del bin\b32\*.tds
    if exist bin\b32\*.map del bin\b32\*.map
+
    if exist lib\b32\*.lib del lib\b32\*.lib
    if exist lib\b32\*.bak del lib\b32\*.bak
+
+   if exist obj\dll\b32\*.obj del obj\dll\b32\*.obj
+   if exist obj\dll\b32\*.c   del obj\dll\b32\*.c
+   if exist obj\dll\b32\*.h   del obj\dll\b32\*.h
+
    if exist obj\b32\*.obj del obj\b32\*.obj
    if exist obj\b32\*.c   del obj\b32\*.c
    if exist obj\b32\*.h   del obj\b32\*.h
+
+   if exist lib\*.lib del lib\*.lib
+
    if exist make_b32.log  del make_b32.log
    goto EXIT
 
-:EXIT
+rem ---------------------------------------------------------------
 
+:EXIT
+   set HB_BUILD_MODE=
+   set HB_BUILD_DLL=
+   set HB_DEBUG_BUILD=
+   set SHOW_COMMANDS=
+   set MAKE_PROGRAM=
--- harbour/make_vc.bat
+++ harbour/make_vc.bat
@@ -1,65 +1,91 @@
 @echo off
-rem 
+rem
 rem $Id$
-rem 
+rem
 
 rem ---------------------------------------------------------------
-rem This is a generic template file, if it doesn't fit your own needs 
+rem This is a generic template file, if it doesn't fit your own needs
 rem please DON'T MODIFY IT.
 rem
-rem Instead, make a local copy and modify that one, or make a call to 
+rem Instead, make a local copy and modify that one, or make a call to
 rem this batch file from your customized one. [vszakats]
 rem ---------------------------------------------------------------
 
 set rem=rem
 if "%1"=="/?" set rem=echo.
 %rem% ---------------------------------------------------------------
-%rem% Usage: make_vc [/y] [/a or CLEAN or other specific target]
-%rem% Call with nothing, /Y, /A, or CLEAN
+%rem% Usage: make_vc [CLEAN or <TARGET>]
+%rem% Call with nothing or CLEAN
 %rem% nothing - compiles what needs it.
 %rem% CLEAN, delete targets.
-%rem% /A clean, then compile all
-%rem% /Y non batch mode (forces makefile.vc)
 %rem% ---------------------------------------------------------------
 set rem=
-if "%1"=="/?" goto exit
+if "%1"=="/?" goto EXIT
 
-if not exist obj md obj
-if not exist obj\vc md obj\vc
-if not exist lib md lib
-if not exist lib\vc md lib\vc
-if not exist bin md bin
-if not exist bin\vc md bin\vc
+rem ---------------------------------------------------------------
 
-set MK_FILE=makefile.vc
-if "%OS%" == "Windows_NT" set MK_FILE=makefile.nt
-if "%1" == "/Y" set MK_FILE=makefile.vc
-if "%1" == "/y" set MK_FILE=makefile.vc
+if "%MAKE_PROGRAM%" == "" set MAKE_PROGRAM=nmake.exe
+
+rem ---------------------------------------------------------------
+
 if "%1" == "clean" goto CLEAN
 if "%1" == "CLEAN" goto CLEAN
 
-:BUILD
+rem ---------------------------------------------------------------
 
-   nmake /f %MK_FILE% %1 %2 %3 > make_vc.log
+rem Unblock any of the below settings
+rem if you want to costumize your build
+rem process
+
+rem set HB_BUILD_MODE=P
+rem set HB_BUILD_DLL=yes
+rem set HB_DEBUG_BUILD=yes
+rem set SHOW_COMMANDS=yes
+
+rem ---------------------------------------------------------------
+
+:BUILD
+   if not exist obj\nul         md obj
+   if not exist obj\vc\nul      md obj\vc
+   if not exist obj\dll\nul     md obj\dll
+   if not exist obj\dll\vc\nul  md obj\dll\vc
+   if not exist lib\nul         md lib
+   if not exist lib\vc\nul      md lib\vc
+   if not exist bin\nul         md bin
+   if not exist bin\vc\nul      md bin\vc
+
+   %MAKE_PROGRAM% %MKFLAGS% /f makefile.vc %1 %2 %3 >make_vc.log
    if errorlevel 1 goto BUILD_ERR
 
-:BUILD_OK
+rem ---------------------------------------------------------------
 
+:BUILD_OK
    copy bin\vc\*.exe bin\*.* > nul
    copy lib\vc\*.lib lib\*.* > nul
    goto EXIT
 
-:BUILD_ERR
+rem ---------------------------------------------------------------
 
+:BUILD_ERR
    notepad make_vc.log
    goto EXIT
 
-:CLEAN
+rem ---------------------------------------------------------------
 
-   nmake /f %MK_FILE% %1
-   rem in this case, the makefile handles most cleanup. Add what you need here
+:CLEAN
+   %MAKE_PROGRAM% %MKFLAGS% /f makefile.vc clean
+
+   rem in this case, the makefile handles
+   rem most cleanup. Add what you need here
+
    if exist make_vc.log del make_vc.log
-   rem etc.
+   goto EXIT
+
+rem ---------------------------------------------------------------
 
 :EXIT
-SET MK_FILE=
+   set HB_BUILD_MODE=
+   set HB_BUILD_DLL=
+   set HB_DEBUG_BUILD=
+   set SHOW_COMMANDS=
+   set MAKE_PROGRAM=
