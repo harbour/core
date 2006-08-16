f9e7bd031466512b8d37917a721199901286a550
Przemyslaw Czerpak <druzus@priv.onet.pl>
2006-08-16 16:52:06 +0000

    2006-08-16 18:50 UTC+0200 Przemyslaw Czerpak (druzus/at/priv.onet.pl)   * harbour/config/bsd/gcc.cf   * harbour/config/darwin/gcc.cf     * strip lib prefix from CONTRIBS library list
      * harbour/source/codepage/cp_tpl.c
        * removed redundant ;

--- harbour/source/codepage/cp_tpl.c
+++ harbour/source/codepage/cp_tpl.c
@@ -89,7 +89,7 @@ static HB_CODEPAGE s_codepage = { "EN",
     "abcdefghijklmnopqrstuvwxyz",
     IS_LATIN, ACCENTED_EQUAL, ACCENTED_INTERLEAVED, 0, 0, NULL, NULL, NULL, NULL, 0, NULL };
 
-HB_CODEPAGE_INIT( EN );
+HB_CODEPAGE_INIT( EN )
 
 #if defined(HB_PRAGMA_STARTUP)
    #pragma startup hb_codepage_Init_EN
