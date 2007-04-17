083e8763d24ebcb7a929d7ba3ed4d20c6202116e
Przemyslaw Czerpak <druzus@priv.onet.pl>
2007-04-17 22:01:39 +0000

    2007-04-18 00:00 UTC+0200 Przemyslaw Czerpak (druzus/at/priv.onet.pl)   * harbour/contrib/mysql/Makefile     * changed library name from mysql to hbmysql   * harbour/bin/hb-func.sh     + added hbgd hbole hbpg hbmysql to linked library list in hb* scripts

--- harbour/bin/hb-func.sh
+++ harbour/bin/hb-func.sh
@@ -74,7 +74,7 @@ mk_hbgetlibsctb()
 {
     if [ -z "$@" ]
     then
-        echo -n "rddads hbodbc tip ct nf"
+        echo -n "rddads ct nf tip hbgd hbodbc hbole hbpg hbmysql"
     else
         echo -n "$@"
     fi
