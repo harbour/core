83fc963e67fe16bf284f57a1587c36fe83f954fb
Przemyslaw Czerpak <druzus@priv.onet.pl>
2007-05-03 14:23:52 +0000

    2007-05-03 16:22 UTC+0200 Przemyslaw Czerpak (druzus/at/priv.onet.pl)   * harbour/source/rdd/usrrdd/usrrdd.c     * updated casting for recent modifications in RDD API

--- harbour/source/rdd/usrrdd/usrrdd.c
+++ harbour/source/rdd/usrrdd/usrrdd.c
@@ -2963,7 +2963,7 @@ HB_FUNC( USRRDD_GETFUNCTABLE )
          ++pRddFunction;
          ++pFunction;
       }
-      uiResult = hb_rddInherit( pSelfTable, &funcTable, pSuperTable, ( BYTE * ) szSuperRDD );
+      uiResult = hb_rddInherit( pSelfTable, &funcTable, pSuperTable, szSuperRDD );
       if( uiResult == SUCCESS )
       {
          pSelfTable->whoCares = ( DBENTRYP_SVP ) hb_itemNew( pMethods );
