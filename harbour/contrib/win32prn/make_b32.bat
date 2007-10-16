cdb7ed37123db1595fc2812d6919b81234c3c286
Viktor Szakats <harbour@syenar.net>
2007-10-16 13:20:22 +0000

    2007-10-16 15:18 UTC+0100 Viktor Szakats (harbour.01 syenar.hu)    + contrib/win32prn    + contrib/win32prn/makefile.bc    + contrib/win32prn/make_b32.bat    + contrib/win32prn/w32_pcls.prg    + contrib/win32prn/w32_papi.c    + contrib/win32prn/tprinter.c    + contrib/win32prn/testw32p.prg      + Ported Win32 specific bitmap printing class from xharbour core.      ; Please test.

---
+++ harbour/contrib/win32prn/make_b32.bat
@@ -0,0 +1,33 @@
+@echo off
+rem
+rem $Id$
+rem
+
+if "%1" == "clean" goto CLEAN
+if "%1" == "CLEAN" goto CLEAN
+
+:BUILD
+
+   make -fmakefile.bc %1 %2 %3 > make_b32.log
+   if errorlevel 1 goto BUILD_ERR
+
+:BUILD_OK
+
+   copy ..\..\lib\b32\hbwin32prn.lib ..\..\lib\*.* > nul
+   if exist ..\..\lib\b32\hbwin32prn.bak del ..\..\lib\b32\hbwin32prn.bak
+   goto EXIT
+
+:BUILD_ERR
+
+   notepad make_b32.log
+   goto EXIT
+
+:CLEAN
+   if exist ..\..\lib\b32\hbwin32prn.lib del ..\..\lib\b32\hbwin32prn.lib
+   if exist ..\..\lib\b32\hbwin32prn.bak del ..\..\lib\b32\hbwin32prn.bak
+   if exist ..\..\obj\b32\w32_papi.obj   del ..\..\obj\b32\w32_papi.obj
+   if exist ..\..\obj\b32\w32_pcls.obj   del ..\..\obj\b32\w32_pcls.obj
+
+   goto EXIT
+
+:EXIT
