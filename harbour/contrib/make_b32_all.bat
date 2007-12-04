1087aa836f9bc0ac7d130d3113b0f9f86d40cac7
Marek Paliwoda <paliwoda@inetia.pl>
2007-12-04 09:17:40 +0000

    2007-12-04 10:17 UTC+0100 Marek Paliwoda (mpaliwoda at interia pl)   * harbour/contrib/make_b32_all.bat   * harbour/contrib/make_vc_all.bat     - Removed the dependancy on a worker bat file       named make_<b32|vc>.bat. Now a worker bat file       is created on a fly.
      - harbour/contrib/make_b32.bat
      - harbour/contrib/make_vc.bat
        - Removed. No longer neccesary.

--- harbour/contrib/make_b32_all.bat
+++ harbour/contrib/make_b32_all.bat
@@ -3,18 +3,35 @@ rem
 rem $Id$
 rem
 
-rem The compilation is done in three steps. PLEASE DO NOT MODIFY
-rem IT or you will break Win9x command.com line length limit !!!
+rem *******************************************************
+rem The compilation is done in three steps. PLEASE DO NOT MODIFY IT
+rem or you will break a Windows9x command.com line length limit !!!
+rem *******************************************************
 
 set HB_SHOW_ERRORS=no
 
-rem set DO_NOT_COMPILE=examples hbclip hgf hbwhat32
+rem *******************************************************
+rem Creating a worker bat file ...
+rem *******************************************************
+
+set __BATWORKER__=_hbwrk_.bat
+
+echo @echo off                                          >%__BATWORKER__%
+echo if not exist %%1\make_b32.bat goto EXIT           >>%__BATWORKER__%
+echo echo Entering: %%1                                >>%__BATWORKER__%
+echo cd %%1                                            >>%__BATWORKER__%
+echo call make_b32.bat %%2 %%3 %%4 %%5 %%6 %%7 %%8 %%9 >>%__BATWORKER__%
+echo cd ..                                             >>%__BATWORKER__%
+
+rem *******************************************************
+rem Compiling contrib dirs ...
+rem *******************************************************
 
 set _HB_DIRS=hbrddado hbbmcdx hbbtree hbgtwvg hbct hbgt hbmisc hbnf hbmsql
-for %%n in ( %_HB_DIRS% ) do %COMSPEC% /c make_b32.bat %%n %1 %2 %3 %4 %5 %6 %7 %8 %9
+for %%n in ( %_HB_DIRS% ) do %COMSPEC% /c %__BATWORKER__% %%n %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 set _HB_DIRS=hbole hbziparch hbodbc hbtpathy hbtip hbw32 xhb
-for %%n in ( %_HB_DIRS% ) do %COMSPEC% /c make_b32.bat %%n %1 %2 %3 %4 %5 %6 %7 %8 %9
+for %%n in ( %_HB_DIRS% ) do %COMSPEC% /c %__BATWORKER__% %%n %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 set _HB_DIRS=hbclipsm hbw32ddr
 if not "%APOLLO_DIR%"    == "" set _HB_DIRS=%_HB_DIRS% hbapollo
@@ -24,7 +41,14 @@ if not "%GD_DIR%"        == "" set _HB_DIRS=%_HB_DIRS% hbgd
 if not "%MYSQL_DIR%"     == "" set _HB_DIRS=%_HB_DIRS% hbmysql
 if not "%PGSQL_DIR%"     == "" set _HB_DIRS=%_HB_DIRS% hbpgsql
 if not "%ADS_DIR%"       == "" set _HB_DIRS=%_HB_DIRS% hbrddads
-for %%n in ( %_HB_DIRS% ) do %COMSPEC% /c make_b32.bat %%n %1 %2 %3 %4 %5 %6 %7 %8 %9
+for %%n in ( %_HB_DIRS% ) do %COMSPEC% /c %__BATWORKER__% %%n %1 %2 %3 %4 %5 %6 %7 %8 %9
 
-set HB_SHOW_ERRORS=
+rem *******************************************************
+rem Cleaning ...
+rem *******************************************************
+
+del %__BATWORKER__% > nul
+
 set _HB_DIRS=
+set __BATWORKER__=
+set HB_SHOW_ERRORS=
