9fc3562477655697145c1a942ca2f95198aab856
Miguel Angel Marchuet Frutos <miguelangel@marchuet.net>
2008-01-15 11:33:42 +0000

    2008-01-14 12:28 UTC+0100 Miguel Angel Marchuet Frutos <miguelangel@marchuet.net>   * source/vm/extend.c     * Added conversion from logical params in hb_par* functions.       ( thks to Juan Gálvez )

--- harbour/source/vm/extend.c
+++ harbour/source/vm/extend.c
@@ -434,6 +434,8 @@ HB_EXPORT double  hb_parnd( int iParam, ... )
          return ( double ) pItem->item.asInteger.value;
       else if( HB_IS_LONG( pItem ) )
          return ( double ) pItem->item.asLong.value;
+      else if( HB_IS_LOGICAL( pItem ) )
+         return ( double ) pItem->item.asLogical.value;
       else if( HB_IS_ARRAY( pItem ) )
       {
          va_list va;
@@ -467,6 +469,8 @@ HB_EXPORT int  hb_parni( int iParam, ... )
          return ( int ) pItem->item.asLong.value;
       else if( HB_IS_DOUBLE( pItem ) )
          return ( int ) pItem->item.asDouble.value;
+      else if( HB_IS_LOGICAL( pItem ) )
+         return ( int ) pItem->item.asLogical.value;
       else if( HB_IS_ARRAY( pItem ) )
       {
          va_list va;
@@ -506,6 +510,8 @@ HB_EXPORT long  hb_parnl( int iParam, ... )
 #endif
       else if( HB_IS_DATE( pItem ) )
          return ( long ) pItem->item.asDate.value;
+      else if( HB_IS_LOGICAL( pItem ) )
+         return ( long ) pItem->item.asLogical.value;
       else if( HB_IS_ARRAY( pItem ) )
       {
          va_list va;
@@ -546,6 +552,8 @@ HB_EXPORT LONGLONG  hb_parnll( int iParam, ... )
 #endif
       else if( HB_IS_DATE( pItem ) )
          return ( LONGLONG ) pItem->item.asDate.value;
+      else if( HB_IS_LOGICAL( pItem ) )
+         return ( LONGLONG ) pItem->item.asLogical.value;
       else if( HB_IS_ARRAY( pItem ) )
       {
          va_list va;
@@ -586,6 +594,8 @@ HB_EXPORT HB_LONG  hb_parnint( int iParam, ... )
 #endif
       else if( HB_IS_DATE( pItem ) )
          return ( HB_LONG ) pItem->item.asDate.value;
+      else if( HB_IS_LOGICAL( pItem ) )
+         return ( HB_LONG ) pItem->item.asLogical.value;
       else if( HB_IS_ARRAY( pItem ) )
       {
          va_list va;
