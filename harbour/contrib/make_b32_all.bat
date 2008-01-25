49a0e3e484875b97626bb48c583eee8e714317c5
Marek Paliwoda <paliwoda@inetia.pl>
2008-01-25 09:46:54 +0000

    2008-01-25 10:47 UTC+0100 Marek Paliwoda (mpaliwoda at interia pl)    * contrib/make_b32_all.bat    * contrib/make_vc_all.bat     ! moved hbzlib compilation into "conditional section" of above batfiles

--- harbour/contrib/make_b32_all.bat
+++ harbour/contrib/make_b32_all.bat
@@ -32,7 +32,7 @@ rem *******************************************************
 set _HB_DIRS=hbrddado hbbmcdx hbbtree hbgtwvg hbct hbgt hbmisc hbnf hbmsql
 for %%n in ( %_HB_DIRS% ) do %COMSPEC% /c %__BATWORKER__% %%n %1 %2 %3 %4 %5 %6 %7 %8 %9
 
-set _HB_DIRS=hbole hbziparch hbodbc hbtpathy hbtip hbw32 hbzlib xhb
+set _HB_DIRS=hbole hbziparch hbodbc hbtpathy hbtip hbw32 xhb
 for %%n in ( %_HB_DIRS% ) do %COMSPEC% /c %__BATWORKER__% %%n %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 set _HB_DIRS=hbclipsm hbw32ddr
@@ -43,6 +43,7 @@ if not "%GD_DIR%"        == "" set _HB_DIRS=%_HB_DIRS% hbgd
 if not "%MYSQL_DIR%"     == "" set _HB_DIRS=%_HB_DIRS% hbmysql
 if not "%PGSQL_DIR%"     == "" set _HB_DIRS=%_HB_DIRS% hbpgsql
 if not "%ADS_DIR%"       == "" set _HB_DIRS=%_HB_DIRS% hbrddads
+if not "%ZLIB_DIR%"      == "" set _HB_DIRS=%_HB_DIRS% hbzlib
 for %%n in ( %_HB_DIRS% ) do %COMSPEC% /c %__BATWORKER__% %%n %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 rem *******************************************************
