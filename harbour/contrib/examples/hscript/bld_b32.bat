07ea129a9d82d1f7f4e0873c199533aeb01ba81d
Viktor Szakats <harbour@syenar.net>
2008-01-31 21:11:40 +0000

    2008-01-31 22:10 UTC+0100 Viktor Szakats (harbour.01 syenar.hu)    * contrib/examples/guestbk/bld_b32.bat    * contrib/examples/pe/bld_b32.bat    * contrib/examples/dbu/bld_b32.bat    * contrib/examples/hscript/bld_b32.bat      ! Lib name changes.

--- harbour/contrib/examples/hscript/bld_b32.bat
+++ harbour/contrib/examples/hscript/bld_b32.bat
@@ -6,5 +6,5 @@ rem
 rem NOTE: This sample program need nf.lib from contrib/libnf
 
 ..\..\..\bin\harbour hscript /n /i..\..\..\include
-bcc32 -O2 -I..\..\..\include -L..\..\..\lib -ehscript.exe hscript.c debug.lib vm.lib rtl.lib gtwin.lib lang.lib rdd.lib macro.lib pp.lib dbfntx.lib dbfcdx.lib dbffpt.lib codepage.lib hbsix.lib common.lib hbpcre.lib hsx.lib nf.lib
+bcc32 -O2 -I..\..\..\include -L..\..\..\lib -ehscript.exe hscript.c hbdebug.lib hbvm.lib hbrtl.lib gtwin.lib hblang.lib hbrdd.lib hbmacro.lib hbpp.lib rddntx.lib rddcdx.lib rddfpt.lib hbcpage.lib hbsix.lib hbcommon.lib hbpcre.lib hbhsx.lib hbnf.lib
 del hscript.c
--- harbour/contrib/examples/pe/bld_b32.bat
+++ harbour/contrib/examples/pe/bld_b32.bat
@@ -9,19 +9,19 @@ rem
 echo -O2 -I..\..\include -L..\..\lib > build.tmp
 echo -epe.exe >> build.tmp
 echo pe.c editorhi.c editorlo.c >> build.tmp
-echo debug.lib >> build.tmp
-echo vm.lib >> build.tmp
-echo rtl.lib >> build.tmp
+echo hbdebug.lib >> build.tmp
+echo hbvm.lib >> build.tmp
+echo hbrtl.lib >> build.tmp
 echo gtwin.lib >> build.tmp
-echo lang.lib >> build.tmp
-echo rdd.lib >> build.tmp
-echo macro.lib >> build.tmp
-echo pp.lib >> build.tmp
-echo dbfntx.lib >> build.tmp
-echo dbfcdx.lib >> build.tmp
-echo dbffpt.lib >> build.tmp
+echo hblang.lib >> build.tmp
+echo hbrdd.lib >> build.tmp
+echo hbmacro.lib >> build.tmp
+echo hbpp.lib >> build.tmp
+echo rddntx.lib >> build.tmp
+echo rddcdx.lib >> build.tmp
+echo rddfpt.lib >> build.tmp
 echo hbsix.lib >> build.tmp
-echo common.lib >> build.tmp
+echo hbcommon.lib >> build.tmp
 bcc32 @build.tmp
 del build.tmp
 
