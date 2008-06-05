73b29af5222b03af49f8f9c19ed697048a1b1517
Viktor Szakats <harbour@syenar.net>
2008-06-05 20:06:24 +0000

    2008-06-05 20:30 UTC+0100 Viktor Szakats (harbour.01 syenar hu)    - contrib/examples/pp/hbpp.c    + contrib/examples/pp/pp.c    * contrib/examples/pp/bld_b32.bat    * contrib/examples/pp/Makefile      * hbpp.c -> pp.c to generate pp.exe using GNU make files.
       * contrib/examples/pp/bld_b32.bat
         % Removed unnecessary libs.

--- harbour/contrib/examples/pp/bld_b32.bat
+++ harbour/contrib/examples/pp/bld_b32.bat
@@ -5,22 +5,9 @@ rem
 
 echo -O2 -I..\..\..\include -L..\..\..\lib > build.tmp
 echo -epp.exe >> build.tmp
-echo hbpp.c hbppcomp.c hbppcore.c hbpptbl.c hbpragma.c >> build.tmp
-echo hbdebug.lib >> build.tmp
-echo hbvm.lib >> build.tmp
-echo hbrtl.lib >> build.tmp
-echo gtwin.lib >> build.tmp
-echo hblang.lib >> build.tmp
-echo hbrdd.lib >> build.tmp
-echo hbmacro.lib >> build.tmp
-echo hbpp.lib >> build.tmp
-echo rddntx.lib >> build.tmp
-echo rddcdx.lib >> build.tmp
-echo rddfpt.lib >> build.tmp
-echo hbsix.lib >> build.tmp
+echo pp.c hbppcomp.c hbppcore.c hbpptbl.c hbpragma.c >> build.tmp
 echo hbcommon.lib >> build.tmp
 bcc32 @build.tmp
 del build.tmp
 
 pp
-
