059883e598c6dce5fd4b323804ba1da97d8865d2
Viktor Szakats <harbour@syenar.net>
2008-07-05 00:00:40 +0000

    2008-07-05 01:59 UTC+0200 Viktor Szakats (harbour.01 syenar hu)    * include/hbver.h      ! Typo in comment.

--- harbour/include/hbver.h
+++ harbour/include/hbver.h
@@ -65,6 +65,6 @@
 #define HB_VER_MINOR    0        /* Minor version number */
 #define HB_VER_REVISION 0        /* Revision number */
 #define HB_VER_STATUS   "rc2"    /* Build status */
-#define __HARBOUR__     0x010000 /* Two bytes: Major + Minor + Revision. This is recommented for 3rd party .c and .prg level code. */
+#define __HARBOUR__     0x010000 /* Three bytes: Major + Minor + Revision. This is recommented for 3rd party .c and .prg level code. */
 
 #endif /* HB_VER_H_ */
