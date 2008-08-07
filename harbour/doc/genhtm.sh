fe6afd4252f744c87b7fe069f9ea1ad06b2162c8
Viktor Szakats <harbour@syenar.net>
2008-08-07 22:36:46 +0000

    2008-08-08 00:33 UTC+0200 Viktor Szakats (harbour.01 syenar hu)    + doc/genhtm.sh      + Added Linux/Unix make file for docs (sf.net #1673897)        Thanks to Wilfried Brunken - df7be.

---
+++ harbour/doc/genhtm.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+hbdoc -htm genhtm.lnk genhtm.rsp
+cd htm
+#
+echo "renaming harbour.htm to index.htm"
+mv harbour.htm index.htm
+rm -f genwww.lnk
+mv genwww.old genwww.lnk
