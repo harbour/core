13817e089801c1b9899d3fd15deaf7127bdab426
Viktor Szakats <harbour@syenar.net>
2008-09-03 14:37:53 +0000

    2008-09-03 16:36 UTC+0200 Viktor Szakats (harbour.01 syenar hu)    * contrib/hbziparc/make_b32.bat    * contrib/hbziparc/make_vc.bat      % Special C flags not needed anymore.
       - contrib/hbziparc/hbzipold.cpp
       + contrib/hbziparc/hbzipcpp.old
         * Renamed to not look like something live.

--- harbour/contrib/hbziparc/make_b32.bat
+++ harbour/contrib/hbziparc/make_b32.bat
@@ -3,8 +3,4 @@ rem
 rem $Id$
 rem
 
-set CFLAGS=-w-inl- -w-stl- -w-ncf-
-
 call ..\mtpl_b32.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
-
-set CFLAGS=
--- harbour/contrib/hbziparc/make_vc.bat
+++ harbour/contrib/hbziparc/make_vc.bat
@@ -3,8 +3,4 @@ rem
 rem $Id$
 rem
 
-set CFLAGS=-EHsc
-
 call ..\mtpl_vc.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
-
-set CFLAGS=
