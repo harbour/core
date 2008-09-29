9de9b0d5d95b21dfed5dc663420e2f1dcbeaae7a
Przemyslaw Czerpak <druzus@priv.onet.pl>
2008-09-29 16:50:43 +0000

    2008-09-29 18:49 UTC+0200 Przemyslaw Czerpak (druzus/at/priv.onet.pl)   * harbour/config/os2/gcc.cf   * harbour/make_gnu_os2.cmd     * moved -DTCPV40HDRS to make_gnu_os2.cmd and added small description       about this macro. By default is enabled, can be disabled by       setting environment variable HB_OS2_TCP32 to 'yes', f.e.          SET HB_OS2_TCP32=yes       David and Maurilio, please check me.

--- harbour/make_gnu_os2.cmd
+++ harbour/make_gnu_os2.cmd
@@ -9,4 +9,29 @@ rem for further information about see make_gnu.bat
 if "%HB_ARCHITECTURE%" == "" set HB_ARCHITECTURE=os2
 if "%HB_COMPILER%" == "" set HB_COMPILER=gcc
 
+
+rem In GCC3.2.2 the TCP/IP headers and libraries scheme have been changed.
+rem The default is the current OS/2 tcpip toolkit (BSD 4.4 based).
+rem To target the older OS/2 tcpip stack (BSD 4.3 based) and create
+rem binaries which can be executed also on older OS2 versions you must
+rem define TCPV40HDRS before including any TCP/IP headers and make
+rem sure usr/lib/tcpipv4 is searched before usr/lib (this is to
+rem get the right libsocket). It is recommended to use the -D
+rem compiler option for the define and either the LIBRARY_PATH or
+rem the -L compiler/linker option for the library.
+rem For building Harbour you can also use L_USR environment variable,
+rem f.e.
+rem         SET L_USR=-Le:\usr\lib\tcpipv4
+rem 
+rem If you are using newer OS2 version with tcp/ip stack >= 4.1
+rem (eComStation, for example) and you do not need backward binary
+rem compatibility then you can disable it by setting HB_OS2_TCP32
+rem environment variable, f.e.
+rem         SET HB_OS2_TCP32=yes
+
+if "%HB_OS2_TCP32%" == "yes" goto tcp32
+if "%HB_OS2_TCP32%" == "YES" goto tcp32
+   set C_USR=-DTCPV40HDRS %C_USR%
+:tcp32
+
 make -r %1 %2 %3 %4 %5 %6 %7 %8 %9 > make_gnu.log
