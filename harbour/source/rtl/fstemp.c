73c7f2a2bdded71a950ae6b9f00e6b84c1afb1c5
Przemyslaw Czerpak <druzus@priv.onet.pl>
2009-02-07 01:11:03 +0000

    2009-02-07 02:15 UTC+0100 Przemyslaw Czerpak (druzus/at/priv.onet.pl)   * harbour/source/rtl/fstemp.c     * casting for C++ mode

--- harbour/source/rtl/fstemp.c
+++ harbour/source/rtl/fstemp.c
@@ -157,7 +157,7 @@ static HB_FHANDLE hb_fsCreateTempLow( const BYTE * pszDir, const BYTE * pszPrefi
       {
          hb_strncat( ( char * ) pszName, "XXXXXX", _POSIX_PATH_MAX );
          if( pszExt )
-            hb_strncat( ( char * ) pszName, pszExt, _POSIX_PATH_MAX );
+            hb_strncat( ( char * ) pszName, ( const char * ) pszExt, _POSIX_PATH_MAX );
          hb_vmUnlock();
          fd = ( HB_FHANDLE ) mkstemp( ( char * ) pszName );
          hb_fsSetIOError( fd != ( HB_FHANDLE ) -1, 0 );
