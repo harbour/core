dc2a07f2b75ec0dbc507ff220f4e8bdc343d3f22
Viktor Szakats <harbour@syenar.net>
2009-02-11 18:05:27 +0000

    2009-02-11 19:03 UTC+0100 Viktor Szakats (harbour.01 syenar hu)   * COPYING   * source/rtl/philes.c     ! Fixed license to have the Harbour exception, in agreement       with the group and main copyright holders.     * Minor cleanup to HB_FGETDATETIME().

--- harbour/source/rtl/philes.c
+++ harbour/source/rtl/philes.c
@@ -6,20 +6,16 @@
  * Harbour Project source code:
  * The FileSys API (Harbour level)
  *
+ * Copyright 1999-2009 Viktor Szakats <harbour.01 syenar.hu>
+ * Copyright 2008 Przemyslaw Czerpak <druzus / at / priv.onet.pl>
+ * Copyright 2000 David G. Holm <dholm@jsd-llc.com>
  * Copyright 1999 Manuel Ruiz <mrt@joca.es>
  * www - http://www.harbour-project.org
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version, with one exception:
- *
- * The exception is that if you link the Harbour Runtime Library (HRL)
- * and/or the Harbour Virtual Machine (HVM) with other files to produce
- * an executable, this does not by itself cause the resulting executable
- * to be covered by the GNU General Public License. Your use of that
- * executable is in no way restricted on account of linking the HRL
- * and/or HVM code into it.
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -27,30 +23,33 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA (or visit
- * their web site at http://www.gnu.org/).
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307 USA (or visit the web site http://www.gnu.org/).
  *
- */
-
-/*
- * The following parts are Copyright of the individual authors.
- * www - http://www.harbour-project.org
+ * As a special exception, the Harbour Project gives permission for
+ * additional uses of the text contained in its release of Harbour.
  *
- * Copyright 1999-2009 Viktor Szakats <viktor.szakats@syenar.hu>
- *    CURDIR()
- *    HB_FLOCK()
- *    HB_FUNLOCK()
- *    HB_PROGNAME()
- *    HB_DIRBASE()
- *    HB_FSETATTR()
- *    HB_FGETATTR()
- *    HB_FSETDATETIME()
+ * The exception is that, if you link the Harbour libraries with other
+ * files to produce an executable, this does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * Your use of that executable is in no way restricted on account of
+ * linking the Harbour library code into it.
  *
- * Copyright 2000 David G. Holm <dholm@jsd-llc.com>
- *    HB_FEOF()
+ * This exception does not however invalidate any other reasons why
+ * the executable file might be covered by the GNU General Public License.
  *
- * See doc/license.txt for licensing terms.
+ * This exception applies only to the code released by the Harbour
+ * Project under the name Harbour.  If you copy code from other
+ * Harbour Project or Free Software Foundation releases into a copy of
+ * Harbour, as the General Public License permits, the exception does
+ * not apply to the code that you add in this way.  To avoid misleading
+ * anyone as to the status of such modified files, you must delete
+ * this exception notice from them.
+ *
+ * If you write modifications of your own for Harbour, it is your choice
+ * whether to permit this exception to apply to your modifications.
+ * If you do not wish that, delete this exception notice.
  *
  */
 
@@ -404,12 +403,11 @@ HB_FUNC( HB_FSETDATETIME )
 
 HB_FUNC( HB_FGETDATETIME )
 {
-   LONG   lJulian, lMillisec;
-   BOOL   fRet;
+   LONG lJulian, lMillisec;
 
    if( hb_fsGetFileTime( ( UCHAR * ) hb_parcx( 1 ), &lJulian, &lMillisec ) )
    {
-      char  buf[ 13 ];
+      char buf[ 13 ];
 
       hb_stordl( lJulian, 2 );
 
@@ -419,10 +417,10 @@ HB_FUNC( HB_FGETDATETIME )
 
       /* hb_stornl( lMillisec, 4 ); */
 
-      hb_retl( 1 );
+      hb_retl( TRUE );
    }
    else
-      hb_retl( 0 );
+      hb_retl( FALSE );
 }
 
 HB_FUNC( HB_OSERROR )
