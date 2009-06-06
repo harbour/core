dd5f087fd0169c065cd5cef8ebc33195ecd8f5f5
Pritpal Bedi <bedipritpal@hotmail.com>
2009-06-06 17:44:29 +0000

    2009-06-06 10:33 UTC-0800 Pritpal Bedi (pritpal@vouchcac.com)   * harbour/contrib/hbqt/Makefile_gen
      + harbour/contrib/hbqt/hbqt_qstyleoptionslider.cpp
      + harbour/contrib/hbqt/hbqt_qstyleoptionspinbox.cpp
      + harbour/contrib/hbqt/hbqt_qstyleoptiontab.cpp
      + harbour/contrib/hbqt/hbqt_qstyleoptiontabbarbase.cpp
      + harbour/contrib/hbqt/hbqt_qstyleoptiontabwidgetframe.cpp
      + harbour/contrib/hbqt/hbqt_qstyleoptiontitlebar.cpp
      + harbour/contrib/hbqt/hbqt_qstyleoptiontoolbar.cpp
      + harbour/contrib/hbqt/hbqt_qstyleoptiontoolbox.cpp
      + harbour/contrib/hbqt/hbqt_qstyleoptiontoolbutton.cpp
      + harbour/contrib/hbqt/hbqt_qstyleoptionviewitem.cpp
      + harbour/contrib/hbqt/hbqt_qstylepainter.cpp
    
      * harbour/contrib/hbqt/hbqt.h
    
      * harbour/contrib/hbqt/generator/hbqtgen.prg
    
      + harbour/contrib/hbqt/TQStyleOptionSlider.prg
      + harbour/contrib/hbqt/TQStyleOptionSpinBox.prg
      + harbour/contrib/hbqt/TQStyleOptionTab.prg
      + harbour/contrib/hbqt/TQStyleOptionTabBarBase.prg
      + harbour/contrib/hbqt/TQStyleOptionTabWidgetFrame.prg
      + harbour/contrib/hbqt/TQStyleOptionTitleBar.prg
      + harbour/contrib/hbqt/TQStyleOptionToolBar.prg
      + harbour/contrib/hbqt/TQStyleOptionToolBox.prg
      + harbour/contrib/hbqt/TQStyleOptionToolButton.prg
      + harbour/contrib/hbqt/TQStyleOptionViewItem.prg
      + harbour/contrib/hbqt/TQStylePainter.prg
    
      + harbour/contrib/hbqt/qth/QStyleOptionSlider.qth
      + harbour/contrib/hbqt/qth/QStyleOptionSpinBox.qth
      + harbour/contrib/hbqt/qth/QStyleOptionTab.qth
      + harbour/contrib/hbqt/qth/QStyleOptionTabBarBase.qth
      + harbour/contrib/hbqt/qth/QStyleOptionTabWidgetFrame.qth
      + harbour/contrib/hbqt/qth/QStyleOptionTitleBar.qth
      + harbour/contrib/hbqt/qth/QStyleOptionToolBar.qth
      + harbour/contrib/hbqt/qth/QStyleOptionToolBox.qth
      + harbour/contrib/hbqt/qth/QStyleOptionToolButton.qth
      + harbour/contrib/hbqt/qth/QStyleOptionViewItem.qth
      + harbour/contrib/hbqt/qth/QStylePainter.qth
    
      * harbour/contrib/hbqt/generator/qt45.qtp
       
        + Added more classes - count == 179

---
+++ harbour/contrib/hbqt/hbqt_qstyleoptiontab.cpp
@@ -0,0 +1,135 @@
+/*
+ * $Id$
+ */
+
+/*
+ * Harbour Project source code:
+ * QT wrapper main header
+ *
+ * Copyright 2009 Marcos Antonio Gambeta <marcosgambeta at gmail dot com>
+ * Copyright 2009 Pritpal Bedi <pritpal@vouchcac.com>
+ * www - http://www.harbour-project.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307 USA (or visit the web site http://www.gnu.org/).
+ *
+ * As a special exception, the Harbour Project gives permission for
+ * additional uses of the text contained in its release of Harbour.
+ *
+ * The exception is that, if you link the Harbour libraries with other
+ * files to produce an executable, this does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * Your use of that executable is in no way restricted on account of
+ * linking the Harbour library code into it.
+ *
+ * This exception does not however invalidate any other reasons why
+ * the executable file might be covered by the GNU General Public License.
+ *
+ * This exception applies only to the code released by the Harbour
+ * Project under the name Harbour.  If you copy code from other
+ * Harbour Project or Free Software Foundation releases into a copy of
+ * Harbour, as the General Public License permits, the exception does
+ * not apply to the code that you add in this way.  To avoid misleading
+ * anyone as to the status of such modified files, you must delete
+ * this exception notice from them.
+ *
+ * If you write modifications of your own for Harbour, it is your choice
+ * whether to permit this exception to apply to your modifications.
+ * If you do not wish that, delete this exception notice.
+ *
+ */
+/*----------------------------------------------------------------------*/
+
+#include "hbapi.h"
+#include "hbqt.h"
+
+/*----------------------------------------------------------------------*/
+#if QT_VERSION >= 0x040500
+/*----------------------------------------------------------------------*/
+
+
+#include <QtGui/QStyleOptionTab>
+
+
+/*
+ * QStyleOptionTab ()
+ * QStyleOptionTab ( const QStyleOptionTab & other )
+ */
+HB_FUNC( QT_QSTYLEOPTIONTAB )
+{
+   hb_retptr( ( QStyleOptionTab* ) new QStyleOptionTab() );
+}
+
+/*
+ * CornerWidgets cornerWidgets
+ */
+HB_FUNC( QT_QSTYLEOPTIONTAB_CORNERWIDGETS )
+{
+   hb_retni( ( QStyleOptionTab::CornerWidgets ) hbqt_par_QStyleOptionTab( 1 )->cornerWidgets );
+}
+
+/*
+ * QIcon icon
+ */
+HB_FUNC( QT_QSTYLEOPTIONTAB_ICON )
+{
+   hb_retptr( new QIcon( hbqt_par_QStyleOptionTab( 1 )->icon ) );
+}
+
+/*
+ * TabPosition position
+ */
+HB_FUNC( QT_QSTYLEOPTIONTAB_POSITION )
+{
+   hb_retni( ( QStyleOptionTab::TabPosition ) hbqt_par_QStyleOptionTab( 1 )->position );
+}
+
+/*
+ * int row
+ */
+HB_FUNC( QT_QSTYLEOPTIONTAB_ROW )
+{
+   hb_retni( hbqt_par_QStyleOptionTab( 1 )->row );
+}
+
+/*
+ * SelectedPosition selectedPosition
+ */
+HB_FUNC( QT_QSTYLEOPTIONTAB_SELECTEDPOSITION )
+{
+   hb_retni( ( QStyleOptionTab::SelectedPosition ) hbqt_par_QStyleOptionTab( 1 )->selectedPosition );
+}
+
+/*
+ * QTabBar::Shape shape
+ */
+HB_FUNC( QT_QSTYLEOPTIONTAB_SHAPE )
+{
+   hb_retni( ( QTabBar::Shape ) hbqt_par_QStyleOptionTab( 1 )->shape );
+}
+
+/*
+ * QString text
+ */
+HB_FUNC( QT_QSTYLEOPTIONTAB_TEXT )
+{
+   hb_retc( hbqt_par_QStyleOptionTab( 1 )->text.toLatin1().data() );
+}
+
+
+/*----------------------------------------------------------------------*/
+#endif             /* #if QT_VERSION >= 0x040500 */
+/*----------------------------------------------------------------------*/
+
---
+++ harbour/contrib/hbqt/hbqt_qstyleoptionviewitem.cpp
@@ -0,0 +1,135 @@
+/*
+ * $Id$
+ */
+
+/*
+ * Harbour Project source code:
+ * QT wrapper main header
+ *
+ * Copyright 2009 Marcos Antonio Gambeta <marcosgambeta at gmail dot com>
+ * Copyright 2009 Pritpal Bedi <pritpal@vouchcac.com>
+ * www - http://www.harbour-project.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307 USA (or visit the web site http://www.gnu.org/).
+ *
+ * As a special exception, the Harbour Project gives permission for
+ * additional uses of the text contained in its release of Harbour.
+ *
+ * The exception is that, if you link the Harbour libraries with other
+ * files to produce an executable, this does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * Your use of that executable is in no way restricted on account of
+ * linking the Harbour library code into it.
+ *
+ * This exception does not however invalidate any other reasons why
+ * the executable file might be covered by the GNU General Public License.
+ *
+ * This exception applies only to the code released by the Harbour
+ * Project under the name Harbour.  If you copy code from other
+ * Harbour Project or Free Software Foundation releases into a copy of
+ * Harbour, as the General Public License permits, the exception does
+ * not apply to the code that you add in this way.  To avoid misleading
+ * anyone as to the status of such modified files, you must delete
+ * this exception notice from them.
+ *
+ * If you write modifications of your own for Harbour, it is your choice
+ * whether to permit this exception to apply to your modifications.
+ * If you do not wish that, delete this exception notice.
+ *
+ */
+/*----------------------------------------------------------------------*/
+
+#include "hbapi.h"
+#include "hbqt.h"
+
+/*----------------------------------------------------------------------*/
+#if QT_VERSION >= 0x040500
+/*----------------------------------------------------------------------*/
+
+
+#include <QtGui/QStyleOptionViewItem>
+
+
+/*
+ * QStyleOptionViewItem ()
+ * QStyleOptionViewItem ( const QStyleOptionViewItem & other )
+ */
+HB_FUNC( QT_QSTYLEOPTIONVIEWITEM )
+{
+   hb_retptr( ( QStyleOptionViewItem* ) new QStyleOptionViewItem() );
+}
+
+/*
+ * Qt::Alignment decorationAlignment
+ */
+HB_FUNC( QT_QSTYLEOPTIONVIEWITEM_DECORATIONALIGNMENT )
+{
+   hb_retni( ( Qt::Alignment ) hbqt_par_QStyleOptionViewItem( 1 )->decorationAlignment );
+}
+
+/*
+ * Position decorationPosition
+ */
+HB_FUNC( QT_QSTYLEOPTIONVIEWITEM_DECORATIONPOSITION )
+{
+   hb_retni( ( QStyleOptionViewItem::Position ) hbqt_par_QStyleOptionViewItem( 1 )->decorationPosition );
+}
+
+/*
+ * QSize decorationSize
+ */
+HB_FUNC( QT_QSTYLEOPTIONVIEWITEM_DECORATIONSIZE )
+{
+   hb_retptr( new QSize( hbqt_par_QStyleOptionViewItem( 1 )->decorationSize ) );
+}
+
+/*
+ * Qt::Alignment displayAlignment
+ */
+HB_FUNC( QT_QSTYLEOPTIONVIEWITEM_DISPLAYALIGNMENT )
+{
+   hb_retni( ( Qt::Alignment ) hbqt_par_QStyleOptionViewItem( 1 )->displayAlignment );
+}
+
+/*
+ * QFont font
+ */
+HB_FUNC( QT_QSTYLEOPTIONVIEWITEM_FONT )
+{
+   hb_retptr( new QFont( hbqt_par_QStyleOptionViewItem( 1 )->font ) );
+}
+
+/*
+ * bool showDecorationSelected
+ */
+HB_FUNC( QT_QSTYLEOPTIONVIEWITEM_SHOWDECORATIONSELECTED )
+{
+   hb_retl( hbqt_par_QStyleOptionViewItem( 1 )->showDecorationSelected );
+}
+
+/*
+ * Qt::TextElideMode textElideMode
+ */
+HB_FUNC( QT_QSTYLEOPTIONVIEWITEM_TEXTELIDEMODE )
+{
+   hb_retni( ( Qt::TextElideMode ) hbqt_par_QStyleOptionViewItem( 1 )->textElideMode );
+}
+
+
+/*----------------------------------------------------------------------*/
+#endif             /* #if QT_VERSION >= 0x040500 */
+/*----------------------------------------------------------------------*/
+
