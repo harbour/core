83e3729f683c8906da6ebdcc237d7bb96134fac7
Przemyslaw Czerpak <druzus@priv.onet.pl>
2009-08-18 19:34:59 +0000

    2009-08-18 21:34 UTC+0200 Przemyslaw Czerpak (druzus/at/priv.onet.pl)   * harbour/config/global.mk     * use $(error ...) instead of echo to inform about wrong GNU make       version - it works for any targets i.e. clean

--- harbour/config/global.mk
+++ harbour/config/global.mk
@@ -39,7 +39,7 @@ ok := $(filter $(need),$(firstword $(sort $(MAKE_VERSION) $(need))))
 
 ifeq ($(ok),)
 
-all: ; @$(ECHO) "! Error: GNU Make version $(MAKE_VERSION) found, $(need) or upper needed for Harbour"
+$(error ! Error: GNU Make version $(MAKE_VERSION) found, $(need) or upper needed for Harbour)
 
 else
 
