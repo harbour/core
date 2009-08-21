8de8bb0e755e0be0054ee6a1be569113fb48c814
Viktor Szakats <harbour@syenar.net>
2009-08-21 00:40:05 +0000

    2009-08-21 02:39 UTC+0200 Viktor Szakats (harbour.01 syenar.hu)   + config/darwin/icc.mk   * INSTALL     + Added make file for Intel compiler for OS X.       (not tested yet)

---
+++ harbour/config/darwin/icc.mk
@@ -0,0 +1,85 @@
+#
+# $Id$
+#
+
+ifeq ($(HB_BUILD_MODE),cpp)
+   HB_CMP := icpc
+else
+   HB_CMP := icc
+endif
+
+OBJ_EXT := .o
+LIB_PREF := lib
+LIB_EXT := .a
+
+CC := $(HB_CCACHE) $(HB_CMP)
+CC_IN := -c
+CC_OUT := -o
+
+CPPFLAGS := -I. -I$(HB_INC_COMPILE)
+CFLAGS :=
+LDFLAGS :=
+
+CFLAGS += -D_GNU_SOURCE
+
+ifneq ($(HB_BUILD_WARN),no)
+   #CFLAGS += -w2 -Wall
+endif
+
+ifneq ($(HB_BUILD_OPTIM),no)
+   CFLAGS += -O3
+endif
+
+# uncomment this if you want to force relocateable code for .so libs
+# it's necessary on some platforms but can reduce performance
+#CFLAGS += -fPIC
+
+ifeq ($(HB_BUILD_DEBUG),yes)
+   CFLAGS += -g
+endif
+
+LD := $(HB_CCACHE) $(HB_CMP)
+LD_OUT := -o
+
+LIBPATHS := -L$(LIB_DIR)
+LDLIBS := $(foreach lib,$(LIBS),-l$(lib))
+
+ifneq ($(filter hbrtl, $(LIBS)),)
+   # Add the specified GT driver library
+   ifeq ($(HB_CRS_LIB),)
+      HB_CRS_LIB := ncurses
+   endif
+   ifneq ($(filter gtcrs, $(LIBS)),)
+      LDLIBS += -l$(HB_CRS_LIB)
+   endif
+   ifneq ($(filter gtsln, $(LIBS)),)
+      LDLIBS += -lslang
+      # In BSD, slang still needs curses :(
+      ifeq ($(filter gtcrs, $(LIBS)),)
+         LDLIBS += -l$(HB_CRS_LIB)
+      endif
+   endif
+   ifneq ($(filter gtxwc, $(LIBS)),)
+      LDLIBS += -lX11
+     #LIBPATHS += -L/usr/X11R6/lib64
+      LIBPATHS += -L/usr/X11R6/lib
+   endif
+
+   ifneq ($(filter -DHB_PCRE_REGEX, $(HB_USER_CFLAGS)),)
+      LDLIBS += -lpcre
+   endif
+
+   ifneq ($(filter -DHB_EXT_ZLIB, $(HB_USER_CFLAGS)),)
+      LDLIBS += -lz
+   endif
+endif
+
+LDLIBS += -lm
+
+LDFLAGS += $(LIBPATHS)
+
+AR := xiar
+ARFLAGS :=
+AR_RULE = $(AR) $(ARFLAGS) $(HB_USER_AFLAGS) crs $(LIB_DIR)/$@ $(^F) || ( $(RM) $(LIB_DIR)/$@ && false )
+
+include $(TOP)$(ROOT)config/rules.mk
