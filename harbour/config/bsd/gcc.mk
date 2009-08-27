b1ad2df9c53192a7f6de482cbce9fc7e92fd45c8
Viktor Szakats <harbour@syenar.net>
2009-08-27 18:15:49 +0000

    2009-08-27 20:15 UTC+0200 Viktor Szakats (harbour.01 syenar.hu)   * config/bsd/gcc.mk   * config/hpux/gcc.mk     ! Restored cmdline lenght saver trick for BSD and HPUX after       giving this http://www.in-ulm.de/~mascheck/various/argmax/       a bit more careful read.

--- harbour/config/bsd/gcc.mk
+++ harbour/config/bsd/gcc.mk
@@ -49,6 +49,17 @@ DFLAGS := -shared -fPIC $(foreach dir,$(SYSLIBPATHS),-L$(dir))
 DY_OUT := -o$(subst x,x, )
 DLIBS := $(foreach lib,$(SYSLIBS),-l$(lib))
 
-DY_RULE = $(DY) $(DFLAGS) $(HB_USER_DFLAGS) $(DY_OUT)$(DYN_DIR)/$@ $^ $(DLIBS)
+# NOTE: The empty line directly before 'endef' HAVE TO exist!
+define dyn_object
+   @$(ECHO) $(ECHOQUOTE)INPUT($(subst \,/,$(file)))$(ECHOQUOTE) >> __dyn__.tmp
+
+endef
+define create_dynlib
+   $(if $(wildcard __dyn__.tmp),@$(RM) __dyn__.tmp,)
+   $(foreach file,$^,$(dyn_object))
+   $(DY) $(DFLAGS) $(HB_USER_DFLAGS) $(DY_OUT)$(DYN_DIR)/$@ __dyn__.tmp $(DLIBS)
+endef
+
+DY_RULE = $(create_dynlib)
 
 include $(TOP)$(ROOT)config/rules.mk
