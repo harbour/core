d1008fe5a034e9bebaba0dd48544d1cf8ac69ce5
Viktor Szakats <harbour@syenar.net>
2009-09-17 10:36:52 +0000

    2009-09-17 12:35 UTC+0200 Viktor Szakats (harbour.01 syenar.hu)   - bin/pack_src.sh   + mpkg_src.sh     * Moved and renamed to align with rest of pkg creation scripts.       (this should also go to package dir once in the future)
      * source/common/hbver.c
        * Converted decimal version numbers to hex in BCC guards.
        + Added BCC version detections for old (pre-5.x) versions.
          (Borrowed from xhb/Miguel Angel Marchuet)

--- harbour/source/common/hbver.c
+++ harbour/source/common/hbver.c
@@ -536,8 +536,8 @@ char * hb_verCompiler( void )
 
 #elif defined( __BORLANDC__ )
 
-   #if ( __BORLANDC__ >= 1424 ) /* Version 5.9 */
-      #if ( __BORLANDC__ >= 1568 ) /* Version 6.2 */
+   #if ( __BORLANDC__ >= 0x0590 ) /* Version 5.9 */
+      #if ( __BORLANDC__ >= 0x0620 ) /* Version 6.2 */
          pszName = "Borland/Embarcadero C++";
       #else
          pszName = "Borland/CodeGear C++";
@@ -545,11 +545,23 @@ char * hb_verCompiler( void )
    #else
       pszName = "Borland C++";
    #endif
-   #if ( __BORLANDC__ == 1040 ) /* Version 3.1 */
+   #if   ( __BORLANDC__ == 0x0400 ) /* Version 3.0 */
+      iVerMajor = 3;
+      iVerMinor = 0;
+      iVerPatch = 0;
+   #elif ( __BORLANDC__ == 0x0410 ) /* Version 3.1 */
       iVerMajor = 3;
       iVerMinor = 1;
       iVerPatch = 0;
-   #elif ( __BORLANDC__ >= 1280 ) /* Version 5.x */
+   #elif ( __BORLANDC__ == 0x0452 ) /* Version 4.0 */
+      iVerMajor = 4;
+      iVerMinor = 0;
+      iVerPatch = 0;
+   #elif ( __BORLANDC__ == 0x0460 ) /* Version 4.5 */
+      iVerMajor = 4;
+      iVerMinor = 5;
+      iVerPatch = 0;
+   #elif ( __BORLANDC__ >= 0x0500 ) /* Version 5.x */
       iVerMajor = __BORLANDC__ >> 8;
       iVerMinor = ( __BORLANDC__ & 0xFF ) >> 4;
       iVerPatch = __BORLANDC__ & 0xF;
