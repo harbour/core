4551f563c3e56f3e79041179864cf0d5d76930f3
Viktor Szakats <harbour@syenar.net>
2009-09-21 22:44:55 +0000

    2009-09-22 00:43 UTC+0200 Viktor Szakats (harbour.01 syenar.hu)   * config/win/global.mk     + Enabled UNICODE mode for all 64-bit Windows targets.       Here there is no concern about Win9x compatibility, so       it can be safely done, and I hope this move will make it       easier to catch UNICODE coding problems, as it will be now       part of some default builds.       The ultimate litmus test is on Windows is msvc64, where       C++, 64-bit and Unicode compatibility is tested at the       same time.

--- harbour/config/win/global.mk
+++ harbour/config/win/global.mk
@@ -9,6 +9,11 @@ DYN_EXT := .dll
 
 HB_GT_LIBS += gtwvt gtgui gtwin
 
+# enable UNICODE by default when building for any non-x86 targets (x86_64 or ia64)
+ifneq ($(HB_CPU),x86)
+   HB_CFLAGS += -DUNICODE
+endif
+
 # kernel32: needed by some compilers (pocc/watcom)
 # user32: *Clipboard*(), MessageBox(), CharToOemBuff(), OemToCharBuff(), GetKeyState(), GetKeyboardState(), SetKeyboardState()
 # ws2_32: hbsocket
