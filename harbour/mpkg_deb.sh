790a0671d8b9670f0919dfaa17d470876c6338fa
Przemyslaw Czerpak <druzus@priv.onet.pl>
2009-10-11 23:15:09 +0000

    2009-10-12 01:14 UTC+0200 Przemyslaw Czerpak (druzus/at/priv.onet.pl)   * harbour/debian/rules     ! fixed typo: HB_INC_INSTALL<->HB_LIB_INSTALL
      * harbour/debian/changelog
        * updated version number
    
      * harbour/mpkg_deb.sh
        * do not create unnecessary source code archive

--- harbour/mpkg_deb.sh
+++ harbour/mpkg_deb.sh
@@ -31,23 +31,7 @@ done
 
 if [ -z "${TOINST_LST}" ] || [ "$1" = "--force" ]
 then
-    . ./mpkg_src.sh
-    stat="$?"
-    if [ -z "${hb_filename}" ]
-    then
-        echo "The script ./mpkg_src.sh doesn't set archive name to \${hb_filename}"
-        exit 1
-    elif [ "${stat}" != 0 ]
-    then
-        echo "Error during packing the sources in ./mpkg_src.sh"
-        exit 1
-    elif [ -f ${hb_filename} ]
-    then
-        dpkg-buildpackage -b
-    else
-        echo "Cannot find archive file: ${hb_filename}"
-        exit 1
-    fi
+    dpkg-buildpackage -b
 else
     echo "If you want to build Harbour compiler"
     echo "you have to install the folowing packages:"
