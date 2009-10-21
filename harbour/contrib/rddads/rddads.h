5855e3e82728aaaf3995ef566764012e078e252c
Przemyslaw Czerpak <druzus@priv.onet.pl>
2009-10-21 10:12:14 +0000

    2009-10-21 12:11 UTC+0200 Przemyslaw Czerpak (druzus/at/priv.onet.pl)   * harbour/contrib/rddads/rddads.h     * minor fix in casting

--- harbour/contrib/rddads/rddads.h
+++ harbour/contrib/rddads/rddads.h
@@ -185,7 +185,7 @@ typedef ADSAREA * ADSAREAP;
 #define HB_ADS_PARCONNECTION( n )      ( ISNUM( n ) ? ( ADSHANDLE ) hb_parnint( n ) : hb_ads_hConnect )
 #define HB_ADS_RETCONNECTION( h )      hb_retnint( h )
 #define HB_ADS_GETCONNECTION( p )      ( ( hb_itemType( p ) & HB_IT_NUMERIC ) ? ( ADSHANDLE ) hb_itemGetNInt( p ) : hb_ads_hConnect )
-#define HB_ADS_PUTCONNECTION( p, h )   hb_itemPutNInt( ( p ), ( HB_PTRDIFF ) ( h ) )
+#define HB_ADS_PUTCONNECTION( p, h )   hb_itemPutNInt( ( p ), ( ADSHANDLE ) ( h ) )
 #define HB_ADS_DEFCONNECTION( v )      ( ( v ) ? ( ADSHANDLE ) ( v ) : hb_ads_hConnect )
 
 extern int        hb_ads_iFileType; /* current global setting */
