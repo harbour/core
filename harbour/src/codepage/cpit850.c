66468dc25805b2be10dfa439c7ebb443634935ba
Viktor Szakats <harbour@syenar.net>
2009-11-10 14:11:46 +0000

    2009-11-10 15:05 UTC+0100 Viktor Szakats (harbour.01 syenar.hu)   * codepage/Makefile   - codepage/cpesisoc.c   - codepage/cpeswinc.c     - Deletes ISO and Win pairs of ES850C.
      * codepage/Makefile
      + codepage/cpgr437.c
      + codepage/cpgr851.c
      + codepage/cpis850.c
      + codepage/cpis861.c
      + codepage/cpdk865.c
      + codepage/cpfi850.c
      + codepage/cpnl850.c
      + codepage/cpno865.c
      + codepage/cppt860.c
        + Added CPs generated via cpinfo.prg from ntx*.obj (C5.2e).
          For these language, I've only 'guessed' the CP assigment:
              cpdk865
              cpfi850
              cpnl850
              cpno865
              cppt860
          TOFIX: Any mistakes in above.
    
      * codepage/Makefile
      - codepage/cpsvclip.c
      + codepage/cpsv437c.c
        * Renamed to use naming scheme like all others.
        ; TOFIX: 437 is most certainly wrong CP assigment, and 
                 it should probably be 850 or 865.
    
      * codepage/cpfr850.c
      * codepage/cpes850c.c
      * codepage/cpde850.c
      * codepage/cpit850.c
        * Replaced with versions generated by cpinfo.prg.
    
      ; TODO: cpinfo.prg isn't generating short format of CP source files, 
              so all above may need to be converted to it for better readability.

--- harbour/src/codepage/cpit850.c
+++ harbour/src/codepage/cpit850.c
@@ -4,63 +4,39 @@
 
 /*
  * Harbour Project source code:
- * National Collation Support Module ( Italian MS-DOS cp 850 )
+ * National Collation Support Module ( IT850 )
  *
- * Copyright 2002 Alexander S.Kresin <alex@belacy.belgorod.su>
+ * Copyright 2009 Przemyslaw Czerpak <druzus / at / priv.onet.pl>
  * www - http://www.harbour-project.org
- * Spanish MS-DOS support by Antonio Linares <alinares@fivetechsoft.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this software; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA 02111-1307 USA (or visit the web site http://www.gnu.org/).
- *
- * As a special exception, the Harbour Project gives permission for
- * additional uses of the text contained in its release of Harbour.
- *
- * The exception is that, if you link the Harbour libraries with other
- * files to produce an executable, this does not by itself cause the
- * resulting executable to be covered by the GNU General Public License.
- * Your use of that executable is in no way restricted on account of
- * linking the Harbour library code into it.
- *
- * This exception does not however invalidate any other reasons why
- * the executable file might be covered by the GNU General Public License.
- *
- * This exception applies only to the code released by the Harbour
- * Project under the name Harbour.  If you copy code from other
- * Harbour Project or Free Software Foundation releases into a copy of
- * Harbour, as the General Public License permits, the exception does
- * not apply to the code that you add in this way.  To avoid misleading
- * anyone as to the status of such modified files, you must delete
- * this exception notice from them.
- *
- * If you write modifications of your own for Harbour, it is your choice
- * whether to permit this exception to apply to your modifications.
- * If you do not wish that, delete this exception notice.
- *
+ * This file is generated automatically by cpinfo.prg
  */
+#include "hbapicdp.h"
 
-/* Language name: Italian */
-/* ISO language code (2 chars): IT */
-/* Codepage: 850 */
+#define HB_CP_RAW       &s_codePage
 
-#define HB_CP_ID        IT850
-#define HB_CP_INFO      "Italian CP-850"
-#define HB_CP_UNITB     HB_UNITB_850
-#define HB_CP_ACSORT    HB_CDP_ACSORT_NONE
-#define HB_CP_UPPER     "A∑µ∂«éèBCDE‘êFGHIﬁ÷JKLMNO„‡PQRSTUÎÈVWXYZ"
-#define HB_CP_LOWER     "aÖ†É∆ÑÜbcdeäÇfghiç°jklmnoï¢pqrstuó£vwxyz"
+static const unsigned char s_flags[ 256 ] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,0,0,0,0,0,0,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,0,0,0,0,0,0,0,6,0,0,6,0,0,0,0,6,0,0,6,0,0,10,0,0,0,0,6,0,6,0,0,0,0,0,0,0,0,6,6,6,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+static const unsigned char s_upper[ 256 ] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,123,124,125,126,127,128,129,144,131,132,65,134,135,136,137,69,139,140,73,142,143,144,145,146,147,148,79,150,85,152,153,154,155,156,157,158,159,65,73,79,85,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255 };
+static const unsigned char s_lower[ 256 ] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,130,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255 };
+static const unsigned char s_sort [ 256 ] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,101,102,103,104,107,108,109,110,113,114,115,116,117,118,121,122,123,124,125,126,129,130,131,132,133,134,135,136,137,138,139,140,105,141,142,99,143,144,145,146,106,147,148,111,149,150,70,151,152,153,154,119,155,127,156,157,158,159,160,161,162,163,100,112,120,128,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255 };
+
+static HB_CODEPAGE s_codePage =
+{
+   "IT850",
+   "Italian CP-852 (ntxita.obj compatible)",
+   HB_UNITB_850,
+   s_flags,
+   s_upper,
+   s_lower,
+   s_sort,
+   NULL,
+   0,
+   0,
+   0,
+   NULL,
+   NULL,
+   NULL,
+};
 
 /* include CP registration code */
 #include "hbcdpreg.h"
