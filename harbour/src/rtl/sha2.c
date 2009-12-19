5907e1d204a34ef185b79ce6331d00bb286e6b7d
Viktor Szakats <harbour@syenar.net>
2009-12-19 12:34:45 +0000

    2009-12-19 13:34 UTC+0200 Viktor Szakats (harbour.01 syenar.hu)   * src/rtl/sha2.c     ! Added trick to make it compile with Pelles C 6.00.6.       Without this trick Pelles C compiler exits with internal error.
      * doc/whatsnew.txt
        + Added NOTE about Pelles C 6.

--- harbour/src/rtl/sha2.c
+++ harbour/src/rtl/sha2.c
@@ -35,7 +35,7 @@
  * SUCH DAMAGE.
  */
 
-#if 0
+#if ( defined( __POCC__ ) && __POCC__ >= 600 ) /* Trick to avoid Pelles C 6.00.6 compiler internal error. [vszakats] */
 #define UNROLL_LOOPS /* Enable loops unrolling */
 #endif
 
