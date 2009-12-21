b9fcb849933d36bacc9b0e06a3c585dfa8db17de
Pritpal Bedi <bedipritpal@hotmail.com>
2009-12-21 23:03:42 +0000

    2009-12-21 15:04 UTC-0800 Pritpal Bedi (pritpal@vouchcac.com)   * contrib/hbqt/qtgui/QMenuBar.cpp     ! Regenerated after rebuilding hbqtgen.prg.

--- harbour/contrib/hbqt/qtgui/QMenuBar.cpp
+++ harbour/contrib/hbqt/qtgui/QMenuBar.cpp
@@ -83,12 +83,12 @@ typedef struct
   QPointer< QMenuBar > pq;
 } QGC_POINTER_QMenuBar;
 
-QT_G_FUNC( release_QMenuBar )
+QT_G_FUNC( hbqt_gcRelease_QMenuBar )
 {
    QGC_POINTER_QMenuBar * p = ( QGC_POINTER_QMenuBar * ) Cargo;
 
-   HB_TRACE( HB_TR_DEBUG, ( "release_QMenuBar                     p=%p", p));
-   HB_TRACE( HB_TR_DEBUG, ( "release_QMenuBar                    ph=%p pq=%p", p->ph, (void *)(p->pq)));
+   HB_TRACE( HB_TR_DEBUG, ( "hbqt_gcRelease_QMenuBar                     p=%p", p));
+   HB_TRACE( HB_TR_DEBUG, ( "hbqt_gcRelease_QMenuBar                    ph=%p pq=%p", p->ph, (void *)(p->pq)));
 
    if( p && p->ph && p->pq )
    {
@@ -108,16 +108,16 @@ QT_G_FUNC( release_QMenuBar )
             break;
          }
          p->ph = NULL;
-         HB_TRACE( HB_TR_DEBUG, ( "release_QMenuBar                    Object deleted! %i B %i KB", ( int ) hb_xquery( 1001 ), hbqt_getmemused() ) );
+         HB_TRACE( HB_TR_DEBUG, ( "hbqt_gcRelease_QMenuBar                    Object deleted! %i B %i KB", ( int ) hb_xquery( 1001 ), hbqt_getmemused() ) );
       }
       else
       {
-         HB_TRACE( HB_TR_DEBUG, ( "NO release_QMenuBar                    Object Name Missing!" ) );
+         HB_TRACE( HB_TR_DEBUG, ( "NO hbqt_gcRelease_QMenuBar                    Object Name Missing!" ) );
       }
    }
    else
    {
-      HB_TRACE( HB_TR_DEBUG, ( "DEL release_QMenuBar                    Object Already deleted!" ) );
+      HB_TRACE( HB_TR_DEBUG, ( "DEL hbqt_gcRelease_QMenuBar                    Object Already deleted!" ) );
    }
 }
 
@@ -126,7 +126,7 @@ void * hbqt_gcAllocate_QMenuBar( void * pObj )
    QGC_POINTER_QMenuBar * p = ( QGC_POINTER_QMenuBar * ) hb_gcAllocate( sizeof( QGC_POINTER_QMenuBar ), hbqt_gcFuncs() );
 
    p->ph = pObj;
-   p->func = release_QMenuBar;
+   p->func = hbqt_gcRelease_QMenuBar;
    new( & p->pq ) QPointer< QMenuBar >( ( QMenuBar * ) pObj );
    HB_TRACE( HB_TR_DEBUG, ( "          new_QMenuBar                    %i B %i KB", ( int ) hb_xquery( 1001 ), hbqt_getmemused() ) );
    return( p );
