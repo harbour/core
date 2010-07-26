575e7302a8c3a62af444bed63ff5efa57b738061
Viktor Szakats <harbour@syenar.net>
2010-07-26 19:17:56 +0000

    2010-07-26 21:17 UTC+0200 Viktor Szakats (harbour.01 syenar.hu)   * bin/hb-func.sh     - Deleted one more now unused function.
      * config/postinst.hbs
        ! Fix to prev.

--- harbour/bin/hb-func.sh
+++ harbour/bin/hb-func.sh
@@ -11,32 +11,6 @@
 # See COPYING for licensing terms.
 # ---------------------------------------------------------------
 
-get_hbplatform()
-{
-   if [ "$OSTYPE" = "msdosdjgpp" ]; then
-      id="djgpp"
-   else
-      # please add your distro suffix if it not belong to the one recognized below
-      # and remember that order checking can be important
-      [ "${id}" = "" ] && id=`rel=$(rpm -q --queryformat='.%{VERSION}' mandriva-release-One 2>/dev/null) && echo "mdk$rel"|tr -d "."`
-      [ "${id}" = "" ] && id=`rel=$(rpm -q --queryformat='.%{VERSION}' mandriva-release 2>/dev/null) && echo "mdk$rel"|tr -d "."`
-      [ "${id}" = "" ] && id=`rel=$(rpm -q --queryformat='.%{VERSION}' mandrake-release 2>/dev/null) && echo "mdk$rel"|tr -d "."`
-      [ "${id}" = "" ] && id=`rel=$(rpm -q --queryformat='.%{VERSION}' redhat-release 2>/dev/null) && echo "rh$rel"|tr -d "."`
-      [ "${id}" = "" ] && id=`rel=$(rpm -q --queryformat='.%{VERSION}' fedora-release 2>/dev/null) && echo "fc$rel"|tr -d "."`
-      [ "${id}" = "" ] && id=`rel=$(rpm -q --queryformat='.%{VERSION}' suse-release 2>/dev/null) && echo "sus$rel"|tr -d "."`
-      [ "${id}" = "" ] && id=`rel=$(rpm -q --queryformat='.%{VERSION}' openSUSE-release 2>/dev/null) && echo "sus$rel"|tr -d "."`
-      [ "${id}" = "" ] && id=`rel=$(rpm -q --queryformat='.%{VERSION}' conectiva-release 2>/dev/null) && echo "cl$rel"|tr -d "."`
-      [ "${id}" = "" ] && id=`rel=$(rpm -q --queryformat='.%{VERSION}' aurox-release 2>/dev/null) && echo "cl$rel"|tr -d "."`
-      [ "${id}" = "" ] && id=`[ -f /etc/pld-release ] && cat /etc/pld-release|sed -e '/1/ !d' -e 's/[^0-9]//g' -e 's/^/pld/'`
-      [ "${id}" = "" ] && id=`uname -s | tr '[ A-Z]' '[_a-z]'`
-      case "${id}" in
-         mingw*) id="mingw" ;;
-         *) ;;
-      esac
-   fi
-   echo "${id}"
-}
-
 get_hbver()
 {
    hb_rootdir="${1-.}"
