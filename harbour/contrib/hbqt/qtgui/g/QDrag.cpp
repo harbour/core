5c1b3e7a26fb34896af3bd5f3d523298120cdf42
Pritpal Bedi <bedipritpal@hotmail.com>
2010-08-19 09:25:53 +0000

    2010-08-19 02:23 UTC-0800 Pritpal Bedi (bedipritpal@hotmail.com)   * contrib/hbqt/qtgui/g/QDrag.cpp   * contrib/hbqt/qtgui/qth/QDrag.qth     % Shifted: ownership of QMimeData to QDrag object.

--- harbour/contrib/hbqt/qtgui/g/QDrag.cpp
+++ harbour/contrib/hbqt/qtgui/g/QDrag.cpp
@@ -66,6 +66,15 @@
 #if QT_VERSION >= 0x040500
 /*----------------------------------------------------------------------*/
 
+/*
+ *  Constructed[ 11/12 [ 91.67% ] ]
+ *
+ *  *** Unconvered Prototypes ***
+ *  -----------------------------
+ *
+ *  }
+ */
+
 #include <QtCore/QPointer>
 
 #include <QtGui/QDrag>
@@ -255,12 +264,14 @@ HB_FUNC( QT_QDRAG_SETHOTSPOT )
  */
 HB_FUNC( QT_QDRAG_SETMIMEDATA )
 {
-   QDrag * p = hbqt_par_QDrag( 1 );
-   if( p )
-      ( p )->setMimeData( hbqt_par_QMimeData( 2 ) );
-   else
+   HBQT_GC_T * p = ( HBQT_GC_T * ) hb_parptrGC( hbqt_gcFuncs(), 1 );
+   HBQT_GC_T * q = ( HBQT_GC_T * ) hb_parptrGC( hbqt_gcFuncs(), 2 );
+   HB_TRACE( HB_TR_DEBUG, ( "Entering function QT_QDRAG_SETMIMEDATA()" ) );
+   if( p && p->ph && q && q->ph )
    {
-      HB_TRACE( HB_TR_DEBUG, ( "............................... F=QT_QDRAG_SETMIMEDATA FP=( p )->setMimeData( hbqt_par_QMimeData( 2 ) ); p is NULL" ) );
+      HB_TRACE( HB_TR_DEBUG, ( "QT_QDRAG_SETMIMEDATA() Qt object: %p is attached to: %p", p->ph, q->ph ) );
+      q->bNew = HB_FALSE;
+      hbqt_par_QDrag( 1 )->setMimeData( hbqt_par_QMimeData( 2 ) );
    }
 }
 
