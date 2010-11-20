57d7a2ab2d6501bc8468fbaaef2ce87d2cce3df2
Viktor Szakats <harbour@syenar.net>
2010-11-20 23:22:27 +0000

    2010-11-21 00:20 UTC+0100 Viktor Szakats (harbour.01 syenar.hu)   * include/hbcpage.hbx   * src/codepage/Makefile   + src/codepage/cpsk852c.c     + Added Clipper compatible Slovak collation.
      * src/codepage/cpsk852.c
      * src/codepage/cpskkam.c
      * src/codepage/cpskiso.c
      * src/codepage/cpskwin.c
        ! Fixed Slovak collation.
        ; INCOMPATIBLE: All users using any of the above codepages will
                        have to reindex.
    
      ; Thanks to Julius Bartal for sorting it out.

--- harbour/src/codepage/cpsk852.c
+++ harbour/src/codepage/cpsk852.c
@@ -6,56 +6,18 @@
  * Harbour Project source code:
  * National Collation Support Module ( SK852 )
  *
- * Copyright 2007 Viktor Szakats (harbour.01 syenar.hu)
+ * Copyright 2009 Przemyslaw Czerpak <druzus / at / priv.onet.pl>
  * www - http://harbour-project.org
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this software; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA 02111-1307 USA (or visit the web site http://www.gnu.org/).
- *
- * As a special exception, the Harbour Project gives permission for
- * additional uses of the text contained in its release of Harbour.
- *
- * The exception is that, if you link the Harbour libraries with other
- * files to produce an executable, this does not by itself cause the
- * resulting executable to be covered by the GNU General Public License.
- * Your use of that executable is in no way restricted on account of
- * linking the Harbour library code into it.
- *
- * This exception does not however invalidate any other reasons why
- * the executable file might be covered by the GNU General Public License.
- *
- * This exception applies only to the code released by the Harbour
- * Project under the name Harbour.  If you copy code from other
- * Harbour Project or Free Software Foundation releases into a copy of
- * Harbour, as the General Public License permits, the exception does
- * not apply to the code that you add in this way.  To avoid misleading
- * anyone as to the status of such modified files, you must delete
- * this exception notice from them.
- *
- * If you write modifications of your own for Harbour, it is your choice
- * whether to permit this exception to apply to your modifications.
- * If you do not wish that, delete this exception notice.
- *
+ * This file is generated automatically by cpinfo.prg
  */
 
 #define HB_CP_ID        SK852
 #define HB_CP_INFO      "Slovak CP-852"
 #define HB_CP_UNITB     HB_UNITB_852
-#define HB_CP_ACSORT    HB_CDP_ACSORT_INTERLEAVED
-#define HB_CP_UPPER     "A~µ~éBC¨D~“.DZ..D¶.E~êFGH.CH.I~÷JKL~ë~ïMN~’O~‡~‚PQR~ËSÊT~õU~ÈVWXY~ÌZ¶"
-#define HB_CP_LOWER     "a~†~Ñbcüd~‘.dz..dß.e~Çfgh.ch.i~°jkl~í~ñmn~Âo~¢~ìpqr~ÍsÁt~úu~£vwxy~Ïzß"
+#define HB_CP_ACSORT    HB_CDP_ACSORT_NONE
+#define HB_CP_UPPER     "AµéBC¨D“Eê∑”FGHI÷JKLëïMN’O‡‚ôäPQRË¸SÊTõUÈﬁöÎVWXYÌZ¶"
+#define HB_CP_LOWER     "a†Ñbcüd‘eÇÿâfghi°jklíñmnÂo¢ìîãpqrÍ˝sÁtúu£ÖÅ˚vwxyÏzß"
 
 /* include CP registration code */
 #include "hbcdpreg.h"
---
+++ harbour/src/codepage/cpsk852c.c
@@ -0,0 +1,23 @@
+/*
+ * $Id$
+ */
+
+/*
+ * Harbour Project source code:
+ * National Collation Support Module ( SK852 )
+ *
+ * Copyright 2009 Przemyslaw Czerpak <druzus / at / priv.onet.pl>
+ * www - http://harbour-project.org
+ *
+ * This file is generated automatically by cpinfo.prg
+ */
+
+#define HB_CP_ID        SK852C
+#define HB_CP_INFO      "Slovak CP-852 (ntxsl852.obj compatible)"
+#define HB_CP_UNITB     HB_UNITB_852
+#define HB_CP_ACSORT    HB_CDP_ACSORT_NONE
+#define HB_CP_UPPER     "AµBC¬DÒE·ÓFGHIÖJKL‘•MNÕOàâ™PQRèüSæT›UéŞšVWXYíZ¦"
+#define HB_CP_LOWER     "a „bcŸdÔe‚Ø‰fghi¡jkl’–mnåo¢“”pqrêısçtœu£…vwxyìz§"
+
+/* include CP registration code */
+#include "hbcdpreg.h"
