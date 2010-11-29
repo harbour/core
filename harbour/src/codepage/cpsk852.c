85d282c49c2a8a9c3e0db1918d345d1291bc4864
Przemyslaw Czerpak <druzus@priv.onet.pl>
2010-11-29 14:47:50 +0000

    2010-11-29 15:47 UTC+0100 Przemyslaw Czerpak (druzus/at/priv.onet.pl)   * harbour/src/codepage/cpsk852.c   * harbour/src/codepage/cpskiso.c     ! fixed typo in CP declaration:       HB_CDP_ACSORT_INTERLEAVED -> HB_CDP_ACSORT_NONE

--- harbour/src/codepage/cpsk852.c
+++ harbour/src/codepage/cpsk852.c
@@ -15,7 +15,7 @@
 #define HB_CP_ID        SK852
 #define HB_CP_INFO      "Slovak CP-852"
 #define HB_CP_UNITB     HB_UNITB_852
-#define HB_CP_ACSORT    HB_CDP_ACSORT_INTERLEAVED
+#define HB_CP_ACSORT    HB_CDP_ACSORT_NONE
 #define HB_CP_UPPER     "AµBC¬DÒ.DZ..D¦.E·ÓFGH.CH.IÖJKL‘•MNÕOàâ™ŠPQRèüSæT›UéŞšëVWXYíZ¦"
 #define HB_CP_LOWER     "a „bcŸdÔ.dz..d§.e‚Ø‰fgh.ch.i¡jkl’–mnåo¢“”‹pqrêısçtœu£…ûvwxyìz§"
 
--- harbour/src/codepage/cpskiso.c
+++ harbour/src/codepage/cpskiso.c
@@ -15,7 +15,7 @@
 #define HB_CP_ID        SKISO
 #define HB_CP_INFO      "Slovak ISO-8859-2"
 #define HB_CP_UNITB     HB_UNITB_8859_2
-#define HB_CP_ACSORT    HB_CDP_ACSORT_INTERLEAVED
+#define HB_CP_ACSORT    HB_CDP_ACSORT_NONE
 #define HB_CP_UPPER     "AÁÄBCÈDÏ.DZ..D®.EÉÌËFGH.CH.IÍJKLÅ¥MNÒOÓÔÖÕPQRÀØS©T«UÚÙÜÛVWXYİZ®"
 #define HB_CP_LOWER     "aáäbcèdï.dz..d¾.eéìëfgh.ch.iíjklåµmnòoóôöõpqràøs¹t»uúùüûvwxyız¾"
 
