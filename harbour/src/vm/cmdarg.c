ea845b95f8c706cc22f2fcb14bc9703d4946a605
Przemyslaw Czerpak <druzus@priv.onet.pl>
2011-01-07 09:19:06 +0000

    2011-01-07 10:18 UTC+0100 Przemyslaw Czerpak (druzus/at/priv.onet.pl)   * harbour/src/vm/cmdarg.c     + added support for //F:<nFiles> application runtime switch in DOS       and OS2 Open Watcom builds, code by Lorenzo Fiorini and Maurilio       Longo borrowed from xHarbour.

--- harbour/src/vm/cmdarg.c
+++ harbour/src/vm/cmdarg.c
@@ -516,6 +516,7 @@ HB_U32 hb_cmdargProcessVM( int * pCancelKey, int * pCancelKeyEx )
 {
    char * cFlags;
    HB_U32 ulFlags = HB_VMFLAG_HARBOUR;
+   int iHandles;
 
    if( hb_cmdargCheck( "INFO" ) )
    {
@@ -547,6 +548,18 @@ HB_U32 hb_cmdargProcessVM( int * pCancelKey, int * pCancelKeyEx )
 
    if( hb_cmdargCheck( "BUILD" ) )
       hb_verBuildInfo();
+
+   iHandles = hb_cmdargNum( "F" );
+   if( iHandles > 20 )
+   {
+      #if defined( __WATCOMC__ )
+         #if defined(HB_OS_OS2)
+            DosSetMaxFH( iHandles );
+         #elif defined(HB_OS_DOS)
+            _grow_handles( iHandles );
+         #endif
+      #endif
+   }
 
    if( ( cFlags = hb_cmdargString( "FLAGS" ) ) != NULL )
    {
