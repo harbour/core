788ed22d9368df80caba781945d87ea11541dfc2
Viktor Szakats <harbour@syenar.net>
2011-02-22 14:55:12 +0000

    2011-02-22 15:53 UTC+0100 Viktor Szakats (harbour.01 syenar.hu)   * src/dynlib/2nd/Makefile   * src/dynlib/Makefile   * config/none.mk   * config/dyn.mk     % Dropped DYNNAME. Now using DYNNAME2 only.     % Dropped HB_DYNLIB_NAME*. Now using HB_DYNLIB_BASE* only.     ; TODO: Rename DYNNAME2 to DYNNAME, rename HB_DYNLIB_BASE* to HB_DYNLIB_NAME*.

--- harbour/config/none.mk
+++ harbour/config/none.mk
@@ -10,8 +10,8 @@ ifneq ($(HB_COMPILER),)
 ifneq ($(LIBNAME),)
    DIR_RULE := @$(ECHO) $(ECHOQUOTE)! '$(LIBNAME)' library skipped $(if $(HB_SKIP_REASON),($(HB_SKIP_REASON)),)$(ECHOQUOTE)
 else
-ifneq ($(DYNNAME),)
-   DIR_RULE := @$(ECHO) $(ECHOQUOTE)! '$(DYNNAME)' dynamic library skipped$(ECHOQUOTE)
+ifneq ($(DYNNAME2),)
+   DIR_RULE := @$(ECHO) $(ECHOQUOTE)! '$(DYNNAME2)' dynamic library skipped$(ECHOQUOTE)
 else
    DIR_RULE :=
 endif
