c027764c852b1350513303fb8850512d4c7b3308
Viktor Szakats <harbour@syenar.net>
2012-09-04 19:58:57 +0000

    2012-09-04 21:57 UTC+0200 Viktor Szakats (harbour syenar.net)   * contrib/rddads/rddads.h     + autodetection for ADS client v11.00

--- harbour/contrib/rddads/rddads.h
+++ harbour/contrib/rddads/rddads.h
@@ -72,8 +72,10 @@
 #include "ace.h"
 
 /* Autodetect ACE version. */
-#if   defined( ADS_GET_UTF8 )
-   #define _ADS_LIB_VERSION 1010 /* or upper */
+#if   defined( ADS_GET_FORMAT_WEB )
+   #define _ADS_LIB_VERSION 1100 /* or upper */
+#elif defined( ADS_GET_UTF8 )
+   #define _ADS_LIB_VERSION 1010
 #elif defined( ADS_DEFAULT_SQL_TIMEOUT )
    #define _ADS_LIB_VERSION 1000
 #elif defined( DANISH_ADS_CS_AS_1252 )
