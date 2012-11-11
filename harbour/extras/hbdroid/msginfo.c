de902d4a407b976850512dfb93952fddf0db1494
Viktor Szakats <harbour@syenar.net>
2012-11-11 13:01:36 +0000

    2012-11-11 13:59 UTC+0100 Viktor Szakats (harbour syenar.net)   - contrib/hbdroid   + extras/hbdroid   + extras/hbdroid/hbdroid.hbx   * extras/hbdroid/hbdroid.hbc     * moved hbdroid to 'extras'     + added .hbx file     * adapted build files to 'extras' environment.     ; Now can be built as dynamic lib

---
+++ harbour/extras/hbdroid/msginfo.c
@@ -0,0 +1,18 @@
+/*
+ * $Id$
+ */
+
+#include "hbapi.h"
+#include "hbdroid.h"
+
+HB_FUNC( JNI_MSGINFO )
+{
+   JNIEnv * jni_env = __hbdroid_jni_env();
+   jobject  jni_obj = __hbdroid_jni_obj();
+
+   jclass    cls = ( *jni_env )->GetObjectClass( jni_env, jni_obj );
+   jmethodID mid = ( *jni_env )->GetMethodID( jni_env, cls, "MsgInfo", "(Ljava/lang/String;)V" );
+
+   if( mid )
+      ( *jni_env )->CallVoidMethod( jni_env, jni_obj, mid, ( *jni_env )->NewStringUTF( jni_env, hb_parcx( 1 ) ) );
+}
