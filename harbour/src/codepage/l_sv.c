413443cb464c46c2fbc64fdc4485928e40586047
Viktor Szakats <harbour@syenar.net>
2013-01-30 14:28:23 +0000

    2013-01-30 15:27 UTC+0100 Viktor Szakats (harbour syenar.net)   * src/codepage/l_sv.c     ! fixed again after last patch where wrong value creeped back

--- harbour/src/codepage/l_sv.c
+++ harbour/src/codepage/l_sv.c
@@ -4,5 +4,5 @@
 
 /* Klas Engwall <klas dot engwall at engwall dot com> */
 
-#define HB_CP_UPPER     "A~Á~À~Â~ÃBC~ÇD~ÐE~É~È~Ê~ËFGHI~Í~Ì~Î~ÏJKLMN~ÑO~Ó~Ò~Ô~ÕPQRSTU~Ú~Ù~ÛV~WXY~Ý~~ÜZÅÄ~ÆÖ~Ø~"
+#define HB_CP_UPPER     "A~Á~À~Â~ÃBC~ÇD~ÐE~É~È~Ê~ËFGHI~Í~Ì~Î~ÏJKLMN~ÑO~Ó~Ò~Ô~ÕPQRSTU~Ú~Ù~ÛV~WXY~Ý~Ÿ~ÜZÅÄ~ÆÖ~Ø~Œ"
 #define HB_CP_LOWER     "a~á~à~â~ãbc~çd~ðe~é~è~ê~ëfghi~í~ì~î~ïjklmn~ño~ó~ò~ô~õpqrstu~ú~ù~ûv~wxy~ý~ÿ~üzåä~æö~ø~œ"
