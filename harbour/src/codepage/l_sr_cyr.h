4df8b28fc6e2b1da52de8e912668b68ae3d7448d
Viktor Szakats <harbour@syenar.net>
2013-02-19 01:07:21 +0000

    2013-02-19 02:06 UTC+0100 Viktor Szakats (harbour syenar.net)   * ChangeLog.txt   * src/codepage/l_sr_cyr.h     * typos/cleanups in comment

--- harbour/src/codepage/l_sr_cyr.h
+++ harbour/src/codepage/l_sr_cyr.h
@@ -2,7 +2,7 @@
  * $Id$
  */
 
-/* SERBIAN collating sequence done by Srdjan Dragojlovic <digikv@yahoo.com> */
+/* Srdjan Dragojlovic <digikv@yahoo.com> */
 
 #define HB_CP_UPPER     "АБВГДЂЕЖЗИЈКЛЉМНЊОПРСТЋУФХЦЧЏШ"
 #define HB_CP_LOWER     "абвгдђежзијклљмнњопрстћуфхцчџш"
