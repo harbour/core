b6ad77f6e3a1a44a4c22c8f09b380b4b1557f524
Tamas TEVESZ <ice@extreme.hu>
2013-05-28 02:04:55 +0200

    2013-05-28 01:59 UTC+0200 Tamas TEVESZ (ice extreme.hu)   * src/vm/dynlibhb.c     * Report dlopen() errors at HB_TR_LEVEL_DEBUG   * ChangeLog.txt     ! Put in a changelog entry I have completely missed previously.

--- src/vm/dynlibhb.c
+++ src/vm/dynlibhb.c
@@ -138,6 +138,11 @@ PHB_ITEM hb_libLoad( PHB_ITEM pLibName, PHB_ITEM pArgs )
          }
 #elif defined( HB_HAS_DLFCN )
          hDynLib = ( void * ) dlopen( hb_itemGetCPtr( pLibName ), RTLD_LAZY | RTLD_GLOBAL );
+
+         if( ! hDynLib )
+         {
+            HB_TRACE( HB_TR_DEBUG, ( "hb_libLoad(): dlopen(): %s", dlerror() ) );
+         }
 #else
          {
             int iTODO;
