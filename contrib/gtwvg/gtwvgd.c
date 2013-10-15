3e87fe05d2faaf32923b2546ba403aaa221fa5fb
Mindaugas Kavaliauskas <dbtopas@dbtopas.lt>
2013-10-15 18:58:26 +0300

    2013-10-15 18:58 UTC+0300 Mindaugas Kavaliauskas (dbtopas/at/dbtopas.lt)   * contrib/gtwvg/gtwvgd.c     ! fixed broken compile after 2013-10-11 21:36 UTC-0800 Pritpal Bedi

--- contrib/gtwvg/gtwvgd.c
+++ contrib/gtwvg/gtwvgd.c
@@ -3128,10 +3128,10 @@ static HB_BOOL hb_gt_wvt_Info( PHB_GT pGT, int iType, PHB_GT_INFO pInfo )
          iVal = hb_itemGetNI( pInfo->pNewVal );
          if( iVal > 0 && ! pWVT->bMaximized && ! pWVT->bFullScreen && pWVT->hWnd )  /* Don't allow if Maximized or FullScreen */
          {
+            RECT ci;
             HB_GTSELF_SETMODE( pGT, ( iVal / pWVT->PTEXTSIZE.y ), pWVT->COLS );
 
             /* Now conforms to pWVT->ResizeMode setting, resize by FONT or ROWS as applicable [HVB] */
-            RECT ci;
             GetClientRect( pWVT->hWnd, &ci );
             if( ci.bottom != iVal )
             {
@@ -3149,10 +3149,10 @@ static HB_BOOL hb_gt_wvt_Info( PHB_GT pGT, int iType, PHB_GT_INFO pInfo )
          iVal = hb_itemGetNI( pInfo->pNewVal );
          if( iVal > 0 && ! pWVT->bMaximized && ! pWVT->bFullScreen && pWVT->hWnd )  /* Don't allow if Maximized or FullScreen */
          {
+            RECT ci;
             HB_GTSELF_SETMODE( pGT, ( iVal / pWVT->PTEXTSIZE.y ), pWVT->COLS );
 
             /* Now conforms to pWVT->ResizeMode setting, resize by FONT or ROWS as applicable [HVB] */
-            RECT ci;
             GetClientRect( pWVT->hWnd, &ci );
             if( ci.right != iVal )
             {
