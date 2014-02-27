f683dcd6543bd6e4c5e2687a1727820bc4097322
Viktor Szakáts <vszakats@users.noreply.github.com>
2014-02-27 16:37:47 +0100

    2014-02-27 16:36 UTC+0100 Viktor Szakáts (vszakats users.noreply.github.com)   * contrib/hbnetio/netiosrv.c     * HB_VFDIRECTORY() to return timestampt when using hbnetio       (there is only one mode when using VF so the protocol doesn't       have to be changed)

--- contrib/hbnetio/netiosrv.c
+++ contrib/hbnetio/netiosrv.c
@@ -895,7 +895,7 @@ HB_FUNC( NETIO_SERVER )
                         HB_SIZE itmSize = 0;
                         char * itmData = NULL;
                         const char * pszAttr = size2 ? ( const char * ) msg : NULL;
-                        PHB_ITEM pResult = hb_fileDirectory( pszDirSpec, pszAttr, HB_FALSE /* TODO */ );
+                        PHB_ITEM pResult = hb_fileDirectory( pszDirSpec, pszAttr, HB_TRUE );
 
                         errFsCode = hb_fsError();
 
