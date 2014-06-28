harbour/core

commit 122195bd747a7c4832bf5b7c7b4da076850dd014

2014-01-21 18:49 UTC+0100 Viktor Szakáts (vszakats users.noreply.github.com)   * contrib/hbmisc/ntp.prg   * contrib/hbnf/sleep.prg   * contrib/hbnf/tempfile.prg   * contrib/hbtip/*.prg   * contrib/hbunix/tests/daemon.prg   * contrib/hbwin/tests/ole.prg   * utils/hbmk2/hbmk2.prg     % minor optimizations     * cleanups
  * contrib/hbhttpd/tests/eshop.prg
  * contrib/hbmisc/tests/numtxtru.prg
    * output cleanup

  * contrib/xhb/tests/xml1.prg
  * tests/dynobj.prg
  * tests/extend1.prg
  * tests/memvar.prg
    * WAIT cmd cleanup

  * ChangeLog.txt
    ! typos

  * contrib/gtwvg/tests/_xbp.prg
  * contrib/hbnf/wda.prg
    * formatting

contrib/hbtip/ftpcli.prg
@@ -384,17 +384,12 @@ METHOD List( cSpec ) CLASS TIPClientFTP
    ELSE
       cSpec := " " + cSpec
    ENDIF
-   IF ::bUsePasv
-      IF ! ::Pasv()
-         // ::bUsePasv := .F.
-         RETURN NIL
-      ENDIF
+   IF ::bUsePasv .AND. ! ::Pasv()
+      // ::bUsePasv := .F.
+      RETURN NIL
    ENDIF
-
-   IF ! ::bUsePasv
-      IF ! ::Port()
-         RETURN NIL
-      ENDIF
+   IF ! ::bUsePasv .AND. ! ::Port()
+      RETURN NIL
    ENDIF
 
    ::inetSendAll( ::SocketCon, "LIST" + cSpec + ::cCRLF )
@@ -405,22 +400,17 @@ METHOD List( cSpec ) CLASS TIPClientFTP
 
 METHOD UserCommand( cCommand, lPasv, lReadPort, lGetReply ) CLASS TIPClientFTP
 
-   hb_default( @cCommand, "" )
-   hb_default( @lPasv, .T. )
-   hb_default( @lReadPort, .T. )
-   hb_default( @lGetReply, .F. )
-
-   IF ::bUsePasv .AND. lPasv .AND. ! ::Pasv()
+   IF ::bUsePasv .AND. hb_defaultValue( lPasv, .T. ) .AND. ! ::Pasv()
       RETURN .F.
    ENDIF
 
-   ::inetSendAll( ::SocketCon, cCommand )
+   ::inetSendAll( ::SocketCon, hb_defaultValue( cCommand, "" ) )
 
-   IF lReadPort
+   IF hb_defaultValue( lReadPort, .T. )
       lReadPort := ::ReadAuxPort()
    ENDIF
 
-   IF lGetReply
+   IF hb_defaultValue( lGetReply, .F. )
       lGetReply := ::GetReply()
    ENDIF
 
@@ -460,11 +450,9 @@ METHOD ReadAuxPort( cLocalFile ) CLASS TIPClientFTP
 
 METHOD Stor( cFile ) CLASS TIPClientFTP
 
-   IF ::bUsePasv
-      IF ! ::Pasv()
-         // ::bUsePasv := .F.
-         RETURN .F.
-      ENDIF
+   IF ::bUsePasv .AND. ! ::Pasv()
+      // ::bUsePasv := .F.
+      RETURN .F.
    ENDIF
 
    ::inetSendAll( ::SocketCon, "STOR " + cFile + ::cCRLF )
@@ -513,11 +501,9 @@ METHOD Read( nLen ) CLASS TIPClientFTP
 
    IF ! ::bInitialized
 
-      IF ! Empty( ::oUrl:cPath )
-         IF ! ::CWD( ::oUrl:cPath )
-            ::bEof := .T.  // no data for this transaction
-            RETURN NIL
-         ENDIF
+      IF ! Empty( ::oUrl:cPath ) .AND. ! ::CWD( ::oUrl:cPath )
+         ::bEof := .T.  // no data for this transaction
+         RETURN NIL
       ENDIF
 
       IF Empty( ::oUrl:cFile )
@@ -533,9 +519,7 @@ METHOD Read( nLen ) CLASS TIPClientFTP
       ::bInitialized := .T.
    ENDIF
 
-   cRet := ::super:Read( nLen )
-
-   IF cRet == NIL
+   IF ( cRet := ::super:Read( nLen ) ) == NIL
       ::Commit()
       ::bEof := .T.
    ENDIF
@@ -551,10 +535,8 @@ METHOD Write( cData, nLen ) CLASS TIPClientFTP
          RETURN -1
       ENDIF
 
-      IF ! Empty( ::oUrl:cPath )
-         IF ! ::CWD( ::oUrl:cPath )
-            RETURN -1
-         ENDIF
+      IF ! Empty( ::oUrl:cPath ) .AND. ! ::CWD( ::oUrl:cPath )
+         RETURN -1
       ENDIF
 
       IF ! ::Stor( ::oUrl:cFile )
@@ -569,11 +551,9 @@ METHOD Write( cData, nLen ) CLASS TIPClientFTP
 
 METHOD Retr( cFile ) CLASS TIPClientFTP
 
-   IF ::bUsePasv
-      IF ! ::Pasv()
-         // ::bUsePasv := .F.
-         RETURN .F.
-      ENDIF
+   IF ::bUsePasv .AND. ! ::Pasv()
+      // ::bUsePasv := .F.
+      RETURN .F.
    ENDIF
 
    ::inetSendAll( ::SocketCon, "RETR " + cFile + ::cCRLF )
@@ -589,33 +569,29 @@ METHOD MGET( cSpec, cLocalPath ) CLASS TIPClientFTP
 
    LOCAL cStr, cFile
 
-   hb_default( @cSpec, "" )
+   IF ::bUsePasv .AND. ! ::Pasv()
+      // ::bUsePasv := .F.
+      RETURN .F.
+   ENDIF
+
    hb_default( @cLocalPath, "" )
 
-   IF ::bUsePasv
-      IF ! ::Pasv()
-         // ::bUsePasv := .F.
-         RETURN .F.
-      ENDIF
-   ENDIF
+   ::inetSendAll( ::SocketCon, "NLST " + hb_defaultValue( cSpec, "" ) + ::cCRLF )
 
-   ::inetSendAll( ::SocketCon, "NLST " + cSpec + ::cCRLF )
    cStr := ::ReadAuxPort()
 
-   IF ! Empty( cStr )
-      FOR EACH cFile IN hb_ATokens( StrTran( cStr, Chr( 13 ) ), Chr( 10 ) )
-         IF ! Empty( cFile )
-            ::downloadfile( cLocalPath + RTrim( cFile ), RTrim( cFile ) )
-         ENDIF
-      NEXT
-   ENDIF
+   FOR EACH cFile IN hb_ATokens( StrTran( cStr, Chr( 13 ) ), Chr( 10 ) )
+      IF ! Empty( cFile )
+         ::downloadfile( cLocalPath + RTrim( cFile ), RTrim( cFile ) )
+      ENDIF
+   NEXT
 
    RETURN cStr
 
 METHOD MPUT( cFileSpec, cAttr ) CLASS TIPClientFTP
 
    LOCAL cPath, cFile, cExt, aFile
-   LOCAL cStr := ""
+   LOCAL cStr
 
    IF ! HB_ISSTRING( cFileSpec )
       RETURN 0
@@ -623,6 +599,7 @@ METHOD MPUT( cFileSpec, cAttr ) CLASS TIPClientFTP
 
    hb_FNameSplit( cFileSpec, @cPath, @cFile, @cExt  )
 
+   cStr := ""
    FOR EACH aFile IN Directory( cPath + cFile + cExt, cAttr )
       IF ::uploadFile( cPath + aFile[ F_NAME ], aFile[ F_NAME ] )
          cStr += e"\r\n" + aFile[ F_NAME ]
@@ -633,7 +610,6 @@ METHOD MPUT( cFileSpec, cAttr ) CLASS TIPClientFTP
                 Why emulate a platform specific and ill-defined format? */
    RETURN SubStr( cStr, Len( e"\r\n" ) + 1 )
 
-
 METHOD UpLoadFile( cLocalFile, cRemoteFile ) CLASS TIPClientFTP
 
    LOCAL cPath
@@ -642,10 +618,8 @@ METHOD UpLoadFile( cLocalFile, cRemoteFile ) CLASS TIPClientFTP
 
    hb_FNameSplit( cLocalFile, @cPath, @cFile, @cExt  )
 
-   hb_default( @cRemoteFile, cFile + cExt )
-
    ::bEof := .F.
-   ::oUrl:cFile := cRemoteFile
+   ::oUrl:cFile := hb_defaultValue( cRemoteFile, cFile + cExt )
 
    IF ! ::bInitialized
 
@@ -653,12 +627,8 @@ METHOD UpLoadFile( cLocalFile, cRemoteFile ) CLASS TIPClientFTP
          RETURN .F.
       ENDIF
 
-      IF ! Empty( ::oUrl:cPath )
-
-         IF ! ::CWD( ::oUrl:cPath )
-            RETURN .F.
-         ENDIF
-
+      IF ! Empty( ::oUrl:cPath ) .AND. ! ::CWD( ::oUrl:cPath )
+         RETURN .F.
       ENDIF
 
       IF ! ::bUsePasv .AND. ! ::Port()
@@ -679,8 +649,6 @@ METHOD LS( cSpec ) CLASS TIPClientFTP
 
    LOCAL cStr
 
-   hb_default( @cSpec, "" )
-
    IF ::bUsePasv .AND. ! ::Pasv()
       // ::bUsePasv := .F.
       RETURN .F.
@@ -690,7 +658,7 @@ METHOD LS( cSpec ) CLASS TIPClientFTP
       RETURN .F.
    ENDIF
 
-   ::inetSendAll( ::SocketCon, "NLST " + cSpec + ::cCRLF )
+   ::inetSendAll( ::SocketCon, "NLST " + hb_defaultValue( cSpec, "" ) + ::cCRLF )
    IF ::GetReply()
       cStr := ::ReadAuxPort()
    ELSE
@@ -699,12 +667,11 @@ METHOD LS( cSpec ) CLASS TIPClientFTP
 
    RETURN cStr
 
-/* Rename file on FTP */
 METHOD Rename( cFrom, cTo ) CLASS TIPClientFTP
 
-   ::inetSendAll( ::SocketCon, "RNFR " + cFrom + ::cCRLF )
+   ::inetSendAll( ::SocketCon, "RNFR " + hb_defaultValue( cFrom, "" ) + ::cCRLF )
    IF ::GetReply()
-      ::inetSendAll( ::SocketCon, "RNTO " + cTo + ::cCRLF )
+      ::inetSendAll( ::SocketCon, "RNTO " + hb_defaultValue( cTo, "" ) + ::cCRLF )
       RETURN ::GetReply()
    ENDIF
 
@@ -718,10 +685,8 @@ METHOD DownLoadFile( cLocalFile, cRemoteFile ) CLASS TIPClientFTP
 
    hb_FNameSplit( cLocalFile, @cPath, @cFile, @cExt  )
 
-   hb_default( @cRemoteFile, cFile + cExt )
-
    ::bEof := .F.
-   ::oUrl:cFile := cRemoteFile
+   ::oUrl:cFile := hb_defaultValue( cRemoteFile, cFile + cExt )
 
    IF ! ::bInitialized
 

contrib/hbtip/popcli.prg
@@ -122,13 +122,10 @@ METHOD OpenDigest( cUrl ) CLASS TIPClientPOP
    ENDIF
 
    IF ::GetOk()
-      nPos := At( "<", ::cReply )
-      IF nPos > 0
-         nPos2 := hb_At( ">", ::cReply, nPos + 1 )
-         IF nPos2 > nPos
+      IF ( nPos := At( "<", ::cReply ) ) > 0
+         IF ( nPos2 := hb_At( ">", ::cReply, nPos + 1 ) ) > nPos
             cDigest := hb_MD5( SubStr( ::cReply, nPos, ( nPos2 - nPos ) + 1 ) + ::oUrl:cPassword )
-            ::inetSendAll( ::SocketCon, "APOP " + ::oUrl:cUserid + " " ;
-               + cDigest + ::cCRLF )
+            ::inetSendAll( ::SocketCon, "APOP " + ::oUrl:cUserid + " " + cDigest + ::cCRLF )
             IF ::GetOK()
                ::isOpen := .T.
                RETURN .T.
@@ -141,11 +138,9 @@ METHOD OpenDigest( cUrl ) CLASS TIPClientPOP
 
 METHOD Close( lAutoQuit ) CLASS TIPClientPOP
 
-   hb_default( @lAutoQuit, .T. )
-
    ::InetTimeOut( ::SocketCon )
 
-   IF lAutoQuit
+   IF hb_defaultValue( lAutoQuit, .T. )
       ::Quit()
    ENDIF
 
@@ -360,7 +355,7 @@ METHOD Read( nLen ) CLASS TIPClientPOP
    ENDIF
 
    IF Val( ::oUrl:cFile ) < 0
-      IF ::Delete( - Val( ::oUrl:cFile ) )
+      IF ::Delete( -Val( ::oUrl:cFile ) )
          RETURN ::Quit()
       ELSE
          RETURN .F.

contrib/gtwvg/tests/_xbp.prg
@@ -7,7 +7,7 @@
 #include "inkey.ch"
 #include "hbgtinfo.ch"
 
-#define MUSIC_WAITON          {800, 1600}
+#define MUSIC_WAITON          { 800, 1600 }
 
 //
 //                     A Pure Xbase++ Implementation

contrib/hbhttpd/tests/eshop.prg
@@ -83,7 +83,7 @@ PROCEDURE Main()
 
    IF ! oLogAccess:Add( "" )
       oLogAccess:Close()
-      ? "Access log file open error " + hb_ntos( FError() )
+      ? "Access log file open error", hb_ntos( FError() )
       RETURN
    ENDIF
 
@@ -92,7 +92,7 @@ PROCEDURE Main()
    IF ! oLogError:Add( "" )
       oLogError:Close()
       oLogAccess:Close()
-      ? "Error log file open error " + hb_ntos( FError() )
+      ? "Error log file open error", hb_ntos( FError() )
       RETURN
    ENDIF
 

commit d96e039cf94b8b259b6f207ad4ddc31f4e1c530f

2014-06-28 23:54 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/hbtip/ftpcli.prg     * TIPClientFTP():MPUT() changed to return NIL instead of 0 in case       of wrong parameter type [INCOMPATIBLE]       (this way the method returns NIL/string types only)     * TIPClientFTP():MGET() changed to return NIL instead of .F. if       passive mode is requested but not available [INCOMPATIBLE]       (this way the method returns NIL/string types only)     ! TIPClientFTP():MGET() fixed regression from 122195bd747a7c4832bf5b7c7b4da076850dd014       where an RTE could occur in case of error       (instead of returning NIL)     * TIPClientFTP():ReadAuxPort() changed to return a string       when the <cLocalFile> (probably debug) parameter is       passed. Previously it returned .T. [INCOMPATIBLE]       (this way the method returns NIL/string types only)     * TIPClientFTP():UserCommand() added two QUESTIONs about its parameters     % minor optimizations
  * contrib/hbtip/popcli.prg
    % TIPClientPOP():Read() optimized and comments added

  * extras/rddado/adordd.prg
    ! protect internal variables from unitialized values
      or assigment of wrong types
    % use SWITCH
    * do not pass 3rd param as reference to ADO_ORDINFO(), it's unnecessary
    * marked params expected by reference

  * config/detect.mk
  * config/detfun.mk
    ! typo

  * extras/rddado/adordd.ch
    * formatting

config/detect.mk
@@ -3,7 +3,7 @@
 # See COPYING.txt for licensing terms.
 # ---------------------------------------------------------------
 
-# This make file will detect optional 3rd party components
+# This makefile will detect optional 3rd party components
 # used in Harbour core code.
 
 # config.mk if present is root, is able to override HB_HAS_* values.

config/detfun.mk
@@ -3,7 +3,7 @@
 # See COPYING.txt for licensing terms.
 # ---------------------------------------------------------------
 
-# This make file will detect optional 3rd party components
+# This makefile will detect optional 3rd party components
 # used in Harbour core code. Generic function.
 
 # USAGE:

contrib/hbtip/ftpcli.prg
@@ -142,10 +142,9 @@ CREATE CLASS TIPClientFTP FROM TIPClient
    METHOD UploadFile( cLocalFile, cRemoteFile )    // new method for file upload
    METHOD DownLoadFile( cLocalFile, cRemoteFile )  // new method to download file
    METHOD MKD( cPath )                             // new method to create an directory on ftp server
-
    METHOD RMD( cPath )
    METHOD listFiles( cFileSpec )
-   METHOD MPut
+   METHOD MPut( cFileSpec, cAttr )
    METHOD fileSize( cFileSpec )
 
 ENDCLASS
@@ -286,14 +285,14 @@ METHOD CWD( cPath ) CLASS TIPClientFTP
 
 METHOD PWD() CLASS TIPClientFTP
 
-   ::inetSendAll( ::SocketCon, "PWD"  + ::cCRLF )
-   IF ! ::GetReply()
-      RETURN .F.
+   ::inetSendAll( ::SocketCon, "PWD" + ::cCRLF )
+   IF ::GetReply()
+      ::cReply := SubStr( ::cReply, At( '"', ::cReply ) + 1, ;
+         RAt( '"', ::cReply ) - At( '"', ::cReply ) - 1 )
+      RETURN .T.
    ENDIF
-   ::cReply := SubStr( ::cReply, At( '"', ::cReply ) + 1, ;
-      RAt( '"', ::cReply ) - At( '"', ::cReply ) - 1 )
 
-   RETURN .T.
+   RETURN .F.
 
 METHOD DELE( cPath ) CLASS TIPClientFTP
 
@@ -406,11 +405,11 @@ METHOD UserCommand( cCommand, lPasv, lReadPort, lGetReply ) CLASS TIPClientFTP
    ::inetSendAll( ::SocketCon, hb_defaultValue( cCommand, "" ) )
 
    IF hb_defaultValue( lReadPort, .T. )
-      lReadPort := ::ReadAuxPort()
+      lReadPort := ::ReadAuxPort()  /* QUESTION: is this assignment intentional? */
    ENDIF
 
    IF hb_defaultValue( lGetReply, .F. )
-      lGetReply := ::GetReply()
+      lGetReply := ::GetReply()  /* QUESTION: is this assignment intentional? */
    ENDIF
 
    RETURN .T.
@@ -418,20 +417,24 @@ METHOD UserCommand( cCommand, lPasv, lReadPort, lGetReply ) CLASS TIPClientFTP
 METHOD ReadAuxPort( cLocalFile ) CLASS TIPClientFTP
 
    LOCAL cRet
-   LOCAL cList := ""
-   LOCAL nFile := F_ERROR
+   LOCAL cList
+   LOCAL nFile
 
    IF ! ::TransferStart()
       RETURN NIL
    ENDIF
-   IF ! Empty( cLocalFile )
+
+   IF HB_ISSTRING( cLocalFile ) .AND. ! Empty( cLocalFile )
       nFile := FCreate( cLocalFile )
+   ELSE
+      nFile := F_ERROR
    ENDIF
+
+   cList := ""
    DO WHILE ( cRet := ::super:Read( 512 ) ) != NIL .AND. Len( cRet ) > 0
+      cList += cRet
       IF nFile != F_ERROR
          FWrite( nFile, cRet )
-      ELSE
-         cList += cRet
       ENDIF
    ENDDO
 
@@ -440,7 +443,6 @@ METHOD ReadAuxPort( cLocalFile ) CLASS TIPClientFTP
    IF ::GetReply()
       IF nFile != F_ERROR
          FClose( nFile )
-         RETURN .T.
       ENDIF
       RETURN cList
    ENDIF
@@ -569,8 +571,10 @@ METHOD MGET( cSpec, cLocalPath ) CLASS TIPClientFTP
    LOCAL cStr, cFile
 
    IF ::bUsePasv .AND. ! ::Pasv()
-      // ::bUsePasv := .F.
-      RETURN .F.
+#if 0
+      ::bUsePasv := .F.
+#endif
+      RETURN NIL
    ENDIF
 
    hb_default( @cLocalPath, "" )
@@ -579,11 +583,13 @@ METHOD MGET( cSpec, cLocalPath ) CLASS TIPClientFTP
 
    cStr := ::ReadAuxPort()
 
-   FOR EACH cFile IN hb_ATokens( StrTran( cStr, Chr( 13 ) ), Chr( 10 ) )
-      IF ! Empty( cFile )
-         ::downloadfile( cLocalPath + RTrim( cFile ), RTrim( cFile ) )
-      ENDIF
-   NEXT
+   IF cStr != NIL
+      FOR EACH cFile IN hb_ATokens( StrTran( cStr, Chr( 13 ) ), Chr( 10 ) )
+         IF ! Empty( cFile )
+            ::downloadfile( cLocalPath + RTrim( cFile ), RTrim( cFile ) )
+         ENDIF
+      NEXT
+   ENDIF
 
    RETURN cStr
 
@@ -593,7 +599,7 @@ METHOD MPUT( cFileSpec, cAttr ) CLASS TIPClientFTP
    LOCAL cStr
 
    IF ! HB_ISSTRING( cFileSpec )
-      RETURN 0
+      RETURN NIL
    ENDIF
 
    hb_FNameSplit( cFileSpec, @cPath, @cFile, @cExt  )
@@ -646,8 +652,6 @@ METHOD UploadFile( cLocalFile, cRemoteFile ) CLASS TIPClientFTP
 
 METHOD LS( cSpec ) CLASS TIPClientFTP
 
-   LOCAL cStr
-
    IF ::bUsePasv .AND. ! ::Pasv()
       // ::bUsePasv := .F.
       RETURN .F.
@@ -658,13 +662,8 @@ METHOD LS( cSpec ) CLASS TIPClientFTP
    ENDIF
 
    ::inetSendAll( ::SocketCon, "NLST " + hb_defaultValue( cSpec, "" ) + ::cCRLF )
-   IF ::GetReply()
-      cStr := ::ReadAuxPort()
-   ELSE
-      cStr := ""
-   ENDIF
 
-   RETURN cStr
+   RETURN iif( ::GetReply(), ::ReadAuxPort(), "" )
 
 METHOD Rename( cFrom, cTo ) CLASS TIPClientFTP
 
@@ -757,11 +756,8 @@ METHOD listFiles( cFileSpec ) CLASS TIPClientFTP
    FOR EACH cEntry IN aList DESCEND
 
       IF Empty( cEntry )
-
          hb_ADel( aList, cEntry:__enumIndex(), .T. )
-
       ELSE
-
          aFile  := Array( F_LEN + 3 )
          nStart := 1
          nEnd   := hb_At( " ", cEntry, nStart )
@@ -836,7 +832,6 @@ METHOD listFiles( cFileSpec ) CLASS TIPClientFTP
          aFile[ F_TIME ] := cTime
 
          aList[ cEntry:__enumIndex() ] := aFile
-
       ENDIF
    NEXT
 

contrib/hbtip/popcli.prg
@@ -338,24 +338,18 @@ METHOD GetOk() CLASS TIPClientPOP
 
    RETURN ::inetErrorCode( ::SocketCon ) == 0 .AND. hb_LeftEq( ::cReply, "+" )
 
-/* QUESTION: This method will return .T./.F./NIL or string
+/* QUESTION: This method will return logical, NIL or string
              Is it really intended that way? [vszakats] */
 METHOD Read( nLen ) CLASS TIPClientPOP
 
-   /* Set what to read for */
+   /* Decide what to read */
    IF Empty( ::oUrl:cFile )
-      RETURN ::List()
+      RETURN ::List()  /* return NIL or string */
+   ELSEIF Val( ::oUrl:cFile ) < 0
+      RETURN ::Delete( -Val( ::oUrl:cFile ) ) .AND. ::Quit()  /* return logical */
    ENDIF
 
-   IF Val( ::oUrl:cFile ) < 0
-      IF ::Delete( -Val( ::oUrl:cFile ) )
-         RETURN ::Quit()
-      ELSE
-         RETURN .F.
-      ENDIF
-   ENDIF
-
-   RETURN ::Retrieve( Val( ::oUrl:cFile ), nLen )
+   RETURN ::Retrieve( Val( ::oUrl:cFile ), nLen )  /* return NIL or string */
 
 METHOD retrieveAll( lDelete ) CLASS TIPClientPOP
 

extras/rddado/adordd.ch
@@ -167,18 +167,18 @@
 #define adSchemaSets                    43
 
 /* Constant Group: Supports */
-#define adAddNew                        0x1000400 /* Supports the AddNew method to add new records. */
-#define adApproxPosition                0x0004000 /* Supports the AbsolutePosition and AbsolutePage properties. */
-#define adBookmark                      0x0002000 /* Supports the Bookmark property to gain access to specific records. */
-#define adDelete                        0x1000800 /* Supports the Delete method to delete records. */
-#define adFind                          0x0080000 /* Supports the Find method to locate a row in a Recordset. */
-#define adHoldRecords                   0x0000100 /* Retrieves more records or changes the next position without committing all pending changes. */
-#define adIndex                         0x0100000 /* Supports the Index property to name an index. */
-#define adMovePrevious                  0x0000200 /* Supports the MoveFirst and MovePrevious methods, and Move or GetRows methods to move the current record position backward without requiring bookmarks. */
-#define adNotify                        0x0040000 /* Indicates that the underlying data provider supports notifications (which determines whether Recordset events are supported). */
-#define adResync                        0x0020000 /* Supports the Resync method to update the cursor with the data that is visible in the underlying database. */
-#define adSeek                          0x0200000 /* Supports the Seek method to locate a row in a Recordset. */
-#define adUpdate                        0x1008000 /* Supports the Update method to modify existing data. */
+#define adAddNew                        0x1000400  /* Supports the AddNew method to add new records. */
+#define adApproxPosition                0x0004000  /* Supports the AbsolutePosition and AbsolutePage properties. */
+#define adBookmark                      0x0002000  /* Supports the Bookmark property to gain access to specific records. */
+#define adDelete                        0x1000800  /* Supports the Delete method to delete records. */
+#define adFind                          0x0080000  /* Supports the Find method to locate a row in a Recordset. */
+#define adHoldRecords                   0x0000100  /* Retrieves more records or changes the next position without committing all pending changes. */
+#define adIndex                         0x0100000  /* Supports the Index property to name an index. */
+#define adMovePrevious                  0x0000200  /* Supports the MoveFirst and MovePrevious methods, and Move or GetRows methods to move the current record position backward without requiring bookmarks. */
+#define adNotify                        0x0040000  /* Indicates that the underlying data provider supports notifications (which determines whether Recordset events are supported). */
+#define adResync                        0x0020000  /* Supports the Resync method to update the cursor with the data that is visible in the underlying database. */
+#define adSeek                          0x0200000  /* Supports the Seek method to locate a row in a Recordset. */
+#define adUpdate                        0x1008000  /* Supports the Update method to modify existing data. */
 #define adUpdateBatch                   0x0010000
 
 /* Command type */

extras/rddado/adordd.prg
@@ -100,11 +100,11 @@
 
 ANNOUNCE ADORDD
 
-THREAD STATIC t_cTableName
-THREAD STATIC t_cEngine
-THREAD STATIC t_cServer
-THREAD STATIC t_cUserName
-THREAD STATIC t_cPassword
+THREAD STATIC t_cTableName := ""
+THREAD STATIC t_cEngine := ""
+THREAD STATIC t_cServer := ""
+THREAD STATIC t_cUserName := ""
+THREAD STATIC t_cPassword := ""
 THREAD STATIC t_cQuery := ""
 
 STATIC FUNCTION ADO_INIT( nRDD )
@@ -242,44 +242,48 @@ STATIC FUNCTION ADO_CREATEFIELDS( nWA, aStruct )
    aWAData[ WA_SQLSTRUCT ] := ""
 
    FOR EACH field IN aStruct
+
       IF ! field:__enumIsFirst()
          aWAData[ WA_SQLSTRUCT ] += ", "
       ENDIF
+
       aWAData[ WA_SQLSTRUCT ] += "[" + field[ DBS_NAME ] + "]"
-      DO CASE
-      CASE field[ DBS_TYPE ] $ "C,Character"
+
+      SWITCH field[ DBS_TYPE ]
+      CASE "C"
+      CASE "Character"
          aWAData[ WA_SQLSTRUCT ] += " CHAR(" + hb_ntos( field[ DBS_LEN ] ) + ") NULL"
-
-      CASE field[ DBS_TYPE ] == "V"
+         EXIT
+      CASE "V"
          aWAData[ WA_SQLSTRUCT ] += " VARCHAR(" + hb_ntos( field[ DBS_LEN ] ) + ") NULL"
-
-      CASE field[ DBS_TYPE ] == "B"
+         EXIT
+      CASE "B"
          aWAData[ WA_SQLSTRUCT ] += " DOUBLE NULL"
-
-      CASE field[ DBS_TYPE ] == "Y"
+         EXIT
+      CASE "Y"
          aWAData[ WA_SQLSTRUCT ] += " SMALLINT NULL"
-
-      CASE field[ DBS_TYPE ] == "I"
+         EXIT
+      CASE "I"
          aWAData[ WA_SQLSTRUCT ] += " MEDIUMINT NULL"
-
-      CASE field[ DBS_TYPE ] == "D"
+         EXIT
+      CASE "D"
          aWAData[ WA_SQLSTRUCT ] += " DATE NULL"
-
-      CASE field[ DBS_TYPE ] == "T"
+         EXIT
+      CASE "T"
          aWAData[ WA_SQLSTRUCT ] += " DATETIME NULL"
-
-      CASE field[ DBS_TYPE ] == "@"
+         EXIT
+      CASE "@"
          aWAData[ WA_SQLSTRUCT ] += " TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP"
-
-      CASE field[ DBS_TYPE ] == "M"
+         EXIT
+      CASE "M"
          aWAData[ WA_SQLSTRUCT ] += " TEXT NULL"
-
-      CASE field[ DBS_TYPE ] == "N"
+         EXIT
+      CASE "N"
          aWAData[ WA_SQLSTRUCT ] += " NUMERIC(" + hb_ntos( field[ DBS_LEN ] ) + ")"
-
-      CASE field[ DBS_TYPE ] == "L"
+         EXIT
+      CASE "L"
          aWAData[ WA_SQLSTRUCT ] += " LOGICAL"
-      ENDCASE
+      ENDSWITCH
    NEXT
 
    RETURN HB_SUCCESS
@@ -464,7 +468,7 @@ STATIC FUNCTION ADO_CLOSE( nWA )
 
    RETURN UR_SUPER_CLOSE( nWA )
 
-STATIC FUNCTION ADO_GETVALUE( nWA, nField, xValue )
+STATIC FUNCTION ADO_GETVALUE( nWA, nField, /* @ */ xValue )
 
    LOCAL aWAData := USRRDD_AREADATA( nWA )
    LOCAL rs := USRRDD_AREADATA( nWA )[ WA_RECORDSET ]
@@ -596,7 +600,7 @@ STATIC FUNCTION ADO_SKIPRAW( nWA, nToSkip )
 
    RETURN nResult
 
-STATIC FUNCTION ADO_BOF( nWA, lBof )
+STATIC FUNCTION ADO_BOF( nWA, /* @ */ lBof )
 
    LOCAL aWAData := USRRDD_AREADATA( nWA )
 
@@ -604,7 +608,7 @@ STATIC FUNCTION ADO_BOF( nWA, lBof )
 
    RETURN HB_SUCCESS
 
-STATIC FUNCTION ADO_EOF( nWA, lEof )
+STATIC FUNCTION ADO_EOF( nWA, /* @ */ lEof )
 
    LOCAL oRecordSet := USRRDD_AREADATA( nWA )[ WA_RECORDSET ]
    LOCAL nResult := HB_SUCCESS
@@ -619,7 +623,7 @@ STATIC FUNCTION ADO_EOF( nWA, lEof )
 
    RETURN nResult
 
-STATIC FUNCTION ADO_DELETED( nWA, lDeleted )
+STATIC FUNCTION ADO_DELETED( nWA, /* @ */ lDeleted )
 
    LOCAL oRecordSet := USRRDD_AREADATA( nWA )[ WA_RECORDSET ]
 
@@ -641,7 +645,7 @@ STATIC FUNCTION ADO_DELETE( nWA )
 
    RETURN HB_SUCCESS
 
-STATIC FUNCTION ADO_RECNO( nWA, nRecNo )
+STATIC FUNCTION ADO_RECNO( nWA, /* @ */ nRecNo )
 
    LOCAL oRecordSet := USRRDD_AREADATA( nWA )[ WA_RECORDSET ]
    LOCAL nResult := HB_SUCCESS
@@ -660,10 +664,10 @@ STATIC FUNCTION ADO_RECNO( nWA, nRecNo )
 
    RETURN nResult
 
-STATIC FUNCTION ADO_RECID( nWA, nRecNo )
+STATIC FUNCTION ADO_RECID( nWA, /* @ */ nRecNo )
    RETURN ADO_RECNO( nWA, @nRecNo )
 
-STATIC FUNCTION ADO_RECCOUNT( nWA, nRecords )
+STATIC FUNCTION ADO_RECCOUNT( nWA, /* @ */ nRecords )
 
    nRecords := USRRDD_AREADATA( nWA )[ WA_RECORDSET ]:RecordCount()
 
@@ -776,7 +780,7 @@ STATIC FUNCTION ADO_ORDINFO( nWA, nIndex, aOrderInfo )
 
    RETURN nResult
 
-STATIC FUNCTION ADO_RECINFO( nWA, nRecord, nInfoType, uInfo )
+STATIC FUNCTION ADO_RECINFO( nWA, nRecord, nInfoType, /* @ */ uInfo )
 
    LOCAL nResult := HB_SUCCESS
 
@@ -815,7 +819,7 @@ STATIC FUNCTION ADO_RECINFO( nWA, nRecord, nInfoType, uInfo )
 
    RETURN nResult
 
-STATIC FUNCTION ADO_FIELDNAME( nWA, nField, cFieldName )
+STATIC FUNCTION ADO_FIELDNAME( nWA, nField, /* @ */ cFieldName )
 
    LOCAL nResult := HB_SUCCESS
    LOCAL oRecordSet := USRRDD_AREADATA( nWA )[ WA_RECORDSET ]
@@ -829,7 +833,7 @@ STATIC FUNCTION ADO_FIELDNAME( nWA, nField, cFieldName )
 
    RETURN nResult
 
-STATIC FUNCTION ADO_FIELDINFO( nWA, nField, nInfoType, uInfo )
+STATIC FUNCTION ADO_FIELDINFO( nWA, nField, nInfoType, /* @ */ uInfo )
 
    LOCAL nType, nLen
    LOCAL oRecordSet := USRRDD_AREADATA( nWA )[ WA_RECORDSET ]
@@ -1061,7 +1065,7 @@ STATIC FUNCTION ADO_CLEARREL( nWA )
 
    RETURN HB_SUCCESS
 
-STATIC FUNCTION ADO_RELAREA( nWA, nRelNo, nRelArea )
+STATIC FUNCTION ADO_RELAREA( nWA, nRelNo, /* @ */ nRelArea )
 
    LOCAL aWAData := USRRDD_AREADATA( nWA )
 
@@ -1075,7 +1079,7 @@ STATIC FUNCTION ADO_RELAREA( nWA, nRelNo, nRelArea )
 
    RETURN HB_SUCCESS
 
-STATIC FUNCTION ADO_RELTEXT( nWA, nRelNo, cExpr )
+STATIC FUNCTION ADO_RELTEXT( nWA, nRelNo, /* @ */ cExpr )
 
    LOCAL aWAData := USRRDD_AREADATA( nWA )
 
@@ -1129,16 +1133,16 @@ STATIC FUNCTION ADO_RELEVAL( nWA, aRelInfo )
    IF nReturn == HB_SUCCESS
       /* Check the current order */
       aInfo := Array( UR_ORI_SIZE )
-      nReturn := ADO_ORDINFO( nWA, DBOI_NUMBER, @aInfo )
+      nReturn := ADO_ORDINFO( nWA, DBOI_NUMBER, aInfo )
 
       IF nReturn == HB_SUCCESS
          nOrder := aInfo[ UR_ORI_RESULT ]
          IF nOrder != 0
             IF aRelInfo[ UR_RI_SCOPED ]
                aInfo[ UR_ORI_NEWVAL ] := uResult
-               nReturn := ADO_ORDINFO( nWA, DBOI_SCOPETOP, @aInfo )
+               nReturn := ADO_ORDINFO( nWA, DBOI_SCOPETOP, aInfo )
                IF nReturn == HB_SUCCESS
-                  nReturn := ADO_ORDINFO( nWA, DBOI_SCOPEBOTTOM, @aInfo )
+                  nReturn := ADO_ORDINFO( nWA, DBOI_SCOPEBOTTOM, aInfo )
                ENDIF
             ENDIF
             IF nReturn == HB_SUCCESS
@@ -1226,7 +1230,7 @@ STATIC FUNCTION ADO_ORDDESTROY( nWA, aOrderInfo )
 
    RETURN HB_SUCCESS
 
-STATIC FUNCTION ADO_EVALBLOCK( nArea, bBlock, uResult )
+STATIC FUNCTION ADO_EVALBLOCK( nArea, bBlock, /* @ */ uResult )
 
    LOCAL nCurrArea := Select()
 
@@ -1317,7 +1321,7 @@ STATIC FUNCTION ADO_SEEK( nWA, lSoftSeek, cKey, lFindLast )
 
    RETURN HB_FAILURE
 
-STATIC FUNCTION ADO_FOUND( nWA, lFound )
+STATIC FUNCTION ADO_FOUND( nWA, /* @ */ lFound )
 
    LOCAL aWAData := USRRDD_AREADATA( nWA )
 
@@ -1491,31 +1495,31 @@ STATIC FUNCTION ADO_GETFIELDTYPE( nADOFieldType )
 
 PROCEDURE hb_adoSetTable( cTableName )
 
-   t_cTableName := cTableName
+   t_cTableName := hb_defaultValue( cTableName, "" )
 
    RETURN
 
 PROCEDURE hb_adoSetEngine( cEngine )
 
-   t_cEngine := cEngine
+   t_cEngine := hb_defaultValue( cEngine, "" )
 
    RETURN
 
 PROCEDURE hb_adoSetServer( cServer )
 
-   t_cServer := cServer
+   t_cServer := hb_defaultValue( cServer, "" )
 
    RETURN
 
 PROCEDURE hb_adoSetUser( cUser )
 
-   t_cUserName := cUser
+   t_cUserName := hb_defaultValue( cUser, "" )
 
    RETURN
 
 PROCEDURE hb_adoSetPassword( cPassword )
 
-   t_cPassword := cPassword
+   t_cPassword := hb_defaultValue( cPassword, "" )
 
    RETURN
 