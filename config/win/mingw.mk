harbour/core

commit 6f8508ff54a3955822b36bf4a65a2775a11bab23

2014-07-21 01:04 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * src/rtl/listbox.prg     % ListBox():findText(), ListBox():findData(): use hb_LeftEq[I]()     ! ListBox():findText(), ListBox():findData(): fixed to not RTrim()       while searching in EXACT mode. Regression from f61409bf19731c8485aa29bae273cae090c77502     + ListBox():findText(), ListBox():findData(): allow to search       for any type of data in HB_CLP_STRICT mode to mimic Cl*pper behavior     ! ListBox():findText(): fixed to allow zero length search text,       like Cl*pper. Regression from 93d3a46d843daaf6d08149d83fa5203ff910c484     ! ListBox():addItem( cText, cData ): fixed to allow any type for cData,       not just NIL and string, like Cl*pper     + ListBox():setData(): documented a Cl*pper bug
  * contrib/hbnf/tests/tbwhile.prg
    + use hb_LeftEq()

  * tests/rddtest/rddtst.prg
    + hb_LeftEq() as comment
    ! formatting

  - package/winuni/HARBOUR_README_MINGW64.txt
  * package/winuni/mpkg_win_uni.bat
  * package/winuni/mpkg_win_uni.nsi
  * package/winuni/RELNOTES.txt
    - deleted references to separate mingw x64 build.
      It is integrated into the mainline mingw lately (dual-build).

  * package/winuni/RELNOTES.txt
    + updated for dual-mode mingw
    * deleted references to WinCE, msvc
    + use ANSI dates

  * package/winuni/HARBOUR_README_MINGWARM.txt
    * wording

src/rtl/listbox.prg
@@ -66,7 +66,7 @@
 #define _ITEM_cTEXT         1
 #define _ITEM_cDATA         2
 
-#define _LISTBOX_ITEMDATA( aItem ) iif( aItem[ _ITEM_cDATA ] == NIL, aItem[ _ITEM_cTEXT ], aItem[ _ITEM_cDATA ] )
+#define _LISTBOX_ITEMDATA( aItem )  iif( aItem[ _ITEM_cDATA ] == NIL, aItem[ _ITEM_cTEXT ], aItem[ _ITEM_cDATA ] )
 
 CREATE CLASS ListBox FUNCTION HBListBox
 
@@ -79,7 +79,7 @@ CREATE CLASS ListBox FUNCTION HBListBox
    METHOD delItem( nPos )
    METHOD display()
    METHOD findText( cText, nPos, lCaseSensitive, lExact )
-   METHOD findData( cData, nPos, lCaseSensitive, lExact ) /* NOTE: Undocumented CA-Cl*pper method. */
+   METHOD findData( cData, nPos, lCaseSensitive, lExact )  /* NOTE: Undocumented CA-Cl*pper method. */
    METHOD getData( nPos )
    METHOD getItem( nPos )
    METHOD getText( nPos )
@@ -162,7 +162,7 @@ ENDCLASS
 
 METHOD addItem( cText, cData ) CLASS ListBox
 
-   IF HB_ISSTRING( cText ) .AND. ValType( cData ) $ "CU"
+   IF HB_ISSTRING( cText )
 
       AAdd( ::aItems, { cText, cData } )
 
@@ -311,12 +311,14 @@ METHOD display() CLASS ListBox
 METHOD findText( cText, nPos, lCaseSensitive, lExact ) CLASS ListBox
 
    LOCAL nPosFound
-   LOCAL nLen
    LOCAL bSearch
 
-   IF ! HB_ISSTRING( cText ) .OR. Len( cText ) == 0
+#ifndef HB_CLP_STRICT
+   /* NOTE: Cl*pper will RTE if passed a non-string cText */
+   IF ! HB_ISSTRING( cText )
       RETURN 0
    ENDIF
+#endif
 
    hb_default( @nPos, 1 )
    hb_default( @lCaseSensitive, .T. )
@@ -325,20 +327,17 @@ METHOD findText( cText, nPos, lCaseSensitive, lExact ) CLASS ListBox
    ENDIF
 
    IF lExact
-      cText := RTrim( cText )
       IF lCaseSensitive
-         bSearch := {| aItem | RTrim( aItem[ _ITEM_cTEXT ] ) == cText }
+         bSearch := {| aItem | aItem[ _ITEM_cTEXT ] == cText }
       ELSE
          cText := Lower( cText )
-         bSearch := {| aItem | Lower( RTrim( aItem[ _ITEM_cTEXT ] ) ) == cText }
+         bSearch := {| aItem | Lower( aItem[ _ITEM_cTEXT ] ) == cText }
       ENDIF
    ELSE
-      nLen := Len( cText )
       IF lCaseSensitive
-         bSearch := {| aItem | Left( aItem[ _ITEM_cTEXT ], nLen ) == cText }
+         bSearch := {| aItem | hb_LeftEq( aItem[ _ITEM_cTEXT ], cText ) }
       ELSE
-         cText := Lower( cText )
-         bSearch := {| aItem | Lower( Left( aItem[ _ITEM_cTEXT ], nLen ) ) == cText }
+         bSearch := {| aItem | hb_LeftEqI( aItem[ _ITEM_cTEXT ], cText ) }
       ENDIF
    ENDIF
 
@@ -351,12 +350,14 @@ METHOD findText( cText, nPos, lCaseSensitive, lExact ) CLASS ListBox
 METHOD findData( cData, nPos, lCaseSensitive, lExact ) CLASS ListBox
 
    LOCAL nPosFound
-   LOCAL nLen
    LOCAL bSearch
 
+#ifndef HB_CLP_STRICT
+   /* NOTE: Cl*pper will RTE if passed a non-string cData */
    IF ! HB_ISSTRING( cData )
       RETURN 0
    ENDIF
+#endif
 
    hb_default( @nPos, 1 )
    hb_default( @lCaseSensitive, .T. )
@@ -365,20 +366,16 @@ METHOD findData( cData, nPos, lCaseSensitive, lExact ) CLASS ListBox
    ENDIF
 
    IF lExact
-      cData := RTrim( cData )
       IF lCaseSensitive
-         bSearch := {| aItem | RTrim( _LISTBOX_ITEMDATA( aItem ) ) == cData }
+         bSearch := {| aItem | _LISTBOX_ITEMDATA( aItem ) == cData }
       ELSE
-         cData := Lower( cData )
-         bSearch := {| aItem | Lower( RTrim( _LISTBOX_ITEMDATA( aItem ) ) ) == cData }
+         bSearch := {| aItem | Lower( _LISTBOX_ITEMDATA( aItem ) ) == cData }
       ENDIF
    ELSE
-      nLen := Len( cData )
       IF lCaseSensitive
-         bSearch := {| aItem | Left( _LISTBOX_ITEMDATA( aItem ), nLen ) == cData }
+         bSearch := {| aItem | hb_LeftEq( _LISTBOX_ITEMDATA( aItem ), cData ) }
       ELSE
-         cData := Lower( cData )
-         bSearch := {| aItem | Lower( Left( _LISTBOX_ITEMDATA( aItem ), nLen ) ) == cData }
+         bSearch := {| aItem | hb_LeftEqI( _LISTBOX_ITEMDATA( aItem ), cData ) }
       ENDIF
    ENDIF
 
@@ -739,6 +736,7 @@ METHOD select( xPos ) CLASS ListBox
 
    RETURN ::nValue
 
+/* NOTE: This function does nothing in Cl*pper, due to a bug. */
 METHOD setData( nPos, cData ) CLASS ListBox
 
    IF nPos >= 1 .AND. nPos <= ::nItemCount

contrib/hbnf/tests/tbwhile.prg
@@ -1,11 +1,9 @@
-/*
- *   THIS DEMO SHOWS tbnames.dbf CONSISTING OF LAST, FIRST, ADDR, CITY,
- *   STATE, ZIP WITH ACTIVE INDEX ON LAST + FIRST.  IT SHOWS LAST NAME,
- *   FIRST NAME, CITY ONLY FOR THOSE LAST NAMES THAT BEGIN WITH LETTER
- *   THAT YOU INPUT FOR THE CKEY GET.
- *
- *   tbnames.dbf/.ntx ARE AUTOMATICALLY CREATED BY THIS TEST PROGRAM
- */
+/* This demo shows tbnames.dbf consisting of LAST, FIRST, ADDR, CITY,
+   STATE, ZIP with active index on LAST + FIRST.  It shows last name,
+   first name, city only for those last names that begin with letter
+   that you input for the cKey GET.
+
+   tbnames.dbf/.ntx are automatically created by this test program */
 
 #require "hbnf"
 
@@ -43,16 +41,16 @@ PROCEDURE Main()
    @ 5, 10 SAY "Enter First Letter Of Last Name:" GET cKey PICTURE "!"
    READ
 
-   // tbnames->LAST = cKey is the Conditional Block passed to this function
+   // hb_LeftEq( tbnames->LAST, cKey ) is the Conditional Block passed to this function
    // you can make it as complicated as you want, but you would then
    // have to modify TBWhileSet() to find first and last records
    // matching your key.
-   nRecSel := ft_BrwsWhl( aFields, {|| tbnames->LAST = cKey }, cKey, nFreeze, ;
+   nRecSel := ft_BrwsWhl( aFields, {|| hb_LeftEq( tbnames->LAST ) }, cKey, nFreeze, ;
       lSaveScrn, cColorList, cColorShad, 3, 6, MaxRow() - 2, MaxCol() - 6 )
    // Note you can use Compound Condition
    // such as cLast := "Pierce            " and cFirst := "Hawkeye  "
    // by changing above block to:
-   //    {|| tbnames->LAST = cLast .AND. tbnames->FIRST = cFirst }
+   //    {|| hb_LeftEq( tbnames->LAST, cLast ) .AND. hb_LeftEq( tbnames->FIRST, cFirst ) }
    // and setting cKey := cLast + cFirst
 
    ?

package/winuni/mpkg_win_uni.bat
@@ -32,7 +32,6 @@ xcopy /y /s    %~dp0..\..\tests\*.*                                             
 xcopy /y       %~dp0HARBOUR_README_ADDONS.txt                                             %HB_ABSROOT%addons\
 xcopy /y       %~dp0HARBOUR_README_DJGPP.txt                                              %HB_ABSROOT%comp\djgpp\
 xcopy /y       %~dp0HARBOUR_README_MINGW.txt                                              %HB_ABSROOT%comp\mingw\
-xcopy /y       %~dp0HARBOUR_README_MINGW64.txt                                            %HB_ABSROOT%comp\mingw64\
 xcopy /y       %~dp0HARBOUR_README_MINGWARM.txt                                           %HB_ABSROOT%comp\mingwarm\
 xcopy /y       %~dp0HARBOUR_README_POCC.txt                                               %HB_ABSROOT%comp\pocc\
 xcopy /y       %~dp0HARBOUR_README_WATCOM.txt                                             %HB_ABSROOT%comp\watcom\
@@ -131,7 +130,6 @@ rem echo "%HB_DR%comp\djgpp\HARBOUR_README_DJGPP.txt"       >> _hbfiles
 echo "%HB_DR%comp\watcom\HARBOUR_README_WATCOM.txt"     >> _hbfiles
 echo "%HB_DR%comp\pocc\HARBOUR_README_POCC.txt"         >> _hbfiles
 echo "%HB_DR%comp\mingw\HARBOUR_README_MINGW.txt"       >> _hbfiles
-echo "%HB_DR%comp\mingw64\HARBOUR_README_MINGW64.txt"   >> _hbfiles
 echo "%HB_DR%comp\mingwarm\HARBOUR_README_MINGWARM.txt" >> _hbfiles
 rem echo "%HB_DR%lib\dos\djgpp\*.*"                     >> _hbfiles
 echo "%HB_DR%lib\dos\watcom\*.*"                    >> _hbfiles

package/winuni/mpkg_win_uni.nsi
@@ -24,7 +24,6 @@ CRCCheck on
 
 RequestExecutionLevel user
 
-!define PKG_NO_CC_MINGW64
 !define PKG_NO_CC_MINGWARM
 !define PKG_NO_COMP_DJGPP
 !define PKG_NO_COMP_POCC
@@ -132,13 +131,6 @@ Section "MinGW compiler" hb_mingw
 SectionEnd
 !endif
 
-!ifndef PKG_NO_CC_MINGW64
-Section "MinGW x64 compiler" hb_mingw64
-  SetOutPath $INSTDIR\comp\mingw64
-  File /r "$%HB_ABSROOT%comp\mingw64\*.*"
-SectionEnd
-!endif
-
 !ifndef PKG_NO_CC_MINGWARM
 Section "MinGW WinCE/ARM compiler" hb_mingwarm
   SetOutPath $INSTDIR\comp\mingwarm
@@ -158,8 +150,6 @@ SectionEnd
 Section /o "Libs for MinGW x64" hb_lib_mingw64
   SetOutPath $INSTDIR\lib\win\mingw64
   File "$%HB_ABSROOT%lib\win\mingw64\*.*"
-  SetOutPath $INSTDIR\comp\mingw64
-  File "$%HB_ABSROOT%comp\mingw64\HARBOUR_README_MINGW64.txt"
 SectionEnd
 !endif
 
@@ -358,9 +348,6 @@ SectionEnd
 !ifndef PKG_NO_CC_MINGW
   LangString DESC_hb_mingw        ${LANG_ENGLISH} "MinGW compiler"
 !endif
-!ifndef PKG_NO_CC_MINGW64
-  LangString DESC_hb_mingw64      ${LANG_ENGLISH} "MinGW x64 compiler"
-!endif
 !ifndef PKG_NO_CC_MINGWARM
   LangString DESC_hb_mingwarm     ${LANG_ENGLISH} "MinGW WinCE/ARM compiler"
 !endif
@@ -422,9 +409,6 @@ SectionEnd
 !endif
 !ifndef PKG_NO_CC_MINGW
     !insertmacro MUI_DESCRIPTION_TEXT ${hb_mingw}        $(DESC_hb_mingw)
-!endif
-!ifndef PKG_NO_CC_MINGW64
-    !insertmacro MUI_DESCRIPTION_TEXT ${hb_mingw64}      $(DESC_hb_mingw64)
 !endif
 !ifndef PKG_NO_CC_MINGWARM
     !insertmacro MUI_DESCRIPTION_TEXT ${hb_mingwarm}     $(DESC_hb_mingwarm)

tests/rddtest/rddtst.prg
@@ -5,7 +5,7 @@
  *
  */
 
-//#define _TEST_CREATE_
+// #define _TEST_CREATE_
 
 #ifndef N_LOOP
    #define N_LOOP 15
@@ -41,7 +41,7 @@ field FSTR, FNUM
 #include "fileio.ch"
 
 #ifdef _TEST_CREATE_
-  static s_hMake := F_ERROR
+   static s_hMake := F_ERROR
 #endif
 static s_nTested := 0
 static s_nErrors := 0
@@ -59,11 +59,14 @@ static aBadRetFunc := { "DBSKIP", "DBGOTO", "DBDELETE", "DBRECALL", ;
 #ifdef __HARBOUR__
 #ifdef _TEST_ADS_
 #include "ads.ch"
-REQUEST ADS
+request ADS
+
 init procedure adstest_init()
+
    rddRegister( "ADS", 1 )
    AdsSetServerType( ADS_LOCAL_SERVER )
    // rddSetDefault( "ADS" )
+
    return
 #endif
 #endif
@@ -72,29 +75,34 @@ init procedure adstest_init()
 
 #ifdef _TEST_CREATE_
 procedure main( cOutFile, rdd )
+
    test_init( rdd, cOutFile )
    test_main()
    test_close()
+
    return
 #else
 procedure main( rdd )
+
    test_init( rdd )
    test_main()
    test_close()
+
    return
 #endif
 
 static procedure test_init( rdd, cOutFile )
+
    local n, cOut, aDb := { { "FSTR", "C", 10, 0 }, { "FNUM", "N", 10, 0 } }
 
-if Empty( rdd )
-   #ifdef _TESTRDD
-      rdd := _TESTRDD
-   #else
-      rdd := "DBFCDX"
-   #endif
-endif
-rddSetDefault( rdd )
+   if Empty( rdd )
+      #ifdef _TESTRDD
+         rdd := _TESTRDD
+      #else
+         rdd := "DBFCDX"
+      #endif
+   endif
+   rddSetDefault( rdd )
 #ifdef _TEST_CREATE_
    if Empty( cOutFile )
       ? "Syntax: <outfile.prg> [<rddname>]"
@@ -139,11 +147,14 @@ rddSetDefault( rdd )
    dbCommit()
    dbUnlock()
 #endif
+
    return
 
 
 static procedure test_close()
+
    local cOut
+
 #ifdef _TEST_CREATE_
    if s_hMake != F_ERROR
       cOut := EOL + ;
@@ -164,6 +175,7 @@ static procedure test_close()
    AEval( Directory( "./TG_?.??x" ), {| x | FErase( x[ 1 ] ) } )
    FErase( "./" + _DBNAME + ".dbf" )
    ?
+
    return
 
 static procedure rdd_retval()
@@ -174,7 +186,9 @@ static function rdd_state()
 
 
 static function itm2str( itm )
+
    local cStr := "", i
+
    do case
    case itm == NIL
       cStr += "NIL"
@@ -195,14 +209,16 @@ static function itm2str( itm )
       next
       cStr += "}"
    endcase
+
    return cStr
 
 
 #ifdef _TEST_CREATE_
 static procedure rddtst_wr( cAction, xRet )
+
    local aState, cOut
 
-   if AScan( aBadRetFunc, {| x | Upper( cAction ) = x + "(" } ) > 0
+   if AScan( aBadRetFunc, {| x | Upper( cAction ) = x + "(" } ) > 0  /* hb_LeftEq() */
       xRet := NIL
    endif
    aState := rdd_state()
@@ -215,15 +231,17 @@ static procedure rddtst_wr( cAction, xRet )
       ? "write error."
       quit
    endif
+
    return
 #else
 // rddtst_tst( #<x>, <s>, <x>, <r> )
 static procedure rddtst_tst( cAction, aExState, xRet, xExRet )
+
    local aState, lOK := ( .T. ), s1, s2, i
 
    aState := rdd_state()
    if PCount() >= 4
-      if AScan( aBadRetFunc, {| x | Upper( cAction ) = x + "(" } ) > 0
+      if AScan( aBadRetFunc, {| x | Upper( cAction ) = x + "(" } ) > 0  /* hb_LeftEq() */
          xRet := NIL
       endif
       if ! ValType( xRet ) == ValType( xExRet ) .or. ;
@@ -253,5 +271,6 @@ static procedure rddtst_tst( cAction, aExState, xRet, xExRet )
       s_nErrors++
    endif
    s_nTested++
+
    return
 #endif

commit 4d26c11be8bc36a55bfa30428e6d77ebc9d0454a

2014-07-21 08:56 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * src/rtl/listbox.prg     ! ListBox():findData(): fixed to be able to search for non-string       data, to the same extent Cl*ipper is able to.     ! ListBox():findData(): fixed exact/case-insensitive regression       from 6f8508ff54a3955822b36bf4a65a2775a11bab23
  * config/win/mingw.mk
    + enable -static-libgcc for mingw 4.8 and upper.
      This way harbour dll won't depend on libgcc_s_sjlj-1.dll

  * package/mpkg_win_nightly.bat
    + HB_CPU=x86

  * package/winuni/RELNOTES.txt
    ! minor

  * src/rtl/tlabel.prg
  * utils/hbmk2/hbmk2.prg
    % minor optimizations

  * src/rdd/usrrdd/rdds/arrayrdd.prg
    % use HB_ISSTRING()

  * src/rtl/gete.c
  * src/rtl/philes53.c
    ! added missing HB_CLP_UNDOC guard for GETE() and FSETDEVMOD()

  * tests/clipper.ch
    + added more translations

  * tests/gtchars.prg
  * tests/tb1.prg
    * use hb_DispOutAt(), hb_DispBox()

config/win/mingw.mk
@@ -40,6 +40,12 @@ ifeq ($(filter $(HB_COMPILER_VER),0209 0304 0400),)
 endif
 endif
 
+ifneq ($(HB_COMPILER_VER),)
+   ifeq ($(filter $(HB_COMPILER_VER),0209 0304 0400 0401 0402 0403 0404 0405 0406 0407),)
+      DFLAGS += -static-libgcc
+   endif
+endif
+
 # It is also supported by official mingw 4.4.x and mingw64 4.4.x,
 # but not supported by mingw tdm 4.4.x, so I only enable it on or
 # above 4.5.0.

package/mpkg_win_nightly.bat
@@ -70,6 +70,7 @@ echo ! Building Harbour...
 setlocal
 echo ! Setting environment for using MinGW GCC
 set PATH=%_HB_DIR_COMP%mingw\bin
+set HB_CPU=x86
 win-make clean install %_HB_MAKE_OPTION% > "%~dp0harbour-nightly-win-mingw-log.txt" 2>&1
 if errorlevel 1 goto _EXIT
 endlocal

src/rdd/usrrdd/rdds/arrayrdd.prg
@@ -45,11 +45,9 @@
  *
  */
 
-/*
- * This is a Array RDD, or Memory RDD.
- * It works only in memory and actually supports standard dbf commands
- * except relations
- */
+/* This is a Array RDD, or Memory RDD.
+   It works only in memory and actually supports standard dbf commands
+   except relations. */
 
 #include "dbinfo.ch"
 #include "dbstruct.ch"
@@ -60,7 +58,7 @@
 #include "rddsys.ch"
 
 #define THROW( oErr )      ( Eval( ErrorBlock(), oErr ), Break( oErr ) )
-#define LEFTEQUAL( l, r )  iif( ValType( l ) $ "CM", hb_LeftEq( l, r ), l == r )
+#define LEFTEQUAL( l, r )  iif( HB_ISSTRING( l ), hb_LeftEq( l, r ), l == r )
 
 ANNOUNCE ARRAYRDD
 

src/rtl/gete.c
@@ -86,7 +86,9 @@ HB_FUNC( GETENV )
 }
 
 /* NOTE: Undocumented Clipper function. [vszakats] */
+#ifdef HB_CLP_UNDOC
 HB_FUNC_TRANSLATE( GETE, GETENV )
+#endif
 
 /* NOTE: Harbour extended version of GetEnv(). The 2nd parameter
          can be used to specify a default value, returned if the

src/rtl/listbox.prg
@@ -109,7 +109,7 @@ CREATE CLASS ListBox FUNCTION HBListBox
    METHOD delItem( nPos )
    METHOD display()
    METHOD findText( cText, nPos, lCaseSensitive, lExact )
-   METHOD findData( cData, nPos, lCaseSensitive, lExact )  /* NOTE: Undocumented CA-Cl*pper method. */
+   METHOD findData( xData, nPos, lCaseSensitive, lExact )  /* NOTE: Undocumented CA-Cl*pper method. */
    METHOD getData( nPos )
    METHOD getItem( nPos )
    METHOD getText( nPos )
@@ -144,15 +144,15 @@ CREATE CLASS ListBox FUNCTION HBListBox
    METHOD message( cMessage ) SETGET
    METHOD right( nRight ) SETGET
    METHOD sBlock( bSBlock ) SETGET
-   METHOD style( cStyle ) SETGET                          /* NOTE: Undocumented CA-Cl*pper method. */
-   METHOD textValue() SETGET                              /* NOTE: Undocumented CA-Cl*pper method. */
+   METHOD style( cStyle ) SETGET                           /* NOTE: Undocumented CA-Cl*pper method. */
+   METHOD textValue() SETGET                               /* NOTE: Undocumented CA-Cl*pper method. */
    METHOD top( nTop ) SETGET
    METHOD topItem( nTopItem ) SETGET
    METHOD typeOut() SETGET
-   METHOD value() SETGET                                  /* NOTE: Undocumented CA-Cl*pper method. */
+   METHOD value() SETGET                                   /* NOTE: Undocumented CA-Cl*pper method. */
    METHOD vScroll( oVScroll ) SETGET
 
-   METHOD New( nTop, nLeft, nBottom, nRight, lDropDown )  /* NOTE: This method is a Harbour extension [vszakats] */
+   METHOD New( nTop, nLeft, nBottom, nRight, lDropDown )   /* NOTE: This method is a Harbour extension [vszakats] */
 
    PROTECTED:
 
@@ -207,7 +207,6 @@ METHOD delItem( nPos ) CLASS ListBox
          ELSEIF ::nValue > 0
             ::xBuffer := ::cTextValue
          ENDIF
-
       ENDIF
 
       IF ::nTopItem > ::nItemCount
@@ -348,21 +347,14 @@ METHOD findText( cText, nPos, lCaseSensitive, lExact ) CLASS ListBox
 
    RETURN nPosFound
 
-/* NOTE: Both Cl*pper and Harbour will RTE when search reaches
-         an item with a non-string, non-NIL value. The RTE will
-         be different than in Cl*pper, but will occur under the
-         same conditions. */
-METHOD findData( cData, nPos, lCaseSensitive, lExact ) CLASS ListBox
+/* NOTE: Both Cl*pper and Harbour may RTE when searching for
+         a different type than present in an item value. The RTE
+         will be different and in Cl*pper, but will occur under
+         the same conditions. */
+METHOD findData( xData, nPos, lCaseSensitive, lExact ) CLASS ListBox
 
    LOCAL nPosFound
    LOCAL bSearch
-
-#ifndef HB_CLP_STRICT
-   /* NOTE: Cl*pper will RTE if passed a non-string cData */
-   IF ! HB_ISSTRING( cData )
-      RETURN 0
-   ENDIF
-#endif
 
    hb_default( @nPos, 1 )
    hb_default( @lCaseSensitive, .T. )
@@ -372,15 +364,18 @@ METHOD findData( cData, nPos, lCaseSensitive, lExact ) CLASS ListBox
 
    IF lExact
       IF lCaseSensitive
-         bSearch := {| aItem | _LISTBOX_ITEMDATA( aItem ) == cData }
+         bSearch := {| aItem | _LISTBOX_ITEMDATA( aItem ) == xData }
       ELSE
-         bSearch := {| aItem | Lower( _LISTBOX_ITEMDATA( aItem ) ) == cData }
+         /* Cl*pper will also RTE here, if xData is not a string */
+         xData := Lower( xData )
+         bSearch := {| aItem | Lower( _LISTBOX_ITEMDATA( aItem ) ) == xData }
       ENDIF
    ELSE
       IF lCaseSensitive
-         bSearch := {| aItem | hb_LeftEq( _LISTBOX_ITEMDATA( aItem ), cData ) }
+         bSearch := {| aItem, xItemData | xItemData := _LISTBOX_ITEMDATA( aItem ), iif( HB_ISSTRING( xItemData ), hb_LeftEq( xItemData, xData ), xItemData == xData ) }
       ELSE
-         bSearch := {| aItem | hb_LeftEqI( _LISTBOX_ITEMDATA( aItem ), cData ) }
+         /* Cl*pper will also RTE here, if xData is not a string */
+         bSearch := {| aItem | hb_LeftEqI( _LISTBOX_ITEMDATA( aItem ), xData ) }
       ENDIF
    ENDIF
 

src/rtl/philes53.c
@@ -47,7 +47,7 @@
 #include "hbapi.h"
 #include "hbapifs.h"
 
-#ifdef HB_COMPAT_C53
+#if defined( HB_COMPAT_C53 ) && defined( HB_CLP_UNDOC )
 
 /* NOTE: Clipper 5.3 undocumented */
 

src/rtl/tlabel.prg
@@ -310,7 +310,7 @@ METHOD SampleLabels() CLASS HBLabelForm
       // Prompt for more
       DispOutAt( Row(), 0, __natMsg( _LF_SAMPLES ) + " (" + __natMsg( _LF_YN ) + ")" )
       cKey := hb_keyChar( Inkey( 0 ) )
-      DispOutAt( Row(), Col(), cKey )
+      DispOut( cKey )
       IF Row() == MaxRow()
          hb_Scroll( 0, 0, MaxRow(), MaxCol(), 1 )
          SetPos( MaxRow(), 0 )

utils/hbmk2/hbmk2.prg
@@ -12275,7 +12275,6 @@ STATIC FUNCTION ArchCompFilter( hbmk, cItem, cFileName )
 
    LOCAL cExpr := "hbmk_KEYW( hbmk, cFileName, '%1' )"
    LOCAL cExprWithValue := "hbmk_KEYW( hbmk, cFileName, '%1', '%2', '%3' )"
-   LOCAL tmp
 
    nEnd := 1
    WHILE .T.
@@ -12335,11 +12334,9 @@ STATIC FUNCTION ArchCompFilter( hbmk, cItem, cFileName )
                   ENDIF
                ELSE
                   IF ! Empty( cKeyword ) .AND. ! Empty( cValue )
-                     tmp := cExprWithValue
-                     tmp := hb_StrReplace( tmp, ;
+                     cFilterHarb += hb_StrReplace( cExprWithValue, ;
                         { "%1", "%2", "%3" }, ;
                         { cKeyword, cValue, cOperator } )
-                     cFilterHarb += tmp
                      cKeyword := ""
                      cValue := NIL
                      cOperator := ""
@@ -12349,11 +12346,9 @@ STATIC FUNCTION ArchCompFilter( hbmk, cItem, cFileName )
          NEXT
          IF ! Empty( cKeyword )
             IF ! Empty( cValue )
-               tmp := cExprWithValue
-               tmp := hb_StrReplace( tmp, ;
+               cFilterHarb += hb_StrReplace( cExprWithValue, ;
                   { "%1", "%2", "%3" }, ;
                   { cKeyword, cValue, cOperator } )
-               cFilterHarb += tmp
             ELSE
                cFilterHarb += StrTran( cExpr, "%1", cKeyword )
             ENDIF

tests/clipper.ch
@@ -108,6 +108,8 @@
 #endif
 
 #xtranslate hb_DispBox( [<x,...>] )        => DispBox( <x> )
+#xtranslate hb_DispOutAt( [<x,...>] )      => DispOutAt( <x> )
+#xtranslate hb_DispOutAtBox( [<x,...>] )   => DispOutAt( <x> )
 
 #xtranslate hb_gtVersion( [<n>] )          => "DOS"
 

tests/gtchars.prg
@@ -71,7 +71,7 @@ STATIC PROCEDURE dspboxch( cStr )
    FOR i := 1 TO Len( cStr )
       r := Row()
       c := Col()
-      DispBox( r, c, r, c, SubStr( cStr, i, 1 ) )
+      hb_DispBox( r, c, r, c, SubStr( cStr, i, 1 ) )
       SetPos( r, c + 1 )
    NEXT
 

tests/tb1.prg
@@ -11,26 +11,26 @@
 #include "box.ch"
 
 #ifdef __HARBOUR__
-   #define _DRAW_1 hb_UTF8ToStr( "├" )
-   #define _DRAW_2 hb_UTF8ToStr( "┤" )
-   #define _DRAW_3 hb_UTF8ToStrBox( "┐ ┌─" )
-   #define _DRAW_4 hb_UTF8ToStrBox( "┘ └─" )
-   #define _DRAW_5 hb_UTF8ToStrBox( "│ │" )
-   #define _DRAW_6 hb_UTF8ToStrBox( "┐ ┌─┤HIDE├─" )
-   #define _DRAW_7 hb_UTF8ToStrBox( "╖ ╓─┤HIDE├─" )
-   #define _DRAW_8 hb_UTF8ToStrBox( "╜ ╙─" )
-   #define _DRAW_9 hb_UTF8ToStrBox( "║ ║" )
+   #define _DRAW_1  hb_UTF8ToStr( "├" )
+   #define _DRAW_2  hb_UTF8ToStr( "┤" )
+   #define _DRAW_3  hb_UTF8ToStrBox( "┐ ┌─" )
+   #define _DRAW_4  hb_UTF8ToStrBox( "┘ └─" )
+   #define _DRAW_5  hb_UTF8ToStrBox( "│ │" )
+   #define _DRAW_6  hb_UTF8ToStrBox( "┐ ┌─┤HIDE├─" )
+   #define _DRAW_7  hb_UTF8ToStrBox( "╖ ╓─┤HIDE├─" )
+   #define _DRAW_8  hb_UTF8ToStrBox( "╜ ╙─" )
+   #define _DRAW_9  hb_UTF8ToStrBox( "║ ║" )
 #else
-   #define _DRAW_1 Chr( 195 )
-   #define _DRAW_2 Chr( 180 )
-   #define _DRAW_3 Chr( 191 ) + " " + Chr( 218 ) + Chr( 196 )
-   #define _DRAW_4 Chr( 217 ) + " " + Chr( 192 ) + Chr( 196 )
-   #define _DRAW_5 Chr( 179 ) + " " + Chr( 179 )
-   #define _DRAW_6 Chr( 191 ) + " " + Chr( 218 ) + Chr( 196 ) + "HIDE" + Chr( 195 ) + Chr( 196 )
-   #define _DRAW_7 Chr( 183 ) + " " + Chr( 214 ) + Chr( 196 ) + "HIDE" + Chr( 195 ) + Chr( 196 )
-   #define _DRAW_8 Chr( 189 ) + " " + Chr( 211 ) + Chr( 196 )
-   #define _DRAW_9 Chr( 186 ) + " " + Chr( 186 )
-   #define hb_keyCode( n ) Asc( n )
+   #include "clipper.ch"
+   #define _DRAW_1  Chr( 195 )
+   #define _DRAW_2  Chr( 180 )
+   #define _DRAW_3  Chr( 191 ) + " " + Chr( 218 ) + Chr( 196 )
+   #define _DRAW_4  Chr( 217 ) + " " + Chr( 192 ) + Chr( 196 )
+   #define _DRAW_5  Chr( 179 ) + " " + Chr( 179 )
+   #define _DRAW_6  Chr( 191 ) + " " + Chr( 218 ) + Chr( 196 ) + "HIDE" + Chr( 195 ) + Chr( 196 )
+   #define _DRAW_7  Chr( 183 ) + " " + Chr( 214 ) + Chr( 196 ) + "HIDE" + Chr( 195 ) + Chr( 196 )
+   #define _DRAW_8  Chr( 189 ) + " " + Chr( 211 ) + Chr( 196 )
+   #define _DRAW_9  Chr( 186 ) + " " + Chr( 186 )
 #endif
 
 PROCEDURE Main()
@@ -58,18 +58,17 @@ PROCEDURE Main()
    MSetCursor( .T. )
 #endif
 
-
    CLS
 #ifdef HB_B_DOUBLE_SINGLE_UNI
-   DispBox( nTop, nLeft, nBottom, nRight, HB_B_DOUBLE_SINGLE_UNI, cColor )
+   hb_DispBox( nTop, nLeft, nBottom, nRight, HB_B_DOUBLE_SINGLE_UNI, cColor )
 #else
-   DispBox( nTop, nLeft, nBottom, nRight, B_DOUBLE_SINGLE, cColor )
+   hb_DispBox( nTop, nLeft, nBottom, nRight, B_DOUBLE_SINGLE, cColor )
 #endif
    oBrw := TBrowseNew( nTop + 1, nLeft + 1, nBottom - 1, nRight - 1 )
-   DispOutAt( nTop + 3,    nLeft,  _DRAW_1, cColor )
-   DispOutAt( nTop + 3,    nRight, _DRAW_2, cColor )
-   DispOutAt( nBottom - 2, nLeft,  _DRAW_1, cColor )
-   DispOutAt( nBottom - 2, nRight, _DRAW_2, cColor )
+   hb_DispOutAt( nTop + 3,    nLeft,  _DRAW_1, cColor )
+   hb_DispOutAt( nTop + 3,    nRight, _DRAW_2, cColor )
+   hb_DispOutAt( nBottom - 2, nLeft,  _DRAW_1, cColor )
+   hb_DispOutAt( nBottom - 2, nRight, _DRAW_2, cColor )
 
    oBrw:colorSpec := cColor
    oBrw:headSep := _DRAW_3