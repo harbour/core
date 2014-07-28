harbour/core

commit 759ed84b28a92f9cb3b68fb012298944b078a848

2014-07-19 15:02 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * config/global.mk   * utils/hbmk2/hbmk2.prg     + allow to choose between x86 and x86_64 targets when using       dual-target mingw distros, by setting the HB_CPU envvar       (or using -cpu= hbmk2 option)       It allows to use compiler autodetection with these dual       versions, even if the intended target platform differs       from the default target (the host platform) of the compiler       (f.e. using the 64-bit hosted build to create x86 apps,       where instead of "HB_COMPILER=mingw" + "HB_COMPILER_VER=0409"       now it's enough to set "HB_CPU=x86")       [EXPERIMENTAL]

utils/hbmk2/hbmk2.prg
@@ -2380,6 +2380,14 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
                                                         hb_DirSepToOS( hbmk[ _HBMK_cBUILD ] ) )
 #endif
                         hbmk[ _HBMK_cCOMP ] := tmp[ _COMPDET_cCOMP ]
+                        /* Allow override of compiler using the CPU setting for
+                           dual-mode mingw distros */
+                        DO CASE
+                        CASE hbmk[ _HBMK_cCOMP ] == "mingw" .AND. hbmk[ _HBMK_cCPU ] == "x86_64"
+                           hbmk[ _HBMK_cCOMP ] := "mingw64"
+                        CASE hbmk[ _HBMK_cCOMP ] == "mingw64" .AND. hbmk[ _HBMK_cCPU ] == "x86"
+                           hbmk[ _HBMK_cCOMP ] := "mingw"
+                        ENDCASE
                         IF Len( tmp ) >= _COMPDET_cCCPREFIX .AND. tmp[ _COMPDET_cCCPREFIX ] != NIL
                            hbmk[ _HBMK_cCCPREFIX ] := tmp[ _COMPDET_cCCPREFIX ]
                         ENDIF
@@ -2425,6 +2433,14 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
                IF hbmk[ _HBMK_cPLAT ] == tmp[ _COMPDETE_cPLAT ] .AND. ;
                   ! Empty( cPath_CompC := Eval( tmp[ _COMPDETE_bBlock ], tmp[ _COMPDETE_cCCPREFIX ], tmp[ _COMPDETE_cCCPATH ] ) )
                   hbmk[ _HBMK_cCOMP ] := tmp[ _COMPDETE_cCOMP ]
+                  /* Allow override of compiler using the CPU setting for
+                     dual-mode mingw distros */
+                  DO CASE
+                  CASE hbmk[ _HBMK_cCOMP ] == "mingw" .AND. hbmk[ _HBMK_cCPU ] == "x86_64"
+                     hbmk[ _HBMK_cCOMP ] := "mingw64"
+                  CASE hbmk[ _HBMK_cCOMP ] == "mingw64" .AND. hbmk[ _HBMK_cCPU ] == "x86"
+                     hbmk[ _HBMK_cCOMP ] := "mingw"
+                  ENDCASE
                   hbmk[ _HBMK_cCCPREFIX ] := tmp[ _COMPDETE_cCCPREFIX ]
                   hbmk[ _HBMK_cCCPATH ] := cPath_CompC
                   IF HB_ISEVALITEM( tmp[ _COMPDETE_bSetup ] )

config/global.mk
@@ -587,14 +587,22 @@ ifeq ($(HB_COMPILER),)
                      # tdragon DWARF-2 build (4.4.1)
                      HB_COMP_PATH := $(call find_in_path,gcc-dw2)
                      ifneq ($(HB_COMP_PATH),)
-                        HB_COMPILER := mingw
                         HB_CCSUFFIX := -dw2
+                        ifeq ($(HB_CPU),x86_64)
+                           HB_COMPILER := mingw64
+                        else
+                           HB_COMPILER := mingw
+                        endif
                      else
                         # Equation Solution build (requires x86_64 host)
                         HB_COMP_PATH := $(call find_in_path,x86_64-pc-mingw32-gcc)
                         ifneq ($(HB_COMP_PATH),)
-                           HB_COMPILER := mingw64
-                           HB_CPU := x86_64
+                           ifeq ($(HB_CPU),x86)
+                              HB_COMPILER := mingw
+                           else
+                              HB_COMPILER := mingw64
+                              HB_CPU := x86_64
+                           endif
                            ifneq ($(wildcard $(dir $(HB_COMP_PATH))x86_64-*-mingw32-gcc-4.9*),)
                               HB_COMPILER_VER := 0409
                            else
@@ -619,7 +627,12 @@ ifeq ($(HB_COMPILER),)
                            ifneq ($(HB_COMP_PATH),)
                               # Check for a mingw64-tdm gcc executable in the same directory
                               ifneq ($(wildcard $(dir $(HB_COMP_PATH))x86_64-w64-mingw32-gcc$(HB_HOST_BIN_EXT)),)
-                                 HB_COMPILER := mingw64
+                                 ifeq ($(HB_CPU),x86)
+                                    HB_COMPILER := mingw
+                                 else
+                                    HB_COMPILER := mingw64
+                                    HB_CPU := x86_64
+                                 endif
                                  ifneq ($(wildcard $(dir $(HB_COMP_PATH))x86_64-w64-mingw32-gcc-4.9*),)
                                     HB_COMPILER_VER := 0409
                                  else
@@ -640,7 +653,11 @@ ifeq ($(HB_COMPILER),)
                                  endif
                                  endif
                               else
-                                 HB_COMPILER := mingw
+                                 ifeq ($(HB_CPU),x86_64)
+                                    HB_COMPILER := mingw64
+                                 else
+                                    HB_COMPILER := mingw
+                                 endif
                                  ifneq ($(wildcard $(dir $(HB_COMP_PATH))mingw32-gcc-4.9*),)
                                     HB_COMPILER_VER := 0409
                                  else
@@ -837,9 +854,13 @@ ifeq ($(HB_COMPILER),)
                                                                      # mingw-w64 build
                                                                      HB_COMP_PATH := $(call find_in_path,i686-w64-mingw32-gcc)
                                                                      ifneq ($(HB_COMP_PATH),)
-                                                                        HB_COMPILER := mingw64
                                                                         HB_CCPREFIX := i686-w64-mingw32-
-                                                                        HB_CPU := x86_64
+                                                                        ifeq ($(HB_CPU),x86)
+                                                                           HB_COMPILER := mingw
+                                                                        else
+                                                                           HB_COMPILER := mingw64
+                                                                           HB_CPU := x86_64
+                                                                        endif
                                                                         ifneq ($(wildcard $(dir $(HB_COMP_PATH))$(HB_CCPREFIX)gcc-4.9*),)
                                                                            HB_COMPILER_VER := 0409
                                                                         else
@@ -864,9 +885,13 @@ ifeq ($(HB_COMPILER),)
                                                                            # mingw-w64 build
                                                                            HB_COMP_PATH := $(call find_in_path,x86_64-w64-mingw32-gcc)
                                                                            ifneq ($(HB_COMP_PATH),)
-                                                                              HB_COMPILER := mingw64
                                                                               HB_CCPREFIX := x86_64-w64-mingw32-
-                                                                              HB_CPU := x86_64
+                                                                              ifeq ($(HB_CPU),x86)
+                                                                                 HB_COMPILER := mingw
+                                                                              else
+                                                                                 HB_COMPILER := mingw64
+                                                                                 HB_CPU := x86_64
+                                                                              endif
                                                                               ifneq ($(wildcard $(dir $(HB_COMP_PATH))$(HB_CCPREFIX)gcc-4.9*),)
                                                                                  HB_COMPILER_VER := 0409
                                                                               else

commit 05d760679e43bfec055eb68e626dbe978beee44a

2014-07-29 01:12 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * utils/hbmk2/hbmk2.prg     ! fixed another regression since 759ed84b28a92f9cb3b68fb012298944b078a848       when the autodetection process was looking for the wrong Harbour libs     ! fixed a collateral bug which might have left CCPREFIX/CCSUFFIX       wrongly set after skipping autodetected targets due to missing       matching Harbour libs     ; Should fix https://github.com/vszakats/harbour-core/issues/31       please retest and confirm or resubmit

utils/hbmk2/hbmk2.prg
@@ -2384,9 +2384,13 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
                      ENDCASE
                      IF Len( tmp ) >= _COMPDET_cCCPREFIX .AND. tmp[ _COMPDET_cCCPREFIX ] != NIL
                         hbmk[ _HBMK_cCCPREFIX ] := tmp[ _COMPDET_cCCPREFIX ]
+                     ELSE
+                        hbmk[ _HBMK_cCCPREFIX ] := ""
                      ENDIF
                      IF Len( tmp ) >= _COMPDET_cCCSUFFIX .AND. tmp[ _COMPDET_cCCSUFFIX ] != NIL
                         hbmk[ _HBMK_cCCSUFFIX ] := tmp[ _COMPDET_cCCSUFFIX ]
+                     ELSE
+                        hbmk[ _HBMK_cCCSUFFIX ] := ""
                      ENDIF
                      tmp1 := hbmk[ _HBMK_cPLAT ]
                      IF Len( tmp ) >= _COMPDET_cPLAT .AND. tmp[ _COMPDET_cPLAT ] != NIL
@@ -2408,7 +2412,7 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
                      IF ! lDoSupportDetection .OR. ;
                         hb_DirExists( hb_PathNormalize( hb_DirSepAdd( hbmk[ _HBMK_cHB_INSTALL_PFX ] ) ) + "lib" + ;
                                                         hb_ps() + hbmk[ _HBMK_cPLAT ] + ;
-                                                        hb_ps() + tmp[ _COMPDET_cCOMP ] + ;
+                                                        hb_ps() + hbmk[ _HBMK_cCOMP ] + ;
                                                         hb_DirSepToOS( hbmk[ _HBMK_cBUILD ] ) )
 #endif
                         IF !( hbmk[ _HBMK_cPLAT ] == tmp1 ) .AND. hbmk[ _HBMK_lInfo ]