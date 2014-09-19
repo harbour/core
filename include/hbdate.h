427f6ed040d25ade224abb06bdfd14e6ea4e6b1c
Teo Fonrouge <tfonrouge@gmail.com>
2014-09-18 21:28:11 -0500

    Added HB_OS_DARWIN to determine HB_HAS_LOCALTIME_R, * reverted changes on hbdate.c

--- include/hbdate.h
+++ include/hbdate.h
@@ -153,8 +153,9 @@ HB_EXTERN_END
 #if ( defined( _POSIX_C_SOURCE ) || defined( _XOPEN_SOURCE ) || \
       defined( _BSD_SOURCE ) || defined( _SVID_SOURCE ) || \
       defined( HB_OS_SUNOS ) || defined( HB_OS_BEOS ) || \
-      defined( HB_OS_ANDROID ) ) && \
-   ! defined( HB_OS_DARWIN_5 ) && ! defined( HB_HAS_LOCALTIME_R )
+      defined( HB_OS_ANDROID ) || defined( HB_OS_DARWIN ) ) && \
+   ! defined( HB_OS_DARWIN_5 ) && ! defined( __WATCOMC__ ) && \
+   ! defined( HB_HAS_LOCALTIME_R )
 #  define HB_HAS_LOCALTIME_R
 #endif
 
