22ca481ea40e3b0751464b1637b1217d6b9b9ca3
Viktor Szakats <vszakats@users.noreply.github.com>
2014-12-12 20:07:45 +0100

    2014-12-12 20:07 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * rtl/hbtoken.c     ! reverted bad workaround in prev commit

--- src/rtl/hbtoken.c
+++ src/rtl/hbtoken.c
@@ -242,7 +242,6 @@ static HB_BOOL hb_tokenParam( int iParam, HB_SIZE nSkip,
       else if( hb_parl( iParam ) )
       {
          iFlags |= _HB_TOK_EOL_DELIM;
-         nDelim = 1;
       }
       else
       {
