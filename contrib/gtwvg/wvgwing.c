harbour/core

commit 19a1fdb52d3b675b6f13f9c14c683d346f3ecfce

2015-02-15 23:05 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/hbtip/mailsend.prg     * update tip_MailSend() to allow STARTTLS on port 587
  * contrib/hbtip/client.prg
    * move SSL connect code to  __tip_SSLConnectFD( ssl, fd )
    * change all actual ref of 'TLS' to 'SSL' to distinguish it from STARTTLS
    * rename TIPClient():EnableTLS() to :EnableSSL() [INCOMPATIBLE]
    ; correct terminology is SSL (or TLS) for port 465 mode and STARTTLS for port 587 mode [vszakats])

  * contrib/hbtip/smtpcli.prg
    + add TIPClientSMTP():StartTLS() to allow starting SSL after receiving the STARTTLS command only
    + add TIPClientSMTP():DetectSecurity() to detect authentication methods reading 250-xxx lines

  * contrib/hbtip/tests/email.prg
    + add STARTTLS examples [vszakats]

  ; manual recreation of upstream patch:
    2015-01-29 23:55 UTC+0100 Jean Lefebvre (jfl/at/mafact.com)
    https://github.com/harbour/core/commit/873c60d54fe8d22d69dadc4a9caf526e739b64fd
    Related:
    https://github.com/vszakats/harbour-core/issues/87
    https://github.com/vszakats/harbour-core/issues/84
    [vszakats]

contrib/hbtip/smtpcli.prg
@@ -4,6 +4,7 @@
  * Copyright 2003 Giancarlo Niccolai <gian@niccolai.ws>
  * Copyright 2007 Hannes Ziegler <hz AT knowlexbase.com> (sendMail())
  * Copyright 2009 Viktor Szakats (vszakats.net/harbour) (SSL support)
+ * Copyright 2015 Jean Lefebvre (STARTTLS support)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -52,8 +53,12 @@
 
 CREATE CLASS TIPClientSMTP FROM TIPClient
 
+   VAR lAuthLOGIN INIT .F.
+   VAR lAuthPLAIN INIT .F.
+   VAR lTLS       INIT .F.
+
    METHOD New( oUrl, xTrace, oCredentials, cClientHost )
-   METHOD Open( cUrl, lTLS )
+   METHOD Open( cUrl, lSSL )
    METHOD Close()
    METHOD Write( cData, nLen, bCommit )
    METHOD Mail( cFrom )
@@ -65,10 +70,12 @@ CREATE CLASS TIPClientSMTP FROM TIPClient
    METHOD SendMail( oTIpMail )
 
    /* Methods for smtp server that require login */
-   METHOD OpenSecure( cUrl, lTLS )
+   METHOD OpenSecure( cUrl, lSSL )
    METHOD Auth( cUser, cPass )      /* Auth by login method */
    METHOD AuthPlain( cUser, cPass ) /* Auth by plain method */
    METHOD ServerSuportSecure( lAuthPlain, lAuthLogin )
+   METHOD StartTLS()
+   METHOD DetectSecurity()
 
    HIDDEN:
 
@@ -88,7 +95,7 @@ METHOD New( oUrl, xTrace, oCredentials, cClientHost ) CLASS TIPClientSMTP
 
    RETURN Self
 
-METHOD Open( cUrl, lTLS ) CLASS TIPClientSMTP
+METHOD Open( cUrl, lSSL ) CLASS TIPClientSMTP
 
    IF ! ::super:Open( cUrl )
       RETURN .F.
@@ -98,18 +105,19 @@ METHOD Open( cUrl, lTLS ) CLASS TIPClientSMTP
       RETURN .F.
    ENDIF
 
-   IF hb_defaultValue( lTLS, .F. )
-      ::inetSendAll( ::SocketCon, "STARTTLS" + ::cCRLF )
-      IF ::GetOk()
-         ::EnableTLS( .T. )
-      ENDIF
+   IF hb_defaultValue( lSSL, .F. )
+      ::EnableSSL( .T. )
+      ::lAuthLogin := .T.
+      ::lAuthPlain := .T.
    ENDIF
 
    ::inetSendAll( ::SocketCon, "HELO " + iif( Empty( ::cClientHost ), "TIPClientSMTP", ::cClientHost ) + ::cCRLF )
 
    RETURN ::GetOk()
 
-METHOD OpenSecure( cUrl, lTLS ) CLASS TIPClientSMTP
+METHOD OpenSecure( cUrl, lSSL ) CLASS TIPClientSMTP
+
+   LOCAL lOk
 
    IF ! ::super:Open( cUrl )
       RETURN .F.
@@ -119,16 +127,21 @@ METHOD OpenSecure( cUrl, lTLS ) CLASS TIPClientSMTP
       RETURN .F.
    ENDIF
 
-   IF hb_defaultValue( lTLS, .F. )
-      ::inetSendAll( ::SocketCon, "STARTTLS" + ::cCRLF )
-      IF ::GetOk()
-         ::EnableTLS( .T. )
-      ENDIF
+   IF hb_defaultValue( lSSL, .F. )
+      ::EnableSSL( .T. )
+      ::lAuthLogin := .T.
+      ::lAuthPlain := .T.
    ENDIF
 
    ::inetSendAll( ::SocketCon, "EHLO " + iif( Empty( ::cClientHost ), "TIPClientSMTP", ::cClientHost ) + ::cCRLF )
 
-   RETURN ::GetOk()
+   lOk := ::DetectSecurity()
+
+   IF lOk .AND. ! lSSL
+      lOk := ::StartTLS()
+   ENDIF
+
+   RETURN lOk
 
 METHOD GetOk() CLASS TIPClientSMTP
 
@@ -138,6 +151,51 @@ METHOD GetOk() CLASS TIPClientSMTP
    ENDIF
 
    RETURN .T.
+
+METHOD StartTLS() CLASS TIPClientSMTP
+
+   ::inetSendAll( ::SocketCon, "STARTTLS" + ::cCRLF )
+
+   IF ::GetOk()
+      ::EnableSSL( .T. )
+      __tip_SSLConnectFD( ::ssl, hb_inetFD( ::SocketCon ) )
+      RETURN .T.
+   ENDIF
+
+   RETURN .F.
+
+METHOD DetectSecurity() CLASS TIPClientSMTP
+
+   LOCAL lOk
+
+   DO WHILE .T.
+      IF !( lOk := ::GetOk() )
+         EXIT
+      ENDIF
+      IF ::cReply == NIL
+         EXIT
+      ENDIF
+      IF "LOGIN" $ ::cReply
+         ::lAuthLogin := .T.
+      ENDIF
+      IF "PLAIN" $ ::cReply
+         ::lAuthPlain := .T.
+      ENDIF
+      IF ::HasSSL() .AND. "STARTTLS" $ ::cReply
+         ::lTLS := .T.
+         ::lAuthLogin := .T.
+         ::lAuthPlain := .T.
+      ENDIF
+
+      DO CASE
+      CASE hb_LeftEq( ::cReply, "250-" )
+         LOOP
+      CASE hb_LeftEq( ::cReply, "250 " )
+         EXIT
+      ENDCASE
+   ENDDO
+
+   RETURN lOk
 
 METHOD Close() CLASS TIPClientSMTP
 

contrib/hbtip/client.prg
@@ -6,6 +6,7 @@
  * Copyright 2007 Toninho@fwi (::nWrite to work like ::nRead)
  * Copyright 2009 Luiz Rafael Culik (luiz at xharbour dot com dot br) (Proxy connection)
  * Copyright 2009 Viktor Szakats (vszakats.net/harbour) (SSL support)
+ * Copyright 2015 Jean Lefebvre (STARTTLS support)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -97,7 +98,7 @@ CREATE CLASS TIPClient
 
    VAR exGauge              /* Gauge control; it can be a codeblock or a function pointer. */
 
-   VAR lTLS              INIT .F.
+   VAR lSSL              INIT .F.
 
    VAR lHasSSL           INIT tip_SSL()
    VAR ssl_ctx
@@ -115,7 +116,7 @@ CREATE CLASS TIPClient
    METHOD New( oUrl, xTrace, oCredentials )
    METHOD Open( cUrl )
 
-   METHOD EnableTLS( lEnable )
+   METHOD EnableSSL( lEnable )
 
    METHOD Read( nLen )
    METHOD ReadToFile( cFile, nMode, nSize )
@@ -204,13 +205,13 @@ METHOD New( oUrl, xTrace, oCredentials ) CLASS TIPClient
    ENDIF
 
    IF ::lHasSSL .AND. hb_AScan( aProtoSSL, oURL:cProto,,, .T. ) > 0
-      ::EnableTLS( .T. )
+      ::EnableSSL( .T. )
    ENDIF
 
    ::oUrl         := oUrl
    ::oCredentials := oCredentials
 
-   RETURN self
+   RETURN Self
 
 METHOD Open( cUrl ) CLASS TIPClient
 
@@ -238,11 +239,11 @@ METHOD Open( cUrl ) CLASS TIPClient
 
    RETURN .T.
 
-METHOD EnableTLS( lEnable ) CLASS TIPClient
+METHOD EnableSSL( lEnable ) CLASS TIPClient
 
    LOCAL lSuccess
 
-   IF ::lTLS == lEnable
+   IF ::lSSL == lEnable
       RETURN .T.
    ENDIF
 
@@ -250,14 +251,14 @@ METHOD EnableTLS( lEnable ) CLASS TIPClient
       IF ::lHasSSL
          ::ssl_ctx := SSL_CTX_new()
          ::ssl := SSL_new( ::ssl_ctx )
-         ::lTLS := .T.
+         ::lSSL := .T.
          lSuccess := .T.
       ELSE
          lSuccess := .F.
       ENDIF
    ELSE
       IF ::lHasSSL
-         ::lTLS := .F.
+         ::lSSL := .F.
          lSuccess := .T.
       ELSE
          lSuccess := .T.
@@ -335,7 +336,7 @@ METHOD Close() CLASS TIPClient
    ELSE
       nRet := hb_inetClose( ::SocketCon )
 
-      IF ::lTLS .AND. ::lHasSSL
+      IF ::lSSL .AND. ::lHasSSL
          SSL_shutdown( ::ssl )
          ::ssl := NIL
          ::ssl_ctx := NIL
@@ -388,7 +389,7 @@ METHOD Read( nLen ) CLASS TIPClient
       // read an amount of data
       cStr0 := Space( nLen )
 
-      IF ::lTLS .AND. ::lHasSSL
+      IF ::lSSL .AND. ::lHasSSL
          /* Getting around implementing the hack used in non-SSL branch for now.
             IMO the proper fix would have been done to hb_inetRecvAll(). [vszakats] */
          ::nLastRead := ::inetRecvAll( ::SocketCon, @cStr0, nLen )
@@ -549,7 +550,7 @@ METHOD inetSendAll( SocketCon, cData, nLen ) CLASS TIPClient
       nLen := hb_BLen( cData )
    ENDIF
 
-   IF ::lTLS
+   IF ::lSSL
       IF ::lHasSSL
 #if defined( _SSL_DEBUG_TEMP )
          ? "SSL_write()", cData
@@ -583,7 +584,7 @@ METHOD inetRecv( SocketCon, cStr1, len ) CLASS TIPClient
 
    LOCAL nRet
 
-   IF ::lTLS
+   IF ::lSSL
       IF ::lHasSSL
 #if defined( _SSL_DEBUG_TEMP )
          ? "SSL_read()"
@@ -607,7 +608,7 @@ METHOD inetRecvLine( SocketCon, nRet, size ) CLASS TIPClient
 
    LOCAL cRet
 
-   IF ::lTLS
+   IF ::lSSL
       IF ::lHasSSL
          nRet := hb_SSL_read_line( ::ssl, @cRet, size, ::nConnTimeout )
 #if defined( _SSL_DEBUG_TEMP )
@@ -635,7 +636,7 @@ METHOD inetRecvAll( SocketCon, cRet, size ) CLASS TIPClient
 
    LOCAL nRet
 
-   IF ::lTLS
+   IF ::lSSL
       IF ::lHasSSL
          nRet := hb_SSL_read_all( ::ssl, @cRet, size, ::nConnTimeout )
 #if defined( _SSL_DEBUG_TEMP )
@@ -663,7 +664,7 @@ METHOD inetErrorCode( SocketCon ) CLASS TIPClient
 
    LOCAL nRet
 
-   IF ::lTLS
+   IF ::lSSL
       IF ::lHasSSL
          nRet := iif( ::nSSLError == 0, 0, SSL_get_error( ::ssl, ::nSSLError ) )
       ELSE
@@ -686,7 +687,7 @@ METHOD inetErrorDesc( SocketCon ) CLASS TIPClient
    hb_default( @SocketCon, ::SocketCon )
 
    IF ! Empty( SocketCon )
-      IF ::lTLS
+      IF ::lSSL
          IF ::lHasSSL .AND. ::nSSLError != 0
             RETURN ERR_error_string( SSL_get_error( ::ssl, ::nSSLError ) )
          ENDIF
@@ -697,7 +698,7 @@ METHOD inetErrorDesc( SocketCon ) CLASS TIPClient
 
    RETURN ""
 
-/* BROKEN, should test number of parameters and act accordingly, see doc\inet.txt */
+/* BROKEN, should test number of parameters and act accordingly, see doc/inet.txt */
 METHOD PROCEDURE inetConnect( cServer, nPort, SocketCon ) CLASS TIPClient
 
    hb_inetConnect( cServer, nPort, SocketCon )
@@ -716,10 +717,8 @@ METHOD PROCEDURE inetConnect( cServer, nPort, SocketCon ) CLASS TIPClient
       ::InetRcvBufSize( SocketCon, ::nDefaultRcvBuffSize )
    ENDIF
 
-   IF ::lTLS .AND. ::lHasSSL
-      SSL_set_mode( ::ssl, HB_SSL_MODE_AUTO_RETRY )
-      SSL_set_fd( ::ssl, hb_inetFD( SocketCon ) )
-      SSL_connect( ::ssl )
+   IF ::lSSL .AND. ::lHasSSL
+      __tip_SSLConnectFD( ::ssl, hb_inetFD( SocketCon ) )
       /* TODO: Add error handling */
    ENDIF
 
@@ -814,3 +813,13 @@ METHOD SetProxy( cProxyHost, nProxyPort, cProxyUser, cProxyPassword ) CLASS TIPC
 
 FUNCTION tip_SSL()
    RETURN hb_IsFunction( "__HBEXTERN__HBSSL__" )
+
+FUNCTION __tip_SSLConnectFD( ssl, fd )
+
+   SSL_set_mode( ssl, HB_SSL_MODE_AUTO_RETRY )
+   SSL_set_fd( ssl, fd )
+   SSL_connect( ssl )
+
+   /* TODO: Add error handling */
+
+   RETURN .T.

contrib/hbtip/hbtip.hbx
@@ -68,6 +68,7 @@ DYNAMIC tip_URLDecode
 DYNAMIC tip_URLEncode
 DYNAMIC TUrl
 DYNAMIC __tip_FAttrToUmask
+DYNAMIC __tip_SSLConnectFD
 
 #if defined( __HBEXTREQ__ ) .OR. defined( __HBEXTERN__HBTIP__REQUEST )
    #uncommand DYNAMIC <fncs,...> => EXTERNAL <fncs>

contrib/hbtip/mailsend.prg
@@ -3,6 +3,7 @@
  *
  * Copyright 2007 Luiz Rafael Culik Guimaraes and Patrick Mast
  * Copyright 2009 Viktor Szakats (vszakats.net/harbour) (SSL support)
+ * Copyright 2015 Jean Lefebvre (STARTTLS support)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -71,13 +72,17 @@ FUNCTION hb_SendMail( ... )
    lNoAuth     -> Optional. Disable Autentication methods
    nTimeOut    -> Optional. Number os ms to wait default 10000 (10s)
    cReplyTo    -> Optional.
+   lSSl        -> Optional. Need SSL at connect time (TLS need this param set to False)
+   cSMTPPass   -> Optional.
+   cCharset    -> Optional.
+   cEncoding   -> Optional.
    cClientHost -> Optional. Domain name of the SMTP client in the format smtp.example.com OR client IP surrounded by brackets as in [200.100.100.5]
                             Note: This parameter is optional for backwards compatibility, but should be provided to comply with RFC 2812.
  */
 FUNCTION tip_MailSend( cServer, nPort, cFrom, xTo, xCC, xBCC, cBody, cSubject, ;
       aFiles, cUser, cPass, cPopServer, nPriority, lRead, ;
       xTrace, lPopAuth, lNoAuth, nTimeOut, cReplyTo, ;
-      lTLS, cSMTPPass, cCharset, cEncoding, cClientHost )
+      lSSL, cSMTPPass, cCharset, cEncoding, cClientHost )
 
    LOCAL cTmp
    LOCAL cTo
@@ -91,8 +96,6 @@ FUNCTION tip_MailSend( cServer, nPort, cFrom, xTo, xCC, xBCC, cBody, cSubject, ;
 
    LOCAL lBodyHTML     := .F.
    LOCAL lConnectPlain := .F.
-   LOCAL lAuthLogin    := .F.
-   LOCAL lAuthPlain    := .F.
    LOCAL lAuthTLS      := .F.
    LOCAL lConnect      := .T.
    LOCAL oPop
@@ -112,7 +115,7 @@ FUNCTION tip_MailSend( cServer, nPort, cFrom, xTo, xCC, xBCC, cBody, cSubject, ;
    hb_default( @lPopAuth, .T. )
    hb_default( @lNoAuth, .F. )
    hb_default( @nTimeOut, 10000 )
-   hb_default( @lTLS, .F. )
+   hb_default( @lSSL, .F. )
    hb_default( @cSMTPPass, cPass )
 
    // cTo
@@ -180,7 +183,7 @@ FUNCTION tip_MailSend( cServer, nPort, cFrom, xTo, xCC, xBCC, cBody, cSubject, ;
    IF HB_ISSTRING( cPopServer ) .AND. lPopAuth
 
       BEGIN SEQUENCE WITH {| oErr | Break( oErr ) }
-         oUrl1 := TUrl():New( iif( lTLS, "pop3s://", "pop://" ) + cUser + ":" + cPass + "@" + cPopServer + "/" )
+         oUrl1 := TUrl():New( iif( lSSL, "pop3s://", "pop://" ) + cUser + ":" + cPass + "@" + cPopServer + "/" )
          oUrl1:cUserid := StrTran( cUser, "&at;", "@" )
          oPop := TIPClientPOP():New( oUrl1, xTrace )
       RECOVER
@@ -195,7 +198,7 @@ FUNCTION tip_MailSend( cServer, nPort, cFrom, xTo, xCC, xBCC, cBody, cSubject, ;
    ENDIF
 
    BEGIN SEQUENCE WITH {| oErr | Break( oErr ) }
-      oUrl := TUrl():New( iif( lTLS, "smtps://", "smtp://" ) + cUser + iif( Empty( cSMTPPass ), "", ":" + cSMTPPass ) + "@" + cServer )
+      oUrl := TUrl():New( iif( lSSL, "smtps://", "smtp://" ) + cUser + iif( Empty( cSMTPPass ), "", ":" + cSMTPPass ) + "@" + cServer )
    RECOVER
       RETURN .F.
    END SEQUENCE
@@ -216,27 +219,11 @@ FUNCTION tip_MailSend( cServer, nPort, cFrom, xTo, xCC, xBCC, cBody, cSubject, ;
 
    oInmail:nConnTimeout := nTimeOut
 
-   IF ! lNoAuth .AND. oInMail:OpenSecure()
+   IF ! lNoAuth .AND. oInMail:OpenSecure( , lSSL )
 
-      DO WHILE .T.
-         IF ! oInMail:GetOk()
-            EXIT
-         ENDIF
-         DO CASE
-         CASE oInMail:cReply == NIL
-            EXIT
-         CASE "LOGIN" $ oInMail:cReply
-            lAuthLogin := .T.
-         CASE "PLAIN" $ oInMail:cReply
-            lAuthPlain := .T.
-         CASE oInMail:HasSSL() .AND. "STARTTLS" $ oInMail:cReply
-            lAuthTLS := .T.
-         CASE hb_LeftEq( oInMail:cReply, "250 " )
-            EXIT
-         ENDCASE
-      ENDDO
+      lAuthTLS := oInMail:lTLS
 
-      IF lAuthLogin
+      IF oInMail:lAuthLogin
          IF oInMail:Auth( cUser, cSMTPPass )
             lConnectPlain := .T.
          ELSE
@@ -244,7 +231,7 @@ FUNCTION tip_MailSend( cServer, nPort, cFrom, xTo, xCC, xBCC, cBody, cSubject, ;
          ENDIF
       ENDIF
 
-      IF lAuthPlain .AND. ! lConnect
+      IF oInMail:lAuthPlain .AND. ! lConnect
          IF ! oInMail:AuthPlain( cUser, cSMTPPass )
             lConnect := .F.
          ENDIF
@@ -275,13 +262,13 @@ FUNCTION tip_MailSend( cServer, nPort, cFrom, xTo, xCC, xBCC, cBody, cSubject, ;
       ENDIF
 
       DO WHILE .T.
-         IF hb_LeftEq( oInMail:cReply, "250 " )
-            EXIT
-         ENDIF
          IF ! oInMail:GetOk()
             EXIT
          ENDIF
          IF oInMail:cReply == NIL
+            EXIT
+         ENDIF
+         IF hb_LeftEq( oInMail:cReply, "250 " )
             EXIT
          ENDIF
       ENDDO

contrib/hbtip/tests/email.prg
@@ -12,8 +12,7 @@ REQUEST __HBEXTERN__HBSSL__
 PROCEDURE Main( cFrom, cPassword, cTo, cProvider )
 
    LOCAL cHost
-   LOCAL nPort := 465
-   LOCAL lTLS := .T.
+   LOCAL lSTARTTLS := .F.
 
    IF ! tip_SSL()
       ? "Error: Requires SSL support"
@@ -28,14 +27,22 @@ PROCEDURE Main( cFrom, cPassword, cTo, cProvider )
    cProvider := Lower( cProvider )
 
    DO CASE
+   CASE cProvider == "apple" .OR. "@icloud.com" $ cFrom .OR. "@mac.com" $ cFrom .OR. "@me.com" $ cFrom
+      cHost := "smtp.mail.me.com"; lSTARTTLS := .T.
    CASE cProvider == "fastmail" .OR. "@fastmail.com" $ cFrom .OR. "@fastmail.fm" $ cFrom
       cHost := "mail.messagingengine.com"
+   CASE cProvider == "gmx.net" .OR. "@gmx.net" $ cFrom .OR. "@gmx.ch" $ cFrom .OR. "@gmx.de" $ cFrom
+      cHost := "mail.gmx.net"; lSTARTTLS := .T.
    CASE cProvider == "google" .OR. "@gmail.com" $ cFrom .OR. "@googlemail.com" $ cFrom
       cHost := "smtp.gmail.com"
    CASE cProvider == "mail.ru" .OR. "@mail.ru" $ cFrom
       cHost := "smtp.mail.ru"
    CASE cProvider == "netease" .OR. "@163.com" $ cFrom
       cHost := "smtp.163.com"
+   CASE cProvider == "office365"
+      cHost := "smtp.office365.com"; lSTARTTLS := .T.
+   CASE cProvider == "outlook" .OR. "@outlook.com" $ cFrom .OR. "@hotmail.com" $ cFrom
+      cHost := "smtp-mail.outlook.com"; lSTARTTLS := .T.
    CASE cProvider == "sina" .OR. "@sina.com" $ cFrom
       cHost := "smtp.vip.sina.com"
    CASE cProvider == "uol" .OR. "@uol.com.br" $ cFrom
@@ -49,7 +56,7 @@ PROCEDURE Main( cFrom, cPassword, cTo, cProvider )
 
    ? tip_MailSend( ;
       cHost, ;
-      nPort, ;
+      iif( lSTARTTLS, 587, 465 ), ;
       cFrom, ;
       cTo, ;
       NIL /* CC */, ;
@@ -67,6 +74,6 @@ PROCEDURE Main( cFrom, cPassword, cTo, cProvider )
       NIL /* lNoAuth */, ;
       NIL /* nTimeOut */, ;
       NIL /* cReplyTo */, ;
-      lTLS )
+      ! lSTARTTLS )
 
    RETURN

commit 20c425da9695723053a2d9382b36d50d1702d730

2015-02-23 15:54 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/hbtip/smtpcli.prg     ! fix TIPClientSMTP():StartTLS() to check if the SSL backend is       linked before calling SSL functions       Should fix https://github.com/vszakats/harbour-core/issues/106       Regression from https://github.com/vszakats/harbour-core/commit/19a1fdb52d3b675b6f13f9c14c683d346f3ecfce
  * contrib/gtwvg/wvgwing.c
    * standardize var name

  * contrib/gtwvw/tooltip.c
  * extras/gfspell/spellc.c
    * cleanup

  * contrib/gtwvw/wvwutils.c
    ! WVW_ADDTOOLTIPEX() add missing TOOLINFO structure size

  * package/winuni/mpkg_win_uni.bat
    % delete alternate CPU target and other non-essential parts of
      mingw64/nixman distros

contrib/gtwvg/wvgwing.c
@@ -1033,17 +1033,17 @@ HB_FUNC( WVG_CREATETOOLTIPWINDOW )
 
    if( hwndTip )
    {
-      TOOLINFO toolInfo;
+      TOOLINFO ti;
 
-      memset( &toolInfo, 0, sizeof( toolInfo ) );
+      memset( &ti, 0, sizeof( ti ) );
 
-      toolInfo.cbSize   = sizeof( toolInfo );
-      toolInfo.hwnd     = hbwapi_par_raw_HWND( 1 );
-      toolInfo.uFlags   = TTF_IDISHWND | TTF_SUBCLASS;
-      toolInfo.uId      = ( UINT_PTR ) toolInfo.hwnd;
-      toolInfo.lpszText = ( LPTSTR ) TEXT( "" );
+      ti.cbSize   = sizeof( ti );
+      ti.hwnd     = hbwapi_par_raw_HWND( 1 );
+      ti.uFlags   = TTF_IDISHWND | TTF_SUBCLASS;
+      ti.uId      = ( UINT_PTR ) ti.hwnd;
+      ti.lpszText = ( LPTSTR ) TEXT( "" );
 
-      if( SendMessage( hwndTip, TTM_ADDTOOL, 0, ( LPARAM ) &toolInfo ) )
+      if( SendMessage( hwndTip, TTM_ADDTOOL, 0, ( LPARAM ) &ti ) )
       {
          hbwapi_ret_raw_HANDLE( hwndTip );
          return;
@@ -1055,18 +1055,18 @@ HB_FUNC( WVG_CREATETOOLTIPWINDOW )
 
 HB_FUNC( WVG_SETTOOLTIPTEXT )
 {
-   TOOLINFO toolInfo;
+   TOOLINFO ti;
    void *   hText;
 
-   memset( &toolInfo, 0, sizeof( toolInfo ) );
+   memset( &ti, 0, sizeof( ti ) );
 
-   toolInfo.cbSize   = sizeof( toolInfo );
-   toolInfo.hwnd     = hbwapi_par_raw_HWND( 1 );
-   toolInfo.uFlags   = TTF_IDISHWND | TTF_SUBCLASS;
-   toolInfo.uId      = ( UINT_PTR ) toolInfo.hwnd;
-   toolInfo.lpszText = ( LPTSTR ) HB_PARSTRDEF( 3, &hText, NULL );
+   ti.cbSize   = sizeof( ti );
+   ti.hwnd     = hbwapi_par_raw_HWND( 1 );
+   ti.uFlags   = TTF_IDISHWND | TTF_SUBCLASS;
+   ti.uId      = ( UINT_PTR ) ti.hwnd;
+   ti.lpszText = ( LPTSTR ) HB_PARSTRDEF( 3, &hText, NULL );
 
-   SendMessage( hbwapi_par_raw_HWND( 2 ), TTM_SETTOOLINFO, 0, ( LPARAM ) &toolInfo );
+   SendMessage( hbwapi_par_raw_HWND( 2 ), TTM_SETTOOLINFO, 0, ( LPARAM ) &ti );
 
    hb_strfree( hText );
 }

contrib/gtwvw/tooltip.c
@@ -97,7 +97,7 @@ HB_FUNC( WVW_SETTOOLTIP )
 
       memset( &ti, 0, sizeof( ti ) );
 
-      ti.cbSize = sizeof( TOOLINFO );
+      ti.cbSize = sizeof( ti );
       ti.hwnd   = wvw_win->hWnd;
       ti.uId    = WVW_ID_BASE_TOOLTIP + wvw_win->nWinId;
 
@@ -137,7 +137,7 @@ HB_FUNC( WVW_SETTOOLTIPTEXT )
    {
       TOOLINFO ti;
 
-      ti.cbSize = sizeof( TOOLINFO );
+      ti.cbSize = sizeof( ti );
       ti.hwnd   = wvw_win->hWnd;
       ti.uId    = 100000;
 

contrib/gtwvw/wvwutils.c
@@ -260,6 +260,7 @@ HB_FUNC( WVW_ADDTOOLTIPEX )  /* changed by MAG */
 
          memset( &ti, 0, sizeof( ti ) );
 
+         ti.cbSize   = sizeof( ti );
          ti.uFlags   = TTF_SUBCLASS | TTF_IDISHWND;
          ti.hwnd     = wvw_win->hWnd;
          ti.uId      = ( UINT_PTR ) wvw_win->hWnd;

contrib/hbtip/smtpcli.prg
@@ -156,7 +156,7 @@ METHOD StartTLS() CLASS TIPClientSMTP
 
    ::inetSendAll( ::SocketCon, "STARTTLS" + ::cCRLF )
 
-   IF ::GetOk()
+   IF ::GetOk() .AND. ::lHasSSL
       ::EnableSSL( .T. )
       __tip_SSLConnectFD( ::ssl, hb_inetFD( ::SocketCon ) )
       RETURN .T.

extras/gfspell/spellc.c
@@ -8,7 +8,6 @@ static const char * s_cSearch = "INEDTIERESTEON";
 static const char * s_cRepl   = "[\\]^_`a";
 
 /**
- *   Function: XForm()
  *    Purpose: Internal function to translate words to dictionary
  *  Arguments: cWord    - upper case word to format
  *    Returns: cXformed - translated word
@@ -72,7 +71,6 @@ HB_FUNC( XFORM )
 }
 
 /**
- *   Function: XUnForm()
  *    Purpose: Internal function to translate words from dictionary
  *  Arguments: cWord    - formatted word
  *    Returns: cXformed - unformatted word
@@ -127,7 +125,6 @@ HB_FUNC( XUNFORM )
 }
 
 /**
- *   Function: Sp_Rate()
  *     Syntax: cRating := Sp_Rate( cFound, cWord )
  *    Purpose: Returns a letter code indicating how similar the two
  *             words are.  This is primarily used to sort the list
@@ -185,7 +182,7 @@ HB_FUNC( SP_RATE )
  * Author: Clayton Neff
  * Copyright (c) 1992 by CoN Computer Consultants
  *
- * Revision: 1.0 Last Revised: 8/23/1992 at 16:47
+ * Revision: 1.0 Last Revised: 1992-08-23 at 16:47
  * Description: Original Creation.
  *---------------------------- ALL RIGHTS RESERVED ----------------------------*/
 

package/winuni/mpkg_win_uni.bat
@@ -85,6 +85,44 @@ if exist "%HB_DIR_MINGW%\bin\libgcc_s_sjlj-1.dll" xcopy /y "%HB_DIR_MINGW%\bin\l
 if exist "%HB_DIR_MINGW%\bin\libgcc_s_dw2-1.dll"  xcopy /y "%HB_DIR_MINGW%\bin\libgcc_s_dw2-1.dll"  "%HB_ABSROOT%bin\"
 if exist "%HB_DIR_MINGW%\bin\mingwm10.dll"        xcopy /y "%HB_DIR_MINGW%\bin\mingwm10.dll"        "%HB_ABSROOT%bin\"
 
+rem Delete stuff from compiler folder we don't need
+
+rmdir /q /s "%HB_ABSROOT%comp\mingw\etc\"
+rmdir /q /s "%HB_ABSROOT%comp\mingw\opt\"
+rmdir /q /s "%HB_ABSROOT%comp\mingw\share\"
+del /q /f   "%HB_ABSROOT%comp\mingw\bin\gdb*.exe"
+del /q /f   "%HB_ABSROOT%comp\mingw\bin\gfortran.exe"
+del /q /f   "%HB_ABSROOT%comp\mingw\bin\gnat*.exe"
+del /q /f   "%HB_ABSROOT%comp\mingw\bin\libgfortran-*.dll"
+del /q /f   "%HB_ABSROOT%comp\mingw\bin\libgnarl-*.dll"
+del /q /f   "%HB_ABSROOT%comp\mingw\bin\libgnat-*.dll"
+
+rem 32-bit hosted
+for /f %%i in ('dir /b "%HB_ABSROOT%comp\mingw\lib\gcc\i686-w64-mingw32\?.*"') do set _GCCVER=%%i
+rmdir /q /s "%HB_ABSROOT%comp\mingw\i686-w64-mingw32\lib64\"
+rmdir /q /s "%HB_ABSROOT%comp\mingw\lib\gcc\i686-w64-mingw32\%_GCCVER%\64\"
+rmdir /q /s "%HB_ABSROOT%comp\mingw\lib\gcc\i686-w64-mingw32\%_GCCVER%\adainclude\"
+rmdir /q /s "%HB_ABSROOT%comp\mingw\lib\gcc\i686-w64-mingw32\%_GCCVER%\adalib\"
+del /q /f   "%HB_ABSROOT%comp\mingw\bin\i686-w64-mingw32-gfortran.exe"
+del /q /f   "%HB_ABSROOT%comp\mingw\i686-w64-mingw32\lib\libgfortran-*.dll"
+del /q /f   "%HB_ABSROOT%comp\mingw\i686-w64-mingw32\lib\libgnarl-*.dll"
+del /q /f   "%HB_ABSROOT%comp\mingw\i686-w64-mingw32\lib\libgnat-*.dll"
+del /q /f   "%HB_ABSROOT%comp\mingw\libexec\gcc\i686-w64-mingw32\%_GCCVER%\f951.exe"
+del /q /f   "%HB_ABSROOT%comp\mingw\libexec\gcc\i686-w64-mingw32\%_GCCVER%\gnat1.exe"
+
+rem 64-bit hosted
+for /f %%i in ('dir /b "%HB_ABSROOT%comp\mingw\lib\gcc\x86_64-w64-mingw32\?.*"') do set _GCCVER=%%i
+rmdir /q /s "%HB_ABSROOT%comp\mingw\x86_64-w64-mingw32\lib32\"
+rmdir /q /s "%HB_ABSROOT%comp\mingw\lib\gcc\x86_64-w64-mingw32\%_GCCVER%\32\"
+rmdir /q /s "%HB_ABSROOT%comp\mingw\lib\gcc\x86_64-w64-mingw32\%_GCCVER%\adainclude\"
+rmdir /q /s "%HB_ABSROOT%comp\mingw\lib\gcc\x86_64-w64-mingw32\%_GCCVER%\adalib\"
+del /q /f   "%HB_ABSROOT%comp\mingw\bin\x86_64-w64-mingw32-gfortran.exe"
+del /q /f   "%HB_ABSROOT%comp\mingw\x86_64-w64-mingw32\lib\libgfortran-*.dll"
+del /q /f   "%HB_ABSROOT%comp\mingw\x86_64-w64-mingw32\lib\libgnarl-*.dll"
+del /q /f   "%HB_ABSROOT%comp\mingw\x86_64-w64-mingw32\lib\libgnat-*.dll"
+del /q /f   "%HB_ABSROOT%comp\mingw\libexec\gcc\x86_64-w64-mingw32\%_GCCVER%\f951.exe"
+del /q /f   "%HB_ABSROOT%comp\mingw\libexec\gcc\x86_64-w64-mingw32\%_GCCVER%\gnat1.exe"
+
 rem Create unified installer
 
 pushd