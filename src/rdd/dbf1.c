harbour/core

commit b52173a3f9dba66fcfd36c8f96b75c2b96db18d8

2014-03-14 23:03 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/hbssl/readme.txt     + documented -with openssl pkg build option
  * debian/control
    * moved each contrib dependency to separate line + sorted

debian/control
@@ -5,11 +5,21 @@ Maintainer: Luis Mayoral <mayoral@linuxadicto.org>
 Build-Depends: debhelper (>= 5.0.0),
  libjpeg62-dev, libpcre3-dev, libpng12-dev, zlib1g-dev,
  libslang2-dev, libncurses5-dev | libncursesw5-dev, libslang2-dev, libx11-dev,
- libsqlite3-dev, liballegro4.2-dev, libbz2-dev,
- libcairo2-dev, libcups2-dev, libexpat1-dev, libfreeimage-dev,
- libcurl4-openssl-dev | libcurl4-gnutls-dev, firebird2.0-dev | firebird2.1-dev,
- libgd2-noxpm-dev | libgd2-xpm-dev, libmagic-dev, libmariadbd-dev | libmysqlclient-dev,
- unixodbc-dev, libpq-dev, libssl-dev
+ liballegro4.2-dev,
+ libbz2-dev,
+ libcairo2-dev,
+ libcups2-dev,
+ libcurl4-openssl-dev | libcurl4-gnutls-dev,
+ libexpat1-dev,
+ libfreeimage-dev,
+ firebird2.0-dev | firebird2.1-dev,
+ libgd2-noxpm-dev | libgd2-xpm-dev,
+ libmagic-dev,
+ libmariadbd-dev | libmysqlclient-dev,
+ libpq-dev,
+ libsqlite3-dev,
+ libssl-dev
+ unixodbc-dev,
 
 Standards-Version: 3.6.2
 

commit 1e98abe60d0ad05747d2eb952d8be50721cc22f6

2015-04-29 00:50 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * debian/control     ! fix typo. Regression from b52173a3f9dba66fcfd36c8f96b75c2b96db18d8
  * src/rdd/dbf1.c
    ! fix bad typo. Regression from 54819d7ed75f3aee7faf3c75645c1ca7498356b6

  ; Both of these nicely spotted by Rolf (aka 'elch'). Thanks a lot!
    https://groups.google.com/d/msg/harbour-users/Efpv_JIi9eM/jvNUASPbnd0J

debian/control
@@ -18,8 +18,8 @@ Build-Depends: debhelper (>= 5.0.0),
  libmariadbd-dev | libmysqlclient-dev,
  libpq-dev,
  libsqlite3-dev,
- libssl-dev
- unixodbc-dev,
+ libssl-dev,
+ unixodbc-dev
 
 Standards-Version: 3.6.2
 

src/rdd/dbf1.c
@@ -2329,7 +2329,7 @@ static HB_ERRCODE hb_dbfGetValue( DBFAREAP pArea, HB_USHORT uiIndex, PHB_ITEM pI
          else
             hb_itemPutNIntLen( pItem, lVal, ( int ) pField->uiLen );
          break;
-
+      }
       case HB_FT_FLOAT:
          pszVal = ( char * ) pArea->pRecord + pArea->pFieldOffset[ uiIndex ];
          dVal = hb_strVal( pszVal, pField->uiLen );
@@ -2352,7 +2352,7 @@ static HB_ERRCODE hb_dbfGetValue( DBFAREAP pArea, HB_USHORT uiIndex, PHB_ITEM pI
                           ( int ) ( pField->uiLen - pField->uiDec - 1 ),
                           ( int ) pField->uiDec );
          break;
-      }
+
       case HB_FT_ANY:
          if( pField->uiLen == 3 )
             hb_itemPutDL( pItem, hb_sxPtoD( ( char * ) pArea->pRecord + pArea->pFieldOffset[ uiIndex ] ) );