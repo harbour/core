harbour/core

commit 7349602fcbd9b336183d2fa355454c60cb2f91e1

2014-04-01 12:33 UTC+0200 Przemyslaw Czerpak (druzus/at/poczta.onet.pl)   * src/rtl/filebuf.c     ! unlock HVM stack before locking local mutex and calling hb_fs*()       functions which also unlocks HVM. It fixes possible deadlock condition       when hb_gcAll( .T. ) is executed.
  * include/hbapi.h
  * src/vm/garbage.c
    ! changed mark function semantic.
      Adding support for user defined mark function I created race condition
      in MT GC code. It happens because blocks marked as deleted were not
      scanned by GC mark code so their subitems where not accessible.
      To fix it we have to change mark function semantic. Now mark function
      can be executed also for blocks currently deleted. It means that GC
      block destructor should clean references to just removed items and
      subblocks. The best place to make it is clearing pointers to GC blocks
      just after hb_itemRelease() or hb_gcRefFree().
      It is save to clean the reference just before hb_itemRelease() or
      hb_gcRefFree() but only for the single block passed to these functions.
      It is not save to clear reference to more then one block and then
      execute above functions.
    + check reference count after destructor execution for all blocks
      not only arrays - warning it may exploit some wrong C code.
    - removed not longer used hb_gcRefCheck() function.

  * include/hbvm.h
  * src/vm/hvm.c
    + added new internal function hb_vmLockForce()
    ! fixed to mark GC blocks with active threads

  * src/vm/thread.c
  * include/hbthread.h
    % modified sync mutexes used by xBase++ signal class emulation
      to not use item array internally
    * updated mutex destructor to new GC mark semantic
    ! mark GT items if GT is bound with thread item
    ! fixed mutex notify/subscribe code to not change GC items
      when HVM stack is unlocked
    ! use hb_vmLockForce() to eliminate potential deadlock
    + added new C function hb_threadEnterCriticalSectionGC().

contrib/hbmemio/memio.c
@@ -118,6 +118,11 @@ typedef struct _HB_MEMFS_FS
    PHB_MEMFS_FILE *  pFiles;
 } HB_MEMFS_FS, * PHB_MEMFS_FS;
 
+typedef struct _HB_MEMFS_DIRENTRY
+{
+   char *     szName;
+   HB_FOFFSET llSize;
+} HB_MEMFS_DIRENTRY, * PHB_MEMFS_DIRENTRY;
 
 static HB_MEMFS_FS s_fs;
 static HB_ERRCODE  s_error;
@@ -365,10 +370,11 @@ HB_MEMFS_EXPORT HB_BOOL hb_memfsRename( const char * szName, const char * szNewN
 
 HB_MEMFS_EXPORT PHB_ITEM hb_memfsDirectory( const char * pszDirSpec, const char * pszAttr )
 {
+   PHB_MEMFS_DIRENTRY pDirEn = NULL;
    char *   pszFree = NULL;
    PHB_ITEM pDirArray;
    HB_SIZE  nLen;
-   HB_ULONG ul;
+   HB_ULONG ulCount, ul;
 
    HB_SYMBOL_UNUSED( pszAttr );
 
@@ -387,25 +393,38 @@ HB_MEMFS_EXPORT PHB_ITEM hb_memfsDirectory( const char * pszDirSpec, const char 
       pszDirSpec = HB_OS_ALLFILE_MASK;
 
    HB_MEMFSMT_LOCK();
-   pDirArray = hb_itemArrayNew( s_fs.ulInodeCount );
+   ulCount = s_fs.ulInodeCount;
    nLen = 0;
-   for( ul = 0; ul < s_fs.ulInodeCount; ul++ )
+   if( ulCount )
    {
-      if( hb_strMatchFile( s_fs.pInodes[ ul ]->szName, pszDirSpec ) )
+      pDirEn = ( PHB_MEMFS_DIRENTRY ) hb_xgrab( ulCount * sizeof( HB_MEMFS_DIRENTRY ) );
+      for( ul = 0; ul < ulCount; ul++ )
       {
-         PHB_ITEM pSubarray = hb_arrayGetItemPtr( pDirArray, ++nLen );
-
-         hb_arrayNew    ( pSubarray, F_LEN );
-         hb_arraySetC   ( pSubarray, F_NAME, s_fs.pInodes[ ul ]->szName );
-         hb_arraySetNInt( pSubarray, F_SIZE, s_fs.pInodes[ ul ]->llSize );
-         hb_arraySetDL  ( pSubarray, F_DATE, 0 );
-         hb_arraySetC   ( pSubarray, F_TIME, "00:00:00" );
-         hb_arraySetC   ( pSubarray, F_ATTR, "" );
+         if( hb_strMatchFile( s_fs.pInodes[ ul ]->szName, pszDirSpec ) )
+         {
+            pDirEn[ nLen ].szName = hb_strdup( s_fs.pInodes[ ul ]->szName );
+            pDirEn[ nLen ].llSize = s_fs.pInodes[ ul ]->llSize;
+            nLen++;
+         }
       }
    }
    HB_MEMFSMT_UNLOCK();
-   hb_arraySize( pDirArray, nLen );
 
+   pDirArray = hb_itemArrayNew( nLen );
+   for( ul = 0; ( HB_SIZE ) ul < nLen; ul++ )
+   {
+      PHB_ITEM pSubarray = hb_arrayGetItemPtr( pDirArray, ++nLen );
+
+      hb_arrayNew    ( pSubarray, F_LEN );
+      hb_arraySetCPtr( pSubarray, F_NAME, pDirEn[ ul ].szName );
+      hb_arraySetNInt( pSubarray, F_SIZE, pDirEn[ ul ].llSize );
+      hb_arraySetDL  ( pSubarray, F_DATE, 0 );
+      hb_arraySetC   ( pSubarray, F_TIME, "00:00:00" );
+      hb_arraySetC   ( pSubarray, F_ATTR, "" );
+   }
+
+   if( pDirEn )
+      hb_xfree( pDirEn );
    if( pszFree )
       hb_xfree( pszFree );
 

contrib/hbcurl/core.c
@@ -567,13 +567,9 @@ static HB_GARBAGE_FUNC( PHB_CURL_release )
    /* Check if pointer is not NULL to avoid multiple freeing */
    if( hb_curl_ptr && *hb_curl_ptr )
    {
-      PHB_CURL hb_curl = *hb_curl_ptr;
-
-      /* set pointer to NULL to avoid multiple freeing */
-      *hb_curl_ptr = NULL;
-
       /* Destroy the object */
-      PHB_CURL_free( hb_curl, HB_TRUE );
+      PHB_CURL_free( *hb_curl_ptr, HB_TRUE );
+      *hb_curl_ptr = NULL;
    }
 }
 
@@ -642,13 +638,9 @@ HB_FUNC( CURL_EASY_CLEANUP )
 
       if( ph && *ph )
       {
-         PHB_CURL hb_curl = ( PHB_CURL ) *ph;
-
-         /* set pointer to NULL to avoid multiple freeing */
-         *ph = NULL;
-
          /* Destroy the object */
-         PHB_CURL_free( hb_curl, HB_TRUE );
+         PHB_CURL_free( ( PHB_CURL ) *ph, HB_TRUE );
+         *ph = NULL;
       }
    }
    else

contrib/hbexpat/core.c
@@ -678,11 +678,9 @@ static HB_GARBAGE_FUNC( PHB_EXPAT_release )
    {
       PHB_EXPAT hb_expat = *hb_expat_ptr;
 
-      /* set pointer to NULL to avoid multiple freeing */
-      *hb_expat_ptr = NULL;
-
       /* Destroy the object */
       PHB_EXPAT_free( hb_expat, HB_TRUE );
+      *hb_expat_ptr = NULL;
    }
 }
 
@@ -810,11 +808,9 @@ HB_FUNC( XML_PARSERFREE )
       {
          PHB_EXPAT hb_expat = ( PHB_EXPAT ) *ph;
 
-         /* set pointer to NULL to avoid multiple freeing */
-         *ph = NULL;
-
          /* Destroy the object */
          PHB_EXPAT_free( hb_expat, HB_TRUE );
+         *ph = NULL;
       }
    }
    else

contrib/hbxpp/dllx.c
@@ -62,7 +62,6 @@
 typedef struct
 {
    PHB_ITEM pLibraryHandle;
-   HB_BOOL  bFreeLibrary; /* Free library handle on destroy? */
    int      iFuncFlags;
    void *   pFunctionPtr; /* Function Address */
 } HB_DLLEXEC, * PHB_DLLEXEC;
@@ -71,8 +70,12 @@ static HB_GARBAGE_FUNC( _DLLUnload )
 {
    PHB_DLLEXEC xec = ( PHB_DLLEXEC ) Cargo;
 
-   if( xec->pLibraryHandle && xec->bFreeLibrary )
+   if( xec->pLibraryHandle )
    {
+      /* by default we do not free library in pLibraryHandle destructor
+       * (look at hb_libRelease() in dynlibhb.c). If you want to free
+       * library call here hb_libFree( xec->pLibraryHandle ).
+       */
       hb_gcRefFree( xec->pLibraryHandle );
       xec->pLibraryHandle = NULL;
    }
@@ -105,14 +108,17 @@ HB_FUNC( DLLCALL )
    PHB_ITEM pLibraryHandle = NULL;
    HB_BOOL  bFreeLibrary   = HB_FALSE;
 
-   if( HB_IS_STRING( pLibrary ) )
+   if( pLibrary )
    {
-      pLibraryHandle = hb_libLoad( pLibrary, NULL );
-      if( pLibraryHandle )
-         bFreeLibrary = HB_TRUE;
+      if( HB_IS_STRING( pLibrary ) )
+      {
+         pLibraryHandle = hb_libLoad( pLibrary, NULL );
+         if( pLibraryHandle )
+            bFreeLibrary = HB_TRUE;
+      }
+      else if( hb_libHandle( pLibrary ) )
+         pLibraryHandle = pLibrary;
    }
-   else if( hb_libHandle( pLibrary ) )
-      pLibraryHandle = pLibrary;
 
    if( pLibraryHandle )
    {
@@ -143,41 +149,46 @@ HB_FUNC( DLLCALL )
 
 HB_FUNC( DLLPREPARECALL )
 {
-   PHB_DLLEXEC  xec      = ( PHB_DLLEXEC ) hb_gcAllocate( sizeof( HB_DLLEXEC ), &s_gcDllFuncs );
-   PHB_ITEM     pLibrary = hb_param( 1, HB_IT_ANY );
+   PHB_ITEM pLibrary       = hb_param( 1, HB_IT_ANY );
+   PHB_ITEM pLibraryHandle = NULL;
+   HB_BOOL  bFreeLibrary   = HB_FALSE;
    const char * pszErrorText;
 
-   memset( xec, 0, sizeof( HB_DLLEXEC ) );
-
-   if( HB_IS_STRING( pLibrary ) )
+   if( pLibrary )
    {
-      xec->pLibraryHandle = hb_libLoad( pLibrary, NULL );
-      if( xec->pLibraryHandle )
+      if( HB_IS_STRING( pLibrary ) )
       {
-         hb_gcRefInc( xec->pLibraryHandle );
-         xec->bFreeLibrary = HB_TRUE;
+         pLibraryHandle = hb_libLoad( pLibrary, NULL );
+         if( pLibraryHandle )
+            bFreeLibrary = HB_TRUE;
       }
-   }
-   else if( hb_libHandle( pLibrary ) )
-   {
-      xec->pLibraryHandle = pLibrary;
-      hb_gcRefInc( xec->pLibraryHandle );
+      else if( hb_libHandle( pLibrary ) )
+         pLibraryHandle = pLibrary;
    }
 
-   if( xec->pLibraryHandle )
+   if( pLibraryHandle )
    {
-      xec->pFunctionPtr = hb_libSymAddr( xec->pLibraryHandle, hb_parcx( 3 ) );
-      if( xec->pFunctionPtr )
+      void * pFunctionPtr = hb_libSymAddr( pLibraryHandle, hb_parcx( 3 ) );
+
+      if( pFunctionPtr )
       {
-         int iXPPFlags = hb_parni( 2 );
+         int iXPPFlags = hb_parni( 2 ), iFuncFlags = 0;
+         PHB_DLLEXEC xec;
 
          if( ( iXPPFlags & DLL_CDECL ) != 0 )
-            xec->iFuncFlags |= HB_DYN_CALLCONV_CDECL;
+            iFuncFlags |= HB_DYN_CALLCONV_CDECL;
          if( ( iXPPFlags & DLL_STDCALL ) != 0 )
-            xec->iFuncFlags |= HB_DYN_CALLCONV_STDCALL;
+            iFuncFlags |= HB_DYN_CALLCONV_STDCALL;
          if( ( iXPPFlags & DLL_SYSTEM ) != 0 )
-            xec->iFuncFlags |= HB_DYN_CALLCONV_SYSCALL;
+            iFuncFlags |= HB_DYN_CALLCONV_SYSCALL;
 
+         if( !bFreeLibrary )
+            hb_gcRefInc( pLibraryHandle );
+
+         xec = ( PHB_DLLEXEC ) hb_gcAllocate( sizeof( HB_DLLEXEC ), &s_gcDllFuncs );
+         xec->pLibraryHandle = pLibraryHandle;
+         xec->iFuncFlags     = iFuncFlags;
+         xec->pFunctionPtr   = pFunctionPtr;
          hb_retptrGC( xec );
          return;
       }
@@ -186,7 +197,8 @@ HB_FUNC( DLLPREPARECALL )
    else
       pszErrorText = HB_ISCHAR( 1 ) ? "Invalid library name" : "Invalid library handle";
 
-   hb_gcFree( xec );
+   if( bFreeLibrary )
+      hb_libFree( pLibraryHandle );
 
    hb_errRT_BASE( EG_ARG, 2010, pszErrorText, HB_ERR_FUNCNAME, HB_ERR_ARGS_BASEPARAMS );
 }

contrib/xhb/hbserv.c
@@ -163,7 +163,7 @@ static void s_signalHandler( int sig, siginfo_t * info, void * v )
    #endif
 
    /* let's find the right signal handler. */
-   hb_threadEnterCriticalSection( &s_ServiceMutex );
+   hb_threadEnterCriticalSectionGC( &s_ServiceMutex );
 
    /* avoid working if PRG signal handling has been disabled */
    if( ! bSignalEnabled )
@@ -417,7 +417,7 @@ static LONG s_signalHandler( int type, int sig, PEXCEPTION_RECORD exc )
    int      iRet;
 
    /* let's find the right signal handler. */
-   hb_threadEnterCriticalSection( &s_ServiceMutex );
+   hb_threadEnterCriticalSectionGC( &s_ServiceMutex );
 
    /* avoid working if PRG signal handling has been disabled */
    if( ! bSignalEnabled )
@@ -885,7 +885,7 @@ HB_FUNC( HB_PUSHSIGNALHANDLER )
       s_signalHandlersInit();
    }
 
-   hb_threadEnterCriticalSection( &s_ServiceMutex );
+   hb_threadEnterCriticalSectionGC( &s_ServiceMutex );
 
    hb_arrayAddForward( sp_hooks, pHandEntry );
 
@@ -901,7 +901,7 @@ HB_FUNC( HB_POPSIGNALHANDLER )
 
    if( sp_hooks != NULL )
    {
-      hb_threadEnterCriticalSection( &s_ServiceMutex );
+      hb_threadEnterCriticalSectionGC( &s_ServiceMutex );
 
       nLen = hb_arrayLen( sp_hooks );
       if( nLen > 0 )

include/hbapi.h
@@ -607,7 +607,6 @@ extern void       hb_vmIsStackRef( void ); /* hvm.c - mark all local variables a
 extern void       hb_vmIsStaticRef( void ); /* hvm.c - mark all static variables as used */
 extern void       hb_gcReleaseAll( void ); /* release all memory blocks unconditionally */
 
-extern void       hb_gcRefCheck( void * pBlock ); /* Check if block still cannot be access after destructor execution */
 extern HB_COUNTER hb_gcRefCount( void * pAlloc );  /* return number of references */
 
 #if 0

include/hbthread.h
@@ -375,6 +375,7 @@ extern HB_EXPORT HB_BOOL     hb_atomic_dec( volatile HB_COUNTER * pCounter ); /*
 
 /* Critical sections or fast non recursive MUTEXes */
 extern HB_EXPORT void     hb_threadEnterCriticalSection( HB_CRITICAL_T * critical );
+extern HB_EXPORT void     hb_threadEnterCriticalSectionGC( HB_CRITICAL_T * critical );
 extern HB_EXPORT void     hb_threadLeaveCriticalSection( HB_CRITICAL_T * critical );
 
 /* conditional variables */

include/hbvm.h
@@ -175,6 +175,7 @@ extern HB_EXPORT void     hb_vmPushItemRef( PHB_ITEM pItem ); /* push item refer
 
 extern HB_EXPORT HB_BOOL  hb_vmIsMt( void ); /* return HB_TRUE if HVM is compiled with thread support */
 extern HB_EXPORT void     hb_vmLock( void ); /* lock VM blocking GC execution by other threads */
+extern HB_EXPORT void     hb_vmLockForce( void ); /* lock VM blocking GC execution by other threads, ignore GC request */
 extern HB_EXPORT void     hb_vmUnlock( void ); /* unlock VM, allow GC execution */
 #ifdef _HB_API_INTERNAL_
 extern HB_EXPORT HB_BOOL  hb_vmSuspendThreads( HB_BOOL fWait ); /* (try to) stop all threads except current one */

commit 775ee9ed2fc4fd9bef177d34a88b0f4656a0a33d

2015-05-03 21:44 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/hbmemio/memio.c     ! fix memory corruption in *Directory() functions since       regression in 7349602fcbd9b336183d2fa355454c60cb2f91e1       Reported and fix suggested by Rolf:       https://groups.google.com/d/msg/harbour-devel/8gjH0fYrIhM/5RQjwck4EaYJ

contrib/hbmemio/memio.c
@@ -408,7 +408,7 @@ HB_MEMFS_EXPORT PHB_ITEM hb_memfsDirectory( const char * pszDirSpec, const char 
    pDirArray = hb_itemArrayNew( nLen );
    for( ul = 0; ( HB_SIZE ) ul < nLen; ul++ )
    {
-      PHB_ITEM pSubarray = hb_arrayGetItemPtr( pDirArray, ++nLen );
+      PHB_ITEM pSubarray = hb_arrayGetItemPtr( pDirArray, ul + 1 );
 
       hb_arrayNew    ( pSubarray, F_LEN );
       hb_arraySetCPtr( pSubarray, F_NAME, pDirEn[ ul ].szName );