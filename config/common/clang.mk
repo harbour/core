fc8c2dca6fbd9a4489b53a5f44532cf322bfa2d3
Viktor Szakats <vszakats@users.noreply.github.com>
2015-08-07 19:12:50 +0200

    2015-08-07 19:12 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com) [ci skip]   * config/common/clang.mk     * minor in comment
      * config/win/clang.mk
        * sync options with config/common/clang.mk

--- config/common/clang.mk
+++ config/common/clang.mk
@@ -35,7 +35,7 @@ ifneq ($(HB_BUILD_WARN),no)
    ifeq ($(filter $(HB_COMPILER_VER),0305),)
       CFLAGS += -Wno-reserved-id-macro
    endif
-   # these are potentially useful. -Wsign-conversion would require proper HB_SIZE/HB_ISIZ cleanup.
+   # These are potentially useful. -Wsign-conversion would require proper HB_SIZE/HB_ISIZ cleanup.
    CFLAGS += -Wno-sign-conversion -Wno-shorten-64-to-32 -Wno-conversion -Wno-bad-function-cast
 else
    CFLAGS += -Wmissing-braces -Wreturn-type -Wformat
--- config/win/clang.mk
+++ config/win/clang.mk
@@ -4,6 +4,10 @@ ifneq ($(HB_BUILD_WARN),no)
    CFLAGS += -Weverything
    CFLAGS += -Wno-padded -Wno-cast-align -Wno-float-equal -Wno-missing-prototypes
    CFLAGS += -Wno-disabled-macro-expansion -Wno-undef -Wno-unused-macros -Wno-variadic-macros -Wno-documentation
+   ifeq ($(filter $(HB_COMPILER_VER),0305),)
+      CFLAGS += -Wno-reserved-id-macro
+   endif
+   # These are potentially useful. -Wsign-conversion would require proper HB_SIZE/HB_ISIZ cleanup.
    CFLAGS += -Wno-sign-conversion -Wno-shorten-64-to-32 -Wno-conversion -Wno-bad-function-cast
    CFLAGS += -Wno-language-extension-token
 else
