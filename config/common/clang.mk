348f62a21a2ec6c9ae8dd474e639e4bb27935bbd
Viktor Szakats <vszakats@users.noreply.github.com>
2015-08-07 18:58:50 +0200

    2015-08-07 18:58 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * config/common/clang.mk     ! fix typo in recent condition

--- config/common/clang.mk
+++ config/common/clang.mk
@@ -32,7 +32,7 @@ ifneq ($(HB_BUILD_WARN),no)
    CFLAGS += -W -Weverything
    CFLAGS += -Wno-padded -Wno-cast-align -Wno-float-equal -Wno-missing-prototypes
    CFLAGS += -Wno-disabled-macro-expansion -Wno-undef -Wno-unused-macros -Wno-variadic-macros -Wno-documentation
-   ifneq ($(filter $(HB_COMPILER_VER),0305),)
+   ifeq ($(filter $(HB_COMPILER_VER),0305),)
       CFLAGS += -Wno-reserved-id-macro
    endif
    # these are potentially useful. -Wsign-conversion would require proper HB_SIZE/HB_ISIZ cleanup.
