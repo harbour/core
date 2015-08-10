harbour/core

commit 537f283743ebec5a6b78d5cb856620753aa80646

2015-07-27 14:26 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * bin/commit.hb   * config/postinst.hb   * contrib/hbcomm/tests/test.prg   * contrib/hbfimage/tests/test.prg   * contrib/hbmisc/hbedit.prg   * contrib/hbmysql/tests/dbf2mysq.prg   * contrib/hbnetio/utils/hbnetio/netiomgm.hb   * contrib/hbpgsql/tests/dbf2pg.prg   * contrib/hbtest/core.prg   * contrib/hbtip/client.prg   * contrib/hbtip/ftpcli.prg   * contrib/hbtip/mail.prg   * contrib/hbtip/tests/tipmail.prg   * contrib/hbtip/tests/tipmmail.prg   * contrib/hbtip/url.prg   * contrib/hbwin/win_err.prg   * contrib/hbwin/win_reg.prg   * contrib/xhb/tests/xml1.prg   * contrib/xhb/trpc.prg   * contrib/xhb/trpccli.prg   * ChangeLog.txt   * package/mpkg.hb   * src/rdd/dblist.prg   * src/rdd/dbstrux.prg   * src/rtl/adir.prg   * src/rtl/alert.prg   * src/rtl/dircmd.prg   * src/rtl/hbfilehi.prg   * src/rtl/hbini.prg   * src/rtl/tlabel.prg   * src/rtl/treport.prg   * utils/hbmk2/hbmk2.prg     ! fix several sloppy checks where Empty() were       used on strings while in fact a zero length       check would have been correct.       It fixes critical bugs in TIPClient():inetRecvLine()       and TIPClient():inetRecvAll() in SSL mode.       It also fixes places where valid file, buffer content,       username, filename or directory consisting only of       whitespace was ignored, considered an error or       resulted in using a default value.     ; Empty() is a "weasel-word" when used on strings,       should be used carefully and sparingly.
  * contrib/xhb/trpccli.prg
    ! fix TRPCClient():SetEncryption() to reset state properly
      when called with an invalid key

contrib/hbtip/mail.prg
@@ -556,7 +556,7 @@ METHOD attachFile( cFileName ) CLASS TIPMail
    LOCAL oAttach
    LOCAL nAttr
 
-   IF Empty( cContent )
+   IF hb_BLen( cContent ) == 0
       RETURN .F.
    ENDIF
 
@@ -577,7 +577,7 @@ METHOD detachFile( cPath ) CLASS TIPMail
    LOCAL cContent := ::getBody()
    LOCAL cFileName := ::getFileName()
 
-   IF Empty( cFileName )
+   IF Len( cFileName ) == 0
       RETURN .F.
    ENDIF
 

contrib/hbwin/win_reg.prg
@@ -119,7 +119,7 @@ FUNCTION win_regDelete( cRegPath, nRegSam )
 
    win_regPathSplit( cRegPath, @nHKEY, @cKey, @cEntry )
 
-   IF Empty( cEntry )
+   IF Len( cEntry ) == 0
       lRetVal := win_regDeleteKey( nHKEY, cKey )
    ELSE
       IF win_regOpenKeyEx( nHKEY, cKey, 0, hb_bitOr( KEY_SET_VALUE, hb_defaultValue( nRegSam, 0 ) ), @pKeyHandle )

src/rdd/dblist.prg
@@ -73,7 +73,7 @@ PROCEDURE __dbList( lOff, abEval, lAll, bFor, bWhile, nNext, nRecord, lRest, lTo
    IF ! Empty( lToPrint )  /* => hb_defaultValue( lToPrint, .F. ) */
       lOldPrinter := Set( _SET_PRINTER, .T. )
    ENDIF
-   IF ! Empty( cToFileName )
+   IF Len( cToFileName ) > 0
       lOldExtra := Set( _SET_EXTRA, .T. )
       cOldExtraFile := Set( _SET_EXTRAFILE, ;
                             iif( Set( _SET_DEFEXTENSIONS ), ;
@@ -88,8 +88,8 @@ PROCEDURE __dbList( lOff, abEval, lAll, bFor, bWhile, nNext, nRecord, lRest, lTo
       IF Empty( lAll ) .AND. ;  /* => hb_defaultValue( lAll, .F. ) */
          Empty( bFor ) .AND. ;
          Empty( bWhile ) .AND. ;
-         Empty( nNext ) .AND. ;
-         Empty( nRecord ) .AND. ;
+         Empty( nNext ) .AND. ;  /* => nNext == NIL .OR. nNext == 0 */
+         Empty( nRecord ) .AND. ;  /* => nRecord == NIL .OR. nRecord == 0 */
          Empty( lRest )  /* => hb_defaultValue( lRest, .F. ) */
 
          Eval( bOutBlock )
@@ -106,7 +106,7 @@ PROCEDURE __dbList( lOff, abEval, lAll, bFor, bWhile, nNext, nRecord, lRest, lTo
    IF ! Empty( lToPrint )  /* => hb_defaultValue( lToPrint, .F. ) */
       Set( _SET_PRINTER, lOldPrinter )
    ENDIF
-   IF ! Empty( cToFileName )
+   IF Len( cToFileName ) > 0
       Set( _SET_EXTRAFILE, cOldExtraFile )
       Set( _SET_EXTRA, lOldExtra )
    ENDIF

src/rdd/dbstrux.prg
@@ -118,7 +118,7 @@ FUNCTION __dbCreate( cFileName, cFileFrom, cRDD, lNew, cAlias, cCodePage, nConne
 
    BEGIN SEQUENCE
 
-      IF Empty( cFileFrom )
+      IF Len( cFileFrom ) == 0
 
          dbCreate( cFileName, { ;
             { "FIELD_NAME", "C", 10, 0 }, ;

src/rtl/tlabel.prg
@@ -150,7 +150,7 @@ METHOD PROCEDURE New( cLBLName, lPrinter, cAltFile, lNoConsole, bFor, ;
    lConsoleOn := Set( _SET_CONSOLE )
    Set( _SET_CONSOLE, ! lNoConsole .AND. lConsoleOn )
 
-   IF ! Empty( cAltFile )         // To file
+   IF Len( cAltFile ) > 0         // To file
       lExtraState := Set( _SET_EXTRA, .T. )
       cExtraFile  := Set( _SET_EXTRAFILE, cAltFile )
    ENDIF
@@ -201,7 +201,7 @@ METHOD PROCEDURE New( cLBLName, lPrinter, cAltFile, lNoConsole, bFor, ;
    Set( _SET_PRINTER, lPrintOn ) // Set the printer back to prior state
    Set( _SET_CONSOLE, lConsoleOn )  // Set the console back to prior state
 
-   IF ! Empty( cAltFile )           // Set extrafile back
+   IF Len( cAltFile ) > 0           // Set extrafile back
       Set( _SET_EXTRAFILE, cExtraFile )
       Set( _SET_EXTRA, lExtraState )
    ENDIF

src/rtl/treport.prg
@@ -230,7 +230,7 @@ METHOD PROCEDURE New( cFrmName AS STRING, ;
 
    ::lFormFeeds := lPrinter
 
-   IF ! Empty( cAltFile )        // To file
+   IF Len( cAltFile ) > 0       // To file
       lExtraState := Set( _SET_EXTRA, .T. )
       cExtraFile := Set( _SET_EXTRAFILE, cAltFile )
    ENDIF
@@ -413,7 +413,7 @@ METHOD PROCEDURE New( cFrmName AS STRING, ;
    Set( _SET_PRINTER, lPrintOn )    // Set the printer back to prior state
    Set( _SET_CONSOLE, lConsoleOn )  // Set the console back to prior state
 
-   IF ! Empty( cAltFile )           // Set extrafile back
+   IF Len( cAltFile ) > 0          // Set extrafile back
       Set( _SET_EXTRAFILE, cExtraFile )
       Set( _SET_EXTRA, lExtraState )
    ENDIF

bin/commit.hb
@@ -216,7 +216,7 @@ STATIC FUNCTION InstallHook( cDir, cHookName, cCommand )
       RETURN .T.
    ENDIF
 
-   IF Empty( cFile )
+   IF hb_BLen( cFile ) == 0
       cFile += "#!/bin/sh" + hb_eol()
    ENDIF
 

config/postinst.hb
@@ -529,7 +529,7 @@ STATIC PROCEDURE mk_hb_vfCopyFile( cSrc, cDst, lEOL, l644, lTS )
    ENDIF
    cDst := hb_FNameMerge( cDir, cName, cExt )
 
-   IF ! Empty( cFile := hb_MemoRead( cSrc ) ) .AND. ;
+   IF hb_BLen( cFile := hb_MemoRead( cSrc ) ) > 0 .AND. ;
       hb_MemoWrit( cDst, iif( hb_defaultValue( lEOL, .F. ), EOLConv( cFile ), cFile ) )
 
       IF hb_defaultValue( lTS, .F. )
@@ -688,7 +688,7 @@ STATIC FUNCTION LoadHBX( cFileName, hAll )
    LOCAL aDynamic := {}
    LOCAL cFilter
 
-   IF ! Empty( cFile := hb_MemoRead( cFileName ) )
+   IF hb_BLen( cFile := hb_MemoRead( cFileName ) ) > 0
 
       FOR EACH cFilter IN { ;
          "^DYNAMIC ([a-zA-Z0-9_]*)$", ;
@@ -804,7 +804,7 @@ STATIC PROCEDURE __hb_extern_get_exception_list( cInputName, /* @ */ aInclude, /
    aExclude := {}
    hDynamic := { => }
 
-   IF ! Empty( cFile := MemoRead( cInputName ) )
+   IF hb_BLen( cFile := MemoRead( cInputName ) ) > 0
       IF ! Empty( pRegex := hb_regexComp( "[\s]" + _HB_FUNC_INCLUDE_ + "[\s]([a-zA-Z0-9_].[^ \t\n\r]*)", .T., .T. ) )
          FOR EACH tmp IN hb_regexAll( pRegex, StrTran( cFile, Chr( 13 ) ),,,,, .T. )
             AAdd( aInclude, tmp[ 2 ] )

commit 4d2f075681836ab58893e424c58c13c1eba277bd

2015-08-10 16:40 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/hbwin/win_reg.prg     % optimizations
  * contrib/hbtip/mail.prg
    * protect for ::cBody == NIL (it was silently failing already,
      but made the check explicit for clarity)

  * src/rdd/dblist.prg
  * src/rdd/dbstrux.prg
  * src/rtl/tlabel.prg
  * src/rtl/treport.prg
    ! check for value type before checking string length
      regressions after https://github.com/vszakats/harbour-core/commit/537f283743ebec5a6b78d5cb856620753aa80646
      (these result in minor incompatibilities with Cl*pper
      when passing non-string, non-empty (invalid) values
      as filenames - in Harbour these are correctly handled
      as invalid)

contrib/hbtip/mail.prg
@@ -574,7 +574,7 @@ METHOD detachFile( cPath ) CLASS TIPMail
    LOCAL cContent := ::getBody()
    LOCAL cFileName := ::getFileName()
 
-   IF hb_BLen( cFileName ) == 0
+   IF hb_BLen( cFileName ) == 0 .OR. ::cBody == NIL
       RETURN .F.
    ENDIF
 

contrib/hbwin/win_reg.prg
@@ -76,20 +76,13 @@ PROCEDURE win_regPathSplit( cRegPath, /* @ */ nHKEY, /* @ */ cKey, /* @ */ cEntr
       ENDIF
 
       DO CASE
-      CASE hb_LeftEq( cHKEY, "HKCU"                  ) ; nHKEY := WIN_HKEY_CURRENT_USER
-      CASE hb_LeftEq( cHKEY, "HKLM"                  ) ; nHKEY := WIN_HKEY_LOCAL_MACHINE
-      CASE hb_LeftEq( cHKEY, "HKCR"                  ) ; nHKEY := WIN_HKEY_CLASSES_ROOT
-      CASE hb_LeftEq( cHKEY, "HKU"                   ) ; nHKEY := WIN_HKEY_USERS
-      CASE hb_LeftEq( cHKEY, "HKPD"                  ) ; nHKEY := WIN_HKEY_PERFORMANCE_DATA
-      CASE hb_LeftEq( cHKEY, "HKCC"                  ) ; nHKEY := WIN_HKEY_CURRENT_CONFIG
-      CASE hb_LeftEq( cHKEY, "HKDD"                  ) ; nHKEY := WIN_HKEY_DYN_DATA
-      CASE hb_LeftEq( cHKEY, "HKEY_CURRENT_USER"     ) ; nHKEY := WIN_HKEY_CURRENT_USER
-      CASE hb_LeftEq( cHKEY, "HKEY_LOCAL_MACHINE"    ) ; nHKEY := WIN_HKEY_LOCAL_MACHINE
-      CASE hb_LeftEq( cHKEY, "HKEY_CLASSES_ROOT"     ) ; nHKEY := WIN_HKEY_CLASSES_ROOT
-      CASE hb_LeftEq( cHKEY, "HKEY_USERS"            ) ; nHKEY := WIN_HKEY_USERS
-      CASE hb_LeftEq( cHKEY, "HKEY_PERFORMANCE_DATA" ) ; nHKEY := WIN_HKEY_PERFORMANCE_DATA
-      CASE hb_LeftEq( cHKEY, "HKEY_CURRENT_CONFIG"   ) ; nHKEY := WIN_HKEY_CURRENT_CONFIG
-      CASE hb_LeftEq( cHKEY, "HKEY_DYN_DATA"         ) ; nHKEY := WIN_HKEY_DYN_DATA
+      CASE hb_LeftEq( cHKEY, "HKCU" ) .OR. hb_LeftEq( cHKEY, "HKEY_CURRENT_USER"     ) ; nHKEY := WIN_HKEY_CURRENT_USER
+      CASE hb_LeftEq( cHKEY, "HKLM" ) .OR. hb_LeftEq( cHKEY, "HKEY_LOCAL_MACHINE"    ) ; nHKEY := WIN_HKEY_LOCAL_MACHINE
+      CASE hb_LeftEq( cHKEY, "HKCR" ) .OR. hb_LeftEq( cHKEY, "HKEY_CLASSES_ROOT"     ) ; nHKEY := WIN_HKEY_CLASSES_ROOT
+      CASE hb_LeftEq( cHKEY, "HKU"  ) .OR. hb_LeftEq( cHKEY, "HKEY_USERS"            ) ; nHKEY := WIN_HKEY_USERS
+      CASE hb_LeftEq( cHKEY, "HKPD" ) .OR. hb_LeftEq( cHKEY, "HKEY_PERFORMANCE_DATA" ) ; nHKEY := WIN_HKEY_PERFORMANCE_DATA
+      CASE hb_LeftEq( cHKEY, "HKCC" ) .OR. hb_LeftEq( cHKEY, "HKEY_CURRENT_CONFIG"   ) ; nHKEY := WIN_HKEY_CURRENT_CONFIG
+      CASE hb_LeftEq( cHKEY, "HKDD" ) .OR. hb_LeftEq( cHKEY, "HKEY_DYN_DATA"         ) ; nHKEY := WIN_HKEY_DYN_DATA
       ENDCASE
    ENDIF
 

src/rdd/dblist.prg
@@ -73,7 +73,7 @@ PROCEDURE __dbList( lOff, abEval, lAll, bFor, bWhile, nNext, nRecord, lRest, lTo
    IF ! Empty( lToPrint )  /* => hb_defaultValue( lToPrint, .F. ) */
       lOldPrinter := Set( _SET_PRINTER, .T. )
    ENDIF
-   IF hb_BLen( cToFileName ) > 0
+   IF HB_ISSTRING( cToFileName ) .AND. hb_BLen( cToFileName ) > 0
       lOldExtra := Set( _SET_EXTRA, .T. )
       cOldExtraFile := Set( _SET_EXTRAFILE, ;
                             iif( Set( _SET_DEFEXTENSIONS ), ;
@@ -106,7 +106,7 @@ PROCEDURE __dbList( lOff, abEval, lAll, bFor, bWhile, nNext, nRecord, lRest, lTo
    IF ! Empty( lToPrint )  /* => hb_defaultValue( lToPrint, .F. ) */
       Set( _SET_PRINTER, lOldPrinter )
    ENDIF
-   IF hb_BLen( cToFileName ) > 0
+   IF HB_ISSTRING( cToFileName ) .AND. hb_BLen( cToFileName ) > 0
       Set( _SET_EXTRAFILE, cOldExtraFile )
       Set( _SET_EXTRA, lOldExtra )
    ENDIF

src/rdd/dbstrux.prg
@@ -96,8 +96,9 @@ FUNCTION __dbCopyXStruct( cFileName )
 
    RETURN .T.
 
-/* NOTE: Compared to CA-Cl*pper, Harbour has two extra parameters
-         (cCodePage, nConnection). */
+/* NOTE: Compared to CA-Cl*pper, Harbour:
+         - has two extra parameters: <cCodePage>, <nConnection>
+         - non-string, non-empty <cFileFrom> values do not cause an RTE */
 
 FUNCTION __dbCreate( cFileName, cFileFrom, cRDD, lNew, cAlias, cCodePage, nConnection )
 
@@ -118,7 +119,7 @@ FUNCTION __dbCreate( cFileName, cFileFrom, cRDD, lNew, cAlias, cCodePage, nConne
 
    BEGIN SEQUENCE
 
-      IF hb_BLen( cFileFrom ) == 0
+      IF ! HB_ISSTRING( cFileFrom ) .OR. hb_BLen( cFileFrom ) == 0
 
          dbCreate( cFileName, { ;
             { "FIELD_NAME", "C", 10, 0 }, ;

src/rtl/tlabel.prg
@@ -150,7 +150,7 @@ METHOD PROCEDURE New( cLBLName, lPrinter, cAltFile, lNoConsole, bFor, ;
    lConsoleOn := Set( _SET_CONSOLE )
    Set( _SET_CONSOLE, ! lNoConsole .AND. lConsoleOn )
 
-   IF hb_BLen( cAltFile ) > 0     // To file
+   IF HB_ISSTRING( cAltFile ) .AND. hb_BLen( cAltFile ) > 0     // To file
       lExtraState := Set( _SET_EXTRA, .T. )
       cExtraFile  := Set( _SET_EXTRAFILE, cAltFile )
    ENDIF
@@ -201,7 +201,7 @@ METHOD PROCEDURE New( cLBLName, lPrinter, cAltFile, lNoConsole, bFor, ;
    Set( _SET_PRINTER, lPrintOn ) // Set the printer back to prior state
    Set( _SET_CONSOLE, lConsoleOn )  // Set the console back to prior state
 
-   IF hb_BLen( cAltFile ) > 0       // Set extrafile back
+   IF HB_ISSTRING( cAltFile ) .AND. hb_BLen( cAltFile ) > 0       // Set extrafile back
       Set( _SET_EXTRAFILE, cExtraFile )
       Set( _SET_EXTRA, lExtraState )
    ENDIF

src/rtl/treport.prg
@@ -230,7 +230,7 @@ METHOD PROCEDURE New( cFrmName AS STRING, ;
 
    ::lFormFeeds := lPrinter
 
-   IF hb_BLen( cAltFile ) > 0   // To file
+   IF HB_ISSTRING( cAltFile ) .AND. hb_BLen( cAltFile ) > 0   // To file
       lExtraState := Set( _SET_EXTRA, .T. )
       cExtraFile := Set( _SET_EXTRAFILE, cAltFile )
    ENDIF
@@ -413,7 +413,7 @@ METHOD PROCEDURE New( cFrmName AS STRING, ;
    Set( _SET_PRINTER, lPrintOn )    // Set the printer back to prior state
    Set( _SET_CONSOLE, lConsoleOn )  // Set the console back to prior state
 
-   IF hb_BLen( cAltFile ) > 0       // Set extrafile back
+   IF HB_ISSTRING( cAltFile ) .AND. hb_BLen( cAltFile ) > 0       // Set extrafile back
       Set( _SET_EXTRAFILE, cExtraFile )
       Set( _SET_EXTRA, lExtraState )
    ENDIF