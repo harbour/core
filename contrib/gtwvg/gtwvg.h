harbour/core

commit 59a8a754fb44a0e49cff8dfbd4cc5063e4ea64ec

2015-08-07 13:26 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/gtwvg/gtwgud.c   * contrib/gtwvg/gtwvg.h   * contrib/gtwvg/gtwvgd.c   * contrib/gtwvg/gtwvglo.h   * contrib/gtwvg/wvgwin.c   * contrib/gtwvw/hbgtwvw.h     * untangle and remove dependencies on _WIN32_WINNT     * do not override WINVER (might break things, we will see)

contrib/gtwvg/gtwvg.h
@@ -49,10 +49,6 @@
 
 #define HB_GT_NAME  WVG
 
-#ifndef WINVER
-   #define WINVER  0x0500
-#endif
-
 #define WVT_CHAR_QUEUE_SIZE         256
 #define WVT_MAX_TITLE_SIZE          128
 #define WVT_MAX_ROWS                256

contrib/gtwvg/gtwvgd.c
@@ -69,10 +69,6 @@
 #define WS_EX_COMPOSITED  0x02000000
 #endif
 
-#ifndef WS_EX_LAYERED
-#define WS_EX_LAYERED     0x00080000
-#endif
-
 #ifndef LWA_ALPHA
 #define LWA_ALPHA         0x00000002
 #endif
@@ -3921,7 +3917,7 @@ static HB_BOOL hb_gt_wvt_Info( PHB_GT pGT, int iType, PHB_GT_INFO pInfo )
 
                if( pWVT->hWnd )
                {
-#if ( _WIN32_WINNT >= 0x0500 ) && ! defined( HB_OS_WIN_CE )
+#if ! defined( HB_OS_WIN_CE )
                   if( s_guiData->pfnLayered )
                   {
                      SetWindowLongPtr( pWVT->hWnd,

contrib/gtwvg/gtwgud.c
@@ -64,6 +64,10 @@
 
 #include "gtwgu.h"
 
+#ifndef WS_EX_LAYERED
+#define WS_EX_LAYERED     0x00080000
+#endif
+
 static int s_GtId;
 static HB_GT_FUNCS SuperTable;
 #define HB_GTSUPER   ( &SuperTable )
@@ -1871,7 +1875,7 @@ static HB_BOOL hb_gt_wvt_Info( PHB_GT pGT, int iType, PHB_GT_INFO pInfo )
             case HB_GTS_FACTOR:
                if( pWVT->hWnd )
                {
-#if ( _WIN32_WINNT >= 0x0500 )
+#if ! defined( HB_OS_WIN_CE )
                   if( pWVT->pfnLayered )
                   {
                      SetWindowLong( pWVT->hWnd,

contrib/gtwvg/gtwvglo.h
@@ -244,6 +244,10 @@ typedef enum
 
 /* - */
 
+#ifndef WS_EX_LAYERED
+#define WS_EX_LAYERED  0x00080000
+#endif
+
 #ifndef INVALID_FILE_SIZE
 #define INVALID_FILE_SIZE  ( DWORD ) 0xFFFFFFFF
 #endif

contrib/gtwvg/wvgwin.c
@@ -257,7 +257,6 @@ HB_FUNC( WVG_GETCURRENTFONT )
 /* wvg_SetLayeredWindowAttributes( hWnd, nRGB, nOpacityFactor [0-255] ) */
 HB_FUNC( WVG_SETLAYEREDWINDOWATTRIBUTES )
 {
-#if ( _WIN32_WINNT >= 0x0500 )
    HINSTANCE h = GetModuleHandle( TEXT( "user32.dll" ) );
 
    if( h )
@@ -278,7 +277,6 @@ HB_FUNC( WVG_SETLAYEREDWINDOWATTRIBUTES )
       }
       FreeLibrary( h );
    }
-#endif
 }
 
 HB_FUNC( WVG_SENDTOOLBARMESSAGE )

contrib/gtwvw/hbgtwvw.h
@@ -61,10 +61,6 @@
 #define _WIN32_IE   0x0500
 #endif
 
-#ifndef WINVER
-#define WINVER      0x0500
-#endif
-
 #include "hbwapi.h"
 #include "hbwinole.h"
 

commit 7fbe4d946aa42e46dedfa43cdbd4eed86f97b586

2015-08-24 19:22 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/gtwvg/gtwvg.h   * contrib/gtwvg/gtwvgd.c     ! fix wce build regression after 59a8a754fb44a0e49cff8dfbd4cc5063e4ea64ec       Fixes https://github.com/vszakats/harbour-core/issues/157

contrib/gtwvg/gtwvg.h
@@ -317,4 +317,8 @@ extern HB_EXPORT void          hb_gt_wvt_PaintGObjects( PHB_GTWVT pWVT, RECT *uR
 
 HB_EXTERN_END
 
+#ifndef LWA_ALPHA
+#define LWA_ALPHA         0x00000002
+#endif
+
 #endif /* HB_WVG_H_ */

contrib/gtwvg/gtwvgd.c
@@ -69,10 +69,6 @@
 #define WS_EX_COMPOSITED  0x02000000
 #endif
 
-#ifndef LWA_ALPHA
-#define LWA_ALPHA         0x00000002
-#endif
-
 #ifndef SM_REMOTESESSION
 #define SM_REMOTESESSION  0x1000
 #endif