cf73ddd2bbd386456af096387d7328f0da03e5ca
Viktor Szakats <vszakats@users.noreply.github.com>
2015-08-29 19:51:37 +0200

    2015-08-29 19:50 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * package/getmingw.bat     + display expected SHA256 so it can be visually compared against       the actual downloaded ones     ! fix SHA256 check in prev

--- package/getmingw.bat
+++ package/getmingw.bat
@@ -5,20 +5,29 @@
 setlocal
 pushd "%~dp0"
 
-set "_MINGW_32=https://www.mirrorservice.org/sites/dl.sourceforge.net/pub/sourceforge/m/mi/mingw-w64/Toolchains targetting Win32/Personal Builds/mingw-builds/5.1.0/threads-posix/sjlj/i686-5.1.0-release-posix-sjlj-rt_v4-rev0.7z"
-set "_MINGW_64=https://www.mirrorservice.org/sites/dl.sourceforge.net/pub/sourceforge/m/mi/mingw-w64/Toolchains targetting Win64/Personal Builds/mingw-builds/5.1.0/threads-posix/sjlj/x86_64-5.1.0-release-posix-sjlj-rt_v4-rev0.7z"
+set "_MINGW_URL_32=https://www.mirrorservice.org/sites/dl.sourceforge.net/pub/sourceforge/m/mi/mingw-w64/Toolchains targetting Win32/Personal Builds/mingw-builds/5.1.0/threads-posix/sjlj/i686-5.1.0-release-posix-sjlj-rt_v4-rev0.7z"
+set  _MINGW_SUM_32=e9b69423a026a0106e7b34a6674bfd9894f78208fed91a5fdf547d11a775334a
+set "_MINGW_URL_64=https://www.mirrorservice.org/sites/dl.sourceforge.net/pub/sourceforge/m/mi/mingw-w64/Toolchains targetting Win64/Personal Builds/mingw-builds/5.1.0/threads-posix/sjlj/x86_64-5.1.0-release-posix-sjlj-rt_v4-rev0.7z"
+set  _MINGW_SUM_64=65b30e0469f03d0e219a1ebe022f135692ed6ecdaf4f9a86923ece7689157244
 
-set _URL_CLIENT=%_MINGW_32%
 if not exist "harbour.exe" (
    echo Error: This script has to be run from a Harbour binary installation.
    exit /b
 )
-harbour.exe -build 2>&1 | findstr "64-bit" > nul 2>&1 && set _URL_CLIENT=%_MINGW_64%
 
-if "%_URL_CLIENT%" == "%_MINGW_32%" echo Downloading 32-bit hosted dual MinGW...
-if "%_URL_CLIENT%" == "%_MINGW_64%" echo Downloading 64-bit hosted dual MinGW...
+harbour.exe -build 2>&1 | findstr "64-bit" > nul 2>&1
+if %ERRORLEVEL% equ 0 (
+   set _MINGW_URL=%_MINGW_URL_64%
+   set _MINGW_SUM=%_MINGW_SUM_64%
+) else (
+   set _MINGW_URL=%_MINGW_URL_32%
+   set _MINGW_SUM=%_MINGW_SUM_32%
+)
 
-set _DL_URL=%_URL_CLIENT%
+if "%_MINGW_URL%" == "%_MINGW_URL_32%" echo Downloading 32-bit hosted dual-target MinGW...
+if "%_MINGW_URL%" == "%_MINGW_URL_64%" echo Downloading 64-bit hosted dual-target MinGW...
+
+set _DL_URL=%_MINGW_URL%
 set _DL_DST=%TEMP%\mingw.7z
 set _TMP=%TEMP%\_webdl.tmp
 echo var http = new ActiveXObject(^"WinHttp.WinHttpRequest.5.1^");> "%_TMP%"
@@ -36,15 +45,17 @@ pushd ..
 set _TRG=%CD%\comp\
 popd
 
+echo Expected SHA256 hash: %_MINGW_SUM%
+
 :: Requires Windows 7 or OpenSSL in PATH
 certutil > nul 2>&1
 if %ERRORLEVEL% equ 0 (
-   certutil -hashfile "%_TRG%" SHA256
+   certutil -hashfile "%TEMP%\mingw.7z" SHA256
    goto _DONE
 )
 openssl version > nul 2>&1
 if %ERRORLEVEL% equ 0 (
-   openssl dgst -sha256 "%_TRG%"
+   openssl dgst -sha256 "%TEMP%\mingw.7z"
 )
 :_DONE
 
