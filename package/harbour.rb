44d52453b2fa0f7bbaeb55ef886d4fccfdbf32c6
Viktor Szakats <vszakats@users.noreply.github.com>
2015-09-21 15:51:19 +0200

    2015-09-21 15:50 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com) [ci skip]   * .travis.yml   * package/harbour.rb     + install openssl package (required in El Capitan)

--- package/harbour.rb
+++ package/harbour.rb
@@ -32,6 +32,7 @@ class Harbour < Formula
   depends_on "mariadb" => :optional
   depends_on :mysql => :optional
   depends_on "ncurses" => :optional
+  depends_on :openssl => :optional
   depends_on :postgresql => :optional
   depends_on "qt5" => :optional
   depends_on "unixodbc" => :optional
