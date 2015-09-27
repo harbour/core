841af799b0cd16b2052115c6ea29755fddf309ee
Viktor Szakats <vszakats@users.noreply.github.com>
2015-09-27 20:26:05 +0200

    2015-09-27 20:25 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com) [ci skip]   * package/mpkg_src.sh     ! silence shellcheck warning       Thanks to Tamas Tevesz for the patch!

--- package/mpkg_src.sh
+++ package/mpkg_src.sh
@@ -49,7 +49,7 @@ hb_collect_all_tree()
    do
       echo "$f" | awk '{ string=substr($0, 2); print string; }'
    done
-   find config -type f -exec echo {} \;
+   find config -type f -exec echo '{}' \;
 }
 
 hb_rmflst='yes'
