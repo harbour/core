72ac055d1018e8d6a68dfcc74c4acf221c873502
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-23 03:06:53 +0100

    use .json extension

--- package/mpkg_win.bat
+++ package/mpkg_win.bat
@@ -170,8 +170,8 @@ touch -c "%HB_ABSROOT%RELNOTES.txt" -r "%HB_ABSROOT%README.md"
 :: Create tag update JSON request
 :: https://developer.github.com/v3/git/refs/#update-a-reference
 
-for /f %%I in ('git rev-parse --verify HEAD') do echo {"sha":"%%I","force":true}> git_tag_patch.txt
-dos2unix git_tag_patch.txt
+for /f %%I in ('git rev-parse --verify HEAD') do echo {"sha":"%%I","force":true}> git_tag_patch.json
+dos2unix git_tag_patch.json
 
 :: Register build information
 
