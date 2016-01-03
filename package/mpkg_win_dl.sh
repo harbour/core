83ed73712ad858fee8f4943ee8901bce35696a88
Viktor Szakats <vszakats@users.noreply.github.com>
2016-01-04 00:08:20 +0100

    2016-01-04 00:07 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * appveyor.yml     ! typos
      * package/mpkg_win_dl.sh
        * be less verbose

--- package/mpkg_win_dl.sh
+++ package/mpkg_win_dl.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/sh
 
 # Copyright 2015-2016 Viktor Szakats (vszakats.net/harbour)
 
@@ -12,27 +12,31 @@ set -e
 
 # Dependencies for the Windows distro package
 
-curl -fsS -o pack.bin -L 'http://www.7-zip.org/a/7z1514-extra.7z'
-openssl dgst -sha256 pack.bin | grep -q 4fb7b51e93cabbede23281eae0d024a63f485dc339c85e20c305f328a76e90c0
-7z x -y -o7z pack.bin > /dev/null
+(
+   set -x
 
-curl -fsS -o pack.bin 'http://7zsfx.info/files/7zsd_150_2712.7z'
-openssl dgst -sha256 pack.bin | grep -q e5a2a05997553cde6318149951da1e449b0fd277a6e671ac06bfde8572754739
-7z x -y -o7zsfx pack.bin > /dev/null
+   curl -fsS -o pack.bin -L 'http://www.7-zip.org/a/7z1514-extra.7z'
+   openssl dgst -sha256 pack.bin | grep -q 4fb7b51e93cabbede23281eae0d024a63f485dc339c85e20c305f328a76e90c0
+   7z x -y -o7z pack.bin > /dev/null
 
-curl -fsS -o pack.bin -L --proto-redir =https 'https://fossies.org/windows/misc/upx391w.zip'
-openssl dgst -sha256 pack.bin | grep -q d7d4817f46d2616c209c46fb8bce44e4bec93ab5adef5e4dfc93ee879527be1b
-7z e -y -oupx pack.bin > /dev/null
+   curl -fsS -o pack.bin 'http://7zsfx.info/files/7zsd_150_2712.7z'
+   openssl dgst -sha256 pack.bin | grep -q e5a2a05997553cde6318149951da1e449b0fd277a6e671ac06bfde8572754739
+   7z x -y -o7zsfx pack.bin > /dev/null
 
-# curl -fsS -o pack.bin 'https://www.mirrorservice.org/sites/dl.sourceforge.net/pub/sourceforge/m/mi/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/5.3.0/threads-posix/dwarf/i686-5.3.0-release-posix-dwarf-rt_v4-rev0.7z'
-# openssl dgst -sha256 pack.bin | grep -q 6e067b2917583e9c654b611263d5d5e8c3215b67d76d55fa3f5f484f16f0f0b6
-# curl -fsS -o pack.bin 'https://www.mirrorservice.org/sites/dl.sourceforge.net/pub/sourceforge/m/mi/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.3.0/threads-posix/seh/x86_64-5.3.0-release-posix-seh-rt_v4-rev0.7z'
-# openssl dgst -sha256 pack.bin | grep -q 7f0e1f081d173b4a98bde3f9d1a90daf391219e6738f1f40120336b40545f090
-# curl -fsS -o pack.bin 'https://www.mirrorservice.org/sites/dl.sourceforge.net/pub/sourceforge/m/mi/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.3.0/threads-posix/sjlj/x86_64-5.3.0-release-posix-sjlj-rt_v4-rev0.7z'
-  curl -fsS -o pack.bin -L 'https://downloads.sourceforge.net/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.3.0/threads-posix/sjlj/x86_64-5.3.0-release-posix-sjlj-rt_v4-rev0.7z'
-openssl dgst -sha256 pack.bin | grep -q ec28b6640ad4f183be7afcd6e9c5eabb24b89729ca3fec7618755555b5d70c19
-# Will unpack into "./mingw64"
-7z x -y pack.bin > /dev/null
+   curl -fsS -o pack.bin -L --proto-redir =https 'https://fossies.org/windows/misc/upx391w.zip'
+   openssl dgst -sha256 pack.bin | grep -q d7d4817f46d2616c209c46fb8bce44e4bec93ab5adef5e4dfc93ee879527be1b
+   7z e -y -oupx pack.bin > /dev/null
+
+   # curl -fsS -o pack.bin 'https://www.mirrorservice.org/sites/dl.sourceforge.net/pub/sourceforge/m/mi/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/5.3.0/threads-posix/dwarf/i686-5.3.0-release-posix-dwarf-rt_v4-rev0.7z'
+   # openssl dgst -sha256 pack.bin | grep -q 6e067b2917583e9c654b611263d5d5e8c3215b67d76d55fa3f5f484f16f0f0b6
+   # curl -fsS -o pack.bin 'https://www.mirrorservice.org/sites/dl.sourceforge.net/pub/sourceforge/m/mi/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.3.0/threads-posix/seh/x86_64-5.3.0-release-posix-seh-rt_v4-rev0.7z'
+   # openssl dgst -sha256 pack.bin | grep -q 7f0e1f081d173b4a98bde3f9d1a90daf391219e6738f1f40120336b40545f090
+   # curl -fsS -o pack.bin 'https://www.mirrorservice.org/sites/dl.sourceforge.net/pub/sourceforge/m/mi/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.3.0/threads-posix/sjlj/x86_64-5.3.0-release-posix-sjlj-rt_v4-rev0.7z'
+     curl -fsS -o pack.bin -L 'https://downloads.sourceforge.net/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.3.0/threads-posix/sjlj/x86_64-5.3.0-release-posix-sjlj-rt_v4-rev0.7z'
+   openssl dgst -sha256 pack.bin | grep -q ec28b6640ad4f183be7afcd6e9c5eabb24b89729ca3fec7618755555b5d70c19
+   # Will unpack into "./mingw64"
+   7z x -y pack.bin > /dev/null
+)
 
 # Dependencies for Windows builds
 
@@ -47,9 +51,12 @@ for plat in '32' '64' ; do
    ; do
       ver="$(echo "${name}" | tr '[:lower:]' '[:upper:]' 2> /dev/null)_VER"
       hash="$(echo "${name}" | tr '[:lower:]' '[:upper:]' 2> /dev/null)_HASH_${plat}"
-      curl -fsS -o pack.bin -L --proto-redir =https "${base}${name}-${!ver}-win${plat}-mingw.7z"
-      openssl dgst -sha256 pack.bin | grep -q "${!hash}"
-      7z x -y pack.bin > /dev/null
-      mv "${name}-${!ver}-win${plat}-mingw" "${name}-mingw${plat}"
+      (
+         set -x
+         curl -fsS -o pack.bin -L --proto-redir =https "${base}${name}-${!ver}-win${plat}-mingw.7z"
+         openssl dgst -sha256 pack.bin | grep -q "${!hash}"
+         7z x -y pack.bin > /dev/null
+         mv "${name}-${!ver}-win${plat}-mingw" "${name}-mingw${plat}"
+      )
    done
 done
