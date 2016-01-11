harbour/core

commit 24058ddcf70ad1a170e29eba4954da6a4b6ef06d

2015-07-20 00:19 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/xhb/stream.prg   * contrib/xhb/txml.prg     * use VF IO API     + implement support for <nOffset> parameter in:          TStreamFileReader():Read()          TStreamFileWriter():Write()       Lightly tested but implemented safely unlike the xhb version       which relies on buggy FREAD()/FWRITE() hidden extensions that       make it very easy to read/write arbitrary out-of-bounds memory       via unchecked buffer indexes.

contrib/xhb/stream.prg
@@ -113,12 +113,13 @@ METHOD PROCEDURE CopyTo( oTargetStream ) CLASS TStream
 CREATE CLASS TStreamFileReader INHERIT TStream
 
    VAR cFile
-   VAR Handle INIT F_ERROR
+   VAR fhnd
 
    METHOD New( cFile, nMode ) CONSTRUCTOR
+   METHOD Handle() INLINE hb_vfHandle( ::fhnd )
 
-   METHOD Close() INLINE iif( ::Handle != F_ERROR, FClose( ::Handle ), ), ::Handle := F_ERROR
-   METHOD Seek( nOffset Origin ) INLINE FSeek( ::Handle, nOffset, Origin )
+   METHOD Close() INLINE iif( ::fhnd != NIL, hb_vfClose( ::fhnd ), ), ::fhnd := NIL
+   METHOD Seek( nOffset Origin ) INLINE hb_vfSeek( ::fhnd, nOffset, Origin )
 
    METHOD Read( sBuffer, nOffset, nCount )
    METHOD ReadByte()
@@ -133,14 +134,14 @@ METHOD New( cFile, nMode ) CLASS TStreamFileReader
 
    ::cFile := cFile
 
-   IF ( ::Handle := FOpen( cFile, nMode ) ) == F_ERROR
+   IF ( ::fhnd := hb_vfOpen( cFile, nMode ) ) == NIL
       Throw( xhb_ErrorNew( "Stream", 0, 1004, ProcName(), "Open Error: " + hb_ntos( FError() ), hb_AParams() ) )
    ENDIF
 
    ::nPosition := 0
-   ::nLength := FSeek( ::Handle, 0, FS_END )
+   ::nLength := hb_vfSize( ::fhnd )
 
-   FSeek( ::Handle, 0, FS_SET )
+   hb_vfSeek( ::fhnd, 0, FS_SET )
 
    RETURN Self
 
@@ -152,7 +153,16 @@ METHOD PROCEDURE Finalize CLASS TStreamFileReader
 
 METHOD Read( sBuffer, nOffset, nCount ) CLASS TStreamFileReader
 
-   LOCAL nRead := FRead( ::Handle, @sBuffer, nCount, nOffset )
+   LOCAL nRead
+   LOCAL cBuffer
+
+   IF HB_ISNUMERIC( nOffset ) .AND. nOffset >= 1
+      cBuffer := Space( nCount )
+      nRead := hb_vfRead( ::fhnd, @cBuffer, nCount )
+      sBuffer := Stuff( sBuffer, nOffSet + 1, nRead, hb_BLeft( cBuffer, nRead ) )
+   ELSE
+      nRead := hb_vfRead( ::fhnd, @sBuffer, nCount )
+   ENDIF
 
    ::nPosition += nRead
 
@@ -162,7 +172,7 @@ METHOD ReadByte() CLASS TStreamFileReader
 
    LOCAL sBuffer := Space( 1 )
 
-   IF FRead( ::Handle, @sBuffer, 1 ) == 1
+   IF hb_vfRead( ::fhnd, @sBuffer, 1 ) == 1
       ::nPosition++
       RETURN sBuffer
    ENDIF
@@ -172,12 +182,13 @@ METHOD ReadByte() CLASS TStreamFileReader
 CREATE CLASS TStreamFileWriter INHERIT TStream
 
    VAR cFile
-   VAR Handle INIT F_ERROR
+   VAR fhnd
 
    METHOD New( cFile, nMode, nAttr ) CONSTRUCTOR
+   METHOD Handle() INLINE hb_vfHandle( ::fhnd )
 
-   METHOD Close() INLINE iif( ::Handle != F_ERROR, FClose( ::Handle ), ), ::Handle := F_ERROR
-   METHOD Seek( nOffset Origin ) INLINE ::nPosition := FSeek( ::Handle, nOffset, Origin )
+   METHOD Close() INLINE iif( ::fhnd != NIL, hb_vfClose( ::fhnd ), ), ::fhnd := NIL
+   METHOD Seek( nOffset Origin ) INLINE ::nPosition := hb_vfSeek( ::fhnd, nOffset, Origin )
 
    METHOD Write( sBuffer, nOffset, nCount )
    METHOD WriteByte( cByte )
@@ -192,17 +203,21 @@ METHOD New( cFile, nMode, nAttr ) CLASS TStreamFileWriter
 
    ::cFile := cFile
 
-   IF hb_FileExists( cFile )
-      IF ( ::Handle := FOpen( cFile, hb_defaultValue( nMode, FO_READWRITE ) ) ) == F_ERROR
+   hb_default( @nMode, FO_READWRITE )
+
+   IF hb_vfExists( cFile )
+      IF ( ::fhnd := hb_vfOpen( cFile, nMode ) ) == NIL
          Throw( xhb_ErrorNew( "Stream", 0, 1004, ProcName(), "Open Error: " + hb_ntos( FError() ), hb_AParams() ) )
       ENDIF
 
-      ::nLength := FSeek( ::Handle, 0, FS_END )
+      ::nLength := hb_vfSize( ::fhnd )
       ::nPosition := ::nLength
    ELSE
-      IF ( ::Handle := hb_FCreate( cFile, nAttr, hb_defaultValue( nMode, FO_READWRITE ) ) ) == F_ERROR
+      IF ( ::fhnd := hb_vfOpen( cFile, hb_bitOr( FO_CREAT + FO_TRUNC + FO_WRITE, nMode ) ) ) == NIL
          Throw( xhb_ErrorNew( "Stream", 0, 1004, ProcName(), "Create Error: " + hb_ntos( FError() ), hb_AParams() ) )
       ENDIF
+
+      hb_vfAttrSet( cFile, nAttr )
 
       ::nPosition := 0
       ::nLength := 0
@@ -218,9 +233,16 @@ METHOD PROCEDURE Finalize CLASS TStreamFileWriter
 
 METHOD Write( sBuffer, nOffset, nCount ) CLASS TStreamFileWriter
 
-   LOCAL nWritten := FWrite( ::Handle, sBuffer, nCount )
+   LOCAL nWritten
 
-   HB_SYMBOL_UNUSED( nOffset )
+   IF HB_ISNUMERIC( nOffset ) .AND. nOffset >= 1
+      IF ! HB_ISNUMERIC( nCount )
+         nCount := hb_BLen( sBuffer ) - nOffset
+      ENDIF
+      sBuffer := hb_BSubStr( sBuffer, nOffSet + 1 )
+   ENDIF
+
+   nWritten := hb_vfWrite( ::fhnd, sBuffer, nCount )
 
    ::nPosition += nWritten
 
@@ -232,7 +254,7 @@ METHOD Write( sBuffer, nOffset, nCount ) CLASS TStreamFileWriter
 
 METHOD PROCEDURE WriteByte( cByte ) CLASS TStreamFileWriter
 
-   LOCAL nWritten := FWrite( ::Handle, cByte, 1 )
+   LOCAL nWritten := hb_vfWrite( ::fhnd, cByte, 1 )
 
    ::nPosition += nWritten
 

contrib/xhb/txml.prg
@@ -386,17 +386,17 @@ METHOD Write( fHandle, nStyle ) CLASS TXMLDocument
 
    LOCAL nResult := HBXML_STATUS_ERROR
 
-   IF HB_ISSTRING( fHandle )  // It's a filename!
-      IF ( fHandle := FCreate( fHandle ) ) != F_ERROR
+   IF HB_ISSTRING( fHandle )  // It's a filename
+      IF ( fHandle := hb_vfOpen( fHandle, FO_CREAT + FO_TRUNC + FO_WRITE ) ) != NIL
          IF Empty( ::oRoot:oChild ) .OR. !( ::oRoot:oChild:cName == "xml" )
             IF Empty( ::cHeader )
-               FWrite( fHandle, '<?xml version="1.0"?>' + hb_eol() )
+               hb_vfWrite( fHandle, '<?xml version="1.0"?>' + hb_eol() )
             ELSE
-               FWrite( fHandle, ::cHeader + hb_eol() )
+               hb_vfWrite( fHandle, ::cHeader + hb_eol() )
             ENDIF
          ENDIF
          nResult := ::oRoot:Write( fHandle, nStyle )
-         FClose( fHandle )
+         hb_vfClose( fHandle )
       ENDIF
       RETURN nResult
    ENDIF

commit ff3869d89d154ea72352b4a900deeccf9a1ae369

2016-01-11 16:33 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/xhb/stream.prg     ! TStreamFileWriter():New() fix to seek to the end of output file       when the file already exists       Regression from: https://github.com/vszakats/harbour-core/commit/24058ddcf70ad1a170e29eba4954da6a4b6ef06d     % TStreamFileWriter():New() fix race condition and optimize       code by leveraging hb_vfOpen() capabilities

contrib/xhb/stream.prg
@@ -203,29 +203,20 @@ ENDCLASS
 
 METHOD New( cFile, nMode, nAttr ) CLASS TStreamFileWriter
 
-   ::lCanWrite := .T.
+   LOCAL lExisting := hb_vfExists( cFile )
 
+   ::lCanWrite := .T.
    ::cFile := cFile
 
-   hb_default( @nMode, FO_READWRITE )
-
-   IF hb_vfExists( cFile )
-      IF ( ::hFile := hb_vfOpen( cFile, nMode ) ) == NIL
-         Throw( xhb_ErrorNew( "Stream", 0, 1004, ProcName(), "Open Error: " + hb_ntos( FError() ), hb_AParams() ) )
-      ENDIF
-
-      ::nLength := hb_vfSize( ::hFile )
-      ::nPosition := ::nLength
-   ELSE
-      IF ( ::hFile := hb_vfOpen( cFile, hb_bitOr( FO_CREAT + FO_TRUNC + FO_WRITE, nMode ) ) ) == NIL
-         Throw( xhb_ErrorNew( "Stream", 0, 1004, ProcName(), "Create Error: " + hb_ntos( FError() ), hb_AParams() ) )
-      ENDIF
-
-      hb_vfAttrSet( cFile, nAttr )
-
-      ::nPosition := 0
-      ::nLength := 0
+   IF ( ::hFile := hb_vfOpen( cFile, hb_bitOr( FO_CREAT, hb_defaultValue( nMode, FO_READWRITE ) ) ) ) == NIL
+      Throw( xhb_ErrorNew( "Stream", 0, 1004, ProcName(), "Open/Create Error: " + hb_ntos( FError() ), hb_AParams() ) )
    ENDIF
+
+   IF ! lExisting
+      hb_vfAttrSet( cFile, nAttr )
+   ENDIF
+
+   ::nPosition := ::nLength := hb_vfSeek( ::hFile, 0, FS_END )
 
    RETURN Self
 