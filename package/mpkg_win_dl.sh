f96a60da55a05e6eb0a9f581f0af5a8318067f42
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-13 11:47:27 +0100

    2016-02-13 11:46 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * package/mpkg_win_dl.sh     * minor cleanup

--- package/mpkg_win_dl.sh
+++ package/mpkg_win_dl.sh
@@ -31,7 +31,7 @@ set -e
    openssl dgst -sha256 pack.bin | grep -q d7d4817f46d2616c209c46fb8bce44e4bec93ab5adef5e4dfc93ee879527be1b
    7z e -y -oupx pack.bin > /dev/null
 
-#  if [ "${_BRANCH#*msys*}" = "${_BRANCH}" ] ; then
+#  if [ "${_BRANCH#*msysmingw*}" = "${_BRANCH}" ] ; then
    if [ "${_BRANCH}" != 'lto' ] ; then
       readonly mingwbase='https://downloads.sourceforge.net'; readonly option='-L'
 #     curl -fsS -o pack.bin "${option}" "${mingwbase}/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/5.3.0/threads-posix/dwarf/i686-5.3.0-release-posix-dwarf-rt_v4-rev0.7z"
