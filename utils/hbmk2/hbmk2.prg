harbour/core

commit d008984c16e3964ba1ff36969a1eb47e3c093852

2013-04-22 14:27 UTC+0200 Viktor Szakats (harbour syenar.net)   * utils/hbmk2/hbmk2.prg     + merged -tshead= and -seqhead= into one new common option:       -bldhead=.
  * bin/commit.hb
    * reverted one prev mod causing problems (line ending whitespaces)

utils/hbmk2/hbmk2.prg
@@ -1336,8 +1336,7 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
    LOCAL l_cMAIN := NIL
    LOCAL l_cVCSDIR
    LOCAL l_cVCSHEAD
-   LOCAL l_cSEQHEAD
-   LOCAL l_cTSHEAD
+   LOCAL l_cBLDHEAD
    LOCAL l_cHBSUFFIX := ""
    LOCAL l_lNOHBLIB := .F.
    LOCAL l_lLIBSYSMISC := .T.
@@ -3287,18 +3286,11 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
             l_cVCSHEAD := hb_FNameExtSet( l_cVCSHEAD, ".ch" )
          ENDIF
 
-      CASE Left( cParamL, Len( "-seqhead=" ) ) == "-seqhead="
+      CASE Left( cParamL, Len( "-bldhead=" ) ) == "-bldhead="
 
-         l_cSEQHEAD := PathMakeAbsolute( hb_DirSepToOS( MacroProc( hbmk, SubStr( cParam, Len( "-seqhead=" ) + 1 ), aParam[ _PAR_cFileName ] ) ), aParam[ _PAR_cFileName ] )
-         IF Empty( hb_FNameExt( l_cSEQHEAD ) )
-            l_cSEQHEAD := hb_FNameExtSet( l_cSEQHEAD, ".ch" )
-         ENDIF
-
-      CASE Left( cParamL, Len( "-tshead=" ) ) == "-tshead="
-
-         l_cTSHEAD := PathMakeAbsolute( hb_DirSepToOS( MacroProc( hbmk, SubStr( cParam, Len( "-tshead=" ) + 1 ), aParam[ _PAR_cFileName ] ) ), aParam[ _PAR_cFileName ] )
-         IF Empty( hb_FNameExt( l_cTSHEAD ) )
-            l_cTSHEAD := hb_FNameExtSet( l_cTSHEAD, ".ch" )
+         l_cBLDHEAD := PathMakeAbsolute( hb_DirSepToOS( MacroProc( hbmk, SubStr( cParam, Len( "-bldhead=" ) + 1 ), aParam[ _PAR_cFileName ] ) ), aParam[ _PAR_cFileName ] )
+         IF Empty( hb_FNameExt( l_cBLDHEAD ) )
+            l_cBLDHEAD := hb_FNameExtSet( l_cBLDHEAD, ".ch" )
          ENDIF
 
       CASE Left( cParamL, Len( "-plugin=" ) ) == "-plugin="
@@ -5706,29 +5698,17 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
             hb_MemoWrit( l_cVCSHEAD, tmp )
          ENDIF
       ENDIF
-      IF ! Empty( l_cSEQHEAD )
+      IF ! Empty( l_cBLDHEAD )
          IF hbmk[ _HBMK_lInfo ]
-            _hbmk_OutStd( hbmk, hb_StrFormat( I_( "Creating sequence header: %1$s" ), l_cVCSHEAD ) )
+            _hbmk_OutStd( hbmk, hb_StrFormat( I_( "Creating build info header: %1$s" ), l_cBLDHEAD ) )
          ENDIF
-         /* We can use native EOLs because we never read this file back. */
-         hb_MemoWrit( l_cSEQHEAD, ;
-            "/* Automatically generated by " + _SELF_NAME_ + ". Do not edit. */" + hb_eol() + ;
-            "#define _HBMK_SEQ_ID_ " + '"' + SeqID( l_cSEQHEAD, "_HBMK_SEQ_ID_" ) + '"' + hb_eol() )
-      ENDIF
-      IF ! Empty( l_cTSHEAD )
-
-         /* Use the same EOL for all platforms to avoid unnecessary rebuilds. */
          tmp1 := hb_DateTime()
-         tmp := ;
-            "/* Automatically generated by " + _SELF_NAME_ + ". Do not edit. */" + _FIL_EOL + ;
-            "#define _HBMK_BUILD_DATE_      " + '"' +            DToS( tmp1 )         + '"' + _FIL_EOL + ;
-            "#define _HBMK_BUILD_TIME_      " + '"' + SubStr( hb_TToS( tmp1 ), 9, 6 ) + '"' + _FIL_EOL + ;
-            "#define _HBMK_BUILD_TIMESTAMP_ " + '"' +         hb_TToS( tmp1 )         + '"' + _FIL_EOL
-
-         IF hbmk[ _HBMK_lInfo ]
-            _hbmk_OutStd( hbmk, hb_StrFormat( I_( "Creating timestamp header: %1$s" ), l_cTSHEAD ) )
-         ENDIF
-         hb_MemoWrit( l_cTSHEAD, tmp )
+         hb_MemoWrit( l_cBLDHEAD, ;
+            "/* Automatically generated by " + _SELF_NAME_ + ". Do not edit. */" + hb_eol() + ;
+            "#define _HBMK_BUILD_ID_        " + '"' + SeqID( l_cBLDHEAD, "_HBMK_BUILD_ID_" ) + '"' + hb_eol() + ;
+            "#define _HBMK_BUILD_DATE_      " + '"' +            DToS( tmp1 )                + '"' + hb_eol() + ;
+            "#define _HBMK_BUILD_TIME_      " + '"' + SubStr( hb_TToS( tmp1 ), 9, 6 )        + '"' + hb_eol() + ;
+            "#define _HBMK_BUILD_TIMESTAMP_ " + '"' +         hb_TToS( tmp1 )                + '"' + hb_eol() )
       ENDIF
    ENDIF
 
@@ -15846,8 +15826,7 @@ STATIC PROCEDURE ShowHelp( hbmk, lMore, lLong )
       { "-compr=<level>"     , I_( e"compress executable/dynamic lib (needs UPX tool)\n<level> can be: yes, no, min, max" ) }, ;
       { "-run[-]"            , I_( "run/do not run output executable" ) }, ;
       { "-vcshead=<file>"    , I_( "generate .ch header file with local repository information. Git, SVN, Mercurial, Bazaar, Fossil, CVS and Monotone are currently supported. Generated header will define preprocessor constant _HBMK_VCS_TYPE_ with the name of detected VCS and _HBMK_VCS_ID_ with the unique ID of local repository. If no VCS system is detected, a sequential number will be rolled automatically on each build." ) }, ;
-      { "-seqhead=<file>"    , I_( "generate .ch header file with a sequential number, that will be rolled automatically on each build. Generated header will define preprocessor constant _HBMK_SEQ_ID_." ) }, ;
-      { "-tshead=<file>"     , I_( "generate .ch header file with timestamp information. Generated header will define preprocessor constants _HBMK_BUILD_DATE_, _HBMK_BUILD_TIME_, _HBMK_BUILD_TIMESTAMP_ with the date/time of build" ) }, ;
+      { "-bldhead=<file>"    , I_( "generate .ch header file with build information, like build sequence number and timestamp. Generated header will define preprocessor constants _HBMK_BUILD_ID_ with sequence number (incremented on each build) and _HBMK_BUILD_DATE_, _HBMK_BUILD_TIME_, _HBMK_BUILD_TIMESTAMP_ with the date/time of build" ) }, ;
       { "-icon=<file>"       , I_( "set <file> as application icon. <file> should be a supported format on the target platform (not supported by some platforms/compilers). On Windows, it is implemented by generating and linking a resource file." ) }, ;
       { "-manifest=<file>"   , I_( "embed manifest <file> in executable/dynamic lib (Windows only)" ) }, ;
       { "-sign=<key>"        , I_( "sign executable with <key> (Windows and Darwin only). On Windows signtool.exe is used (part of MS Windows SDK) or posign.exe (part of Pelles C 7), in that order, both autodetected." ) }, ;

bin/commit.hb
@@ -739,7 +739,7 @@ STATIC FUNCTION CheckFile( cName, /* @ */ aErr, lApplyFixes, lRebase )
       IF ! IsBinary( cFile )
 
          IF hb_FileMatch( cName, "ChangeLog.txt" ) .AND. Len( cFile ) > 32768 .AND. ! lApplyFixes
-            cFile := RTrimEOL( Left( cFile, 16384 ) ) + hb_eol() + LTrim( Right( cFile, 16384 ) )
+            cFile := RTrimEOL( Left( cFile, 16384 ) ) + LTrim( Right( cFile, 16384 ) )
          ENDIF
 
          lReBuild := .F.

commit f728009fa70fe0f99d80566a1e501b09db4e26b8

2016-02-21 00:56 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * utils/hbmk2/hbmk2.prg     ! use LF EOL for -bldhead header. It's now consistent with -vcshead       and fixes this regression in d008984c16e3964ba1ff36969a1eb47e3c093852     % use LF EOL internally in hbrun

utils/hbmk2/hbmk2.prg
@@ -6426,14 +6426,14 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
          tmp1 := hb_DateTime()
          tmp2 := SeqID( hbmk, l_cBLDHEAD, "_HBMK_BUILD_ID_" )
          hb_MemoWrit( l_cBLDHEAD, ;
-            "/* Automatically generated by " + _SELF_NAME_ + ". Do not edit. */" + hb_eol() + ;
-            "#define _HBMK_BUILD_ID_            " + '"' +                      tmp2                   + '"' + hb_eol() + ;
-            "#define _HBMK_BUILD_ID_NUM_        " +                            tmp2                         + hb_eol() + ;
-            "#define _HBMK_BUILD_DATE_          " + '"' +                DToS( tmp1 )                 + '"' + hb_eol() + ;
-            "#define _HBMK_BUILD_TIME_          " + '"' +             hb_TToC( tmp1, "", "hh:mm:ss" ) + '"' + hb_eol() + ;
-            "#define _HBMK_BUILD_TIMESTAMP_     " + '"' +             hb_TToS( tmp1 )                 + '"' + hb_eol() + ;
-            "#define _HBMK_BUILD_TIMESTAMP_UTC_ " + '"' + hb_TToS( hb_TSToUTC( tmp1 ) )               + '"' + hb_eol() + ;
-            "#define _HBMK_BUILD_RANDSTR_32_    " + '"' + Lower( hb_StrToHex( hb_randStr( 32 ) ) )    + '"' + hb_eol() )
+            "/* Automatically generated by " + _SELF_NAME_ + ". Do not edit. */" + _FIL_EOL + ;
+            "#define _HBMK_BUILD_ID_            " + '"' +                      tmp2                   + '"' + _FIL_EOL + ;
+            "#define _HBMK_BUILD_ID_NUM_        " +                            tmp2                         + _FIL_EOL + ;
+            "#define _HBMK_BUILD_DATE_          " + '"' +                DToS( tmp1 )                 + '"' + _FIL_EOL + ;
+            "#define _HBMK_BUILD_TIME_          " + '"' +             hb_TToC( tmp1, "", "hh:mm:ss" ) + '"' + _FIL_EOL + ;
+            "#define _HBMK_BUILD_TIMESTAMP_     " + '"' +             hb_TToS( tmp1 )                 + '"' + _FIL_EOL + ;
+            "#define _HBMK_BUILD_TIMESTAMP_UTC_ " + '"' + hb_TToS( hb_TSToUTC( tmp1 ) )               + '"' + _FIL_EOL + ;
+            "#define _HBMK_BUILD_RANDSTR_32_    " + '"' + Lower( hb_StrToHex( hb_randStr( 32 ) ) )    + '"' + _FIL_EOL )
       ENDIF
    ENDIF
 
@@ -16583,11 +16583,11 @@ STATIC PROCEDURE __hbshell_Exec( cCommand )
       } )
 
    cFunc := ;
-      "STATIC FUNCTION __HBDOT()" + hb_eol() + ;
-      "RETURN {||" + hb_eol() + ;
-      "   " + cCommand + hb_eol() + ;
-      "   RETURN __mvSetBase()" + hb_eol() + ;
-      "}" + hb_eol()
+      "STATIC FUNCTION __HBDOT()" + _FIL_EOL + ;
+      "RETURN {||" + _FIL_EOL + ;
+      "   " + cCommand + _FIL_EOL + ;
+      "   RETURN __mvSetBase()" + _FIL_EOL + ;
+      "}" + _FIL_EOL
 
    DevPos( hbsh[ _HBSH_nRow ], hbsh[ _HBSH_nCol ] )
 