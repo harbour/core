harbour/core

commit fe4ef3a72b490f5617c2a0683d9defde7b3715b5

2016-02-20 18:49 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * utils/hbmk2/hbmk2.prg     ! fix bugs in -fullstatic support on win/mingw, os2/gcc* and dos/djgpp       as implemented in commit 98992a3ce1f9375893d4563060073d4902237703       (2015-09-05 01:00 UTC+0200). The option is now properly used as       a prefix, its effect disabled by a suffix. A minor step closer to       standardise dynamice/static lib linking.       Disable it for darwin as it appears to not be supported, according       to the Internet.

utils/hbmk2/hbmk2.prg
@@ -1571,7 +1571,8 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
    LOCAL aCOMPSUP
 
    LOCAL cLibPrefix
-   LOCAL cLibPostOption
+   LOCAL cLibModePrefix
+   LOCAL cLibModeSuffix
    LOCAL cLibExt
    LOCAL cObjPrefix
    LOCAL cObjExt
@@ -4407,7 +4408,10 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
          cObjExt := ".o"
 #if 0
          IF hbmk[ _HBMK_lSTATICFULL ]
-            cLibPostOption := " -Wl,-Bstatic"
+            IF !( hbmk[ _HBMK_cPLAT ] == "darwin" )
+               cLibModePrefix :=  "-Wl,-Bstatic "
+               cLibModeSuffix := " -Wl,-Bdynamic"
+            ENDIF
          ENDIF
 #endif
          IF hbmk[ _HBMK_cPLAT ] == "darwin"
@@ -4814,7 +4818,8 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
          cLibExt := ""
          cObjExt := ".o"
          IF hbmk[ _HBMK_lSTATICFULL ]
-            cLibPostOption := " -Wl,-Bstatic"
+            cLibModePrefix :=  "-Wl,-static "
+            cLibModeSuffix := " -Wl,-dynamic"
          ENDIF
          DO CASE
          CASE hbmk[ _HBMK_cCOMP ] == "clang"
@@ -5091,7 +5096,8 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
          ENDIF
          cOpt_CompC := "-c"
          IF hbmk[ _HBMK_lSTATICFULL ]
-            cLibPostOption := " -Wl,-Bstatic"
+            cLibModePrefix :=  "-Wl,-static "
+            cLibModeSuffix := " -Wl,-dynamic"
          ENDIF
          IF hbmk[ _HBMK_lOPTIM ]
             cOpt_CompC += " -O3"
@@ -5216,7 +5222,8 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
          cBin_CompC := iif( hbmk[ _HBMK_lCPP ] != NIL .AND. hbmk[ _HBMK_lCPP ], cBin_CompCPP, hbmk[ _HBMK_cCCPREFIX ] + "gcc" + hbmk[ _HBMK_cCCSUFFIX ] + hbmk[ _HBMK_cCCEXT ] )
          cOpt_CompC := "-c"
          IF hbmk[ _HBMK_lSTATICFULL ]
-            cLibPostOption := " -Wl,-Bstatic"
+            cLibModePrefix :=  "-Wl,-static "
+            cLibModeSuffix := " -Wl,-dynamic"
          ENDIF
          IF hbmk[ _HBMK_lOPTIM ]
             cOpt_CompC += " -O3"
@@ -7834,7 +7841,7 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
                   "{LO}" => ArrayToList( ArrayJoin( l_aOBJ, hbmk[ _HBMK_aOBJUSER ] ),, nOpt_Esc, nOpt_FNF, cObjPrefix ), ;
                   "{LS}" => ArrayToList( ArrayJoin( ListDirExt( hbmk[ _HBMK_aRESSRC ], hbmk[ _HBMK_cWorkDir ], cResExt ), hbmk[ _HBMK_aRESCMP ] ),, nOpt_Esc, nOpt_FNF, cResPrefix ), ;
                   "{LA}" => ArrayToList( l_aOBJA,, nOpt_Esc, nOpt_FNF ), ;
-                  "{LL}" => ArrayToList( l_aLIB,, nOpt_Esc, nOpt_FNF, cLibPrefix, cLibPostOption ), ;
+                  "{LL}" => ArrayToList( l_aLIB,, nOpt_Esc, nOpt_FNF, hb_defaultValue( cLibModePrefix, "" ) + cLibPrefix, cLibModeSuffix ), ;
                   "{LB}" => ArrayToList( l_aLIBA,, nOpt_Esc, nOpt_FNF ), ;
                   "{LF}" => iif( Empty( hbmk[ _HBMK_aOPTLPOST ] ), "", " " + ArrayToList( hbmk[ _HBMK_aOPTLPOST ] ) ), ;
                   "{IM}" => ArrayToList( hbmk[ _HBMK_aDEF ],, nOpt_Esc, nOpt_FNF, cDefPrefix ), ;
@@ -7974,7 +7981,7 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
                   "{TU}" => hb_ntos( Int( ( Max( hbmk[ _HBMK_tVCSTS ], hb_SToT( "19700101000000" ) ) - hb_SToT( "19700101000000" ) ) * 86400 ) ), ;
                   "{LO}" => tmp, ;
                   "{LS}" => ArrayToList( ArrayJoin( ListDirExt( hbmk[ _HBMK_aRESSRC ], hbmk[ _HBMK_cWorkDir ], cResExt ), hbmk[ _HBMK_aRESCMP ] ),, nOpt_Esc, nOpt_FNF, cResPrefix ), ;
-                  "{LL}" => ArrayToList( l_aLIB,, nOpt_Esc, nOpt_FNF, cLibPrefix, cLibPostOption ), ;
+                  "{LL}" => ArrayToList( l_aLIB,, nOpt_Esc, nOpt_FNF, hb_defaultValue( cLibModePrefix, "" ) + cLibPrefix, cLibModeSuffix ), ;
                   "{LB}" => ArrayToList( l_aLIBA,, nOpt_Esc, nOpt_FNF ), ;
                   "{LF}" => iif( Empty( hbmk[ _HBMK_aOPTDPOST ] ), "", " " + ArrayToList( hbmk[ _HBMK_aOPTDPOST ] ) ), ;
                   "{IM}" => ArrayToList( hbmk[ _HBMK_aDEF ],, nOpt_Esc, nOpt_FNF, cDefPrefix ), ;

commit 2f4733fb31ac3d71fd0bd645e3baad0c6398b598

2016-03-02 20:13 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * package/mpkg_win.sh     * minor cleanup     ! fix another typo in previous mod
  * utils/hbmk2/hbmk2.prg
    ! fix RTE regression after fe4ef3a72b490f5617c2a0683d9defde7b3715b5
      Fixes https://github.com/vszakats/harbour-core/issues/199

package/mpkg_win.sh
@@ -9,7 +9,8 @@ cd "$(dirname "$0")" || exit
 
 # - Requires MSYS2 or Git for Windows to run on Windows
 # - Requires 7z in PATH
-# - Adjust target dir, MinGW dirs, set HB_DIR_UPX, HB_DIR_7Z, HB_DIR_MINGW,
+# - Adjust target dir, MinGW dirs,
+#   set HB_DIR_UPX, HB_DIR_7Z, HB_DIR_MINGW, HB_DIR_MINGW_32, HB_DIR_MINGW_64
 #   create required packages beforehand.
 # - Optional HB_SFX_7Z envvar pointed to 7z SFX module
 # - Run this from vanilla official source tree only.
@@ -33,6 +34,8 @@ readonly HB_RT_DEF=C:/hb
 
 HB_RT="$(echo "${HB_RT}" | sed 's|\\|/|g')"
 HB_DIR_MINGW="$(echo "${HB_DIR_MINGW}" | sed 's|\\|/|g')"
+HB_DIR_MINGW_32="$(echo "${HB_DIR_MINGW_32}" | sed 's|\\|/|g')"
+HB_DIR_MINGW_64="$(echo "${HB_DIR_MINGW_64}" | sed 's|\\|/|g')"
 
 HB_DR="hb${HB_VS}/"
 HB_ABSROOT="${HB_RT}/${HB_DR}"
@@ -136,8 +139,8 @@ rm -f "${HB_ABSROOT}bin/hbmk2-temp.exe"
 # Slow. Requires binutils 2.23 (maybe 2.24/2.25).
 # Short synonym '-D' is not recognized as of binutils 2.25.
 for _cpu in '' '64' ; do
-   [ "${_cpu}" != '64' ] && _mingw_dir="${HB_MINGW_DIR_32}"
-   [ "${_cpu}"  = '64' ] && _mingw_dir="${HB_MINGW_DIR_64}"
+   [ "${_cpu}" != '64' ] && _mingw_dir="${HB_DIR_MINGW_32}"
+   [ "${_cpu}"  = '64' ] && _mingw_dir="${HB_DIR_MINGW_64}"
    for files in \
       "${HB_ABSROOT}lib/win/mingw${_cpu}/*-*.*" \
       "${HB_ABSROOT}lib/win/mingw${_cpu}/*_dll*.*" \

utils/hbmk2/hbmk2.prg
@@ -7846,7 +7846,7 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
                   "{LO}" => ArrayToList( ArrayJoin( l_aOBJ, hbmk[ _HBMK_aOBJUSER ] ),, nOpt_Esc, nOpt_FNF, cObjPrefix ), ;
                   "{LS}" => ArrayToList( ArrayJoin( ListDirExt( hbmk[ _HBMK_aRESSRC ], hbmk[ _HBMK_cWorkDir ], cResExt ), hbmk[ _HBMK_aRESCMP ] ),, nOpt_Esc, nOpt_FNF, cResPrefix ), ;
                   "{LA}" => ArrayToList( l_aOBJA,, nOpt_Esc, nOpt_FNF ), ;
-                  "{LL}" => ArrayToList( l_aLIB,, nOpt_Esc, nOpt_FNF, hb_defaultValue( cLibModePrefix, "" ) + cLibPrefix, cLibModeSuffix ), ;
+                  "{LL}" => ArrayToList( l_aLIB,, nOpt_Esc, nOpt_FNF, hb_defaultValue( cLibModePrefix, "" ) + hb_defaultValue( cLibPrefix, "" ), cLibModeSuffix ), ;
                   "{LB}" => ArrayToList( l_aLIBA,, nOpt_Esc, nOpt_FNF ), ;
                   "{LF}" => iif( Empty( hbmk[ _HBMK_aOPTLPOST ] ), "", " " + ArrayToList( hbmk[ _HBMK_aOPTLPOST ] ) ), ;
                   "{IM}" => ArrayToList( hbmk[ _HBMK_aDEF ],, nOpt_Esc, nOpt_FNF, cDefPrefix ), ;
@@ -7986,7 +7986,7 @@ STATIC FUNCTION __hbmk( aArgs, nArgTarget, nLevel, /* @ */ lPause, /* @ */ lExit
                   "{TU}" => hb_ntos( Int( ( Max( hbmk[ _HBMK_tVCSTS ], hb_SToT( "19700101000000" ) ) - hb_SToT( "19700101000000" ) ) * 86400 ) ), ;
                   "{LO}" => tmp, ;
                   "{LS}" => ArrayToList( ArrayJoin( ListDirExt( hbmk[ _HBMK_aRESSRC ], hbmk[ _HBMK_cWorkDir ], cResExt ), hbmk[ _HBMK_aRESCMP ] ),, nOpt_Esc, nOpt_FNF, cResPrefix ), ;
-                  "{LL}" => ArrayToList( l_aLIB,, nOpt_Esc, nOpt_FNF, hb_defaultValue( cLibModePrefix, "" ) + cLibPrefix, cLibModeSuffix ), ;
+                  "{LL}" => ArrayToList( l_aLIB,, nOpt_Esc, nOpt_FNF, hb_defaultValue( cLibModePrefix, "" ) + hb_defaultValue( cLibPrefix, "" ), cLibModeSuffix ), ;
                   "{LB}" => ArrayToList( l_aLIBA,, nOpt_Esc, nOpt_FNF ), ;
                   "{LF}" => iif( Empty( hbmk[ _HBMK_aOPTDPOST ] ), "", " " + ArrayToList( hbmk[ _HBMK_aOPTDPOST ] ) ), ;
                   "{IM}" => ArrayToList( hbmk[ _HBMK_aDEF ],, nOpt_Esc, nOpt_FNF, cDefPrefix ), ;