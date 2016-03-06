c588281bd4ffe678afcdde4a72b35fbef3eecf20
Viktor Szakats <vszakats@users.noreply.github.com>
2016-03-06 17:02:28 +0100

    2016-03-06 16:58 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/hbssl/tests/mkpem.sh   * contrib/hbssl/tests/privkey.pem   * contrib/hbssl/tests/pubkey.pem     * generate keys with new genpkey option.     * switch to PKCS#8 private key format.     * previous commits:       * switch to AES-256 key encryption       * switch to 2048-bit keys       * use #streaminclude to pull key into string       * add script to generate keypair

--- contrib/hbssl/tests/mkpem.sh
+++ contrib/hbssl/tests/mkpem.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-# Generate RSA keypair with encrypted private key
+# Copyright 2016 Viktor Szakats (vszakats.net/harbour)
+# See LICENSE.txt for licensing terms.
 
-openssl=openssl
+# Generate RSA keypair with encrypted private key
+# Requires: OpenSSL 1.x or upper
+#           (install with `brew install openssl` on OS X)
+
+case "$(uname)" in
+   *Darwin*) alias openssl=/usr/local/opt/openssl/bin/openssl;;
+esac
 
 # Generate
-$openssl genrsa -aes256 -passout pass:test -out privkey-pkcs1.pem 2048
-$openssl pkcs8 -topk8 -inform pem -outform pem -passin pass:test -passout pass:test < privkey-pkcs1.pem > privkey-pkcs8.pem
-$openssl rsa -inform PEM -passin pass:test -in privkey-pkcs8.pem -pubout > pubkey.pem
+$openssl genpkey -algorithm RSA -out privkey.pem -aes-256-cbc -pass pass:test -pkeyopt rsa_keygen_bits:2048
+$openssl rsa -inform PEM -passin pass:test -in privkey.pem -pubout > pubkey.pem
 
 # Generate human-readable
-$openssl rsa -inform PEM -passin pass:test -in privkey-pkcs1.pem -noout -text > privkey-pkcs1.pem.txt
-$openssl rsa -inform PEM -passin pass:test -in privkey-pkcs8.pem -noout -text > privkey-pkcs8.pem.txt
-$openssl rsa -inform PEM -passin pass:test -in pubkey.pem        -noout -text -pubin  > pubkey.pem.txt
-
-cp privkey-pkcs1.pem     privkey.pem
-cp privkey-pkcs1.pem.txt privkey.pem.txt
+$openssl rsa -inform PEM -passin pass:test -in privkey.pem -noout -text > privkey.pem.txt
+$openssl rsa -inform PEM -passin pass:test -in pubkey.pem  -noout -text -pubin > pubkey.pem.txt
