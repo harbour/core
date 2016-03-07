623f828f1b80d2eac0fece44ac06279b86f4a262
Viktor Szakats <vszakats@users.noreply.github.com>
2016-03-07 01:50:11 +0100

    mkpem.sh: simplify [ci skip]

--- contrib/hbssl/tests/mkpem.sh
+++ contrib/hbssl/tests/mkpem.sh
@@ -11,10 +11,12 @@ case "$(uname)" in
    *Darwin*) alias openssl=/usr/local/opt/openssl/bin/openssl;;
 esac
 
+pass='pass:test'
+
 # Generate
-openssl genpkey -algorithm RSA -out privkey.pem -aes-256-cbc -pass pass:test -pkeyopt rsa_keygen_bits:2048
-openssl rsa -inform PEM -passin pass:test -in privkey.pem -pubout > pubkey.pem
+openssl genpkey -algorithm RSA -out privkey.pem -aes-256-cbc -pass "$pass" -pkeyopt rsa_keygen_bits:2048
+openssl rsa -passin "$pass" -in privkey.pem -pubout > pubkey.pem
 
 # Generate human-readable
-openssl rsa -inform PEM -passin pass:test -in privkey.pem -noout -text > privkey.pem.txt
-openssl rsa -inform PEM -passin pass:test -in pubkey.pem  -noout -text -pubin > pubkey.pem.txt
+openssl rsa -passin "$pass" -in privkey.pem -noout -text > privkey.pem.txt
+openssl rsa -passin "$pass" -in pubkey.pem  -noout -text -pubin > pubkey.pem.txt
