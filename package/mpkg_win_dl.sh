baadae1670f5f58a4c7508866e3ca27652dfb1d0
Viktor Szakats <vszakats@users.noreply.github.com>
2016-03-09 14:36:32 +0100

    2016-03-09 14:35 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * package/mpkg_win_dl.sh     + pull custom builder key from Bintray

--- package/mpkg_win_dl.sh
+++ package/mpkg_win_dl.sh
@@ -59,7 +59,12 @@ gpg --version | grep gpg
 
 # Dependencies for Windows builds
 
+# Bintray public key
 gpg -q --keyserver hkps://pgp.mit.edu --recv-keys 379CE192D401AB61
+
+# Builder public key
+curl -fsS -o pack.gpg 'https://bintray.com/user/downloadSubjectPublicKey?username=vszakats'
+gpg -q --import pack.gpg 2> /dev/null
 
 readonly base='https://bintray.com/artifact/download/vszakats/generic/'
 
