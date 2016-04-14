2fbacb9500392e144c48bb17238cf0be1aedab9d
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-15 01:19:55 +0200

    2016-04-15 00:59 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * package/mpkg_win.sh     ! do not upload to VirusTotal (and fail) if API key is empty.       This is an attempt to avoid such failure for Pull Requests       Attempt to fix https://github.com/vszakats/harbour-core/issues/209

--- package/mpkg_win.sh
+++ package/mpkg_win.sh
@@ -408,18 +408,21 @@ if [ "${_BRANCH#*master*}" != "${_BRANCH}" ] ; then
    )
 fi
 
-# https://www.virustotal.com/en/documentation/public-api/#scanning-files
-if [ "$(wc -c < "${_pkgname}")" -lt 32000000 ]; then
-   (
-      set +x
-      out="$(curl -sS \
-         -X POST https://www.virustotal.com/vtapi/v2/file/scan \
-         --form-string "apikey=${VIRUSTOTAL_APIKEY}" \
-         --form "file=@${_pkgname}")"
-      echo "${out}"
-      echo "VirusTotal URL for '${_pkgname}':"
-      echo "${out}" | grep -o 'https://[a-zA-Z0-9./]*'
-   )
-else
-   echo "! File too large for VirusTotal Public API. Upload skipped."
+if [ -n "${VIRUSTOTAL_APIKEY}" ] ; then
+
+   # https://www.virustotal.com/en/documentation/public-api/#scanning-files
+   if [ "$(wc -c < "${_pkgname}")" -lt 32000000 ] ; then
+      (
+         set +x
+         out="$(curl -sS \
+            -X POST https://www.virustotal.com/vtapi/v2/file/scan \
+            --form-string "apikey=${VIRUSTOTAL_APIKEY}" \
+            --form "file=@${_pkgname}")"
+         echo "${out}"
+         echo "VirusTotal URL for '${_pkgname}':"
+         echo "${out}" | grep -o 'https://[a-zA-Z0-9./]*'
+      )
+   else
+      echo "! File too large for VirusTotal Public API. Upload skipped."
+   fi
 fi
