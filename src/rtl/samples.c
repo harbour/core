1166c5613c7646cc81324fe3f165a2151f862cd3
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-26 16:46:48 +0200

    2016-04-26 16:46 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com) [ci skip]   * src/rtl/samples.c     * minor fix in header comment

--- src/rtl/samples.c
+++ src/rtl/samples.c
@@ -1,7 +1,7 @@
 /*
- * Compatibility functions from the SAMPLES directory of Clipper.
+ * Compatibility functions from the SOURCE/SAMPLE directory of CA-Cl*pper.
  *
- * Copyright 1999-2001 Viktor Szakats (vszakats.net/harbour)
+ * Copyright 1999-2016 Viktor Szakats (vszakats.net/harbour)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
