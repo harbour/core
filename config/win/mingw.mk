01065cabc75e53c7d4871cdac9456ecdf755d6be
Viktor Szakats <vszakats@users.noreply.github.com>
2016-06-16 23:30:34 +0200

    2016-06-16 23:30 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * config/win/mingw.mk     ! adjust filter expression to not enable gcc 6.x options for 5.4

--- config/win/mingw.mk
+++ config/win/mingw.mk
@@ -100,7 +100,7 @@ ifneq ($(HB_BUILD_WARN),no)
 #     # https://gcc.gnu.org/gcc-4.2/changes.html
 #     CFLAGS += -Wstrict-overflow=4
 #  endif
-   ifeq ($(filter $(HB_COMPILER_VER),0209 0304 0400 0401 0402 0403 0404 0405 0406 0407 0408 0409 0501 0502 0503),)
+   ifeq ($(filter $(HB_COMPILER_VER),0209 0304 0400 0401 0402 0403 0404 0405 0406 0407 0408 0409 0501 0502 0503 0504),)
       CFLAGS += -Wlogical-op -Wduplicated-cond -Wshift-negative-value -Wnull-dereference -Wunused-variable
    endif
 else
