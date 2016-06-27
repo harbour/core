549d1525b5480382f9ee04e00e8cb7f018f36a59
Viktor Szakats <vszakats@users.noreply.github.com>
2016-06-28 01:21:14 +0200

    2016-06-28 01:19 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * package/mpkg_win_ci.sh     * try enabling GTQTC in the rare moment when used mingw, AppVeyor       and QT versions appear to be compatible with each other

--- package/mpkg_win_ci.sh
+++ package/mpkg_win_ci.sh
@@ -110,7 +110,7 @@ if [ "${_BRANC4}" != 'msvc' ] ; then
 
    export HB_WITH_CURL="${HB_DIR_CURL_32}include"
    export HB_WITH_OPENSSL="${HB_DIR_OPENSSL_32}include"
- # export HB_WITH_QT='C:\Qt\5.7\mingw53_32\include'
+   export HB_WITH_QT='C:\Qt\5.7\mingw53_32\include'
    export PATH="${HB_DIR_MINGW_32}/bin:${_ori_path}"
    gcc -v 2> "${_build_info_32}"
    # shellcheck disable=SC2086
@@ -118,7 +118,7 @@ if [ "${_BRANC4}" != 'msvc' ] ; then
 
    export HB_WITH_CURL="${HB_DIR_CURL_64}include"
    export HB_WITH_OPENSSL="${HB_DIR_OPENSSL_64}include"
- # export HB_WITH_QT=
+   export HB_WITH_QT=
    export PATH="${HB_DIR_MINGW_64}/bin:${_ori_path}"
    gcc -v 2> "${_build_info_64}"
    # shellcheck disable=SC2086
