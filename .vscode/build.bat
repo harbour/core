49f4f571f6d4fdafed0d617d65aa415ab4bd6fba
Fran Garcia <frang@nappgui.com>
2022-12-06 20:27:12 +0100

    VSCode scripting

---
+++ .vscode/build.bat
@@ -0,0 +1,12 @@
+call "%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
+set "cwd=%cd%"
+echo %cd%
+cd .\\contrib\\gtnap
+echo %cd%
+..\\..\\bin\\win\\msvc\\hbmk2.exe gtnap.hbp
+cd %cwd%
+echo %cd%
+cd .\\contrib\\gtnap\\tests\\cuademo\\gtnap_cualib
+echo %cd%
+..\\..\\..\\..\\..\\bin\\win\\msvc\\hbmk2.exe exemplo.hbp
+exemplo --hb:gtnap
