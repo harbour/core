771f8e9a8c78660d7d5e5be6b50ea4b9199c489e
Francisco García <frang@nappgui.com>
2023-04-29 11:15:21 +0200

    [GTNAP-200] Improved build scrip Win32

--- .vscode/build.bat
+++ .vscode/build.bat
@@ -4,27 +4,26 @@
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
 
 :: Generate GTNAP
-set "cwd=%cd%"
-echo %cd%
+set mpath=%cd%
+echo Main path: %mpath%
 cd .\\contrib\\gtnap
-echo %cd%
+set gtpath=%cd%
+echo GTNAP path: %gtpath%
 call build -b Debug
-cd %cwd%
 
 :: Generate exemplo
-echo %cd%
-cd .\\tests\\cuademo\\gtnap_cualib
-echo %cd%
-if not exist %cwd%\src\exemplo mkdir %cwd%\src\exemplo
-..\\..\\..\\..\\..\\bin\\win\\msvc\\hbmk2.exe -debug -trace -keepc -workdir=%cwd%\src\exemplo -o%cwd%\build\exemplo exemplo.hbp || goto error
+cd %gtpath%\\tests\\cuademo\\gtnap_cualib
+echo Exemplo: %gtpath%\src\exemplo
+if not exist %gtpath%\src\exemplo mkdir %gtpath%\src\exemplo
+..\\..\\..\\..\\..\\bin\\win\\msvc\\hbmk2.exe -debug -trace -keepc -workdir=%gtpath%\src\exemplo -o%gtpath%\build\exemplo exemplo.hbp || goto error
 
 :: Generate exemplo debug project
-cd %cwd%
+cd %gtpath%
 cmake -S src -B build_cuademo -DCMAKE_WARN_VS11=OFF -DGTNAP_DEVELOPER_MODE=ON
 
 :: Run exemplo
-cd %cwd%\\build
-::START /B exemplo --hb:gtnap
+cd %gtpath%\\build
+START /B exemplo --hb:gtnap
 ::START /B exemplo --hb:gtwin
 goto end
 
