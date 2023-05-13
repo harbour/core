410bddea3e72ce9ee348e5961d3f515c96778a15
Francisco García <frang@nappgui.com>
2023-05-13 09:03:07 +0200

    [GTNAP-211] Global build script generates 'hello'  executable

--- .vscode/build.bat
+++ .vscode/build.bat
@@ -1,4 +1,4 @@
-:: Build GTNAP and run exemplo (cuademo) in Windows (Developer mode)
+:: Build GTNAP and generate developer mode examples (Windows)
 
 :: Set the compiler
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
@@ -11,21 +11,28 @@ set gtpath=%cd%
 echo GTNAP path: %gtpath%
 call build -b Debug
 
-:: Generate exemplo
+:: Generate exemplo sources
 cd %gtpath%\\tests\\cuademo\\gtnap_cualib
 echo Exemplo: %gtpath%\src\exemplo
 if not exist %gtpath%\src\exemplo mkdir %gtpath%\src\exemplo
-..\\..\\..\\..\\..\\bin\\win\\msvc\\hbmk2.exe -debug -trace -keepc -workdir=%gtpath%\src\exemplo -o%gtpath%\build\exemplo exemplo.hbp || goto error
+..\\..\\..\\..\\..\\bin\\win\\msvc\\hbmk2.exe exemplo.hbp -debug -trace -keepc -workdir=%gtpath%\src\exemplo -o%gtpath%\build\exemplo || goto error
 
-:: Generate exemplo debug project
+:: Generate hello sources
+cd %gtpath%\\tests\\hello
+echo Hello: %gtpath%\src\hello
+if not exist %gtpath%\src\hello mkdir %gtpath%\src\hello
+..\\..\\..\\..\\bin\\win\\msvc\\hbmk2 hello.hbp -debug -trace -keepc -workdir=%gtpath%\src\hello -o%gtpath%\build\hello
+
+:: Generate VS solution
 cd %gtpath%
-cmake -S src -B build_cuademo -DCMAKE_WARN_VS11=OFF -DGTNAP_DEVELOPER_MODE=ON
-
-:: Run exemplo
-cd %gtpath%\\build
-START /B exemplo --hb:gtnap
-::START /B exemplo --hb:gtwin
+cmake -S src -B build-dev -DCMAKE_WARN_VS11=OFF -DGTNAP_DEVELOPER_MODE=ON
 goto end
+
+@REM :: Run exemplo
+@REM cd %gtpath%\\build
+@REM START /B exemplo --hb:gtnap
+@REM ::START /B exemplo --hb:gtwin
+@REM goto end
 
 :error
 exit 1
