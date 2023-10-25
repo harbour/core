fccde2d574479da69fc0d4e3162b471bf2eed1f4
Enrico Maria Giordano <e.m.giordano@emagsoftware.it>
2023-10-25 16:34:56 +0200

    Update bcc.mk
    Fixed to work with BCC 7.60 64 bit (CLANG)

--- config/win/bcc.mk
+++ config/win/bcc.mk
@@ -22,17 +22,19 @@ CC_OUT := -o
 
 CFLAGS += -I. -I$(HB_HOST_INC)
 
-CFLAGS += -q -tWM -CP437
+CFLAGS += -q
 
-ifeq ($(HB_BUILD_WARN),no)
-   CFLAGS += -w-sig- -w-aus- -w-ccc- -w-csu- -w-par- -w-rch- -w-ucp- -w-use- -w-prc- -w-pia-
-else
-   CFLAGS += -w -Q -w-sig-
+ifneq ($(HB_COMPILER),bcc64)
+   ifeq ($(HB_BUILD_WARN),no)
+      CFLAGS += -w-sig- -w-aus- -w-ccc- -w-csu- -w-par- -w-rch- -w-ucp- -w-use- -w-prc- -w-pia-
+   else
+      CFLAGS += -w -Q -w-sig-
+   endif
 endif
 
 ifneq ($(HB_BUILD_OPTIM),no)
    ifeq ($(HB_COMPILER),bcc64)
-      CFLAGS += -d -O2 -OS -Ov -Oc
+      CFLAGS += -O2
    else
       # for some reason -6 generates the exact same code as -4 with both 5.5 and 5.8.
       # -5 seems to be significantly slower than both. [vszakats]
@@ -93,9 +95,9 @@ endif
 LIBPATHS := $(foreach dir,$(LIB_DIR) $(3RDLIB_DIR),$(subst /,$(BACKSLASH),-L"$(dir)"))
 LDFLAGS += $(LIBPATHS) -Gn -Tpe
 ifeq ($(HB_COMPILER),bcc64)
-   LD_RULE = $(LD) $(LDFLAGS) $(HB_LDFLAGS) $(HB_USER_LDFLAGS) c0x64.obj $(filter-out %$(RES_EXT),$(^F)), "$(subst /,$(BACKSLASH),$(BIN_DIR)/$@)", nul, $(LDLIBS) cw64mt import64,, $(filter %$(RES_EXT),$(^F)) $(LDSTRIP)
+   LD_RULE = $(LD) $(LDFLAGS) $(HB_LDFLAGS) $(HB_USER_LDFLAGS) c0x64$(OBJ_EXT) $(filter-out %$(RES_EXT),$(^F)), "$(subst /,$(BACKSLASH),$(BIN_DIR)/$@)", nul, $(LDLIBS) cw64mt import64,, $(filter %$(RES_EXT),$(^F)) $(LDSTRIP)
 else
-   LD_RULE = $(LD) $(LDFLAGS) $(HB_LDFLAGS) $(HB_USER_LDFLAGS) c0x32.obj $(filter-out %$(RES_EXT),$(^F)), "$(subst /,$(BACKSLASH),$(BIN_DIR)/$@)", nul, $(LDLIBS) cw32mt import32,, $(filter %$(RES_EXT),$(^F)) $(LDSTRIP)
+   LD_RULE = $(LD) $(LDFLAGS) $(HB_LDFLAGS) $(HB_USER_LDFLAGS) c0x32$(OBJ_EXT) $(filter-out %$(RES_EXT),$(^F)), "$(subst /,$(BACKSLASH),$(BIN_DIR)/$@)", nul, $(LDLIBS) cw32mt import32,, $(filter %$(RES_EXT),$(^F)) $(LDSTRIP)
 endif
 
 LDLIBS := $(strip $(HB_USER_LIBS) $(LIBS) $(3RDLIBS) $(SYSLIBS))
@@ -158,14 +160,26 @@ define dynlib_object
    @$(ECHO) $(ECHOQUOTE)$(subst /,$(ECHOBACKSLASH),$(file)) +$(ECHOQUOTE) >> __dyn__.tmp
 
 endef
-define create_dynlib
-   $(if $(wildcard __dyn__.tmp),@$(RM) __dyn__.tmp,)
-   $(foreach file,$^,$(dynlib_object))
-   @$(ECHO) $(ECHOQUOTE), $(subst /,$(ECHOBACKSLASH),$(DYN_DIR)/$@),, $(subst /,$(ECHOBACKSLASH),$(DLIBS))$(ECHOQUOTE) >> __dyn__.tmp
-   $(DY) $(DFLAGS) $(HB_USER_DFLAGS) c0d32.obj @__dyn__.tmp
-   @$(CP) $(subst /,$(DIRSEP),$(DYN_DIR)/$(basename $@)$(LIB_EXT)) $(subst /,$(DIRSEP),$(IMP_FILE))
-   @$(RM) $(subst /,$(DIRSEP),$(DYN_DIR)/$(basename $@)$(LIB_EXT))
-endef
+
+ifeq ($(HB_COMPILER),bcc64)
+   define create_dynlib
+      $(if $(wildcard __dyn__.tmp),@$(RM) __dyn__.tmp,)
+      $(foreach file,$^,$(dynlib_object))
+      @$(ECHO) $(ECHOQUOTE), $(subst /,$(ECHOBACKSLASH),$(DYN_DIR)/$@),, $(subst /,$(ECHOBACKSLASH),$(DLIBS))$(ECHOQUOTE) >> __dyn__.tmp
+      $(DY) $(DFLAGS) $(HB_USER_DFLAGS) c0d64$(OBJ_EXT) @__dyn__.tmp
+      @$(CP) $(subst /,$(DIRSEP),$(DYN_DIR)/$(basename $@)$(LIB_EXT)) $(subst /,$(DIRSEP),$(IMP_FILE))
+      @$(RM) $(subst /,$(DIRSEP),$(DYN_DIR)/$(basename $@)$(LIB_EXT))
+   endef
+else
+   define create_dynlib
+      $(if $(wildcard __dyn__.tmp),@$(RM) __dyn__.tmp,)
+      $(foreach file,$^,$(dynlib_object))
+      @$(ECHO) $(ECHOQUOTE), $(subst /,$(ECHOBACKSLASH),$(DYN_DIR)/$@),, $(subst /,$(ECHOBACKSLASH),$(DLIBS))$(ECHOQUOTE) >> __dyn__.tmp
+      $(DY) $(DFLAGS) $(HB_USER_DFLAGS) c0d32$(OBJ_EXT) @__dyn__.tmp
+      @$(CP) $(subst /,$(DIRSEP),$(DYN_DIR)/$(basename $@)$(LIB_EXT)) $(subst /,$(DIRSEP),$(IMP_FILE))
+      @$(RM) $(subst /,$(DIRSEP),$(DYN_DIR)/$(basename $@)$(LIB_EXT))
+   endef
+endif
 
 DY_RULE = $(create_dynlib)
 
