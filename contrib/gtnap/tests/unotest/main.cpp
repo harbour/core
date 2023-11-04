07eb2d05cb4c1cc1659d5c1c2743fc8faea6533f
frang75 <frang@nappgui.com>
2023-11-04 13:21:05 +0100

    [GTNAP-250] Apache UNO integration WIP

--- contrib/gtnap/tests/unotest/main.cpp
+++ contrib/gtnap/tests/unotest/main.cpp
@@ -15,8 +15,8 @@
 //using namespace rtl;
 
 // css == com.sun.star
-using namespace css::uno;
-using namespace css::beans;
+//using namespace css::uno;
+//using namespace css::beans;
 //using namespace css::bridge;
 //using namespace css::frame;
 //using namespace css::lang;
@@ -24,42 +24,42 @@ using namespace css::beans;
 
 int main()
 {
-    rtl::OUString sDocUrl("file:///home/fran/unotest/test.odt"), sPDFUrl("file:///home/fran/unotest/test.pdf");
+
+    rtl::OUString sDocUrl("file:///home/fran/harbour_nappgui/contrib/gtnap/tests/unotest/test.odt"), sPDFUrl("file:///home/fran/harbour_nappgui/contrib/gtnap/tests/unotest/test.pdf");
     rtl::OUString sConnectionString("uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager");
     rtl::Bootstrap::set("URE_MORE_TYPES", "file:///usr/lib/libreoffice/program/types/offapi.rdb");
     rtl::Bootstrap::set("URE_BOOTSTRAP", "vnd.sun.star.pathname:/usr/lib/libreoffice/program/fundamentalrc");
 
     css::uno::Reference<css::uno::XComponentContext> xComponentContext(cppu::defaultBootstrap_InitialComponentContext());
-    auto xMultiComponentFactoryClient(xComponentContext->getServiceManager());
-    auto xInterface = xMultiComponentFactoryClient->createInstanceWithContext(
-            "com.sun.star.bridge.UnoUrlResolver", xComponentContext);
-    auto resolver = Reference<css::bridge::XUnoUrlResolver>(xInterface, UNO_QUERY);
+    css::uno::Reference<css::lang::XMultiComponentFactory> xMultiComponentFactoryClient(xComponentContext->getServiceManager());
+    css::uno::Reference<css::uno::XInterface> xInterface = xMultiComponentFactoryClient->createInstanceWithContext("com.sun.star.bridge.UnoUrlResolver", xComponentContext);
+    css::uno::Reference<css::bridge::XUnoUrlResolver> resolver = css::uno::Reference<css::bridge::XUnoUrlResolver>(xInterface, css::uno::UNO_QUERY);
+
+    std::cout << "xComponentContext=" << xComponentContext.get() << std::endl;
     try
     {
-        xInterface = Reference<XInterface>(resolver->resolve(sConnectionString)
-                                           , UNO_QUERY_THROW);
+        xInterface = css::uno::Reference<css::uno::XInterface>(resolver->resolve(sConnectionString), css::uno::UNO_QUERY_THROW);
     }
-    catch (Exception& e)
+    catch (css::uno::Exception& e)
     {
-        std::cout << "Error: cannot establish a connection using '"
-             << sConnectionString << "'" << std::endl << e.Message << std::endl;
+        std::cout << "Error: cannot establish a connection using '" << sConnectionString << "'" << std::endl << e.Message << std::endl;
         exit(1);
     }
 
-    auto xPropSet = Reference<XPropertySet>(xInterface, UNO_QUERY);
+    css::uno::Reference<css::beans::XPropertySet> xPropSet = css::uno::Reference<css::beans::XPropertySet>(xInterface, css::uno::UNO_QUERY);
     xPropSet->getPropertyValue("DefaultContext") >>= xComponentContext;
-    auto xMultiComponentFactoryServer(xComponentContext->getServiceManager());
-    auto xComponentLoader = css::frame::Desktop::create(xComponentContext);
-    Sequence<PropertyValue> loadProperties(1);
+    std::cout << "xComponentContext=" << xComponentContext.get() << std::endl;
+    //css::uno::Reference<css::lang::XMultiComponentFactory> xMultiComponentFactoryServer(xComponentContext->getServiceManager());
+    css::uno::Reference<css::frame::XDesktop2> xComponentLoader = css::frame::Desktop::create(xComponentContext);
+    css::uno::Sequence<css::beans::PropertyValue> loadProperties(1);
     loadProperties[0].Name = "Hidden";
     loadProperties[0].Value <<= true;
     try
     {
-        auto xComponent = xComponentLoader->loadComponentFromURL(sDocUrl, "_blank"
-                                   , 0, loadProperties);
-        auto xDocument = Reference<css::text::XTextDocument>(xComponent, UNO_QUERY_THROW);
-        auto xStorable = Reference<css::frame::XStorable>(xDocument, UNO_QUERY_THROW);
-        auto storeProps = Sequence<PropertyValue>(3);
+        css::uno::Reference<css::lang::XComponent> xComponent = xComponentLoader->loadComponentFromURL(sDocUrl, "_blank", 0, loadProperties);
+        css::uno::Reference<css::text::XTextDocument> xDocument = css::uno::Reference<css::text::XTextDocument>(xComponent, css::uno::UNO_QUERY_THROW);
+        css::uno::Reference<css::frame::XStorable> xStorable = css::uno::Reference<css::frame::XStorable>(xDocument, css::uno::UNO_QUERY_THROW);
+        css::uno::Sequence<css::beans::PropertyValue> storeProps = css::uno::Sequence<css::beans::PropertyValue>(3);
         storeProps[0].Name = "FilterName";
         storeProps[0].Value <<= rtl::OUString("writer_pdf_Export");
         storeProps[1].Name = "Overwrite";
@@ -67,11 +67,14 @@ int main()
         storeProps[2].Name = "SelectPdfVersion";
         storeProps[2].Value <<= 1;
         xStorable->storeToURL(sPDFUrl, storeProps);
+
+        // This remove the lock.test.odt file
+        xComponent->dispose();
     }
-    catch(Exception& e) {
+    catch(css::uno::Exception& e) {
         std::cout << "Can not open the file ~/test.odt" << std::endl;
     }
 
-    Reference<css::lang::XComponent>::query(xMultiComponentFactoryClient)->dispose();
+    css::uno::Reference<css::lang::XComponent>::query(xMultiComponentFactoryClient)->dispose();
     std::cout << "Output ~/test.pdf generated." << std::endl;
 }
