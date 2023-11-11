2ed5940216664f26135f5e991c467e6473b3f1c8
Phil Krylov <phil@krylov.eu>
2023-11-11 19:59:23 +0100

    2023-11-11 19:42 UTC+0100 Phil Krylov (phil a t krylov.eu) (#333)
    2023-11-11 19:42 UTC+0100 Phil Krylov (phil a t krylov.eu)
      * config/win/mingw.mk
      * utils/hbmk2/hbmk2.prg
        ! Use -D__USE_MINGW_ANSI_STDIO=0 to opt out of C99 printf emulation on
          MinGW-W64 10.0+.

--- config/win/mingw.mk
+++ config/win/mingw.mk
@@ -78,6 +78,13 @@ ifeq ($(HB_BUILD_DEBUG),yes)
    CFLAGS += -g
 endif
 
+ifeq ($(HB_COMPILER),mingw64)
+   # Newer MinGW-W64 versions (10+, IIRC) need this to opt out of C99 format
+   # string emulation and keep our format strings compatible among the
+   # different Windows compilers
+   CFLAGS += -D__USE_MINGW_ANSI_STDIO=0
+endif
+
 RC := $(HB_CCPATH)$(HB_CCPREFIX)windres
 RC_OUT := -o$(subst x,x, )
 RCFLAGS += -I. -I$(HB_HOST_INC) -O coff
