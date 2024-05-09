1c70367da5a8f140259bf46f822f0393bdbd65a5
Przemysław Czerpak <druzus@poczta.onet.pl>
2024-05-09 23:23:26 +0200

    2024-05-09 23:23 UTC+0200 Przemyslaw Czerpak (druzus/at/poczta.onet.pl)   * include/hbapifs.h   * src/rtl/fscopy.c     + added new C function:          HB_BOOL hb_fileCopyEx( const char * pszSource, const char * pszDest,                                 HB_SIZE nBufSize, HB_BOOL fTime,                                 PHB_ITEM pCallBack );       Unlike hb_fileCopy() it is never redirected to remote server and copy       operation is always done locally.       pCallBack is codeblock or function symbol, it's executed at the       beginning and then on each nBufSize bytes written and receives two       parameters: nBytesWritten, nTotalSize.       Warning: nTotalSize could be 0 when non regular files like pipes or                sockets are copied.
      * src/rtl/vfile.c
        + added new PRG function:
             hb_vfCopyFile( <cFileSrc>, <cFileDst>, [<nBufSize>=65536], ;
                            [<lTimePreserve>=.t.], [<bCallBack>] ) --> <nResult>
          It's wrapper to hb_fileCopyEx() C function.
          For very big files setting <nBufSize> to greater value, i.e. 16777216
          may increase performance.

--- src/rtl/fscopy.c
+++ src/rtl/fscopy.c
@@ -45,6 +45,7 @@
  */
 
 #include "hbapi.h"
+#include "hbvm.h"
 #include "hbapifs.h"
 
 #define HB_FSCOPY_BUFFERSIZE  65536
@@ -98,6 +99,94 @@ HB_BOOL hb_fsCopy( const char * pszSource, const char * pszDest )
 
          if( hb_fileAttrGet( pszSource, &ulAttr ) )
             hb_fileAttrSet( pszDest, ulAttr );
+      }
+      hb_fsSetError( errCode );
+   }
+
+   return fResult;
+}
+
+HB_BOOL hb_fileCopyEx( const char * pszSource, const char * pszDest, HB_SIZE nBufSize, HB_BOOL fTime, PHB_ITEM pCallBack )
+{
+   HB_BOOL fResult = HB_FALSE;
+   PHB_FILE pSrcFile;
+
+   if( pCallBack && ! HB_IS_EVALITEM( pCallBack ) )
+      pCallBack = NULL;
+
+   if( ( pSrcFile = hb_fileExtOpen( pszSource, NULL, FO_READ | FO_SHARED | FXO_SHARELOCK, NULL, NULL ) ) != NULL )
+   {
+      PHB_FILE pDstFile;
+      HB_ERRCODE errCode = 0;
+
+      if( ( pDstFile = hb_fileExtOpen( pszDest, NULL, FXO_TRUNCATE | FO_READWRITE | FO_EXCLUSIVE | FXO_SHARELOCK, NULL, NULL ) ) != NULL )
+      {
+         HB_SIZE nTotal = pCallBack ? hb_fileSize( pSrcFile ) : 0, nWritten = 0,
+                 nSize = nBufSize > 0 ? nBufSize : HB_FSCOPY_BUFFERSIZE;
+         void * pbyBuffer = hb_xgrab( nSize );
+
+         if( pCallBack )
+         {
+            hb_vmPushEvalSym();
+            hb_vmPush( pCallBack );
+            hb_vmPushInteger( 0 );
+            hb_vmPushNumInt( ( HB_MAXINT ) nTotal );
+            hb_vmSend( 2 );
+         }
+
+         while( pCallBack == NULL || hb_vmRequestQuery() == 0 )
+         {
+            HB_SIZE nBytesRead;
+
+            if( ( nBytesRead = hb_fileRead( pSrcFile, pbyBuffer, nSize, -1 ) ) > 0 &&
+                nBytesRead != ( HB_SIZE ) FS_ERROR )
+            {
+               if( nBytesRead != hb_fileWrite( pDstFile, pbyBuffer, nBytesRead, -1 ) )
+               {
+                  errCode = hb_fsError();
+                  break;
+               }
+               nWritten += nBytesRead;
+               if( pCallBack )
+               {
+                  hb_vmPushEvalSym();
+                  hb_vmPush( pCallBack );
+                  hb_vmPushNumInt( ( HB_MAXINT ) nWritten );
+                  hb_vmPushNumInt( ( HB_MAXINT ) nTotal );
+                  hb_vmSend( 2 );
+               }
+            }
+            else
+            {
+               errCode = hb_fsError();
+               fResult = errCode == 0;
+               break;
+            }
+         }
+
+         hb_xfree( pbyBuffer );
+
+         hb_fileClose( pDstFile );
+      }
+      else
+         errCode = hb_fsError();
+
+      hb_fileClose( pSrcFile );
+
+      if( fResult )
+      {
+         HB_FATTR ulAttr;
+
+         if( hb_fileAttrGet( pszSource, &ulAttr ) )
+            hb_fileAttrSet( pszDest, ulAttr );
+
+         if( fTime )
+         {
+            long lJulian, lMillisec;
+
+            if( hb_fileTimeGet( pszSource, &lJulian, &lMillisec ) )
+               hb_fileTimeSet( hb_parcx( 1 ), lJulian, lMillisec );
+         }
       }
       hb_fsSetError( errCode );
    }
