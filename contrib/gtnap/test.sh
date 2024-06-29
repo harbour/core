7cb79e06f7413e5b42d2424c69588d3d444c5778
frang75 <frang@nappgui.com>
2024-06-29 17:05:38 +0200

    [GTNAP-364] gtnap build script macOS

--- contrib/gtnap/test.sh
+++ contrib/gtnap/test.sh
@@ -11,9 +11,18 @@ rm -rf build
 
 cd tests/cuademo/gtnap_cualib
 rm exemplo
-rm *.so
-cp ../../../../hboffice/build/Release/bin/libofficesdk.so .
-../../../../../bin/linux/gcc/hbmk2 exemplo.hbp
+
+if [ "$(uname)" == "Darwin" ]; then
+    rm *.dylib
+    cp ../../../../hboffice/build/Release/bin/libofficesdk.dylib .
+    ../../../../../bin/darwin/clang/hbmk2 exemplo.hbp
+    export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$(pwd)
+else
+    rm *.so
+    cp ../../../../hboffice/build/Release/bin/libofficesdk.so .
+    ../../../../../bin/linux/gcc/hbmk2 exemplo.hbp
+fi
+
 ./exemplo --hb:gtnap
 cd ..
 cd ..
