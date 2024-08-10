4da61b9a02b0c22c58af5a56bf5ccf52d8ff4add
Francisco García <frang@nappgui.com>
2024-08-10 11:46:19 +0200

    [GTNAP-398] wip: Working in linker issues

--- contrib/hbaws/tests/aws-hello/hello.cpp
+++ contrib/hbaws/tests/aws-hello/hello.cpp
@@ -12,10 +12,10 @@ int main()
 
     Aws::InitAPI(options);
     {
-        Aws::String access_key_id = "XXX";
-        Aws::String secret_access_key = "XXX";
+        Aws::String access_key_id = "--------------";
+        Aws::String secret_access_key = "-------------------";
+        Aws::String bucket = "-------------";
         Aws::Auth::AWSCredentials credentials(access_key_id, secret_access_key);
-                Aws::S3::Model::ListObjectsV2Request request;
 
         auto start = std::chrono::high_resolution_clock::now();
         std::cout << "Hello HBAWS World!" << std::endl;
@@ -42,7 +42,28 @@ int main()
                 std::cerr << "Error: " << outcome.GetError().GetMessage() << std::endl;
             }
         }
+
+
+        Aws::S3::Model::ListObjectsV2Request request;
+        request.SetBucket(bucket);
+
+        Aws::S3::Model::ListObjectsV2Outcome res = s3_client.ListObjectsV2(request);
+
+        if (res.IsSuccess())
+        {
+            const Aws::S3::Model::ListObjectsV2Result &result = res.GetResult();
+            std::cout << "Archivos en el bucket '" << bucket << "':" << std::endl;
+            const Aws::Vector<Aws::S3::Model::Object> &contents = result.GetContents();
+            for (const auto &object : result.GetContents())
+            {
+                std::cout << "* " << object.GetKey() << std::endl;
+            }
+        }
+
+
     }
+
+
     Aws::ShutdownAPI(options);
     return 0;
 }
