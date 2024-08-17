f87f64323812c1b588e200079e62377eee1fe2c8
frang75 <frang@nappgui.com>
2024-08-17 10:48:05 +0200

    [GTNAP-402] feat: HBAWS build and run in Linux/GCC.

---
+++ contrib/hbaws/build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#
+# HBAWS build script
+#
+# Will generate the hbaws.a with the Harbour AWS wrapper.
+# build -b [Debug|Release]
+#
+
+COMPILER=gcc
+PLATFORM=linux
+BUILD=Release
+CWD=$(pwd)
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
+    if [[ -z "${MACOSX_DEPLOYMENT_TARGET}" ]]; then
+        echo "MACOSX_DEPLOYMENT_TARGET is not set. Please set this environment variable before build."
+        exit 1
+    fi
+fi
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -b)
+      BUILD="$2"
+      shift
+      shift
+      ;;
+    -*|--*)
+      shift
+      ;;
+  esac
+done
+
+#
+# Beginning
+#
+echo ---------------------------
+echo Generating HBAWS
+echo Main path: $CWD
+echo Build type: $BUILD
+echo COMPILER: $COMPILER
+echo ---------------------------
+
+HBMK_PATH=../../bin/$PLATFORM/$COMPILER
+HBMK_FLAGS=
+
+if [ $BUILD == "Debug" ]; then
+    HBMK_FLAGS=-debug
+fi
+
+$HBMK_PATH/hbmk2 $HBMK_FLAGS $CWD/hbaws.hbp || exit 1
+
+echo ------------------------
+echo HBAWS LIB build succeed
+echo ------------------------
+
+cd $CWD
