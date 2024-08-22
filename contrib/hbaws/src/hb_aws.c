dd6d84210209811afc803abbcf0203dd433d44a8
Francisco García <frang@nappgui.com>
2024-08-22 21:00:30 +0200

    [GTNAP-409] wip: 'HBAWS_S3_UPLOAD_MULTIPART' Harbour wrapper.

--- contrib/hbaws/src/hb_aws.c
+++ contrib/hbaws/src/hb_aws.c
@@ -122,6 +122,27 @@ HB_FUNC(HBAWS_S3_UPLOAD_SIMPLE)
 
 /*---------------------------------------------------------------------------*/
 
+HB_FUNC(HBAWS_S3_UPLOAD_MULTIPART)
+{
+    HB_ITEM *bucket_block = hb_param(2, HB_IT_STRING | HB_IT_BLOCK | HB_IT_NIL);
+    HB_ITEM *local_file_block = hb_param(3, HB_IT_STRING | HB_IT_BLOCK | HB_IT_NIL);
+    HB_ITEM *remote_key_block = hb_param(4, HB_IT_STRING | HB_IT_BLOCK | HB_IT_NIL);
+    HB_ITEM *content_type_block = hb_param(5, HB_IT_STRING | HB_IT_BLOCK | HB_IT_NIL);
+    s3_storage_class_t storage = (s3_storage_class_t)hb_parni(6);
+    uint32_t chunk_size = hb_parni(7);
+    uint32_t num_retries = hb_parni(8);
+    HB_BOOL ok = hb_aws_s3_upload_multipart(bucket_block, local_file_block, remote_key_block, content_type_block, storage, chunk_size, num_retries);
+
+    if (ok)
+        hb_storc("", 1);
+    else
+        hb_storc(hb_aws_last_error(), 1);
+
+    hb_retl(ok);
+}
+
+/*---------------------------------------------------------------------------*/
+
 HB_FUNC(HBAWS_S3_DOWNLOAD)
 {
     HB_ITEM *bucket_block = hb_param(2, HB_IT_STRING | HB_IT_BLOCK | HB_IT_NIL);
