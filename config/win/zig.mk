4f8dd3730e322b97b8dbed1b3b288cb80b4566b0
Aleksander Czajczynski <hb@fki.pl>
2025-11-16 22:10:49 +0100

    2025-11-16 22:10 UTC+0200 Aleksander Czajczynski (hb fki.pl)   * config/win/zig.mk     ! in some Zig tests, building for Windows from UNIX shell (MSYS2       or cross compiling) resource compiler "zig rc" preferred options       passed with "-" instead of "/"

--- config/win/zig.mk
+++ config/win/zig.mk
@@ -1,6 +1,11 @@
 RC := zig rc
-RCFLAGS := /I. /I$(HB_HOST_INC) /C 1252
-RC_OUT := /FO$(subst x,x, )
+ifeq ($(HB_SHELL),sh)
+   RCFLAGS := -I. -I$(HB_HOST_INC) -C 1252
+   RC_OUT := -FO$(subst x,x, )
+else
+   RCFLAGS := /I. /I$(HB_HOST_INC) /C 1252
+   RC_OUT := /FO$(subst x,x, )
+endif
 RES_EXT := .res
 
 IMPLIBFLAGS = -Wl,--out-implib,$(IMP_FILE)
